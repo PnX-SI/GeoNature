ccb08e108514e091dcb97f53a23859ca1ee703dc
gil.deluermoz <gil.deluermoz@ecrins-parcnational.fr>
2016-01-14 12:28:32 +0100

    Mise à jour de la base pour un taxref V8 ready - cf #34

--- data/update_1.5to1.6.sql
+++ data/update_1.5to1.6.sql
@@ -104,3 +104,82 @@ ALTER TABLE taxonomie.cor_taxon_liste
   ADD CONSTRAINT cor_taxon_liste_bib_taxons_fkey FOREIGN KEY (id_taxon)
       REFERENCES taxonomie.bib_taxons (id_taxon) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE CASCADE;
+      
+--Préparation à l'accueil du taxref V8
+DROP TABLE import_taxref;
+CREATE TABLE taxonomie.import_taxref
+(
+  regne character varying(20),
+  phylum character varying(50),
+  classe character varying(50),
+  ordre character varying(50),
+  famille character varying(50),
+  group1_inpn character varying(50),
+  group2_inpn character varying(50),
+  cd_nom integer NOT NULL,
+  cd_taxsup integer,
+  cd_ref integer,
+  rang character varying(10),
+  lb_nom character varying(100),
+  lb_auteur character varying(250),
+  nom_complet character varying(255),
+  nom_complet_html character varying(500),
+  nom_valide character varying(255),
+  nom_vern character varying(1000),
+  nom_vern_eng character varying(500),
+  habitat character varying(10),
+  fr character varying(10),
+  gf character varying(10),
+  mar character varying(10),
+  gua character varying(10),
+  sm character varying(10),
+  sb character varying(10),
+  spm character varying(10),
+  may character varying(10),
+  epa character varying(10),
+  reu character varying(10),
+  taaf character varying(10),
+  pf character varying(10),
+  nc character varying(10),
+  wf character varying(10),
+  cli character varying(10),
+  url text,
+  CONSTRAINT pk_import_taxref PRIMARY KEY (cd_nom)
+)
+WITH (
+  OIDS=FALSE
+);
+
+--Si besoin, vous pouvez utiliser le fichier taxref V8 de l'INPN fourni avec GeoNature 1.6 dans "data/inpn/TAXREF_INPN_v8.0.zip"
+-- ainsi que la commande sql (commentée) ci-dessous :
+--COPY import_taxref (regne, phylum, classe, ordre, famille, group1_inpn, group2_inpn, 
+         -- cd_nom, cd_taxsup, cd_ref, rang, lb_nom, lb_auteur, nom_complet, nom_complet_html,
+         -- nom_valide, nom_vern, nom_vern_eng, habitat, fr, gf, mar, gua, 
+         -- sm, sb, spm, may, epa, reu, taaf, pf, nc, wf, cli, url)
+--FROM  'PATH_TO_DIR/data/inpn/TAXREFv80.txt'
+--WITH  CSV HEADER 
+--DELIMITER E'\t'  encoding 'LATIN1';
+
+--MISE A JOUR de la table taxonomie.taxref
+ALTER TABLE taxonomie.taxref ALTER COLUMN lb_auteur TYPE character varying(250);
+ALTER TABLE taxonomie.taxref ALTER COLUMN nom_vern TYPE character varying(1000);
+ALTER TABLE taxonomie.taxref ALTER COLUMN nom_vern_eng TYPE character varying(500);
+ALTER TABLE taxonomie.taxref ALTER COLUMN nom_complet_html TYPE character varying(500);
+--ajout d'un statut non précisé
+INSERT INTO bib_taxref_statuts (id_statut, nom_statut) VALUES (' ', 'Non précisé');
+
+--Si besoin, voici les commandes (commentée) SQL à utiliser pour vider puis remplir la table taxonomie.taxef. Pour vider la table taxonomie.taxef, vous devrez préalablement désactiver toutes les clés étrangères pointant sur cette table.
+--TRUNCATE TABLE taxref;
+--INSERT INTO taxref
+      --SELECT cd_nom, fr as id_statut, habitat::int as id_habitat, rang as  id_rang, regne, phylum, classe, 
+             --ordre, famille, cd_taxsup, cd_ref, lb_nom, substring(lb_auteur, 1, 250), nom_complet, 
+             --nom_valide, nom_vern, nom_vern_eng, group1_inpn, group2_inpn
+        --FROM import_taxref
+        --WHERE regne = 'Animalia'
+        --OR regne = 'Fungi'
+        --OR regne = 'Plantae';
+        
+--Taxref V8 est livré avec les mêmes tables que taxref V7 concernant les statuts juridiques.
+-- * taxonomie.taxref_protection_articles
+-- * taxonomie.taxref_protection_especes
+--Il n'y a donc pas de mise à jour à faire concernant ces tables
