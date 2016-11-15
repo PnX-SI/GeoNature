c19ac31cf790be906fa58fea58704057434b0174
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2016-11-15 17:42:19 +0100

    INSTALL ALL - ajout de l'installation de l'atlas

--- docs/install_all/README.rst
+++ docs/install_all/README.rst
@@ -1,29 +1,37 @@
-INSTALLATION RAPIDE
-===================
+INSTALLATION RAPIDE ET COMPLETE
+===============================
 
-Cette documentation permet une installation rapide et simplifiée de GeoNature et de son environnement : UsersHub, TaxHub et GeoNature-atlas.
-Les scripts proposés installent l'environnement logiciel du serveur, téléchargent les applications sur leur dépots github, les installent et les configurent.
+Cette documentation permet une installation rapide et simplifiée de GeoNature et de ses applications liées : UsersHub, TaxHub et GeoNature-atlas.
+Les scripts proposés installent l'environnement logiciel du serveur, téléchargent les applications sur leur dépots github, les installent et les configurent par défaut.
 
 Pré-requis
 ----------
 
-- Un serveur debian 8 (ubuntu 16.04 LTS devrait fonctionner également - non testé) 
-- Une clé IGN pour l'api geoportail valide pour le domaine sur lequel votre serveur répond.
-- TODO : proposer un sources.list valide et la procédure pour le mettre à jour.
+- Un serveur Debian 8 (Ubuntu 16.04 LTS devrait fonctionner également - non testé)
+- Une clé IGN pour l'API Geoportail valide pour le domaine sur lequel votre serveur répond
+- TODO : proposer un sources.list valide et la procédure pour le mettre à jour
 
 Installation
 ------------
 
 Après installation de l'OS avec OpenSSH server, placez vous dans le home de votre utilisateur et entrez les commandes suivantes :
- 
+
+  ::
+    
+    su
+    apt-get update
+    apt-get install -y sudo ca-certificates
+    exit
+    
+Récupérer les scripts d'installation
+
   ::  
-  
-	wget https://raw.githubusercontent.com/PnEcrins/GeoNature/develop/docs/init_user.sh
-	wget https://raw.githubusercontent.com/PnEcrins/GeoNature/develop/docs/install_all.ini
-	wget https://raw.githubusercontent.com/PnEcrins/GeoNature/develop/docs/install_all.sh
+    
+	wget https://raw.githubusercontent.com/PnEcrins/GeoNature/develop/docs/install_all/init_user.sh
+	wget https://raw.githubusercontent.com/PnEcrins/GeoNature/develop/docs/install_all/install_all.ini
+	wget https://raw.githubusercontent.com/PnEcrins/GeoNature/develop/docs/install_all/install_all.sh
 	chmod +x init_user.sh
 	chmod +x install_all.sh
-
 
 Mettez à jour le fichier ``install_all.ini`` avec vos informations. Attention, ne lancez pas les fichiers .sh tant que vous n'avez pas totalement complété ce fichier.
 
@@ -38,15 +46,25 @@ Lancez ensuite l'installation des applications:
   ::  
   
 	./install_all.sh
-    
 
-Vous devez pouvoir vous connecter à vos applications avec les adresses (adaptez mondomaine.fr à votre domaine ou avec votre adresse IP)
+Le mot de passe sudo vous sera demandé. 
+Lors de l'exécution du script, une question sera posée. 
+Unable to find a suitable version for webcomponentsjs, please choose one by typing one of the numbers below:
+    1) webcomponentsjs#~0.5.4 which resolved to 0.5.5 and is required by App States#0.6.9
+    2) webcomponentsjs#* which resolved to 0.5.5 and is required by core-component-page#0.5.6
+    3) webcomponentsjs#^0.6.0 which resolved to 0.6.3 and is required by polymer#0.5.6
+Répondre ``3``
+
+Vous devez pouvoir vous connecter à vos applications avec les adresses (adaptez mondomaine.fr à votre nom de domaine ou avec votre adresse IP)
 	http://mondomaine.fr/usershub
 	http://mondomaine.fr/geonature
 	http://mondomaine.fr/taxhub
+	http://mondomaine.fr/atlas
 
 
-Ces applications demandent une authentification.
+Les 3 premières applications demandent une authentification.
 
 L'utilisateur ``admin`` avec le mot de passe ``admin`` est disponible par défaut avec des droits administrateur sur toutes les applications. 
 Vous devez utiliser UsersHub pour gérer d'autres utilisateurs.
+
+L'installation des bases de données est loguée dans le répertoire ``log`` des applications : ``log/install_db.log``.
--- docs/install_all/install_all.sh
+++ docs/install_all/install_all.sh
@@ -10,16 +10,18 @@ if [ "$(id -u)" == "0" ]; then
    exit 1
 fi
 
-#installation de l'environnement nécessaire à UsersHub, GeoNature et TaxHub
+#installation de l'environnement nécessaire à UsersHub, GeoNature, TaxHub et GeoNature-atlas
 echo "Installation de l'environnement logiciel..."
-#TODO ntpdate
 sudo apt-get update
+sudo apt-get -y install ntpdate
+sudo ntpdate-debian
 sudo apt-get install -y curl unzip git
-sudo apt-get install -y apache2 php5 libapache2-mod-php5 libapache2-mod-wsgi libapache2-mod-perl2 php5-gd php5-pgsql 
+sudo apt-get install -y apache2 php5 libapache2-mod-php5 libapache2-mod-wsgi libapache2-mod-perl2
 sudo apt-get install -y php5-gd php5-pgsql
-sudo apt-get install -y postgresql postgis 
 sudo apt-get install -y cgi-mapserver gdal-bin libgeos-dev
-sudo apt-get install -y python3 python3-dev python-dev python-pip libpq-dev python-setuptools python-gdal python-virtualenv
+sudo apt-get install -y postgresql postgis postgresql-server-dev-9.4
+sudo apt-get install -y python-dev python-pip libpq-dev python-setuptools python-gdal python-virtualenv build-essential
+# sudo apt-get install -y python3 python3-dev 
 
 sudo pip install virtualenv
 
@@ -47,6 +49,8 @@ sudo -n -u postgres -s psql -c "CREATE ROLE $admin_pg WITH SUPERUSER LOGIN PASSW
 echo "ajout d'un alias 'databases' et 'geonatdbhost' dans /etc/hosts..."
 sudo sh -c 'echo "127.0.1.1       databases" >> /etc/hosts'
 sudo sh -c 'echo "127.0.1.1       geonatdbhost" >> /etc/hosts'
+#suppression de la notice au redémarrage d'apache
+sudo sh -c 'echo "ServerName localhost" >> /etc/apache2/apache2.conf'
 echo "Activation des modules apache rewrite, wsgi et cgi..."
 sudo a2enmod rewrite
 sudo a2enmod cgi
@@ -125,15 +129,15 @@ cd /home/$monuser/taxhub
 #configuration des settings de TaxHub
 echo "Configuration de l'application TaxHub ..."
 cp settings.ini.sample settings.ini
-sed -i "s/drop_apps_db=.*$/drop_apps_db=$drop_taxhubdb/g" settings.ini
+sed -i "s/drop_apps_db=.*$/drop_apps_db=false/g" settings.ini
 sed -i "s/db_host=.*$/db_host=$pg_host/g" settings.ini
 sed -i "s/db_port=.*$/db_port=$pg_port/g" settings.ini
-sed -i "s/db_name=.*$/db_name=$taxhubdb_name/g" settings.ini
+sed -i "s/db_name=.*$/db_name=$geonaturedb_name/g" settings.ini
 sed -i "s/user_pg=.*$/user_pg=$user_pg/g" settings.ini
 sed -i "s/user_pg_pass=.*$/user_pg_pass=$user_pg_pass/g" settings.ini
 sed -i "s/admin_pg=.*$/admin_pg=$admin_pg/g" settings.ini
 sed -i "s/admin_pg_pass=.*$/admin_pg_pass=$admin_pg_pass/g" settings.ini
-sed -i "s/user_schema=.*$/user_schema=$taxhub_users_schema_location/g" settings.ini
+sed -i "s/user_schema=.*$/user_schema=local/g" settings.ini
 sed -i "s/usershub_host=.*$/usershub_host=$pg_host/g" settings.ini
 sed -i "s/usershub_port=.*$/usershub_port=$pg_port/g" settings.ini
 sed -i "s/usershub_db=.*$/usershub_db=$usershubdb_name/g" settings.ini
@@ -154,4 +158,80 @@ sudo sh -c 'echo "#FIN Configuration TaxHub" >> /etc/apache2/sites-available/tax
 sudo a2ensite taxhub
 #installation et configuration de l'application TaxHub
 ./install_app.sh
-sudo apache2ctl restart
+#sudo apache2ctl restart
+
+#installation de l'atlas avec l'utilisateur courant
+echo "téléchargement et installation de GeoNature-atlas ..."
+cd /tmp
+wget https://github.com/PnEcrins/GeoNature-atlas/archive/$atlas_release.zip
+unzip $atlas_release.zip
+rm $atlas_release.zip
+mv GeoNature-atlas-$atlas_release /home/$monuser/atlas/
+cd /home/$monuser/atlas
+
+echo "Création de l'environnement virtuel de GeoNature-atlas ..."
+virtualenv ./venv
+. ./venv/bin/activate
+
+echo "Installation des dépendances pour l'application GeoNature-atlas ..."
+pip install -r requirements.txt
+
+echo "configuration de l'application GeoNature-atlas ..."
+cp ./main/configuration/config.py.sample ./main/configuration/config.py
+cp ./main/configuration/settings.ini.sample ./main/configuration/settings.ini
+cp ./static/custom/templates/footer.html.sample ./static/custom/templates/footer.html
+cp ./static/custom/templates/introduction.html.sample ./static/custom/templates/introduction.html
+cp ./static/custom/templates/presentation.html.sample ./static/custom/templates/presentation.html
+cp ./static/custom/custom.css.sample ./static/custom/custom.css
+cp ./static/custom/glossaire.json.sample ./static/custom/glossaire.json
+
+#configuration des settings de GeoNature-atlas et création de la base de données
+echo " Configuration et installation de la base de données ..."
+sed -i "s/drop_apps_db=.*$/drop_apps_db=$drop_atlasdb/g" main/configuration/settings.ini
+sed -i "s/drop_apps_db=.*$/drop_apps_db=$drop_atlasdb/g" main/configuration/settings.ini
+sed -i "s/db_name=.*$/db_name=$atlasdb_name/g" main/configuration/settings.ini
+sed -i "s/user_pg=.*$/user_pg=$user_atlas/g"  main/configuration/settings.ini
+sed -i "s/user_pg_pass=.*$/user_pg_pass=$user_atlas_pass/g"  main/configuration/settings.ini
+sed -i "s/admin_pg=.*$/admin_pg=$admin_pg/g"  main/configuration/settings.ini
+sed -i "s/admin_pg_pass=.*$/admin_pg_pass=$admin_pg_pass/g"  main/configuration/settings.ini
+sed -i "s/db_source_host=.*$/db_source_host=$pg_host/g"  main/configuration/settings.ini
+sed -i "s/db_source_port=.*$/db_source_port=$pg_port/g"  main/configuration/settings.ini
+sed -i "s/admin_source_user=.*$/admin_source_user=$admin_pg/g"  main/configuration/settings.ini
+sed -i "s/admin_source_pass=.*$/admin_source_pass=$admin_pg_pass/g"  main/configuration/settings.ini
+sed -i "s/atlas_source_user=.*$/atlas_source_user=$user_pg/g"  main/configuration/settings.ini #TODO utiliser la variable $user_atlaset faire des GRANT select et usage pour geonatatlas sur la bd geonature
+sed -i "s/atlas_source_pass=.*$/atlas_source_pass=$user_pg_pass/g"  main/configuration/settings.ini #TODO utiliser la variable $user_atlas_pass
+sed -i -e "s/limit_shp=.*$/limit_shp=\/home\/$monuser\/atlas\/data\/ref\/$limit_shp_name/g" main/configuration/settings.ini
+sed -i -e "s/communes_shp=.*$/communes_shp=\/home\/$monuser\/atlas\/data\/ref\/$communes_shp_name/g" main/configuration/settings.ini
+sed -i "s/colonne_insee=.*$/colonne_insee=$colonne_insee/g"  main/configuration/settings.ini
+sed -i "s/colonne_nom_commune=.*$/colonne_nom_commune=$colonne_nom_commune/g"  main/configuration/settings.ini
+sed -i "s/metropole=.*$/metropole=$metropole/g"  main/configuration/settings.ini
+sed -i "s/taillemaille=.*$/taillemaille=$taillemaille/g"  main/configuration/settings.ini
+sed -i -e "s/chemin_custom_maille=.*$/chemin_custom_maille=\/home\/$monuser\/atlas\/data\/ref\/$custom_maille_name/g" main/configuration/settings.ini
+#mettre à jour config.py
+sed -i "s/database_connection =.*$/database_connection = \"postgresql:\/\/$user_atlas:$user_atlas_pass@$pg_host:$pg_port\/$atlasdb_name\"/g" main/configuration/config.py
+sed -i "s/STRUCTURE = \".*$/STRUCTURE = \"$structure\"/g" main/configuration/config.py
+sed -i "s/NOM_APPLICATION = \".*$/NOM_APPLICATION = \"$nom_application\"/g" main/configuration/config.py
+sed -i "s/URL_APPLICATION =.*$/URL_APPLICATION = \"$url_atlas\"/g" main/configuration/config.py
+sed -i "s/ID_GOOGLE_ANALYTICS =.*$/ID_GOOGLE_ANALYTICS = \"$id_google_analytics\"/g" main/configuration/config.py
+sed -i "s/IGNAPIKEY =.*/IGNAPIKEY = \'$macleign\';/g" main/configuration/config.py
+#sed -i "s/+IGNAPIKEY+/+$macleign+/g" main/configuration/config.py
+sed -i "s/'LAT_LONG':.*$/\'LAT_LONG\': [$y, $x],/g" main/configuration/config.py
+
+sudo ./install_db.sh
+
+#configuration apache de GeoNature-atlas
+sudo rm /etc/apache2/sites-available/atlas.conf
+sudo touch /etc/apache2/sites-available/atlas.conf
+sudo sh -c 'echo "# Configuration de GeoNature-atlas" >> /etc/apache2/sites-available/atlas.conf'
+sudo sh -c 'echo "WSGIScriptAlias /atlas \"/home/'$monuser'/atlas/atlas.wsgi\"" >> /etc/apache2/sites-available/atlas.conf'
+sudo sh -c 'echo "<Directory \"/home/'$monuser'/atlas/\">" >> /etc/apache2/sites-available/atlas.conf'
+sudo sh -c 'echo "WSGIApplicationGroup %{GLOBAL}" >> /etc/apache2/sites-available/atlas.conf'
+sudo sh -c 'echo "WSGIScriptReloading On" >> /etc/apache2/sites-available/atlas.conf'
+sudo sh -c 'echo "Order deny,allow" >> /etc/apache2/sites-available/atlas.conf'
+sudo sh -c 'echo "Allow from all" >> /etc/apache2/sites-available/atlas.conf'
+sudo sh -c 'echo "Require all granted" >> /etc/apache2/sites-available/atlas.conf'
+sudo sh -c 'echo "</Directory>" >> /etc/apache2/sites-available/atlas.conf'
+sudo a2ensite atlas
+sudo apachectl restart
+#nettoyage
+sudo rm /tmp/*.sql
