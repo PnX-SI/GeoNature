c5a3ae905ffbc97a95d7d2a0e91c5741112a9d48
QuangPhamll <quang.pham.fr@gmail.com>
2017-07-27 16:52:50 +0200

    Add toast for login successs and login failed

--- frontend/src/app/components/auth/auth.service.ts
+++ frontend/src/app/components/auth/auth.service.ts
@@ -1,21 +1,33 @@
 import { Router } from '@angular/router';
 import * as firebase from 'firebase';
 import { Injectable } from '@angular/core';
+import { ToastrService, ToastrConfig } from 'ngx-toastr';
 
 @Injectable()
 export class AuthService {
     token: string;
-    constructor(private router: Router) {}
+    toastrConfig: ToastrConfig;
+    constructor(private router: Router,  private toastrService: ToastrService) {
+        this.toastrConfig = {
+            positionClass: 'toast-top-center',
+            tapToDismiss: true,
+            timeOut: 2000
+        };
+    }
   signinUser(email: string, password: string) {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
         response => {
           this.router.navigate(['/']);
+          this.toastrService.success('', 'Login success', this.toastrConfig);
           firebase.auth().currentUser.getIdToken()
             .then(
-              (token: string) => this.token = token
+              (token: string) => this.token = token,
             );
         }
+      )
+      .catch(
+        error => this.toastrService.error('', 'Login failed', this.toastrConfig)
       );
   }
 
--- frontend/src/app/components/auth/signin/signin.component.ts
+++ frontend/src/app/components/auth/signin/signin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from '../auth.service';
+
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
