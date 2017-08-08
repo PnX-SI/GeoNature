c8dcb70f70be3adc08271aca514037b0a363e812
QuangPhamll <quang.pham.fr@gmail.com>
2017-08-08 17:42:00 +0200

    FRONTEND - Add map contactfaune

---
+++ frontend/src/app/apps/contact-faune/cfaune-form/cfaune-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cfaune-form',
+  templateUrl: './cfaune-form.component.html',
+  styleUrls: ['./cfaune-form.component.scss']
+})
+export class CfauneFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
