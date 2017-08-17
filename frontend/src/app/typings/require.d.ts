a2a17eebeef9038c3f48ec7f183fe9c17d5e0478
QuangPhamll <quang.pham.fr@gmail.com>
2017-08-17 10:39:02 +0200

    Frontend - contactF: Map - button add marker work

---
+++ frontend/src/app/typings/require.d.ts
@@ -0,0 +1,10 @@
+// webpack require() definition for ts-loader
+// source: https://github.com/TypeStrong/ts-loader
+
+interface WebpackRequire {
+    <T>(path: string): T;
+    (paths: string[], callback: (...modules: any[]) => void): void;
+    ensure: (paths: string[], callback: (require: <T>(path: string) => T) => void) => void;
+}
+interface NodeRequire extends WebpackRequire {}
+declare var require: NodeRequire;
