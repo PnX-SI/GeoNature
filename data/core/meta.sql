709ee20c516ecf5ac3e5e0eb0ea39dd96ddffb3d
TheoLechemia <lechemia.theo@gmail.com>
2017-08-21 17:56:38 +0200

    SQL: traduction en anglais du schéma méta

--- data/core/meta.sql
+++ data/core/meta.sql
@@ -15,44 +15,44 @@ SET default_with_oids = false;
 ----------
 --TABLES--
 ----------
-CREATE TABLE cor_role_droit_entite (
+CREATE TABLE cor_role_right_entity (
     id_role integer NOT NULL,
-    id_droit integer NOT NULL,
-    nom_entite character varying(255) NOT NULL
+    id_right integer NOT NULL,
+    entity_name character varying(255) NOT NULL
 );
-COMMENT ON TABLE cor_role_droit_entite IS 'Allow to manage rights of a group or user on entities (tables) into backoffice (CRUD depending on rights).';
+COMMENT ON TABLE cor_role_right_entity IS 'Allow to manage rights of a group or user on entities (tables) into backoffice (CRUD depending on rights).';
 
 
-CREATE TABLE cor_role_lot_application (
+CREATE TABLE cor_role_dataset_application (
     id_role integer NOT NULL,
-    id_lot integer NOT NULL,
+    id_dataset integer NOT NULL,
     id_application integer NOT NULL
 );
-COMMENT ON TABLE cor_role_lot_application IS 'Allow to identify for each GeoNature module (1 module = 1 application in UsersHub) among which dataset connected user can create observations. Reminder : A dataset is a dataset or a survey and each observation is attached to a dataset. GeoNature V2 backoffice allows to manage datasets.';
+COMMENT ON TABLE cor_role_dataset_application IS 'Allow to identify for each GeoNature module (1 module = 1 application in UsersHub) among which dataset connected user can create observations. Reminder : A dataset is a dataset or a survey and each observation is attached to a dataset. GeoNature V2 backoffice allows to manage datasets.';
 
 
 CREATE TABLE t_lots (
-    id_lot integer NOT NULL,
-    nom_lot character varying(255),
-    desc_lot text,
+    id_dataset integer NOT NULL,
+    dataset_name character varying(255),
+    dataset_desc text,
     id_programme integer NOT NULL,
-    id_organisme_proprietaire integer NOT NULL,
-    id_organisme_producteur integer NOT NULL,
-    id_organisme_gestionnaire integer NOT NULL,
-    id_organisme_financeur integer NOT NULL,
-    donnees_publiques boolean DEFAULT true NOT NULL,
-    validite_par_defaut boolean,
-    date_create timestamp without time zone,
-    date_update timestamp without time zone
+    id_organisme_owner integer NOT NULL,
+    id_organisme_producer integer NOT NULL,
+    id_organisme_administrator integer NOT NULL,
+    id_organisme_funder integer NOT NULL,
+    public_data boolean DEFAULT true NOT NULL,
+    default_validity boolean,
+    meta_create_date timestamp without time zone,
+    meta_update_date timestamp without time zone
 );
 COMMENT ON TABLE t_lots IS 'A dataset is a dataset or a survey and each observation is attached to a dataset. A lot allows to qualify datas to which it is attached (producer, owner, manager, gestionnaire, financer, public data yes/no). A dataset can be attached to a program. GeoNature V2 backoffice allows to manage datasets.';
 
 
 CREATE TABLE t_programmes (
     id_programme integer NOT NULL,
-    nom_programme character varying(255),
-    desc_programme text,
-    actif boolean
+    programme_name character varying(255),
+    programme_desc text,
+    active boolean
 );
 COMMENT ON TABLE t_programmes IS 'Programs are general objects that can embed datasets and/or protocols. Example : ATBI, raptors, action national plan, etc... GeoNature V2 backoffice allows to manage datasets.';
 
@@ -60,14 +60,14 @@ COMMENT ON TABLE t_programmes IS 'Programs are general objects that can embed da
 ---------------
 --PRIMARY KEY--
 ---------------
-ALTER TABLE ONLY cor_role_droit_entite
-    ADD CONSTRAINT cor_role_droit_entite_pkey PRIMARY KEY (id_role, id_droit, nom_entite);
+ALTER TABLE ONLY cor_role_right_entity
+    ADD CONSTRAINT cor_role_right_entity_pkey PRIMARY KEY (id_role, id_right, entity_name);
 
-ALTER TABLE ONLY cor_role_lot_application
-    ADD CONSTRAINT cor_role_lot_application_pkey PRIMARY KEY (id_role, id_lot, id_application);
+ALTER TABLE ONLY cor_role_dataset_application
+    ADD CONSTRAINT cor_role_dataset_application_pkey PRIMARY KEY (id_role, id_dataset, id_application);
 
 ALTER TABLE ONLY t_lots
-    ADD CONSTRAINT t_lots_pkey PRIMARY KEY (id_lot);
+    ADD CONSTRAINT t_lots_pkey PRIMARY KEY (id_dataset);
 
 ALTER TABLE ONLY t_programmes
     ADD CONSTRAINT t_programmes_pkey PRIMARY KEY (id_programme);
@@ -76,34 +76,34 @@ ALTER TABLE ONLY t_programmes
 ---------------
 --FOREIGN KEY--
 ---------------
-ALTER TABLE ONLY cor_role_droit_entite
-    ADD CONSTRAINT cor_role_droit_application_id_droit_fkey FOREIGN KEY (id_droit) REFERENCES utilisateurs.bib_droits(id_droit) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE ONLY cor_role_right_entity
+    ADD CONSTRAINT cor_role_droit_application_id_right_fkey FOREIGN KEY (id_right) REFERENCES utilisateurs.bib_droits(id_right) ON UPDATE CASCADE ON DELETE CASCADE;
 
-ALTER TABLE ONLY cor_role_droit_entite
-    ADD CONSTRAINT cor_role_droit_entite_t_roles_fkey FOREIGN KEY (id_role) REFERENCES utilisateurs.t_roles(id_role) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE ONLY cor_role_right_entity
+    ADD CONSTRAINT cor_role_right_entity_t_roles_fkey FOREIGN KEY (id_role) REFERENCES utilisateurs.t_roles(id_role) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
-ALTER TABLE ONLY cor_role_lot_application
+ALTER TABLE ONLY cor_role_dataset_application
     ADD CONSTRAINT cor_role_droit_application_id_role_fkey FOREIGN KEY (id_role) REFERENCES utilisateurs.t_roles(id_role) ON UPDATE CASCADE ON DELETE CASCADE;
 
-ALTER TABLE ONLY cor_role_lot_application
-    ADD CONSTRAINT cor_role_lot_application_id_application_fkey FOREIGN KEY (id_application) REFERENCES utilisateurs.t_applications(id_application) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE ONLY cor_role_dataset_application
+    ADD CONSTRAINT cor_role_dataset_application_id_application_fkey FOREIGN KEY (id_application) REFERENCES utilisateurs.t_applications(id_application) ON UPDATE CASCADE ON DELETE CASCADE;
 
-ALTER TABLE ONLY cor_role_lot_application
-    ADD CONSTRAINT cor_role_lot_application_id_droit_fkey FOREIGN KEY (id_lot) REFERENCES t_lots(id_lot) ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE ONLY cor_role_dataset_application
+    ADD CONSTRAINT cor_role_dataset_application_id_right_fkey FOREIGN KEY (id_dataset) REFERENCES t_lots(id_dataset) ON UPDATE CASCADE ON DELETE CASCADE;
 
-
-ALTER TABLE ONLY t_lots
-    ADD CONSTRAINT fk_bib_lots_financeur FOREIGN KEY (id_organisme_financeur) REFERENCES utilisateurs.bib_organismes(id_organisme) ON UPDATE CASCADE;
 
 ALTER TABLE ONLY t_lots
-    ADD CONSTRAINT fk_bib_lots_gestionnaire FOREIGN KEY (id_organisme_gestionnaire) REFERENCES utilisateurs.bib_organismes(id_organisme) ON UPDATE CASCADE;
+    ADD CONSTRAINT fk_bib_lots_financeur FOREIGN KEY (id_organisme_funder) REFERENCES utilisateurs.bib_organismes(id_organisme) ON UPDATE CASCADE;
 
 ALTER TABLE ONLY t_lots
-    ADD CONSTRAINT fk_bib_lots_producteur FOREIGN KEY (id_organisme_producteur) REFERENCES utilisateurs.bib_organismes(id_organisme) ON UPDATE CASCADE;
+    ADD CONSTRAINT fk_bib_lots_gestionnaire FOREIGN KEY (id_organisme_administrator) REFERENCES utilisateurs.bib_organismes(id_organisme) ON UPDATE CASCADE;
 
 ALTER TABLE ONLY t_lots
-    ADD CONSTRAINT fk_bib_lots_proprietaire FOREIGN KEY (id_organisme_proprietaire) REFERENCES utilisateurs.bib_organismes(id_organisme) ON UPDATE CASCADE;
+    ADD CONSTRAINT fk_bib_lots_producteur FOREIGN KEY (id_organisme_producer) REFERENCES utilisateurs.bib_organismes(id_organisme) ON UPDATE CASCADE;
+
+ALTER TABLE ONLY t_lots
+    ADD CONSTRAINT fk_bib_lots_proprietaire FOREIGN KEY (id_organisme_owner) REFERENCES utilisateurs.bib_organismes(id_organisme) ON UPDATE CASCADE;
 
 ALTER TABLE ONLY t_lots
     ADD CONSTRAINT fk_bib_lots_t_programmes FOREIGN KEY (id_programme) REFERENCES t_programmes(id_programme) ON UPDATE CASCADE;
