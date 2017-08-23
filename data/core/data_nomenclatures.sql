79d7c091c86e5ebf9f4ae28fc1d843cb9a334060
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2017-08-23 09:48:23 +0200

    SQL - renommer les schémas : ref_nomenclatures

--- data/core/data_nomenclatures.sql
+++ data/core/data_nomenclatures.sql
@@ -6,7 +6,7 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 --SET row_security = off;
 
-SET search_path = nomenclatures, pg_catalog;
+SET search_path = ref_nomenclatures, pg_catalog;
 
 DELETE FROM t_nomenclatures;
 DELETE FROM bib_nomenclatures_types;
