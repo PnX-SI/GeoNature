61f60a5baa7d8b9ce4e2fe2abe2fa070a8bca4bb
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2017-08-23 16:45:21 +0200

    SQL - anglicisation et révision du log du contact

--- data/modules/contact/install_schema.sh
+++ data/modules/contact/install_schema.sh
@@ -8,17 +8,30 @@ fi
 
 . config/settings.ini
 
-echo "Création du schéma contact..."
-echo "" &>> log/install_schemas.log
-echo "" &>> log/install_schemas.log
-echo "" &>> log/install_schemas.log
-echo "--------------------" &>> log/install_schemas.log
-echo "Création du schéma contact" &>> log/install_schemas.log
-echo "--------------------" &>> log/install_schemas.log
-echo "" &>> log/install_schemas.log
+echo "Create contact schema..."
+echo "" &>> log/install_contact_schema.log
+echo "" &>> log/install_contact_schema.log
+echo "" &>> log/install_contact_schema.log
+echo "--------------------" &>> log/install_contact_schema.log
+echo "Create contact schema" &>> log/install_contact_schema.log
+echo "--------------------" &>> log/install_contact_schema.log
+echo "" &>> log/install_contact_schema.log
 cp data/modules/contact/contact.sql /tmp/contact.sql
 sudo sed -i "s/MYLOCALSRID/$srid_local/g" /tmp/contact.sql
-export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f /tmp/contact.sql  &>> log/install_schemas.log
+export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f /tmp/contact.sql  &>> log/install_contact_schema.log
 
-echo "Droit sur le répertoire log..."
+if $add_sample_data
+	then
+	echo "Insert sample data in contact schema..."
+	echo "" &>> log/install_contact_schema.log
+	echo "" &>> log/install_contact_schema.log
+	echo "" &>> log/install_contact_schema.log
+	echo "--------------------" &>> log/install_contact_schema.log
+	echo "Insert sample data in contact schema..." &>> log/install_contact_schema.log
+	echo "--------------------" &>> log/install_contact_schema.log
+	echo "" &>> log/install_contact_schema.log
+	export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f /data/modules/contact/sample_data.sql  &>> log/install_contact_schema.log
+fi
+
+echo "Privileges on log folder..."
     chmod -R 777 log
