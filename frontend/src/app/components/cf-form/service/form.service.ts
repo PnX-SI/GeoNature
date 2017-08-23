8e4a6d3a33c3f7a0893a96cd88e48f260dfc9b50
TheoLechemia <lechemia.theo@gmail.com>
2017-08-23 12:00:00 +0200

    component nomenclature test

--- frontend/src/app/components/cf-form/service/form.service.ts
+++ frontend/src/app/components/cf-form/service/form.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 @Injectable()
 export class FormService {
 
-  constructor() { }
+  constructor(private _http: HttpModule) { }
 
   getNomenclature(id_nomenclature:number, regne:string, group2_inpn:string){
+    // let params = {id: id_nomenclature, regne: regne, group2_inpn:group2_inpn};
+    // this._http.get(`${baseUrl}/nomenclature/${id_nomenclature}`, params)
+    //   .subscribe(response=>{
+    //     console.log(response.data)
+    //   })
     return [{'id':1, 'name': 'lala' }, {'id':2, 'name': 'lolo' }, {'id':3, 'name': 'toto' } ]
   }
 
