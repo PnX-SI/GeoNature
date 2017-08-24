ed70f0aa404041d427953e92ab22bf52f019cefc
TheoLechemia <lechemia.theo@gmail.com>
2017-08-24 12:32:05 +0200

    component nomenclature connecté avec le super component cf-form #211

--- frontend/src/app/components/cf-form/nomenclature/nomenclature.component.ts
+++ frontend/src/app/components/cf-form/nomenclature/nomenclature.component.ts
@@ -8,28 +8,35 @@ import {FormService} from '../service/form.service';
 })
 export class NomenclatureComponent implements OnInit {
   labels: any[];
+  nomenclature: any;
   selectedId: number;
+  selectedLabel: any;
   @Input() placeholder: string;
   @Input() idNomenclature: number;
   @Input() regne: string;
   @Input() group2Inpn: string;
   @Input() lang: string;
-  @Output('labelSelected') emitter = new EventEmitter<number>();
-  constructor(private _formService:FormService) { }
+  @Output() labelSelected = new EventEmitter<any>();
+  constructor(private _formService: FormService) { }
 
   ngOnInit() {
      this._formService.getNomenclature(this.idNomenclature, this.regne, this.group2Inpn).then(
       data => {
-        this.labels = data;
+        this.labels = data.values;
+        this.nomenclature = data.mnemonique;
       }
     );
   }
 
-
-
-  ngOnChanges(){
-    // todo
-    // when input language change, change which values are display
+  onLabelChange() {
+    this.labelSelected.emit({nomenclature: this.nomenclature, idLabel: this.selectedLabel});
   }
+
+
+
+  // ngOnChanges(){
+    // TODO
+    // when input language change, change which values are display
+  // }
 
 }
