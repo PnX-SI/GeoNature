fd9773cb73fec9d826e3df433637e86da2ba9e77
Amandine SAHL <amandine.sahl@gmail.com>
2017-08-24 09:36:05 +0200

    Ajout du module contact - cf #220, #219

---
+++ backend/modules/pr_contact/routes.py
@@ -0,0 +1,30 @@
+# coding: utf8
+from __future__ import (unicode_literals, print_function,
+                        absolute_import, division)
+
+from flask import Blueprint, request
+
+from .models import TRelevesContact, TOccurrencesContact
+from ..utils.utilssqlalchemy import json_resp
+
+from geojson import Feature, FeatureCollection, dumps
+
+routes = Blueprint('pr_contact', __name__)
+
+@routes.route('/releves', methods=['GET'])
+@json_resp
+def getReleves():
+    q = TRelevesContact.query
+    data = q.all()
+    if data:
+        return FeatureCollection([n.get_geofeature() for n in data])
+    return {'message': 'not found'}, 404
+
+@routes.route('/occurrences', methods=['GET'])
+@json_resp
+def getOccurrences():
+    q = TOccurrencesContact.query
+    data = q.all()
+    if data:
+        return ([n.as_dict() for n in data])
+    return {'message': 'not found'}, 404
