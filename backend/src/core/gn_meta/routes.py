1e51a190156f13079db11e741852edc85cec0a66
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2017-08-24 16:39:53 +0200

    BACKEND - routes meta simples pour le frontend

--- backend/src/core/gn_meta/routes.py
+++ backend/src/core/gn_meta/routes.py
@@ -3,12 +3,24 @@ from __future__ import (unicode_literals, print_function,
                         absolute_import, division)
 
 from flask import Blueprint
+from flask_sqlalchemy import SQLAlchemy
 
 from .models import TProgrammes, TDatasets
 from ...utils.utilssqlalchemy import json_resp
 
+db = SQLAlchemy()
 
 routes = Blueprint('gn_meta', __name__)
+
+
+@routes.route('/list/programmes', methods=['GET'])
+@json_resp
+def getProgrammesList():
+    q = TProgrammes.query
+    data = q.all()
+    if data:
+         return [d.as_dict(columns=('id_programme','programme_desc')) for d in data]
+    return {'message': 'not found'}, 404
 
 @routes.route('/programmes', methods=['GET'])
 @json_resp
@@ -26,6 +38,16 @@ def getProgrammesWithDatasets():
     data = q.all()
     if data:
         return ([n.as_dict(True) for n in data])
+    return {'message': 'not found'}, 404
+
+
+@routes.route('/list/datasets', methods=['GET'])
+@json_resp
+def getDatasetsList():
+    q = TDatasets.query
+    data = q.all()
+    if data:
+         return [d.as_dict(columns=('id_dataset','dataset_name')) for d in data]
     return {'message': 'not found'}, 404
 
 @routes.route('/datasets', methods=['GET'])
