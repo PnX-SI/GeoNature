19c98f3c54c944b9d8b603777f9a35772429a6b4
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2017-08-24 12:00:48 +0200

    BACKEND - mise en place des routes meta

---
+++ backend/src/core/gn_meta/routes.py
@@ -0,0 +1,38 @@
+# coding: utf8
+from __future__ import (unicode_literals, print_function,
+                        absolute_import, division)
+
+from flask import Blueprint
+
+from .models import TProgrammes, TDatasets
+from ...utils.utilssqlalchemy import json_resp
+
+
+routes = Blueprint('gn_meta', __name__)
+
+@routes.route('/programmes', methods=['GET'])
+@json_resp
+def getProgrammes():
+    q = TProgrammes.query
+    data = q.all()
+    if data:
+        return ([n.as_dict(False) for n in data])
+    return {'message': 'not found'}, 404
+
+@routes.route('/programmeswithdatasets', methods=['GET'])
+@json_resp
+def getProgrammesWithDatasets():
+    q = TProgrammes.query
+    data = q.all()
+    if data:
+        return ([n.as_dict(True) for n in data])
+    return {'message': 'not found'}, 404
+
+@routes.route('/datasets', methods=['GET'])
+@json_resp
+def getDatasets():
+    q = TDatasets.query
+    data = q.all()
+    if data:
+        return ([n.as_dict() for n in data])
+    return {'message': 'not found'}, 404
