705fe79b929d669daffe40a21e01b4ebc881a0dd
TheoLechemia <lechemia.theo@gmail.com>
2017-08-25 13:03:28 +0200

    component nomenclature: prise en compte du regne et du group2_inpn #211

--- frontend/src/app/components/cf-form/service/form.service.ts
+++ frontend/src/app/components/cf-form/service/form.service.ts
@@ -12,16 +12,17 @@ export class FormService {
     this.indexContact = 0;
    }
 
-  getNomenclature(id_nomenclature: number, regne?:string, group2_inpn?: string){
-    const params = {id: id_nomenclature, regne: regne, group2_inpn:group2_inpn};
-    if (regne) {
-      params.regne = regne;
-    }
-    if (group2_inpn) {
-      params.group2_inpn = group2_inpn;
-    }
+  getNomenclature(id_nomenclature: number, regne?: string, group2_inpn?: string) {
+    const params: URLSearchParams = new URLSearchParams();
+    // const params = {id: id_nomenclature, regne: regne, group2_inpn:group2_inpn};
+    // if (regne !== undefined) {
+    //   params.set('regne', regne);
+    // }
+    // if (group2_inpn !== undefined) {
+    //   params.set('group2_inpn', group2_inpn);
+    // }
     // TODO: insert regne and group2INPN in the request
-    return this._http.get(`${AppConfigs.API_ENDPOINT}nomenclatures/nomenclature/${id_nomenclature}`)
+    return this._http.get(`${AppConfigs.API_ENDPOINT}nomenclatures/nomenclature/${id_nomenclature}?regne=${regne}?group2_inpn=${group2_inpn}`)
     .toPromise()
     .then(
       response => {
