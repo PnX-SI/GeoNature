1bfef7361aad82abc225fd6e5a980262cb17aa87
TheoLechemia <lechemia.theo@gmail.com>
2017-08-25 10:14:33 +0200

    avancement sur le component cf-form #216

--- frontend/src/app/components/cf-form/service/form.service.ts
+++ frontend/src/app/components/cf-form/service/form.service.ts
@@ -11,7 +11,6 @@ export class FormService {
    }
 
   getNomenclature(id_nomenclature: number, regne?:string, group2_inpn?: string){
-    console.log(AppConfigs.API_ENDPOINT);
     const params = {id: id_nomenclature, regne: regne, group2_inpn:group2_inpn};
     if (regne) {
       params.regne = regne;
@@ -19,6 +18,7 @@ export class FormService {
     if (group2_inpn) {
       params.group2_inpn = group2_inpn;
     }
+    // TODO: insert regne and group2INPN in the request
     return this._http.get(`${AppConfigs.API_ENDPOINT}nomenclatures/nomenclature/${id_nomenclature}`)
     .toPromise()
     .then(
@@ -26,6 +26,10 @@ export class FormService {
         return response.json();
       });
     }
+  
+  getDatasets(){
+    
+  }
 
   getObservers(idMenu) {
      return this._http.get(`${AppConfigs.API_ENDPOINT}users/menu/${idMenu}`).
@@ -35,7 +39,7 @@ export class FormService {
       });
   }
   
-  searchTaxonomy (taxonName: string,id: string) {
+  searchTaxonomy (taxonName: string, id: string) {
     return this._http.get(`${AppConfigs.API_TAXHUB}taxref/allnamebylist/${id}?search_name=${taxonName}`)
     .map(res => res.json());
   }
