538d1a67757d1a953a061c08502be34103f35dee
TheoLechemia <lechemia.theo@gmail.com>
2017-08-29 14:32:41 +0200

    update taxonInput on addTaxon

--- frontend/src/modules/contact/components/contact-form/contact-form.service.ts
+++ frontend/src/modules/contact/components/contact-form/contact-form.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Injectable()
 export class ContactFormService {
   taxon:any;
   indexContact: number;
+  inputTaxon: FormControl;
   constructor() {
     this.taxon = {};
     this.indexContact = 0;
+    this.inputTaxon = new FormControl();
    }
 
    updateTaxon(taxon) {
      this.taxon = taxon;
    }
+
 }
