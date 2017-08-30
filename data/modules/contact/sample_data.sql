432309fc2eadb18197866e0605501b5bba712ba4
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2017-08-30 12:36:51 +0200

    Quelques données exemple en plus

--- data/modules/contact/sample_data.sql
+++ data/modules/contact/sample_data.sql
@@ -9,19 +9,31 @@ SET client_min_messages = warning;
 --DATAS--
 ---------
 
-INSERT INTO gn_meta.t_datasets VALUES (1, 'contact', 'Observation aléatoire de la faune, de la flore ou de la fonge', 1, 2, 2, 2, 2, true, NULL, '2017-06-01 00:00:00', '2017-06-01 00:00:00');
+INSERT INTO gn_meta.t_datasets VALUES 
+(1, 'Contact aléatoire', 'Observation aléatoire de la faune, de la flore ou de la fonge', 1, 2, 2, 2, 2, true, NULL, '2017-06-01 00:00:00', '2017-06-01 00:00:00')
+,(2, 'ATBI Lauvitel', 'Inventaire biologique généralisé sur la réserve du Lauvitel', 1, 2, 2, 2, 2, true, NULL, '2017-06-01 00:00:00', '2017-06-01 00:00:00');
 
-INSERT INTO gn_synthese.t_modules (id_module, name_module, desc_module, entity_module_pk_field, url_module, target, picto_module, groupe_module, active) VALUES (1, 'contact', 'Données issues du contact aléatoire', 'pr_contact.t_occurrences_contact.id_occurrence_contact', '/contact', NULL, NULL, 'CONTACT', true);
+INSERT INTO gn_synthese.t_modules (id_module, name_module, desc_module, entity_module_pk_field, url_module, target, picto_module, groupe_module, active) VALUES 
+(1, 'Contact faune flore', 'Données issues du contact aléatoire', 'pr_contact.t_occurrences_contact.id_occurrence_contact', '/contact', NULL, NULL, 'CONTACT', true);
 
-INSERT INTO pr_contact.t_releves_contact VALUES(1,1,1,'2017-01-01','2017-01-01',1500,1565,FALSE,'web',now(),now(),'exemple test','01010000206A0800002E988D737BCC2D41ECFA38A659805841','0101000020E61000000000000000001A40CDCCCCCCCC6C4640');
-SELECT pg_catalog.setval('pr_contact.t_releves_contact_id_releve_contact_seq', 1, true);
+INSERT INTO pr_contact.t_releves_contact VALUES 
+(1,1,1,'2017-01-01','2017-01-01',1500,1565,FALSE,'web',now(),now(),'Exemple test','01010000206A0800002E988D737BCC2D41ECFA38A659805841','0101000020E61000000000000000001A40CDCCCCCCCC6C4640')
+,(2,1,1,'2017-01-08','2017-01-08',1600,1600,FALSE,'web',now(),now(),'Autre exemple test','01010000206A08000000C60EAE8C922D4173407D272D755841','01010000009A999999999919400000000000604640');;
+SELECT pg_catalog.setval('pr_contact.t_releves_contact_id_releve_contact_seq', 2, true);
 
-INSERT INTO pr_contact.t_occurrences_contact VALUES(1,1,343,65,177,30,182,91,347,163,1,'gil','gees',60612,'Lynx Boréal','Taxref V9.0','','','poil',FALSE, now(),now(),'test');
-SELECT pg_catalog.setval('pr_contact.t_occurrences_contact_id_occurrence_contact_seq', 1, true);
+INSERT INTO pr_contact.t_occurrences_contact VALUES 
+(1,1,343,65,177,30,182,91,347,163,1,'Gil','Gees',60612,'Lynx Boréal','Taxref V9.0','','','Poil',FALSE, now(),now(),'Test')
+,(2,1,343,65,177,30,182,91,347,163,1,'Gil D','Gees',380270,'Requin baleine','Taxref V9.0','','','Poils de plumes',FALSE, now(),now(),'Autre test')
+,(3,2,343,65,177,30,182,91,347,163,1,'Donovan M','Gees',528747,'Singe hurleur roux','Taxref V9.0','','','Poils de plumes',FALSE, now(),now(),'Troisieme test');;
+SELECT pg_catalog.setval('pr_contact.t_occurrences_contact_id_occurrence_contact_seq', 3, true);
 
-INSERT INTO pr_contact.cor_role_releves_contact VALUES(1,1);
+INSERT INTO pr_contact.cor_role_releves_contact VALUES 
+(1,1)
+,(2,1);
 
-INSERT INTO pr_contact.cor_municipality_releves_contact VALUES(1,'05004');
+INSERT INTO pr_contact.cor_municipality_releves_contact VALUES 
+(1,'05004')
+,(2,'05004');
 
 INSERT INTO  pr_contact.cor_counting_contact (id_counting_contact, id_occurrence_contact, id_nomenclature_life_stage, id_nomenclature_sex, id_nomenclature_obj_count, id_nomenclature_type_count, count_min, count_max) VALUES
 (1,1,4,190,166,107,5,5)
