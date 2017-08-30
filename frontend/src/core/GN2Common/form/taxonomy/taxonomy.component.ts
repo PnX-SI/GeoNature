9ab13f4e3346cb8f7d61dd49f831697498534ffd
TheoLechemia <lechemia.theo@gmail.com>
2017-08-30 17:09:22 +0200

    travail sur le super-form, almost done ! #216

--- frontend/src/core/GN2Common/form/taxonomy/taxonomy.component.ts
+++ frontend/src/core/GN2Common/form/taxonomy/taxonomy.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 import {FormControl} from '@angular/forms';
 import { FormService } from '../form.service';
 
@@ -7,29 +7,47 @@ import { FormService } from '../form.service';
   templateUrl: './taxonomy.component.html',
   styleUrls: ['./taxonomy.component.scss']
 })
-export class TaxonomyComponent implements OnInit {
-  @Input() inputTaxon: FormControl;
-  taxonsList: Array<any>;
+export class TaxonomyComponent implements OnInit, OnChanges {
+  @Input('parentFormControl') inputTaxon: FormControl;
+  taxons: Array<any>;
   searchString: any;
   filteredTaxons: any;
   @Output() taxonChanged = new EventEmitter<any>();
   constructor(private _formService: FormService) {}
 
   ngOnInit() {
-
     this.inputTaxon.valueChanges
       .filter(value => (value.length >= 3 && value.length <= 20))
       .debounceTime(400)
       .distinctUntilChanged()
       .switchMap(value => this._formService.searchTaxonomy(value, '1001'))
-        .subscribe(response => this.taxonsList = response);
+        .subscribe(response => this.taxons = response);
+    this.taxons = [];
+    this.inputTaxon.valueChanges.subscribe(value=>{
+      console.log(value);
+    })
   }
 
-  // getTaxonInfo(taxon) {
-  //   this._formService.taxon = taxon;
-  // }
+  displaySearchName(taxon): string {
+    return taxon ? taxon.search_name : '';
+  }
 
-  onTaxonSelected(taxon){
+  ngOnChanges(changes){
+    // if the formcontroller change, we have to reload the observable
+    if(changes.inputTaxon){
+      this.inputTaxon.valueChanges
+      .filter(value => (value.length >= 3 && value.length <= 20))
+      .debounceTime(400)
+      .distinctUntilChanged()
+      .switchMap(value => this._formService.searchTaxonomy(value, '1001'))
+        .subscribe(response => this.taxons = response);
+    }
+  }
+  
+
+
+
+  onTaxonSelected(taxon){    
     this.taxonChanged.emit(taxon);
   }
 
--- frontend/src/modules/contact/components/contact-form/contact-form.service.ts
+++ frontend/src/modules/contact/components/contact-form/contact-form.service.ts
@@ -1,19 +1,109 @@
 import { Injectable } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormBuilder, FormGroup, FormArray } from '@angular/forms';
+import {  } from '@angular/forms';
 
 @Injectable()
 export class ContactFormService {
-  taxon:any;
-  indexContact: number;
-  inputTaxon: FormControl;
-  constructor() {
-    this.taxon = {};
-    this.indexContact = 0;
-    this.inputTaxon = new FormControl();
-   }
+  currentTaxon:any;
+  taxonsList: Array<any>;
+  indexOccurence:number;
+  indexCounting: number;
+  nbCounting: Array<string>;
+  contactForm: FormGroup;
+  countingForm : FormArray;
+  constructor(private _fb: FormBuilder) {
+    this.currentTaxon = {};
+    this.indexOccurence = 0;
+    this.indexCounting = 0;
+    this.nbCounting = [''];
+    this.taxonsList = [];
+
+    // Contact and releve Form
+    this.contactForm = this._fb.group({
+      observers:'',
+      dateMin: '',
+      dateMax: '',
+      dataSet: '',
+      comment: '',
+      t_occurrences_contact: this._fb.array([])
+      });
+    // init occurence contact
+    const occurenceControl = <FormArray>this.contactForm.controls['t_occurrences_contact'];
+    const newOccurenceCtrl = this.initOccurenceForm();
+    occurenceControl.push(newOccurenceCtrl);
+
+    // counting form
+    this.countingForm = this.initCountingArray();
+    
+
+   }// end constructor
+
+   initOccurenceForm(){
+    return this._fb.group({
+      id_nomenclature_obs_technique :'',
+      id_nomenclature_obs_meth: '',
+      id_nomenclature_bio_condition:'',
+      id_nomenclature_bio_status :'',
+      id_nomenclature_naturalness :'',
+      determination_method:'',
+      determiner:'',
+      digital_proof:'',
+      non_digital_proof:'',
+      cd_nom:'',
+      comment:'',
+      cor_counting_contact: ''
+    })
+  }
+
+   initCounting(): FormGroup {
+    return this._fb.group({
+      id_nomenclature_life_stage:'',
+      id_nomenclature_sex:'',
+      id_nomenclature_obj_count: '',
+      id_nomenclature_type_count:'',
+      count_min :'',
+      count_max :''
+    })
+  }
+
+  initCountingArray():FormArray{
+    const arrayForm = this._fb.array([])
+    arrayForm.push(this.initCounting());
+    return arrayForm
+  }
+
+  addOccurence(){
+    // add an other occurence
+    const control = <FormArray>this.contactForm.controls['t_occurrences_contact'];
+    const addCtrl = this.initOccurenceForm();
+    control.push(addCtrl);
+
+    // push the counting(s) in cor_counting_contact
+    this.contactForm.value.t_occurrences_contact[this.indexOccurence].cor_counting_contact = this.countingForm.value;
+     this.indexOccurence += 1;
+
+    // reset counting
+     this.nbCounting = [''];
+     this.indexCounting = 0;
+     this.countingForm = this.initCountingArray();
+
+    // push the current taxon in the taxon list
+    this.taxonsList.push(this.currentTaxon);
+  }
+
+  removeOneOccurenceTaxon(index){
+    this.contactForm.value.t_occurrences_contact.splice(index,1)
+  }
+
+  addCounting(){
+    this.indexCounting += 1;
+    this.nbCounting.push('');
+    const countingCtrl = this.initCounting();
+    this.countingForm.push(countingCtrl)
+    }
 
    updateTaxon(taxon) {
-     this.taxon = taxon;
+     this.currentTaxon = taxon;
    }
 
 }
