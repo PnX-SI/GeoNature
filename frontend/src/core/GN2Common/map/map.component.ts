77336283ffc1dfa5a750633c50fa4400cde3cca3
TheoLechemia <lechemia.theo@gmail.com>
2017-08-31 17:35:40 +0200

    add leaflet draw

--- frontend/src/core/GN2Common/map/map.component.ts
+++ frontend/src/core/GN2Common/map/map.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MapService } from './map.service';
 import {Map} from 'leaflet';
+import 'leaflet-draw';
+
 
 @Component({
   selector: 'pnx-map',
@@ -9,17 +11,70 @@ import {Map} from 'leaflet';
 })
 export class MapComponent implements OnInit {
   public map: Map;
+  public Le: any;
   searchLocation: string;
   constructor(public mapService: MapService) {
     this.mapService.editing = false;
     // this.mapService.removing = false;
     this.searchLocation = '';
+    this.Le = L as any;
   }
 
   ngOnInit() {
     this.mapService.initialize();
-    this.mapService.onMapClick();
+    //this.mapService.onMapClick();
     this.map = this.mapService.map;
+
+    var editableLayers = new L.FeatureGroup();
+    this.map.addLayer(editableLayers);
+
+    var options = {
+      position: 'topleft',
+      draw: {
+          polyline: {
+              shapeOptions: {
+                  color: '#f357a1',
+                  weight: 10
+              }
+          },
+          polygon: {
+              allowIntersection: false, // Restricts shapes to simple polygons
+              drawError: {
+                  color: '#e1e100', // Color the shape will turn when intersects
+                  message: '<strong>Oh snap!<strong> you can\'t draw that!' // Message that will show when intersect
+              },
+              shapeOptions: {
+                  color: '#bada55'
+              }
+          },
+          circle: false, // Turns off this drawing tool
+          rectangle: {
+              shapeOptions: {
+                  clickable: false
+              }
+          },
+          marker: {
+              icon: L.icon({
+                iconUrl: require<any>('../../../../node_modules/leaflet/dist/images/marker-icon.png'),
+                iconSize: [24,36],
+                iconAnchor: [12,36]
+              }),
+          }
+      },
+      edit: {
+          featureGroup: editableLayers, //REQUIRED!!
+          remove: false
+      }
+  };
+
+    const drawControl =  new this.Le.Control.Draw(options);
+    this.map.addControl(drawControl);
+
+    this.map.on(this.Le.Draw.Event.CREATED, (e) => {
+      console.log(e);
+      
+    })
+
   }
 
     gotoLocation() {
--- frontend/src/core/GN2Common/map/map.service.ts
+++ frontend/src/core/GN2Common/map/map.service.ts
@@ -72,6 +72,7 @@ export class MapService {
                         }).openPopup();
                     });
                 }
+                
         });
     }
 
