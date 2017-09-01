f61177d5dd6849b46d0f45665bc2b16ca79ea8a2
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2017-09-01 23:38:33 +0200

    Install REF_GEO schema #228

--- install_db.sh
+++ install_db.sh
@@ -27,7 +27,7 @@ if database_exists $db_name
 then
         if $drop_apps_db
             then
-            echo "Suppression de la base..."
+            echo "Suppression de la BDD..."
             sudo -n -u postgres -s dropdb $db_name
         else
             echo "La base de données existe et le fichier de settings indique de ne pas la supprimer."
@@ -40,17 +40,17 @@ fi
 
 if ! database_exists $db_name
 then
-    echo "Création de la base..."
+    echo "Création de la BDD..."
     echo "--------------------" &> log/install_db.log
-    echo "Création de la base" &>> log/install_db.log
+    echo "Création de la BDD" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
     echo "" &>> log/install_db.log
     sudo -n -u postgres -s createdb -O $user_pg $db_name
-    echo "Ajout de postgis à la base..."
+    echo "Ajout de PostGIS à la BDD..."
     echo "" &>> log/install_db.log
     echo "" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
-    echo "Ajout de postgis à la base" &>> log/install_db.log
+    echo "Ajout de PostGIS à la BDD" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
     echo "" &>> log/install_db.log
     sudo -n -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS postgis;" &>> log/install_db.log
@@ -59,23 +59,23 @@ then
 
 
     # Mise en place de la structure de la base et des données permettant son fonctionnement avec l'application
-    echo "Grant..."
+    echo "GRANT..."
     cp data/grant.sql /tmp/grant.sql
     sudo sed -i "s/MYPGUSER/$user_pg/g" /tmp/grant.sql
     echo "" &>> log/install_db.log
     echo "" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
-    echo "Grant" &>> log/install_db.log
+    echo "GRANT" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
     echo "" &>> log/install_db.log
     sudo -n -u postgres -s psql -d $db_name -f /tmp/grant.sql &>> log/install_db.log
 
 
-    echo "Récupération et création du schéma utilisateurs..."
+    echo "Récupération et création du schéma UTILISATEURS..."
     echo "" &>> log/install_db.log
     echo "" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
-    echo "Création du schéma utilisateurs" &>> log/install_db.log
+    echo "Création du schéma UTILISATEURS" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
     echo "" &>> log/install_db.log
     cd data/utilisateurs
@@ -105,36 +105,36 @@ then
     unzip /tmp/LR_FRANCE_20160000.zip -d /tmp
     cd ..
 
-    echo "Récupération des scripts de création du schéma taxonomie..."
+    echo "Récupération des scripts de création du schéma TAXONOMIE..."
     wget https://raw.githubusercontent.com/PnX-SI/TaxHub/$taxhub_release/data/taxhubdb.sql
     wget https://raw.githubusercontent.com/PnX-SI/TaxHub/$taxhub_release/data/taxhubdata.sql
     wget https://raw.githubusercontent.com/PnX-SI/TaxHub/$taxhub_release/data/taxhubdata_taxon_example.sql
     wget https://raw.githubusercontent.com/PnX-SI/TaxHub/$taxhub_release/data/materialized_views.sql
     cd ../..
 
-    echo "Création du schéma taxonomie..."
+    echo "Création du schéma TAXONOMIE..."
     echo "" &>> log/install_db.log
     echo "" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
-    echo "Création du schéma taxonomie" &>> log/install_db.log
+    echo "Création du schéma TAXONOMIE" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
     echo "" &>> log/install_db.log
     export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f data/taxonomie/taxhubdb.sql  &>> log/install_db.log
 
-    echo "Insertion  des données taxonomiques de l'inpn... (cette opération peut être longue)"
+    echo "Insertion des données taxonomiques de l'INPN... (cette opération peut être longue)"
     echo "" &>> log/install_db.log
     echo "" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
-    echo "Insertion  des données taxonomiques de l'inpn" &>> log/install_db.log
+    echo "Insertion des données taxonomiques de l'INPN" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
     echo "" &>> log/install_db.log
     sudo -n -u postgres -s psql -d $db_name -f data/taxonomie/inpn/data_inpn_v9_taxhub.sql &>> log/install_db.log
 
-    echo "Création des données dictionnaires du schéma taxonomie..."
+    echo "Création des données dictionnaires du schéma TAXONOMIE..."
     echo "" &>> log/install_db.log
     echo "" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
-    echo "Création des données dictionnaires du schéma taxonomie" &>> log/install_db.log
+    echo "Création des données dictionnaires du schéma TAXONOMIE" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
     echo "" &>> log/install_db.log
     export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f data/taxonomie/taxhubdata.sql  &>> log/install_db.log
@@ -158,30 +158,30 @@ then
     export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f data/taxonomie/materialized_views.sql  &>> log/install_db.log
 
 
-    echo "Création du schéma meta..."
+    echo "Création du schéma META..."
     echo "" &>> log/install_db.log
     echo "" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
-    echo "Création du schéma meta" &>> log/install_db.log
+    echo "Création du schéma META" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
     echo "" &>> log/install_db.log
     export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f data/core/meta.sql  &>> log/install_db.log
 
 
-    echo "Création du schéma nomenclatures..."
+    echo "Création du schéma NOMENCLATURES..."
     echo "" &>> log/install_db.log
     echo "" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
-    echo "Création du schéma nomenclatures" &>> log/install_db.log
+    echo "Création du schéma NOMENCLATURES" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
     echo "" &>> log/install_db.log
     export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f data/core/nomenclatures.sql  &>> log/install_db.log
 
-    echo "Insertion de la nomenclature..."
+    echo "Insertion de la NOMENCLATURE..."
     echo "" &>> log/install_db.log
     echo "" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
-    echo "Insertion de la nomenclature" &>> log/install_db.log
+    echo "Insertion de la NOMENCLATURE" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
     echo "" &>> log/install_db.log
     cp data/core/data_nomenclatures.sql /tmp/data_nomenclatures.sql
@@ -189,29 +189,42 @@ then
     export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f /tmp/data_nomenclatures.sql  &>> log/install_db.log
 
 
-    echo "Création et insertion du schéma medias..."
+    echo "Création et insertion du schéma MEDIAS..."
     echo "" &>> log/install_db.log
     echo "" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
-    echo "Création et insertion du schéma medias" &>> log/install_db.log
+    echo "Création et insertion du schéma MEDIAS" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
     echo "" &>> log/install_db.log
     export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f data/core/medias.sql  &>> log/install_db.log
 
 
-    echo "Création du schéma synthese..."
+    echo "Création du schéma SYNTHESE..."
     echo "" &>> log/install_db.log
     echo "" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
-    echo "Création du schéma synthese" &>> log/install_db.log
+    echo "Création du schéma SYNTHESE" &>> log/install_db.log
     echo "--------------------" &>> log/install_db.log
     echo "" &>> log/install_db.log
     cp data/core/synthese.sql /tmp/synthese.sql
     sudo sed -i "s/MYLOCALSRID/$srid_local/g" /tmp/synthese.sql
     export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f /tmp/synthese.sql  &>> log/install_db.log
 
+if $install_sig_layers
+  then
+    echo "Création du schéma REF_GEO..."
+    echo "" &>> log/install_db.log
+    echo "" &>> log/install_db.log
+    echo "--------------------" &>> log/install_db.log
+    echo "Création du schéma REF_GEO" &>> log/install_db.log
+    echo "--------------------" &>> log/install_db.log
+    echo "" &>> log/install_db.log
+    cp data/core/ref_geo/schema.sql /tmp/ref_geo.sql
+    sudo sed -i "s/MYLOCALSRID/$srid_local/g" /tmp/ref_geo.sql
+    export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f /tmp/ref_geo.sql  &>> log/install_db.log
+fi
 
-    # suppression des fichiers : on ne conserve que les fichiers compressés
+    # Suppression des fichiers : on ne conserve que les fichiers compressés
     echo "nettoyage..."
     rm /tmp/*.txt
     rm /tmp/*.csv
