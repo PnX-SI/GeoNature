a456a4511a44a973259eebfac9946701f46dbc50
TheoLechemia <lechemia.theo@gmail.com>
2017-09-12 11:42:28 +0200

    Surchage du CSS des composant générique #243

--- frontend/src/core/GN2Common/form/counting/counting.component.ts
+++ frontend/src/core/GN2Common/form/counting/counting.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormBuilder, FormGroup, FormArray } from '@angular/forms';
 import { FormService } from '../form.service'
 
@@ -6,7 +6,8 @@ import { FormService } from '../form.service'
 @Component({
   selector: 'pnx-counting',
   templateUrl: './counting.component.html',
-  styleUrls: ['./counting.component.scss']
+  styleUrls: ['./counting.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CountingComponent implements OnInit {
   @Input() index: number;
