36d78e71f8b4bc7e3c7b018718db855bf9f32945
TheoLechemia <lechemia.theo@gmail.com>
2017-09-15 11:20:01 +0200

    set popup et style map-list

--- frontend/src/core/GN2Common/map-list/map-list.component.ts
+++ frontend/src/core/GN2Common/map-list/map-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { MapService } from '../map/map.service';
 import {MapListService} from '../map-list/map-list.service';
+import { GeoJSON } from 'leaflet';
 
 
 @Component({
@@ -10,12 +11,56 @@ import {MapListService} from '../map-list/map-list.service';
   providers: [MapService, MapListService]
 })
 export class MapListComponent implements OnInit {
-  public geojson: any;
+  public geojson: GeoJSON;
+  public layerDict: any;
+  public selectedLayer: any;
+  originStyle = {
+    'color': '#3388ff',
+    'fill': true,
+    'fillOpacity': 0.2,
+    'weight': 3
+};
+
+ selectedStyle = {
+  'color': '#ff0000',
+   'weight':3
+};
+
   constructor(private _ms: MapService, private _mapListService: MapListService) {
   }
 
   ngOnInit() {
+    this.layerDict = {};
     this._mapListService.getReleves()
       .subscribe(res => this.geojson = res);
+  }
+
+  onEachFeature(feature, layer) {
+    console.log(this.originStyle);
+    this.layerDict[feature.id] = layer;
+     layer.on({
+       click : (e) => {
+         // remove selected style
+         if (this.selectedLayer !== undefined) {
+           this.selectedLayer.setStyle(this.originStyle);
+         }
+         // set selected style
+         this.selectedLayer = layer;
+         layer.setStyle(this.selectedStyle);
+         // popup
+         const taxonsList = feature.properties.occurrences.map(occ => {
+            return occ.nom_cite;
+         }).join();
+         const observersList = feature.properties.observers.map(obs => {
+          return obs.prenom_role + ' ' + obs.nom_role;
+       }).join();
+         const popupContent = `<b> Id relevé: </b>: ${feature.id} <br>
+                               <b> Observateur(s): </b> ${observersList} <br>
+                               <b> Taxon(s): </b> ${taxonsList}`;
+        this.selectedLayer.bindPopup(popupContent).openPopup();
+         // observable
+        this._mapListService.setCurrentLayerId(feature.id);
+       }
+     });
   }
 }
--- frontend/src/core/GN2Common/map-list/map-list.service.ts
+++ frontend/src/core/GN2Common/map-list/map-list.service.ts
@@ -2,14 +2,21 @@ import { Injectable} from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { Http, URLSearchParams } from '@angular/http';
 import { AppConfig } from '../../../conf/app.config';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class MapListService {
+  private _layerId = new Subject<any>();
+  public gettingLayerId$: Observable<number> = this._layerId.asObservable();
     constructor(private _http: Http) {
   }
 
 getReleves() {
     return this._http.get(`${AppConfig.API_ENDPOINT}contact/releves`)
       .map(res => res.json());
+  }
+
+  setCurrentLayerId(id: number) {
+    this._layerId.next(id);
   }
 }
