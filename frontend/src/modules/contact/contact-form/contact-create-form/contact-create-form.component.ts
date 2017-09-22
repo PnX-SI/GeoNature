1f232dbb8f83121714d00744bff42d0e55bb45d6
TheoLechemia <lechemia.theo@gmail.com>
2017-09-22 15:07:13 +0200

    Edition releve: récuperation des municipalités

--- frontend/src/modules/contact/contact-form/contact-create-form/contact-create-form.component.ts
+++ frontend/src/modules/contact/contact-form/contact-create-form/contact-create-form.component.ts
@@ -31,31 +31,36 @@ export class ContactCreateFormComponent implements OnInit {
     ) {  }
 
   ngOnInit() {
+    // init the form
     this.releveForm = this.fs.initObservationForm();
     this.occurrenceForm = this.fs.initOccurrenceForm();
     this.countingForm = this.fs.initCountingArray();
 
     // if its edition mode
-    if (this.id !== undefined) {
+    if (!isNaN(this.id )) {
       // load one releve
       this._cfs.getReleve(this.id)
         .subscribe(data => {
           // pre fill the form
           this.releveForm = this.fs.initObservationForm(data);
           for (const occ of data.properties.t_occurrences_contact){
+            // push the occ in releveForm
             this.releveForm.value.properties.t_occurrences_contact.push(occ);
-          }
-          for (const occ of data.properties.t_occurrences_contact){
+            // push the taxon list
             this.taxonsList.push({'nom_valide': occ.nom_cite});
           }
+          // push the geometry in releveForm
+          this.releveForm.patchValue({geometry: data.geometry});
           // copy the form data
           this.releveFormData = this.releveForm.value;
-          // load the geometry
+          // load the geometry in the map
           this._ms.loadGeometryReleve(data);
+          // get geoInfo to get municipalities
+          this._dfs.getGeoInfo(data)
+            .subscribe(data => { this.fs.municipalities = data.municipality.map(m => m.area_name).join(', ')})
 
       }); // end subscribe
     }
-
     // init the taxons list
     this.taxonsList = [];
   } // end ngOnInit
@@ -73,6 +78,7 @@ export class ContactCreateFormComponent implements OnInit {
     // add an occurrence
     this.fs.addOccurence(this.occurrenceForm, this.releveForm, this.countingForm);
     // save the data of form
+    // LAAAAAAAAAAA
     this.releveFormData = this.releveForm.value;
     // set the index occurence
     this.fs.indexOccurrence = this.releveForm.value.properties.t_occurrences_contact.length ;
@@ -86,44 +92,44 @@ export class ContactCreateFormComponent implements OnInit {
     // set the current index
     this.fs.indexOccurrence = index;
     // get the occurrence data from releve form
+    // LAAAAAAAAAA
     const occurenceData = this.releveFormData.properties.t_occurrences_contact[index];
+    // const occurenceData = this.releveForm.value.properties.t_occurrences_contact[index];
     const countingData = occurenceData.cor_counting_contact;
     const nbCounting = countingData.length;
     // init occurence form with the data to edit
     this.occurrenceForm = this.fs.initOccurrenceForm(occurenceData);
-    // get counting data from occurence
-
     // init the counting form with the data to edit
     for (let i = 1; i < nbCounting; i++) {
       this.fs.nbCounting.push('');
      }
     this.countingForm = this.fs.initCountingArray(countingData);
     // set the current taxon
-    console.log(occurenceData);
-    // TODO post the all taxon object quand la vue le renverra 
+    // TODO post the all taxon object quand la vue le renverra
     this.fs.currentTaxon = occurenceData.nom_cite;
   }
 
-
   submitData() {
     // Format the final form
-    const finalForm = this.releveForm.value;
-    finalForm.geometry = finalForm.geometry.geometry;
+    // const finalForm = this.releveForm.value;
+    const finalForm = this.releveFormData;
     finalForm.properties.date_min = this._dateParser.format(finalForm.properties.date_min);
     finalForm.properties.date_max = this._dateParser.format(finalForm.properties.date_max);
-    // format cd_nom
+    // format cd_nom and update date
     finalForm.properties.t_occurrences_contact.forEach(occ => {
       occ.nom_cite = occ.cd_nom.nom_valide;
       occ.cd_nom = occ.cd_nom.cd_nom;
+      occ.meta_update_date = new Date();
     });
     // format observers
     finalForm.properties.observers = finalForm.properties.observers
-      .map(observer => observer.id_role )
+      .map(observer => observer.id_role);
 
     console.log(finalForm);
-    
+    // FIX
+    finalForm.properties.municipalities = [74000];
+
     console.log(JSON.stringify(finalForm));
-    
 
     // Post
     this._dfs.postContact(finalForm)
@@ -140,8 +146,6 @@ export class ContactCreateFormComponent implements OnInit {
         (error) => { this.toastr.error("Une erreur s'est produite!", '', {positionClass:'toast-top-center'});}
       );
 
-
-    
   }
 
 
