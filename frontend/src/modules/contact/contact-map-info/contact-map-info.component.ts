cfaa632160e9abb99d4b55725a707dd3f16d578b
TheoLechemia <lechemia.theo@gmail.com>
2017-10-04 14:10:03 +0200

    page info relevé

--- frontend/src/modules/contact/contact-map-info/contact-map-info.component.ts
+++ frontend/src/modules/contact/contact-map-info/contact-map-info.component.ts
@@ -3,11 +3,14 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 import { ContactFormService } from '../contact-map-form/form/contact-form.service';
 import { MapService } from '../../../core/GN2Common/map/map.service';
-
+import { DataFormService } from '../../../core/GN2Common/form/data-form.service';
+import { FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'pnx-contact-map-info',
-  templateUrl: 'contact-map-info.component.html'
+  templateUrl: 'contact-map-info.component.html',
+  styleUrls: ['./contact-map-info.component.scss'],
+
 })
 
 export class ContactMapInfoComponent implements OnInit {
@@ -15,7 +18,16 @@ export class ContactMapInfoComponent implements OnInit {
   public id: number;
   public releve: any;
   public observers: any;
-  constructor(private _cfs: ContactFormService, private _route: ActivatedRoute, private _ms: MapService) { }
+  public selectedOccurrence: any;
+  public occurrenceForm: FormGroup;
+  public countingFormArray: FormArray;
+  public disabled = true;
+  public selectedIndex = 0;
+  public municipalities: string;
+  public dateMin: string;
+  public dateMax: string;
+  constructor(private _cfs: ContactFormService, private _route: ActivatedRoute, private _ms: MapService,
+    private _dfs: DataFormService, private _router: Router) { }
 
   ngOnInit() {
     this._sub = this._route.params.subscribe(params => {
@@ -26,11 +38,47 @@ export class ContactMapInfoComponent implements OnInit {
           .subscribe(data => {
             this.releve = data;
             this.observers = data.properties.observers.map(obs => obs.nom_role + ' ' + obs.prenom_role).join(', ');
-            console.log(this.observers);
+            this.municipalities = data.properties.observers.map(muni => muni.area_name).join(', ');            
+            this.dateMin = data.properties.date_min.substring(0, 10);
+            this.dateMax = data.properties.date_max.substring(0, 10);
 
             this._ms.loadGeometryReleve(data);
+            // init selected occurrence with the first one
+            this.selectedOccurrence = data.properties.t_occurrences_contact[0];
+            console.log(this.selectedOccurrence);
+
+            this.occurrenceForm = this._cfs.initOccurrenceForm(this.selectedOccurrence);
+            // init form array
+            this.countingFormArray = this._cfs.initCountingArray(this.selectedOccurrence.cor_counting_contact);
+            // load taxonomy info
+            data.properties.t_occurrences_contact.forEach(occ => {
+              this._dfs.getTaxonInfo(occ.cd_nom)
+                .subscribe(taxon => occ['taxon'] = taxon);
+            });
+            console.log(data.properties);
+
         });
       }
   });
+  }
+
+  onEditReleve(id) {
+    this._router.navigate(['contact-form', id]);
+  }
+
+  selectOccurrence(occ, index) {
+    this.selectedIndex = index;
+    this.selectedOccurrence = occ;
+    this.loadNomenclaturesOccurrence();
+    this.occurrenceForm = this._cfs.initOccurrenceForm(occ);
+    this.countingFormArray = this._cfs.initCountingArray(occ.cor_counting_contact);
+  }
+
+  loadNomenclaturesOccurrence() {
+    const arrayNomenclatures = [100, 14, 7, 13, 8, 101, 15];
+    this._dfs.getNomenclatures(arrayNomenclatures)
+      .subscribe(data => {
+        console.log(data);
+      });
   }
 }
