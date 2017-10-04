f7c4ab78a77a98de3d6e95cd43b0dbacc80ac763
TheoLechemia <lechemia.theo@gmail.com>
2017-10-04 16:59:32 +0200

    passage de la liste des taxons enregistré dans le bloc relevé

--- frontend/src/modules/contact/contact-map-form/form/contact-form.component.ts
+++ frontend/src/modules/contact/contact-map-form/form/contact-form.component.ts
@@ -17,8 +17,6 @@ import * as L from 'leaflet';
   providers: []
 })
 export class ContactFormComponent implements OnInit {
-  public taxonsList: Array<any>;
-  public contactForm: FormGroup;
   @Input() id: number;
 
   constructor(public fs: ContactFormService, private _ms: MapService,
@@ -32,6 +30,8 @@ export class ContactFormComponent implements OnInit {
     this.fs.releveForm = this.fs.initObservationForm();
     this.fs.occurrenceForm = this.fs.initOccurrenceForm();
     this.fs.countingForm = this.fs.initCountingArray();
+    // reset taxon list of service
+    this.fs.taxonsList = [];
 
 
     // if its edition mode
@@ -46,7 +46,7 @@ export class ContactFormComponent implements OnInit {
             // push the occ in releveForm
             this.fs.releveForm.value.properties.t_occurrences_contact.push(occ);
             // push the taxon list
-            this.taxonsList.push({'nom_valide': occ.nom_cite});
+            this.fs.taxonsList.push({'nom_valide': occ.nom_cite});
           }
           // set the occurrence
           this.fs.indexOccurrence = this.fs.releveForm.value.properties.t_occurrences_contact.length;
@@ -60,71 +60,10 @@ export class ContactFormComponent implements OnInit {
       }); // end subscribe
     }
     // init the taxons list
-    this.taxonsList = [];
+    // this.taxonsList = [];
   } // end ngOnInit
 
-  addOccurence(index) {
-    // push the current taxon in the taxon list and refresh the currentTaxon
-    this.taxonsList.push(this.fs.currentTaxon);
-    // push the counting
-    this.fs.occurrenceForm.controls.cor_counting_contact.patchValue(this.fs.countingForm.value);
-    // format the taxon
-    this.fs.occurrenceForm.value.cd_nom = this.fs.occurrenceForm.value.cd_nom.cd_nom;
-    if (this.fs.releveForm.value.properties.t_occurrences_contact.length === this.fs.indexOccurrence) {
-      this.fs.releveForm.value.properties.t_occurrences_contact.push(this.fs.occurrenceForm.value);
-    }else {
-      this.fs.releveForm.value.properties.t_occurrences_contact[this.fs.indexOccurrence] = this.fs.occurrenceForm.value;
-    }
-    // set occurrence index
-    this.fs.indexOccurrence = this.fs.releveForm.value.properties.t_occurrences_contact.length;
-    // reset counting
-    this.fs.nbCounting = [''];
-    this.fs.indexCounting = 0;
-    // reset current taxon
-    this.fs.currentTaxon = {};
-    // reset occurrence form
-    this.fs.occurrenceForm = this.fs.initOccurrenceForm();
-    // reset the counting
-    this.fs.countingForm = this.fs.initCountingArray();
-  }
 
-  editOccurence(index) {
-    this.taxonsList.splice(index, 1);
-    // set the current index
-    this.fs.indexOccurrence = index;
-    // get the occurrence data from releve form
-    let occurenceData = this.fs.releveForm.value.properties.t_occurrences_contact[index];
-    const countingData = occurenceData.cor_counting_contact;
-    const nbCounting = countingData.length;
-    // load the taxons info
-    this._dfs.getTaxonInfo(occurenceData.cd_nom)
-      .subscribe(taxon => {
-        occurenceData['cd_nom'] = {
-          'cd_nom': taxon.cd_nom,
-          'group2_inpn': taxon.group2_inpn,
-          'lb_nom': taxon.lb_nom,
-          'nom_valide': taxon.nom_valide,
-          'regne': taxon.regne,
-          
-        }
-        // init occurence form with the data to edit
-        this.fs.occurrenceForm = this.fs.initOccurrenceForm(occurenceData);
-        // set the current taxon
-        this.fs.currentTaxon = occurenceData.cd_nom;
-      });
-    // init the counting form with the data to edit
-    for (let i = 1; i < nbCounting; i++) {
-      this.fs.nbCounting.push('');
-     }
-    this.fs.countingForm = this.fs.initCountingArray(countingData);
-
-  }
-
-  removeOneOccurrence(index){
-    this.taxonsList.splice(index, 1);
-    this.fs.releveForm.value.properties.t_occurrences_contact.splice(index, 1);
-    this.fs.indexOccurrence = this.fs.indexOccurrence - 1 ;
-  }
 
   submitData() {
     // set the releveForm
@@ -134,7 +73,7 @@ export class ContactFormComponent implements OnInit {
     finalForm.properties.date_max = this._dateParser.format(finalForm.properties.date_max);
     // format nom_cite and update date
     finalForm.properties.t_occurrences_contact.forEach((occ, index) => {
-      occ.nom_cite = this.taxonsList[index].nom_valide;
+      occ.nom_cite = this.fs.taxonsList[index].nom_valide;
       occ.meta_update_date = new Date();
     });
     // format observers
@@ -156,7 +95,7 @@ export class ContactFormComponent implements OnInit {
         this.fs.releveForm = this.fs.initObservationForm();
         this.fs.occurrenceForm = this.fs.initOccurrenceForm();
         this.fs.countingForm = this.fs.initCountingArray();
-        this.taxonsList = [];
+        this.fs.taxonsList = [];
         this.fs.municipalities = "";
         // redirect
         this.router.navigate(['/contact']);
