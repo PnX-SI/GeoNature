ffe341c20b064fd5dc754e67e0d68788dce044ad
TheoLechemia <lechemia.theo@gmail.com>
2017-10-11 10:02:37 +0200

    focus taxon component when init occurrence component

--- frontend/src/modules/contact/contact-map-form/form/occurrence/occurrence.component.ts
+++ frontend/src/modules/contact/contact-map-form/form/occurrence/occurrence.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewEncapsulation, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ContactFormService } from '../contact-form.service';
 
@@ -11,12 +11,20 @@ import { ContactFormService } from '../contact-form.service';
 export class OccurrenceComponent implements OnInit {
 
   @Input() occurrenceForm: FormGroup;
-
+  @ViewChild('taxon') taxon;
   constructor(public fs: ContactFormService) {
    }
 
   ngOnInit() {
- 
+    console.log('init occurrence');
+    
+    console.log(this.taxon);
+    console.log(this.taxon.inputElementRef);
+    console.log(document.getElementById('taxonInput'));
+    //document.getElementById('taxonInput').focus();
+    
+    
+    //this.taxon.inputElementRef.focus(); 
   }
 
 
