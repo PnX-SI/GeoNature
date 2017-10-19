fe836a6372a7f4474265328e9a8f69bf9b8b1de8
TheoLechemia <lechemia.theo@gmail.com>
2017-10-19 15:53:46 +0200

    authentification + routing

--- frontend/src/core/components/login/login.component.ts
+++ frontend/src/core/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AppConfig } from '../../../conf/app.config';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pnx-login',
@@ -6,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class LoginComponent implements OnInit {
-  constructor() { }
+  constructor(private _authService: AuthService, private _router: Router) {
+    if (AppConfig.CAS_AUTHENTIFICATION) {
+      // document.location.href = 'https://inpn.mnhn.fr/auth/login?service=https://geonature.fr ';
+    }
+   }
 
-  ngOnInit() { }
+    ngOnInit() {
+
+   }
+  register(user) {
+    this._authService.signinUser(user.username, user.password);
+    if (this._authService.isAuthenticated()) {
+      this._router.navigate(['']);
+    }
+
+  }
 }
