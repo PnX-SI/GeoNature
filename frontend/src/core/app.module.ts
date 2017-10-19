cb2305e1555d0d71d44a3aca474034255ac10cd4
TheoLechemia <lechemia.theo@gmail.com>
2017-10-19 15:53:46 +0200

    authentification + routing

--- frontend/src/core/app.module.ts
+++ frontend/src/core/app.module.ts
@@ -23,9 +23,11 @@ import { ContactModule } from '../modules/contact/contact.module';
 // Angular created component
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './routing/app-routing.module'; // RoutingModule
-import { HomeComponent } from './components/home/home.component';
+import { HomeContentComponent } from './components/home-content/home-content.component';
 import { SidenavItemsComponent } from './components/sidenav-items/sidenav-items.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavHomeComponent } from './components/nav-home/nav-home.component';
 
 
 // Service
@@ -37,8 +39,6 @@ import { AuthGuard } from './components/auth/auth-guard.service';
 import { SideNavService } from './components/sidenav-items/sidenav.service';
 import { MapListService } from './GN2Common/map-list/map-list.service';
 
-
-// test 
 
 
 // AoT requires an exported function for factories
@@ -60,10 +60,12 @@ export function HttpLoaderFactory(http: Http) {
   ],
   declarations: [
     AppComponent,
-    HomeComponent,
+    HomeContentComponent,
     SidenavItemsComponent,
     PageNotFoundComponent,
     SigninComponent,
+    LoginComponent,
+    NavHomeComponent,
   ],
   providers: [NavService, AuthService, AuthGuard, SideNavService],
   bootstrap: [AppComponent],
