b79f62529b2998656bb948e9019b5b57c036555d
TheoLechemia <lechemia.theo@gmail.com>
2017-10-19 11:09:55 +0200

    branch login

--- frontend/src/core/components/auth/auth.service.ts
+++ frontend/src/core/components/auth/auth.service.ts
@@ -2,33 +2,55 @@ import { Router } from '@angular/router';
 import * as firebase from 'firebase';
 import { Injectable } from '@angular/core';
 import { ToastrService, ToastrConfig } from 'ngx-toastr';
+import { Http } from '@angular/http';
+
+
+export interface User {
+  userName: string;
+  rights: Array<any>;
+  organism: string;
+}
 
 @Injectable()
 export class AuthService {
+    currentUser: User;
     token: string;
     toastrConfig: ToastrConfig;
-    constructor(private router: Router,  private toastrService: ToastrService) {
+    constructor(private router: Router,  private toastrService: ToastrService, private _http: Http) {
         this.toastrConfig = {
             positionClass: 'toast-top-center',
             tapToDismiss: true,
             timeOut: 2000
         };
     }
-  signinUser(email: string, password: string) {
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(
-        response => {
-          this.router.navigate(['/']);
-          this.toastrService.success('', 'Login success', this.toastrConfig);
-          firebase.auth().currentUser.getIdToken()
-            .then(
-              (token: string) => this.token = token,
-            );
-        }
-      )
-      .catch(
-        error => this.toastrService.error('', 'Login failed', this.toastrConfig)
-      );
+
+  signinUser(username: string, password: string) {
+    this._http.post('/login',
+      {'username': username,
+       'password': password
+    }).map (response => {
+      const json = response.json();
+      this.currentUser = <User>({
+        userName : json.username,
+        rights: json.rigths,
+        organism : json.organism
+      });
+    });
+    // firebase.auth().signInWithEmailAndPassword(email, password)
+    //   .then(
+    //     response => {
+    //       this.router.navigate(['/']);
+    //       this.toastrService.success('', 'Login success', this.toastrConfig);
+    //       firebase.auth().currentUser.getIdToken()
+    //         .then(
+    //           (token: string) => this.token = token,
+    //         );
+    //     }
+    //   )
+    //   .catch(
+    //     error => this.toastrService.error('', 'Login failed', this.toastrConfig)
+    //   );
+
   }
 
   logout() {
