ef0f319d4ca1e6110afc0d5ce8c8b507195e83cf
TheoLechemia <lechemia.theo@gmail.com>
2017-10-19 11:11:09 +0200

    branch login OK

---
+++ frontend/src/core/components/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pnx-login',
+  templateUrl: 'login.component.html'
+})
+
+export class LoginComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
