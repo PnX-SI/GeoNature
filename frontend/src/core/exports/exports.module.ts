0031d986dfbf1795c0237a65d614d7b48a09720e
TheoLechemia <lechemia.theo@gmail.com>
2017-11-02 11:54:26 +0100

    travail sur les exports coté front

--- frontend/src/core/exports/exports.module.ts
+++ frontend/src/core/exports/exports.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { GN2CommonModule } from '../GN2Common/GN2Common.module';
 import { ExportsComponent } from './exports.component';
+import { ExportsService } from './exports.service';
 
 @NgModule({
-  imports: [],
+  imports: [
+    BrowserModule,
+    GN2CommonModule,
+  ],
   exports: [ExportsComponent],
   declarations: [ExportsComponent],
-  providers: [],
+  providers: [ExportsService],
 })
 export class ExportsModule { }
