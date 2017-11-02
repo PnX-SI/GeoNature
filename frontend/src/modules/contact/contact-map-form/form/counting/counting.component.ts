a9ee9746b14418ae6d1700629b4b52a7ed4d8ae5
TheoLechemia <lechemia.theo@gmail.com>
2017-11-02 09:31:15 +0100

    check intégrité count min et max

--- frontend/src/modules/contact/contact-map-form/form/counting/counting.component.ts
+++ frontend/src/modules/contact/contact-map-form/form/counting/counting.component.ts
@@ -18,26 +18,42 @@ export class CountingComponent implements OnInit {
   @Output() countingRemoved = new EventEmitter<any>();
   @Output() countingAdded = new EventEmitter<any>();
   @ViewChild('typeDenombrement') public typeDenombrement: any;
-  public currentCountMin: number;
   constructor(public fs: ContactFormService, private _commonService: CommonService) { }
 
   ngOnInit() {
     // autocomplete count_max
     (this.formArray.controls[this.fs.indexCounting] as FormGroup).controls.count_min.valueChanges
+      .debounceTime(500)
+      .distinctUntilChanged()
       .subscribe(value => {
-        this.currentCountMin = value;
-        (this.formArray.controls[this.fs.indexCounting] as FormGroup).patchValue({'count_max': value});
+        if (this.formArray.controls[this.fs.indexCounting].value.count_max === null) {
+          (this.formArray.controls[this.fs.indexCounting] as FormGroup).patchValue({'count_max': value});
+        }
+        this.checkCountingIntegrity();
       });
+
     // check if count_max is not > count_min
     (this.formArray.controls[this.fs.indexCounting] as FormGroup).controls.count_max.valueChanges
       .debounceTime(500)
+      .distinctUntilChanged()
       .filter(value => value !== null)
       .subscribe( value => {
-        if (value < this.currentCountMin) {
-          this._commonService.translateToaster('error', 'Counting.CountError');
-          (this.formArray.controls[this.fs.indexCounting] as FormGroup).controls.count_max.setErrors([Validators.required]);
+        // autocomplete count_min if null
+        if ((this.formArray.controls[this.fs.indexCounting] as FormGroup).controls.count_min.pristine &&
+             this.formArray.controls[this.fs.indexCounting].value.count_min === null) {
+          (this.formArray.controls[this.fs.indexCounting] as FormGroup).patchValue({'count_min': value});
         }
+        this.checkCountingIntegrity();
       });
+  }
+
+  checkCountingIntegrity() {
+    const count_min = this.formArray.controls[this.fs.indexCounting].value.count_min;
+    const count_max = this.formArray.controls[this.fs.indexCounting].value.count_max;
+    if (count_max < count_min) {
+      this._commonService.translateToaster('error', 'Counting.CountError');
+      (this.formArray.controls[this.fs.indexCounting] as FormGroup).controls.count_max.setErrors([Validators.required]);
+    }
   }
 
   typeDenombrementChanged(event) {
