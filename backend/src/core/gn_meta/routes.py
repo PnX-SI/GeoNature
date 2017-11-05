96474e292b01d359a71edd26d75b193830a231fa
TheoLechemia <lechemia.theo@gmail.com>
2017-11-05 13:57:15 +0100

    utilisation du WS MTD pour créer des cadres d'acquisition

--- backend/src/core/gn_meta/routes.py
+++ backend/src/core/gn_meta/routes.py
@@ -7,8 +7,11 @@ from flask_sqlalchemy import SQLAlchemy
 
 from sqlalchemy import or_
 
-from .models import TPrograms, TDatasets, TParameters, CorDatasetsActor
+from .models import TPrograms, TDatasets, TParameters, CorDatasetsActor, TAcquisitionFramework
 from ...utils.utilssqlalchemy import json_resp
+
+import requests
+from xml.etree import ElementTree
 
 db = SQLAlchemy()
 
@@ -136,3 +139,43 @@ def getOneParameter(param_name, id_org=None):
     if data:
         return [d.as_dict() for d in data]
     return {'message': 'not found'}, 404
+
+@routes.route('/cadre_acquision_mtd', methods=['POST'])
+@json_resp
+def cadre_acquision_ministere():
+    """ Routes pour ajouter des cadres d'acquision à partir
+    du web service MTD"""
+    r = requests.get("https://preprod-inpn.mnhn.fr/mtd/cadre/export/xml/GetRecordById?id=4A9DDA1F-B601-3E13-E053-2614A8C02B7C")
+    if r.status_code == 200:
+        root = ET.fromstring(r.content)
+        attrib = root.attrib
+        namespace = attrib['{http://www.w3.org/2001/XMLSchema-instance}schemaLocation'].split()[0]
+        namespace = '{'+namespace+'}'
+        # tous les cadres d'acquisition
+        for ca in root.findall('.//'+namespace+'CadreAcquisition'):
+            ca_uuid = ca.find(''+namespace+'identifiantCadre').text
+            ca_name = ca.find(''+namespace+'libelle').text
+            ca_desc = ca.find(''+namespace+'description').text
+            ca_start_date = ca.find('.//'+namespace+'dateLancement').text
+            ca_end_date = ca.find('.//'+namespace+'dateCloture').text
+            cadre = TAcquisitionFramework(
+                #mettre un serial
+                #id_acquisition_framework = 3,
+                unique_acquisition_framework_id = ca_uuid,
+                acquisition_framework_name = ca_name,
+                acquisition_framework_desc = ca_desc,
+                acquisition_framework_start_date = ca_start_date,
+                acquisition_framework_end_date = ca_end_date
+            )
+            #TODO: 
+            #- ecrire dans cor_acquisition_framework_actor
+            #- gérer les merge si UUID existe déja
+        try:
+            db.session.add(cadre)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            raise
+        return {'message': 'add with success'}
+    else:
+        return {'message': 'not found'}, 404
