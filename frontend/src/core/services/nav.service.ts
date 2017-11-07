fd67211dbf35a32f5fa1316ae1022accf5f5c520
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2017-11-07 15:06:03 +0100

    changement nom module: Occtax et correction formulaire

--- frontend/src/core/services/nav.service.ts
+++ frontend/src/core/services/nav.service.ts
@@ -11,14 +11,14 @@ export class NavService {
     private _nav = [{}];
     constructor() {
         this._nav = [{route: '/', appName: 'Accueil', icon: 'home', id:'1'},
-            {route: '/synthese', appName: 'Synthèse', icon: 'device_hub', id:'2'},
-            {route: '/contact', appName: 'Contact FF', icon: 'visibility', id: '14'},
-            {route: '/flore-station', appName: 'Flore Station', icon: 'local_florist', id: '15'},
-            {route: '/suivi-flore', appName: 'Suivi Flore', icon: 'filter_vintage', id: '16'},
-            {route: '/suivi-chiro', appName: 'Suivi Chiro', icon: 'youtube_searched_for', id: '17'},
+            // {route: '/synthese', appName: 'Synthèse', icon: 'device_hub', id:'2'},
+            {route: '/occtax', appName: 'OccTax', icon: 'visibility', id: '14'},
+            // {route: '/flore-station', appName: 'Flore Station', icon: 'local_florist', id: '15'},
+            // {route: '/suivi-flore', appName: 'Suivi Flore', icon: 'filter_vintage', id: '16'},
+            // {route: '/suivi-chiro', appName: 'Suivi Chiro', icon: 'youtube_searched_for', id: '17'},
             {route: '/exports', appName: 'Exports', icon: 'cloud_download', id: '18'},
-            {route: '/prospections', appName: 'Prospections', icon: 'feedback', id: '19'},
-            {route: '/parametres', appName: 'Paramètres', icon: 'settings', id: '20'}
+            // {route: '/prospections', appName: 'Prospections', icon: 'feedback', id: '19'},
+            // {route: '/parametres', appName: 'Paramètres', icon: 'settings', id: '20'}
             ];
   }
 
