17e5b6f701e942fea5e0d07c5ebc16bd9571fea5
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2017-11-14 12:23:26 +0100

    CAS: implémentation en backend

--- frontend/src/core/components/login/login.component.ts
+++ frontend/src/core/components/login/login.component.ts
@@ -13,43 +13,32 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 })
 
 export class LoginComponent implements OnInit {
+  isLoged = false;
   constructor(private _authService: AuthService, private _router: Router, private _location: Location,
   private _http: HttpClient) {
 
    }
 
     ngOnInit() {
-      // if (AppConfig.CAS.CAS_AUTHENTIFICATION) {
-      //   const url_redirection_cas = `${AppConfig.CAS.CAS_LOGIN_URL}?service=${AppConfig.URL_APPLICATION}`;
-      //   document.location.href = url_redirection_cas;
-      //    console.log('redirect');
-      //    this._location.subscribe(val => {
-      //     console.log("url changeeeeeee");
+       if (AppConfig.CAS.CAS_AUTHENTIFICATION) {
+         const url_redirection_cas = `${AppConfig.CAS.CAS_LOGIN_URL}?service=${AppConfig.API_ENDPOINT}test_auth/login_cas`;
+        // const url_redirection_cas = `${AppConfig.CAS.CAS_LOGIN_URL}?service=http://localhost:4200`;
+         document.location.href = url_redirection_cas;
 
+         const d1 = new Date();
+         const d2 = new Date(d1);
+         d2.setMinutes(d1.getMinutes() + 60);
+         this._authService.setToken('zrqgoviedfohvfd', d2);
 
-      //     this._authService.authentified = true;
-      //     // recuperer le ticket et le faire le GET login
-      //   });
-      //   const d1 = new Date();
-      //   const d2 = new Date(d1);
-      //   d2.setMinutes(d1.getMinutes() + 60);
-      //   this._authService.setToken('zrqgoviedfohvfd', d2);
-
-      //   // document.location.href = 'http://localhost:4200/#/lalalalala';
-      // }
+         // document.location.href = 'http://localhost:4200/#/lalalalala';
+      this._authService.fakeSigninUser('admin', 'test');
+       }
 
    }
   register(user) {
-    console.log(user);
- 
-    // this._authService.fakeSigninUser(user.username, user.password);
-    this._http.post('https://preprod-inpn.mnhn.fr/auth/login', user)
-    .subscribe(resp => {
-      console.log(resp);
-    });
-  }
-  
-  logUrl(requestDetail) {
-    console.log(requestDetail);
+    this._authService.fakeSigninUser(user.username, user.password);
+
   }
 }
+
+
