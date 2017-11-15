3e79ae63ca34890e3a9940d330cd2a6f286df2c7
camillemonchicourt <camille.monchicourt@ecrins-parcnational.fr>
2017-11-15 10:03:26 +0100

    CAS OKgit add src/core/auth/routes.py

--- backend/src/core/auth/routes.py
+++ backend/src/core/auth/routes.py
@@ -23,40 +23,36 @@ def loginCas():
     configCas = current_app.config['CAS']
     params = request.args
     if 'ticket' in params:
-        urlValidate = "%s?ticket=%s&service=%s"%(configCas['URL_VALIDATION'], params['ticket'], request.base_url)
+        base_url = current_app.config['URL_API']+"/test_auth/login_cas"
+        urlValidate = "%s?ticket=%s&service=%s"%(configCas['URL_VALIDATION'], params['ticket'], base_url)
         r = requests.get(urlValidate)
         user = None
         if r.status_code == 200:
             xmlDict = xmltodict.parse(r.content)
             resp = xmlDict['cas:serviceResponse']
-            print(resp)
-            if 'cas:user' in resp:
-                user = resp['cas:user']
-        print(user)
+            if 'cas:authenticationSuccess' in resp:
+                user = resp['cas:authenticationSuccess']['cas:user']
         if user:
             WSUserUrl = "%s/%s/?verify=false"%(configCas['USER_WS']['URL'], user)
             r  = requests.get(WSUserUrl, auth=(configCas['USER_WS']['ID'], configCas['USER_WS']['PASSWORD']))
             if r.status_code == 200:
                 infoUser = r.json()
-                print(infoUser)
-                idOrganisme = infoUser.codeOrganisme
-                currentUser = infoUser.prenom+" "+infoUser.nom
-                response = make_response(redirect(current_app.config['URL_APPLICATION']))
+                idOrganisme = infoUser['codeOrganisme']
+                currentUser = infoUser['prenom']+" "+infoUser['nom']
+                response = make_response(redirect(current_app.config['URL_APPLICATION']+"/login"))
                 cookieExp = datetime.datetime.utcnow()
                 cookieExp += datetime.timedelta(seconds=current_app.config['COOKIE_EXPIRATION'])
                 response.set_cookie('token',
                                     'test12345',
                                     expires=cookieExp)
                 response.set_cookie('idOrganisme',
-                                     idOrganisme,
+                                     str(idOrganisme),
                                      expires=cookieExp)
-                response.set_cookie('idOrganisme',
+                response.set_cookie('currentUser',
                                      currentUser,
                                      expires=cookieExp)
             return response
         else:
-            # redirect to inpn
-            requests.get(configCas['URL_LOGOUT'])
-            urlRedirect = "%s?service=%s"%(configCas['URL_LOGIN'], request.base_url)
-            return redirect(urlRedirect)
+            # redirect to inpn            
+            return "echec de l'authentification"
             
