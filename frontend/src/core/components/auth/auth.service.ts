be47137dfee4dd984f6f6c742152e22419182c4a
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2017-11-15 10:59:01 +0100

    modif authentification coté front

--- frontend/src/core/components/auth/auth.service.ts
+++ frontend/src/core/components/auth/auth.service.ts
@@ -2,22 +2,21 @@ import { Router } from '@angular/router';
 //import * as firebase from 'firebase';
 import { Injectable } from '@angular/core';
 import { ToastrService, ToastrConfig } from 'ngx-toastr';
-import { Http } from '@angular/http';
-import { AppConfig } from '../../../conf/app.config';
+import { HttpClient } from '@angular/common/http';import { AppConfig } from '../../../conf/app.config';
 import { CookieService } from 'ng2-cookies';
 import { Location } from '@angular/common';
 
 
 export class User {
 
-  constructor(public userName: string, public rights: Array<any>, public organism: any) {
+  constructor(public userName: string, public rights: any, public organism: any) {
     this.userName = userName;
     this.rights = rights;
     this.organism = organism;
 }
 
   getRight(idApplication) {
-    return this.rights.find(obj => obj.idApplication = idApplication);
+    return this.rights[idApplication];
   }
 }
 
@@ -27,18 +26,13 @@ export class AuthService {
     currentUser: User;
     token: string;
     toastrConfig: ToastrConfig;
-    constructor(private router: Router,  private toastrService: ToastrService, private _http: Http,
-    private _cookie: CookieService, private _location: Location) {
-      console.log('init auth service');
-      
-      _location.subscribe(val => {
-          console.log('location change');
-          console.log(val);
-        });
+    constructor(private router: Router,  private toastrService: ToastrService, private _http: HttpClient,
+    private _cookie: CookieService, private _router: Router) {
     }
 
   setCurrentUser(user, expireDate) {
     this._cookie.set('currentUser', JSON.stringify(user), expireDate);
+    this.currentUser = new User(user.userName, user.rights, user.organism)
   }
 
   getCurrentUser(): User {
@@ -71,20 +65,22 @@ export class AuthService {
           'organismName': 'PNE',
           'organismId': 2
         },
-        'rights': [
-         {'idApplication': 14, 'C': 3, 'R': 3, 'U': 3, 'V': 3, 'E': 3, 'D': 3 }
-        ]};
+        'rights': {
+          14 : {'C': 3, 'R': 3, 'U': 3, 'V': 3, 'E': 3, 'D': 3 }
+          }
+        };
 
     } else {
-       response = {'userName': 'contributeur',
-      'organism': {
-        'organismName': 'PNF',
-        'organismId': 1
-      },
-      'rights': [
-         {'idApplication': 14, 'C': 2, 'R': 1, 'U': 1, 'V': 1, 'E': 1, 'D': 1 }
-        ]};
-
+       response = {
+         'userName': 'contributeur',
+          'organism': {
+            'organismName': 'PNF',
+            'organismId': 1
+          },
+      'rights': {
+         14 : {'C': 2, 'R': 1, 'U': 1, 'V': 1, 'E': 1, 'D': 1 }
+        }
+      };
     }
     this.setCurrentUser(response, d2);
     this.setToken('1123345254', d2);
@@ -93,12 +89,11 @@ export class AuthService {
   }
 
   signinUser(username: string, password: string) {
-    this._http.post(`${AppConfig.API_ENDPOINT}/api/auth/login`,
+    this._http.post<any>(`${AppConfig.API_ENDPOINT}/api/auth/login`,
       {'login': username,
        'password': password,
        'id_application': 14
-    }).subscribe(response => {
-      const data = response.json();
+    }).subscribe(data => {
       this.setCurrentUser(data.user, data.expires);
     });
 
@@ -106,7 +101,14 @@ export class AuthService {
   logout() {
     this.router.navigate(['/login']);
     this._cookie.delete('token');
+    if (AppConfig.CAS.CAS_AUTHENTIFICATION) {
+      this._http.get(AppConfig.CAS.CAS_LOGOUT_URL)
+        .subscribe(data => {
+          console.log(data);
+        });
+    }
   }
+
   isAuthenticated(): boolean {
       return this._cookie.get('token') !== null;
   }
