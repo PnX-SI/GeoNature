1c7fc4cf31f5b0bcf0b37c3a011df65a0c9d76e9
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2017-12-01 17:00:35 +0100

    up

--- backend/src/core/auth/routes.py
+++ backend/src/core/auth/routes.py
@@ -94,7 +94,7 @@ def insert_in_cor_role(id_group, id_user):
     exist_user = db.session.query(CorRole
     ).filter(CorRole.id_role_groupe == id_group
     ).filter(CorRole.id_role_utilisateur == id_user
-    ).one()
+    ).all()
     if not exist_user:
         cor_role = CorRole(id_group, id_user)
         try:
