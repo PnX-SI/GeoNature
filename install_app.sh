cf350ee0cb56eb2976bbd10938f01429fab8aa62
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2017-12-15 14:40:28 +0100

    update install

--- install_app.sh
+++ install_app.sh
@@ -11,6 +11,7 @@ fi
 
 
 echo "préparation du fichier config.py..."
+my_url="${my_url//\//\\/}"
 sed -i "s/SQLALCHEMY_DATABASE_URI = .*$/SQLALCHEMY_DATABASE_URI = \"postgresql:\/\/$user_pg:$user_pg_pass@$db_host:$db_port\/$db_name\"/" config.py
 sed -i "s/URL_APPLICATION = .*$/URL_APPLICATION = '${my_url}geonature' /g" config.py
 sed -i "s/URL_API = .*$/URL_API = '${my_url}api'/g" config.py
