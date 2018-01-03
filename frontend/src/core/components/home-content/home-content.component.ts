2feaf4434f9749a4a37df52114624d3ef3d69b3f
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-01-03 15:47:52 +0100

    intro + footer sample

--- frontend/src/core/components/home-content/home-content.component.ts
+++ frontend/src/core/components/home-content/home-content.component.ts
@@ -12,52 +12,14 @@ import { SideNavService } from '../sidenav-items/sidenav.service';
 })
 export class HomeContentComponent implements OnInit {
   private moduleName: string;
-
-  // test chartjs
-  type1 = 'line';
-  type2 = 'bar';
-  type3 = 'radar';
-  type4 = 'polarArea';
-  data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-    datasets: [
-      {
-        label: 'My First dataset',
-        data: [6, 2, 5, 1, 5, 6],
-        borderColor: 'rgba(255, 159, 64, 1)',
-        backgroundColor: 'rgba(255, 159, 64, 0.2)',
-      }, {
-        label: 'My Second dataset',
-        borderColor: 'rgba(255,99,132,1)',
-        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-          data: [1, 5, 2, 6, 6, 1],
-        }, {
-          label: 'My Third dataset',
-          borderColor: 'rgba(54, 162, 235, 1)',
-          backgroundColor: 'rgba(54, 162, 235, 0.2)',
-          data: [7, 5, 6, 4, 2, 1],
-        }, {
-          label: 'My Four dataset',
-          borderColor: 'rgba(75, 192, 192, 1)',
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
-          data: [8, 9, 5, 5, 4, 3],
-        }],
-      }
-  options = {
-    scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
+  public appConfig: any;
 
   constructor(private _navService: NavService, private _SideNavService: SideNavService) {
   }
 
   ngOnInit() {
     this._SideNavService.sidenav.open();
+    this.appConfig = AppConfig;
   }
 
 }
