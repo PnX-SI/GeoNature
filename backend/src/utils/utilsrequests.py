f1174d0f2de49796c4431e601a9774efb4acae36
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-01-08 15:50:11 +0100

    correction utilsrequests

--- backend/src/utils/utilsrequests.py
+++ backend/src/utils/utilsrequests.py
@@ -4,13 +4,19 @@ import requests
 def get(url, auth=None):
     try:
         r = requests.get(url, auth=auth)
+        assert r.status_code == 200
     except requests.exceptions.RequestException as e:
         raise
+    except AssertionError:
+        print('Not 200')
     return r
 
 def post(url, json={}):
     try:
         r = requests.post(url, json=json)
+        assert r.status_code == 200
     except requests.exceptions.RequestException as e:
         raise
+    except AssertionError:
+        print('Not 200')
     return r
