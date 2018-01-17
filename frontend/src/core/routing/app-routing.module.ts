450bfc6a629ac0748b7046d934049650b5a20121
TheoLechemia <lechemia.theo@gmail.com>
2018-01-17 18:24:49 +0100

    modif du tsconfig.ts

--- frontend/src/core/routing/app-routing.module.ts
+++ frontend/src/core/routing/app-routing.module.ts
@@ -4,12 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { ContactMapFormComponent } from '../../modules/contact/contact-map-form/contact-map-form.component';
 import { HomeContentComponent } from '../components/home-content/home-content.component';
 import { PageNotFoundComponent } from '../components/page-not-found/page-not-found.component';
-import { ContactMapListComponent } from '../../modules/contact/contact-map-list/contact-map-list.component';
-import { ContactMapInfoComponent } from '../../modules/contact/contact-map-info/contact-map-info.component';
 import { AuthGuard } from '../components/auth/auth-guard.service';
 import { LoginComponent } from '../components/login/login.component';
 import { NavHomeComponent } from '../components/nav-home/nav-home.component';
 import { ExportsComponent } from '../exports/exports.component';
+
+// import { GnValidationModule } from '@validation/app/gnValidation.module';
+
+//import { ValidationComponent } from '@validation/gnValidation.component';
 
 const appRoutes: Routes = [
   { path: 'login',  component: LoginComponent},
@@ -17,8 +19,11 @@ const appRoutes: Routes = [
    { path: '', component: NavHomeComponent, canActivateChild: [AuthGuard],
      children: [
       { path: '', component: HomeContentComponent },
-      { path: 'exports', loadChildren: 'core/exports/exports.module#ExportsModule'},
-      { path: 'occtax', loadChildren: 'modules/contact/contact.module#ContactModule'},
+      { path: 'exports', loadChildren: '@geonature/core/exports/exports.module#ExportsModule'},
+      { path: 'occtax', loadChildren: '@geonature/modules/contact/contact.module#ContactModule'},
+      //{ path: 'validation',  component: ValidationComponent},
+      //{ path: 'validation', loadChildren: '@validation/gnValidation.module#GnValidationModule'},
+      //{ path: 'validation', loadChildren: '/home/florian/workspace/Ecrins/gn_module_validation/frontend/app/gnValidation.module#GnValidationModule'},
       { path: '**',  component: PageNotFoundComponent }
 
      ] },
