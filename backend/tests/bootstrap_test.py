e1878f55e9f6cd34f3918471a9d56f9b2c123450
amandine-sahl <amandine.sahl@gmail.com>
2018-01-23 13:41:12 +0100

    Correction test app

--- backend/tests/bootstrap_test.py
+++ backend/tests/bootstrap_test.py
@@ -6,12 +6,17 @@ import sys
 # sys.path.append(str(BASE_DIR))
 import server
 
+from geonature.utils.env import load_config, get_config_file_path
+
+
 
 
 @pytest.fixture
 def geonature_app():
     """ set the application context """
-    app = server.get_app()
+    config_path = get_config_file_path()
+    config = load_config(config_path)
+    app = server.get_app(config)
     ctx = app.app_context()
     ctx.push()
     yield app
