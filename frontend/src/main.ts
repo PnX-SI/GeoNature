b3d140a2208a0c91419196c68294cd4327ae6928
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-02-12 17:24:09 +0100

    update install

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,9 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from '@geonature/core/app.module';
-import { environment } from './environments/environment';
+import { AppConfig } from './conf/app.config';
 
-if (environment.production) {
+if (AppConfig.FRONTEND.PROD_MOD) {
   enableProdMode();
 }
 
