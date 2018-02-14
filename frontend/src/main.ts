7f0f6dfff91a3d977d971db2d7790f952778b0f8
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-02-14 16:56:15 +0100

    up

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,16 +2,9 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from '@geonature/core/app.module';
-<<<<<<< HEAD
-import { environment } from './environments/environment';
-import { AppConfig } from './conf/app.config';
-
-if (AppConfig.PROD_MOD) {
-=======
 import { AppConfig } from './conf/app.config';
 
 if (AppConfig.FRONTEND.PROD_MOD) {
->>>>>>> install_all
   enableProdMode();
 }
 
