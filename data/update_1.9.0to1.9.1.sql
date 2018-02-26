4172c695ad5669bc786608c90ec75e632676aee9
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2018-02-26 18:50:56 +0100

    Correction d'un trigger provocant une récursion.

--- data/update_1.9.0to1.9.1.sql
+++ data/update_1.9.0to1.9.1.sql
@@ -1,3 +1,72 @@
+--Correction d'un trigger provocant une récursion
+CREATE OR REPLACE FUNCTION contactflore.update_fiche_cflore()
+  RETURNS trigger AS
+$BODY$
+DECLARE
+macommune character(5);
+nbreleves integer;
+BEGIN
+-------------------------- gestion des infos relatives a la numerisation (srid utilisé et support utilisé : pda ou web ou sig)
+-------------------------- attention la saisie sur le web réalise un insert sur qq données mais the_geom_3857 est "faussement inséré" par un update !!!
+IF (NOT ST_Equals(new.the_geom_local,old.the_geom_local) OR (old.the_geom_local is null AND new.the_geom_local is NOT NULL))
+  OR (NOT ST_Equals(new.the_geom_3857,old.the_geom_3857) OR (old.the_geom_3857 is null AND new.the_geom_3857 is NOT NULL)) 
+   THEN
+	IF NOT ST_Equals(new.the_geom_3857,old.the_geom_3857) OR (old.the_geom_3857 is null AND new.the_geom_3857 is NOT NULL) THEN
+		new.the_geom_local = st_transform(new.the_geom_3857,2154);
+		new.srid_dessin = 3857;
+	ELSIF NOT ST_Equals(new.the_geom_local,old.the_geom_local) OR (old.the_geom_local is null AND new.the_geom_local is NOT NULL) THEN
+		new.the_geom_3857 = st_transform(new.the_geom_local,3857);
+		new.srid_dessin = 2154;
+	END IF;
+-------gestion des divers control avec attributions de la commune : dans le cas d'un insert depuis le nomade uniquement via the_geom_local !!!!
+	IF st_isvalid(new.the_geom_local) = true THEN	-- si la topologie est bonne alors...
+		-- on calcul la commune (celle qui contient le plus de zp en surface)...
+		SELECT INTO macommune c.insee FROM layers.l_communes c WHERE st_intersects(c.the_geom, new.the_geom_local);
+		new.insee = macommune;
+		-- on calcul l'altitude
+		new.altitude_sig = layers.f_isolines20(new.the_geom_local); -- mise à jour de l'altitude sig
+		IF new.altitude_saisie IS null OR new.altitude_saisie = -1 THEN-- mis à jour de l'altitude retenue
+		    new.altitude_retenue = new.altitude_sig;
+		ELSE
+		    new.altitude_retenue = new.altitude_saisie;
+		END IF;
+	ELSE					
+		SELECT INTO macommune c.insee FROM layers.l_communes c WHERE st_intersects(c.the_geom, ST_PointFromWKB(st_centroid(Box2D(new.the_geom_local)),2154));
+		new.insee = macommune;
+		-- on calcul l'altitude
+		new.altitude_sig = layers.f_isolines20(ST_PointFromWKB(st_centroid(Box2D(new.the_geom_local)),2154)); -- mise à jour de l'altitude sig
+		IF new.altitude_saisie IS null OR new.altitude_saisie = -1 THEN-- mis à jour de l'altitude retenue
+			new.altitude_retenue = new.altitude_sig;
+		ELSE
+			new.altitude_retenue = new.altitude_saisie;
+		END IF;
+	END IF;				
+END IF;
+--- divers update
+IF new.altitude_saisie <> old.altitude_saisie THEN
+   new.altitude_retenue = new.altitude_saisie;
+END IF;
+new.date_update = 'now';
+IF new.supprime <> old.supprime THEN	 
+  IF new.supprime = 't' THEN
+    --Pour éviter un bouclage des triggers, on vérifie qu'il y a bien des relevés non supprimés à modifier
+    SELECT INTO nbreleves count(*) FROM contactflore.t_releves_cflore WHERE id_cflore = old.id_cflore AND supprime = false;
+    IF nbreleves > 0 THEN
+	update contactflore.t_releves_cflore set supprime = 't' WHERE id_cflore = old.id_cflore; 
+     END IF;
+  END IF;
+  IF new.supprime = 'f' THEN
+     --action discutable. S'il y a des relevés douteux dans la fiche, il faut les garder supprimés
+     --update contactflore.t_releves_cflore set supprime = 'f' WHERE id_cflore = old.id_cflore; 
+  END IF;
+END IF;
+RETURN NEW; 
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
+ 
 --Corriger les fonction de calcul de la couleur des taxons par unités géo
 
 CREATE OR REPLACE FUNCTION contactflore.couleur_taxon(
