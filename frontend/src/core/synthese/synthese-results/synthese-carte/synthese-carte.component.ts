4d4abcdc6ea21f03a355b776d41411fc8c083f70
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2018-02-27 16:39:41 +0100

    SYNTHESE - init Frontend

---
+++ frontend/src/core/synthese/synthese-results/synthese-carte/synthese-carte.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'pnx-synthese-carte',
+    templateUrl: 'synthese-carte.component.html',
+    styleUrls: ['synthese-carte.component.scss']
+})
+
+export class SyntheseCarteComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
