d272268548ff8c38c97644f45e8d485e7caa0bae
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-03-06 10:18:13 +0100

    Frontend: style module export

--- frontend/src/core/exports/exports.service.ts
+++ frontend/src/core/exports/exports.service.ts
@@ -4,23 +4,17 @@ import { AppConfig } from '../../conf/app.config';
 
 @Injectable()
 export class ExportsService {
+  constructor(private _api: HttpClient) {}
 
-  constructor(private _api: HttpClient) {
-    
-   }
-
-   getFakeViewList() {
-     return [
-       {
-         'id_view':1,
-         'view_name': "Export au format 'Standard d'occurrences de taxons' "
-       }
-     ];
-   }
-   getViewList() {
-    return this._api.get<any>(`${AppConfig.API_ENDPOINT}/export/viewList`)
-      .map(data => data.json());
-   }
-
-
+  getFakeViewList() {
+    return [
+      {
+        id_view: 1,
+        view_name: 'Export au format "Standard d\'occurrences de taxons" '
+      }
+    ];
+  }
+  getViewList() {
+    return this._api.get<any>(`${AppConfig.API_ENDPOINT}/export/viewList`).map(data => data.json());
+  }
 }
