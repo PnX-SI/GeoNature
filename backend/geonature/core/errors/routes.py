4e3f4df78c2adb803831a506184790523d617f8d
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-03-08 13:46:44 +0100

    revu des logs

--- backend/geonature/core/errors/routes.py
+++ backend/geonature/core/errors/routes.py
@@ -15,21 +15,21 @@ gunicorn_error_logger = logging.getLogger('gunicorn.error')
 @current_app.errorhandler(500)
 @json_resp
 def internal_error(error):  # pylint: disable=W0613
-    gunicorn_error_logger.error(error)
+    gunicorn_error_logger.info(error)
     DB.session.rollback()
     return {'message': 'internal server error'}, 500
 
 @current_app.errorhandler(SQLAlchemyError)
 @json_resp
 def sqlalchemy_error(error):  # pylint: disable=W0613
-    gunicorn_error_logger.error(error)
+    gunicorn_error_logger.info(error)
     DB.session.rollback()
     return {'message': 'internal server error'}, 500
 
 
 @current_app.errorhandler(GeonatureApiError)
 def geonature_api_error(error):
-    gunicorn_error_logger.error(error)
+    gunicorn_error_logger.info(error.to_dict())
     DB.session.rollback()
     response = jsonify(error.to_dict())
     response.status_code = error.status_code
