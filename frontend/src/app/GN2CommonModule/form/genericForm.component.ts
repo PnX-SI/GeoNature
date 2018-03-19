2fa9468bd83bf565b0f72f76a19a791818c37bbc
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-03-19 14:08:07 +0100

    formulaire générique: ajout d'un input debounce time

--- frontend/src/app/GN2CommonModule/form/genericForm.component.ts
+++ frontend/src/app/GN2CommonModule/form/genericForm.component.ts
@@ -5,6 +5,8 @@ import { FormControl } from '@angular/forms';
 export class GenericFormComponent implements OnInit, AfterViewInit {
   @Input() parentFormControl: FormControl;
   @Input() label: string;
+  @Input() disabled: boolean;
+  @Input() debounceTime: number;
   @Output() onChange = new EventEmitter<any>();
   @Output() onDelete = new EventEmitter<any>();
 
@@ -15,15 +17,22 @@ export class GenericFormComponent implements OnInit, AfterViewInit {
   ngOnInit() {}
 
   ngAfterViewInit() {
-    this.parentFormControl.valueChanges.subscribe(value => {
-      if (value && (value.length === 0 || value === '')) {
-        console.log('delete');
-        this.onDelete.emit();
-      } else {
-        this.onChange.emit(value);
-        console.log('change');
-        console.log(value);
-      }
-    });
+    if (!this.debounceTime) {
+      console.log('no debouncetime');
+      this.debounceTime = 0;
+    }
+    this.parentFormControl.valueChanges
+      .distinctUntilChanged()
+      .debounceTime(this.debounceTime)
+      .subscribe(value => {
+        if (value && (value.length === 0 || value === '')) {
+          console.log('delete');
+          this.onDelete.emit();
+        } else {
+          this.onChange.emit(value);
+          console.log('change');
+          console.log(value);
+        }
+      });
   }
 }
