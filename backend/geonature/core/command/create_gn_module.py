f19a76d1c1ee624beea53179f0dbf757d521bac1
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-03-19 15:02:03 +0100

    Install gn_module: vérification si module déja activé avant de l'importer

--- backend/geonature/core/command/create_gn_module.py
+++ backend/geonature/core/command/create_gn_module.py
@@ -11,7 +11,11 @@ from pathlib import Path
 
 import click
 
-from geonature.utils.env import DB, DEFAULT_CONFIG_FIlE
+from geonature.utils.env import (
+    DB,
+    DEFAULT_CONFIG_FIlE,
+    GN_MODULES_ETC_ENABLED
+) 
 
 from geonature.utils.command import (
     get_app_for_cmd,
@@ -55,7 +59,7 @@ def install_gn_module(module_path, url, conf_file):
         # Vérification que le chemin module path soit correct
         if not Path(module_path).is_dir():
             raise GeoNatureError("dir {} doesn't exists".format(module_path))
-
+        
         # TODO vérifier que l'utilisateur est root ou du groupe geonature
         app = get_app_for_cmd(conf_file)
 
@@ -67,6 +71,9 @@ def install_gn_module(module_path, url, conf_file):
         #   Vérification de la version de geonature par rapport au manifest
         try:
             module_name = check_manifest(module_path)
+            # Vérification que le module n'est pas déjà activé
+            if (GN_MODULES_ETC_ENABLED / module_name).is_symlink():
+                raise GeoNatureError("...module {} already activated".format(module_name))
         except ConfigError as ex:
             log.critical(str(ex) + "\n")
             sys.exit(1)
