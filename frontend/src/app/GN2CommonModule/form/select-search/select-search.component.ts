f72a974561d5a1de002631b98148584f57a3fb8e
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-04-04 13:53:10 +0200

    refactor composant select-search - ajout multiselect

--- frontend/src/app/GN2CommonModule/form/select-search/select-search.component.ts
+++ frontend/src/app/GN2CommonModule/form/select-search/select-search.component.ts
@@ -8,21 +8,26 @@ import { FormControl } from '@angular/forms';
 })
 export class SelectSearchComponent implements OnInit {
   public currentValue = null;
+  public selectedItems = [];
   public searchControl = new FormControl();
   @Input() parentFormControl: FormControl;
   // value of the options
   @Input() values: Array<any>;
   // key of the array of options
   @Input() key: string;
+  @Input() multiselect: boolean;
   @Input() disabled: boolean;
   @Input() label: any;
   @Input() debounceTime: number;
   @Output() onSearch = new EventEmitter();
+  @Output() onChange = new EventEmitter<any>();
+  @Output() onDelete = new EventEmitter<any>();
   constructor() {}
 
   ngOnInit() {
     this.debounceTime = this.debounceTime || 100;
     this.disabled = this.disabled || false;
+
     this.searchControl.valueChanges
       .debounceTime(this.debounceTime)
       .distinctUntilChanged()
@@ -35,5 +40,34 @@ export class SelectSearchComponent implements OnInit {
     this.currentValue = val[this.key];
     this.searchControl.reset();
     this.parentFormControl.patchValue(val);
+  }
+
+  addItem(item) {
+    // remove element from the items list
+    this.values = this.values.filter(curItem => {
+      return curItem[this.key] !== item[this.key];
+    });
+    if (this.multiselect) {
+      this.selectedItems.push(item);
+      this.parentFormControl.patchValue(this.selectedItems);
+    } else {
+      this.parentFormControl.patchValue(item);
+    }
+    this.searchControl.reset();
+    this.onChange.emit(item);
+  }
+
+  removeItem(item) {
+    // push the element in the items list
+    if (this.multiselect) {
+      this.values.push(item);
+      this.selectedItems = this.selectedItems.filter(curItem => {
+        return curItem[this.key] !== item[this.key];
+      });
+      this.parentFormControl.patchValue(this.selectedItems);
+    } else {
+      this.parentFormControl.patchValue(item);
+    }
+    this.onDelete.emit(item);
   }
 }
