c7df39f227a3842931c4bbaaf8bc18fb3bafbea7
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2018-04-12 16:49:28 +0200

    logs : on ne trace plus dans la table les deleted, date create et update

--- backend/geonature/core/gn_medias/models.py
+++ backend/geonature/core/gn_medias/models.py
@@ -1,8 +1,10 @@
 '''
-    Modèles du schéma gn_medias
+    Modèles du schéma gn_commons
 '''
 
 from sqlalchemy import ForeignKey
+from sqlalchemy.sql import select, func
+from sqlalchemy.dialects.postgresql import UUID
 
 from geonature.utils.utilssqlalchemy import serializable
 from geonature.utils.env import DB
@@ -11,7 +13,7 @@ from geonature.utils.env import DB
 @serializable
 class BibMediaTypes(DB.Model):
     __tablename__ = 'bib_media_types'
-    __table_args__ = {'schema': 'gn_medias'}
+    __table_args__ = {'schema': 'gn_commons'}
     id_type = DB.Column(DB.Integer, primary_key=True)
     label_fr = DB.Column(DB.Unicode)
     label_en = DB.Column(DB.Unicode)
@@ -28,14 +30,21 @@ class BibMediaTypes(DB.Model):
 @serializable
 class TMedias(DB.Model):
     __tablename__ = 't_medias'
-    __table_args__ = {'schema': 'gn_medias'}
+    __table_args__ = {'schema': 'gn_commons'}
     id_media = DB.Column(DB.Integer, primary_key=True)
     id_type = DB.Column(
         DB.Integer,
-        ForeignKey('gn_medias.bib_media_types.id_type')
+        ForeignKey('gn_commons.bib_media_types.id_type')
     )
-    entity_name = DB.Column(DB.Unicode)
-    entity_value = DB.Column(DB.Integer)  # A voir à renommer en entity_id
+    id_table_location = DB.Column(
+        DB.Integer,
+        ForeignKey('gn_commons.bib_tables_location.id_table_location')
+    )
+    unique_id_media = DB.Column(
+        UUID(as_uuid=True),
+        default=select([func.uuid_generate_v4()])
+    )
+    uuid_attached_row = DB.Column(UUID(as_uuid=True))
     title_fr = DB.Column(DB.Unicode)
     title_en = DB.Column(DB.Unicode)
     title_it = DB.Column(DB.Unicode)
@@ -49,7 +58,4 @@ class TMedias(DB.Model):
     description_it = DB.Column(DB.Unicode)
     description_es = DB.Column(DB.Unicode)
     description_de = DB.Column(DB.Unicode)
-    meta_create_date = DB.Column(DB.DateTime)
-    meta_update_date = DB.Column(DB.DateTime)
     is_public = DB.Column(DB.Boolean, default=True)
-    deleted = DB.Column(DB.Boolean, default=False)
