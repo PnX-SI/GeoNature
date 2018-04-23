4b610975412e4816f488cd396825d1dd6705a933
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2018-04-23 14:44:55 +0200

    SQL MONITORING - suppression fonction trigger inutile

--- data/core/monitoring.sql
+++ data/core/monitoring.sql
@@ -144,31 +144,12 @@ CREATE INDEX idx_t_base_sites_type_site ON t_base_sites USING btree (id_nomencla
 ----------------------
 --FUNCTIONS TRIGGERS--
 ----------------------
---Trigger permettant de fournir le geom du site à la visite s'il n'est pas rempli pour la visite.
-CREATE OR REPLACE FUNCTION fct_trg_add_obs_geom()
-  RETURNS trigger AS
-$BODY$
-begin
-	if(NEW.geom is NULL and NEW.id_base_site IS NOT NULL) THEN
-		NEW.geom = (select geom from gn_monitoring.t_base_sites WHERE id_base_site=NEW.id_base_site);
-	end if;
-	return NEW;
-end;
-$BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
-
 --@ TODO Trigger de calcul des intersections avec la table l_areas ????
 
 
 ------------
 --TRIGGERS--
 ------------
-INSERT INTO gn_commons.bib_tables_location(table_desc, schema_name, table_name, pk_field, uuid_field_name)
-VALUES
-('Table centralisant les sites faisant l''objet de protocole de suivis', 'gn_monitoring', 't_base_sites', 'id_base_site', 'uuid_base_site'),
-('Table centralisant les visites réalisées sur un site', 'gn_monitoring', 't_base_visits', 'id_base_visit', 'uuid_base_visit');
-
 CREATE TRIGGER tri_log_changes
   AFTER INSERT OR UPDATE OR DELETE
   ON gn_monitoring.t_base_visits
@@ -181,3 +162,12 @@ CREATE TRIGGER tri_log_changes
   ON gn_monitoring.t_base_sites
   FOR EACH ROW
   EXECUTE PROCEDURE gn_commons.fct_trg_log_changes();
+
+
+---------
+--DATAS--
+---------
+INSERT INTO gn_commons.bib_tables_location(table_desc, schema_name, table_name, pk_field, uuid_field_name)
+VALUES
+('Table centralisant les sites faisant l''objet de protocole de suivis', 'gn_monitoring', 't_base_sites', 'id_base_site', 'uuid_base_site'),
+('Table centralisant les visites réalisées sur un site', 'gn_monitoring', 't_base_visits', 'id_base_visit', 'uuid_base_visit');
