5e20ee0cab147747bc9955956c193c762719d1b9
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-04-24 10:42:14 +0200

    Occtax: clarification interface pour l'export

--- contrib/occtax/data/occtax.sql
+++ contrib/occtax/data/occtax.sql
@@ -481,20 +481,22 @@ CREATE OR REPLACE VIEW pr_occtax.v_releve_list AS
     rel.date_max,
     rel.altitude_min,
     rel.altitude_max,
+    rel.deleted,
     rel.meta_device_entry,
     rel.comment,
     rel.geom_4326,
     rel."precision",
+   dataset.dataset_name,
     string_agg(t.nom_valide::text, ','::text) AS taxons,
-    (((string_agg(t.nom_valide::text, ','::text) || '<br/>'::text) || rel.date_min::date) || '<br/>'::text) || COALESCE(string_agg((obs.nom_role::text || ' '::text) || obs.prenom_role::text, ', '::text), rel.observers_txt) AS leaflet_popup,
+    (((string_agg(t.nom_valide::text, ','::text) || '<br/>'::text) || rel.date_min::date) || '<br/>'::text) || COALESCE(string_agg((obs.nom_role::text || ' '::text) || obs.prenom_role::text, ', '::text), rel.observers_txt::text) AS leaflet_popup,
     COALESCE(string_agg((obs.nom_role::text || ' '::text) || obs.prenom_role::text, ', '::text), rel.observers_txt::text) AS observateurs
    FROM pr_occtax.t_releves_occtax rel
      LEFT JOIN pr_occtax.t_occurrences_occtax occ ON rel.id_releve_occtax = occ.id_releve_occtax
      LEFT JOIN taxonomie.taxref t ON occ.cd_nom = t.cd_nom
      LEFT JOIN pr_occtax.cor_role_releves_occtax cor_role ON cor_role.id_releve_occtax = rel.id_releve_occtax
      LEFT JOIN utilisateurs.t_roles obs ON cor_role.id_role = obs.id_role
-  GROUP BY rel.id_releve_occtax, rel.id_dataset, rel.id_digitiser, rel.date_min, rel.date_max, rel.altitude_min, rel.altitude_max, rel.meta_device_entry, rel.comment, rel.geom_4326, rel."precision";
-
+     LEFT JOIN gn_meta.t_datasets dataset ON dataset.id_dataset = rel.id_dataset
+  GROUP BY dataset.dataset_name, rel.id_releve_occtax, rel.id_dataset, rel.id_digitiser, rel.date_min, rel.date_max, rel.altitude_min, rel.altitude_max, rel.deleted, rel.meta_device_entry
 
 --------------------
 -- ASSOCIATED DATA--
