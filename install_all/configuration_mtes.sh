d9d746496ae441f06650f5449eb16c0b16ab6402
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-04-27 15:58:43 +0200

    Modif liés à l'installation

--- install_all/configuration_mtes.sh
+++ install_all/configuration_mtes.sh
@@ -10,34 +10,33 @@ fi
 
 cd /tmp
 
-wget http://geonature.fr/install_dlb/update_db.sql
-wget http://geonature.fr/install_dlb/map.config.ts
-wget http://geonature.fr/install_dlb/custom_frontend.zip
+wget http://geonature.fr/documents/install_dlb/update_db.sql
+wget http://geonature.fr/documents/install_dlb/configuration_dlb.zip
+wget http://geonature.fr/documents/install_dlb/custom_frontend.zip
 unzip custom_frontend.zip
+unzip configuration_dlb.zip
 
+echo "Ecriture de la liste de taxons"
 
+echo "Ecriture de la liste de taxons" >> /var/log/geonature/install_db.log
 export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f update_db.sql  &>> /var/log/geonature/install_db.log
 
 
-cp -r custom /home/$monuser/geonature/src/custom
-cp -r map.config.ts /home/$monuser/geonature/src/conf/map.config.ts
+cp -r custom /home/$monuser/geonature/frontend/src/custom
+cp configuration/map.config.ts /home/$monuser/geonature/frontend/src/conf/map.config.ts
 
-observer_txt="observers_txt = true \n"
-available_format="available_export_format = ['csv', 'geojson'] \n"
-export_message="export_message = '<p> <b> Attention: </b> </br>  Vous vous apprêtez à télécharger les données de la <b>recherche courante.</b> </br> Pour n'exporter qu'un jeu de données, filtrez par <b>jeu de données</b> et cliquez sur le bouton <b> Rechercher </b> avant d'exporter. </br> </br>
- <small> <b> Notes: </b> </br> - Pour Ginco, utilisez le format CSV </br>  - Les données sont exportées dans le système de réference (SRID) 4326, encodage UTF8 </small> </p>' "
 
-echo -e $observer_txt | sudo tee -a /etc/geonature/mods-enabled/occtax/conf_gn_module.toml
-echo -e $available_format | sudo tee -a /etc/geonature/mods-enabled/occtax/conf_gn_module.toml
-echo -e $export_message | sudo tee -a /etc/geonature/mods-enabled/occtax/conf_gn_module.toml
+cat configuration/occtax.config.toml | sudo tee -a /etc/geonature/mods-enabled/occtax/conf_gn_module.toml
+cat configuration/geonature.config.toml | sudo tee -a /etc/geonature/geonature_config.toml
  
-
 
 cd /home/$monuser/geonature/backend
 source venv/bin/activate
 geonature update_configuration --build=false
-geonature update_module_configuration
+geonature update_module_configuration occtax
+deactivate
 
 rm -r custom
+rm -r configuration
+rm custom.zip
 rm update_db.sql
-rm map.config.ts
