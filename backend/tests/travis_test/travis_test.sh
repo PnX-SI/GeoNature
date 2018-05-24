b31b43632c4ddb0c5c3f6f52dabe6c3bf493c1ed
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-05-24 19:00:36 +0200

    up8

--- backend/tests/travis_test/travis_test.sh
+++ backend/tests/travis_test/travis_test.sh
@@ -4,7 +4,7 @@ sudo mkdir /etc/geonature
 sudo mkdir /etc/geonature/mods-enabled
 sudo mkdir /etc/geonature/mods-available
 
-sudo cp $TRAVIS_BUILD_DIR/backend/tests/travis_test/geonature_config.toml /etc/geonature_config.toml
+sudo cp $TRAVIS_BUILD_DIR/backend/tests/travis_test/geonature_config.toml /etc/geonature/geonature_config.toml
 
 python ../../../geonature_cmd.py install_command
 
