fd60890c08389db728a07d1cc289761df13bd78f
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-05-24 18:26:22 +0200

    up2

--- backend/tests/travis_test/travis_test.sh
+++ backend/tests/travis_test/travis_test.sh
@@ -6,7 +6,7 @@ sudo mkdir /etc/geonature/mods-available
 
 sudo cp ./geonature_config.toml /etc/geonature_config.toml
 
-python geonature_cmd.py install_command
+python ../../../geonature_cmd.py install_command
 
 geonature install_gn_module /home/$(whoami)/GeoNature/contrib/occtax occtax --build=false
 
