cf813e3a7dd6efe6016ae3773ef35dd73098b3fa
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-05-28 16:32:40 +0200

    up

--- backend/tests/travis_test/travis_test.sh
+++ backend/tests/travis_test/travis_test.sh
@@ -4,11 +4,13 @@ sudo mkdir /etc/geonature
 sudo mkdir /etc/geonature/mods-enabled
 sudo mkdir /etc/geonature/mods-available
 
-sudo cp ~/PnX-SI/GeoNature/backend/tests/travis_test/geonature_config_tests.toml /etc/geonature/geonature_config.toml
+mkdir $TRAVIS_BUILD_DIR/frontend/src/external_assets
+
+sudo cp $TRAVIS_BUILD_DIR/backend/tests/travis_test/geonature_config_tests.toml /etc/geonature/geonature_config.toml
 
 python ../../../geonature_cmd.py install_command
 
-geonature install_gn_module /home/travis/PnX-SI/GeoNature/contrib/occtax occtax --build=false
+geonature install_gn_module $TRAVIS_BUILD_DIR/PnX-SI/GeoNature/contrib/occtax occtax --build=false
 
 
 
