7df8d75ea4a3da87205de359d073477f448f6a80
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-05-28 16:37:10 +0200

    up1

--- backend/tests/travis_test/travis_test.sh
+++ backend/tests/travis_test/travis_test.sh
@@ -10,7 +10,7 @@ sudo cp $TRAVIS_BUILD_DIR/backend/tests/travis_test/geonature_config_tests.toml 
 
 python ../../../geonature_cmd.py install_command
 
-geonature install_gn_module $TRAVIS_BUILD_DIR/PnX-SI/GeoNature/contrib/occtax occtax --build=false
+geonature install_gn_module $TRAVIS_BUILD_DIR/contrib/occtax occtax --build=false
 
 
 
