71da61155694b251a7d69985c76ff7218d495182
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-06-11 12:08:49 +0200

    update script migrations

--- migration.sh
+++ migration.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-cp config/settings.ini ../GeoNature/config/settings.ini
-cp frontend/src/conf/map.config.ts ../GeoNature/frontend/src/conf/map.config.ts
-cp -r frontend/src/custom/* ../GeoNature/frontend/src/custom/
-cp -r frontend/src/custom/* ../GeoNature/frontend/src/custom/
-cp -r frontend/src/custom/* ../GeoNature/frontend/src/custom/
+mv ../geonature ../geonature_old
+mv ../GeoNature ../geonature
 
 
-mv geonature/ geonature_old
-mv GeoNature geonature
-
-
+cp ../geonature_old/config/settings.ini config/settings.ini
+cp ../geonature_old/frontend/src/conf/map.config.ts frontend/src/conf/map.config.ts
+cp -r ../geonature_old/frontend/src/custom/* frontend/src/custom/
 
 . config/settings.ini
 
-cd backend
+cd frontend
+npm install
+
+cd ../backend
+
+if [ -d 'venv/' ]
+then
+  sudo rm -rf venv
+fi
 
 if [[ $python_path ]]; then
   echo "Installation du virtual env..."
@@ -23,20 +27,25 @@ else
   virtualenv venv
 fi
 
-source venv/bin/activate
 
-geonature update_configuration
+source venv/bin/activate
+pip install -r requirements.txt
+
+python ../geonature_cmd.py install_command
+
+echo "Update configurations "
+geonature update_configuration --build=false
 geonature generate_frontend_config --build=false
 geonature generate_frontend_modules_route
 geonature generate_frontend_tsconfig
 geonature update_module_configuration occtax --build=false
 
-pip install -r requirements.txt
 
-
-cd geonature/frontend
-npm install
+echo "Rebuild du frontend"
+cd ../frontend
 npm run build
 cd ../
 
 sudo supervisorctl reload
+
+deactivate
