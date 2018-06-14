c4dc2437ec97a264b96c2097c80f21c90f1558cf
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-06-14 14:41:35 +0200

    travis

--- backend/tests/travis_test/travis_test.sh
+++ backend/tests/travis_test/travis_test.sh
@@ -8,6 +8,8 @@ mkdir $TRAVIS_BUILD_DIR/frontend/src/external_assets
 
 sudo cp $TRAVIS_BUILD_DIR/backend/tests/travis_test/geonature_config_tests.toml $TRAVIS_BUILD_DIR/config/geonature_config.toml
 
+export PGPASSWORD=monpassachanger;psql -U geonatuser -h test.ecrins-parcnational.net -d geonaturedb -c "DELETE FROM gn_commons.t_modules WHERE module_name = 'occtax'"
+
 python ../../../geonature_cmd.py install_command
 
 geonature install_gn_module $TRAVIS_BUILD_DIR/contrib/occtax occtax --build=false
