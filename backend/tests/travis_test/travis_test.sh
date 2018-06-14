59830c158a28dc425860977480fce5ae7507bfc7
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-06-14 14:52:36 +0200

    test variable travis

--- backend/tests/travis_test/travis_test.sh
+++ backend/tests/travis_test/travis_test.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo 'FROM TRAViS #########################################'
-echo $db_name
-echo $db_pass
 
 sudo mkdir /etc/geonature
 sudo mkdir /etc/geonature/mods-enabled
@@ -12,7 +9,7 @@ mkdir $TRAVIS_BUILD_DIR/frontend/src/external_assets
 
 sudo cp $TRAVIS_BUILD_DIR/backend/tests/travis_test/geonature_config_tests.toml $TRAVIS_BUILD_DIR/config/geonature_config.toml
 
-export PGPASSWORD=monpassachanger;psql -U geonatuser -h test.ecrins-parcnational.net -d geonaturedb -c "DELETE FROM gn_commons.t_modules WHERE module_name = 'occtax'"
+export PGPASSWORD=$db_pass;psql -U $db_user -h test.ecrins-parcnational.net -d $db_name -c "DELETE FROM gn_commons.t_modules WHERE module_name = 'occtax'"
 
 python ../../../geonature_cmd.py install_command
 
