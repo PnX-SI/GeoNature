dd26b02f0b191b3312d0670354a325372f183d51
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-06-15 13:33:10 +0200

    fix variable travis

--- backend/tests/travis_test/travis_test.sh
+++ backend/tests/travis_test/travis_test.sh
@@ -7,6 +7,8 @@ sudo mkdir /etc/geonature/mods-available
 
 mkdir $TRAVIS_BUILD_DIR/frontend/src/external_assets
 
+sudo sed -i "s/SQLALCHEMY_DATABASE_URI = .*$/SQLALCHEMY_DATABASE_URI = \"postgresql:\/\/$db_user:$db_pass@test.ecrins-parcnational.net:5432\/$db_name\"/" $TRAVIS_BUILD_DIR/config/geonature_config.toml
+
 sudo cp $TRAVIS_BUILD_DIR/backend/tests/travis_test/geonature_config_tests.toml $TRAVIS_BUILD_DIR/config/geonature_config.toml
 
 export PGPASSWORD=$db_pass;psql -U $db_user -h test.ecrins-parcnational.net -d $db_name -c "DELETE FROM gn_commons.t_modules WHERE module_name = 'occtax'"
