0872ce779bc94a6fc873f24dcb99e64e3c1706b6
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-06-19 13:35:32 +0200

    Update dynamicform

--- frontend/src/app/syntheseModule/synthese-search/synthese-search.component.ts
+++ frontend/src/app/syntheseModule/synthese-search/synthese-search.component.ts
@@ -9,6 +9,129 @@ import { SearchService } from '../search.service';
 })
 export class SyntheseSearchComponent implements OnInit {
   public searchForm: FormGroup;
+  public nomenclaturesForms = [
+    {
+      controlType: 'nomenclature',
+      label: "Technique d'observation",
+      key: 'id_nomenclature_obs_technique',
+      idComponent: 100,
+      required: false
+    },
+    {
+      controlType: 'nomenclature',
+      label: 'Type de regroupement',
+      key: 'id_nomenclature_grp_typ',
+      idComponent: 24,
+      required: false
+    },
+    {
+      controlType: 'nomenclature',
+      label: "Statut d'observation",
+      key: 'id_nomenclature_observation_status',
+      idComponent: 18,
+      required: false
+    },
+    {
+      controlType: 'nomenclature',
+      label: "Méthode d'observation",
+      key: 'id_nomenclature_obs_meth',
+      idComponent: 14,
+      required: false
+    },
+    {
+      controlType: 'nomenclature',
+      label: 'Etat biologique',
+      key: 'id_nomenclature_bio_condition',
+      idComponent: 7,
+      required: false
+    },
+    {
+      controlType: 'nomenclature',
+      label: 'Statut biologique',
+      key: 'id_nomenclature_bio_status',
+      idComponent: 13,
+      required: false
+    },
+    {
+      controlType: 'nomenclature',
+      label: 'Naturalité',
+      key: 'id_nomenclature_naturalness',
+      idComponent: 8,
+      required: false
+    },
+    {
+      controlType: 'nomenclature',
+      label: 'Méthode de détermination',
+      key: 'id_nomenclature_determination_method',
+      idComponent: 106,
+      required: false
+    },
+    {
+      controlType: 'nomenclature',
+      label: "Preuve d'existence",
+      key: 'id_nomenclature_exist_proof',
+      idComponent: 15,
+      required: false
+    },
+    {
+      controlType: 'nomenclature',
+      label: 'Niveau de diffusion',
+      key: 'id_nomenclature_diffusion_level',
+      idComponent: 5,
+      required: false
+    },
+    {
+      controlType: 'nomenclature',
+      label: 'Statut source',
+      key: 'id_nomenclature_source_status',
+      idComponent: 19,
+      required: false
+    },
+    {
+      controlType: 'nomenclature',
+      label: 'Floutage',
+      key: 'id_nomenclature_blurring',
+      idComponent: 4,
+      required: false
+    },
+    // counting
+    {
+      controlType: 'nomenclature',
+      label: 'Stade de vie',
+      key: 'id_nomenclature_life_stage',
+      idComponent: 10,
+      required: false
+    },
+    {
+      controlType: 'nomenclature',
+      label: 'Sexe',
+      key: 'id_nomenclature_sex',
+      idComponent: 9,
+      required: false
+    },
+    {
+      controlType: 'nomenclature',
+      label: 'Objet du dénombrement',
+      key: 'id_nomenclature_obj_count',
+      idComponent: 6,
+      required: false
+    },
+    {
+      controlType: 'nomenclature',
+      label: 'Type de dénombrement',
+      key: 'id_nomenclature_type_count',
+      idComponent: 21,
+      required: false
+    },
+    {
+      controlType: 'nomenclature',
+      label: 'Statut de validation',
+      key: 'id_nomenclature_valid_status',
+      idComponent: 101,
+      required: false
+    }
+  ];
+
   @Output() searchClicked = new EventEmitter();
   constructor(private _fb: FormBuilder, public searchService: SearchService) {}
 
