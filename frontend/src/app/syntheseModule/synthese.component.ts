7da9e6f9904e75a9d4e7fd61502bafe98fc53330
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-06-19 14:05:22 +0200

    Synthese: mise du form dans un service et connexion du formulaire et de la carte

--- frontend/src/app/syntheseModule/synthese.component.ts
+++ frontend/src/app/syntheseModule/synthese.component.ts
@@ -1,35 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import {GeoJSON} from 'leaflet';
-import { SearchService } from './search.service';
+import { GeoJSON } from 'leaflet';
+import { DataService } from './services/data.service';
 import { MapListService } from '@geonature_common/map-list/map-list.service';
 
 @Component({
-    selector: 'pnx-synthese',
-    templateUrl: 'synthese.component.html',
-
+  selector: 'pnx-synthese',
+  templateUrl: 'synthese.component.html'
 })
-
 export class SyntheseComponent implements OnInit {
-    public syntheseDataStore: GeoJSON;
+  public syntheseDataStore: GeoJSON;
 
-    constructor(
-        public searchService: SearchService,
-        private _mapListService: MapListService
-    ) {}
+  constructor(public searchService: DataService, private _mapListService: MapListService) {}
 
-    loadAndStoreData(formParams) {
-        this.searchService.dataLoaded = false;
-        this.searchService.getSyntheseData(formParams).subscribe(data => {
-            this.syntheseDataStore = data;
-            this._mapListService.loadTableData(data);
-            this._mapListService.idName = 'id_synthese';
-            this.searchService.dataLoaded = true;
-        });
-    }
-    ngOnInit() {
-        this.loadAndStoreData({});
-    }
-
+  loadAndStoreData(formParams) {
+    this.searchService.dataLoaded = false;
+    this.searchService.getSyntheseData(formParams).subscribe(data => {
+      this.syntheseDataStore = data;
+      this._mapListService.loadTableData(data);
+      this._mapListService.idName = 'id_synthese';
+      this.searchService.dataLoaded = true;
+    });
+  }
+  ngOnInit() {
+    this.loadAndStoreData({});
+  }
 }
-
-
--- frontend/src/app/syntheseModule/synthese.module.ts
+++ frontend/src/app/syntheseModule/synthese.module.ts
@@ -7,27 +7,19 @@ import { SyntheseComponent } from './synthese.component';
 import { SyntheseListComponent } from './synthese-results/synthese-list/synthese-list.component';
 import { SyntheseCarteComponent } from './synthese-results/synthese-carte/synthese-carte.component';
 import { SyntheseSearchComponent } from './synthese-search/synthese-search.component';
-import { SearchService } from './search.service';
+import { DataService } from './services/data.service';
+import { FormService } from './services/form.service';
 
-const routes: Routes = [
-    { path: '', component: SyntheseComponent }
-];
+const routes: Routes = [{ path: '', component: SyntheseComponent }];
 
 @NgModule({
-    imports: [
-        RouterModule.forChild(routes),
-        GN2CommonModule,
-        CommonModule
-    ],
-    declarations: [
-        SyntheseComponent,
-        SyntheseListComponent,
-        SyntheseCarteComponent,
-        SyntheseSearchComponent
-    ],
-    providers: [
-        SearchService
-    ]
+  imports: [RouterModule.forChild(routes), GN2CommonModule, CommonModule],
+  declarations: [
+    SyntheseComponent,
+    SyntheseListComponent,
+    SyntheseCarteComponent,
+    SyntheseSearchComponent
+  ],
+  providers: [DataService, FormService]
 })
-export class SyntheseModule {
-}
+export class SyntheseModule {}
