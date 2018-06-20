dd7e8a6bffdc72ae7bae34ed119dc6aa11765db2
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-06-20 16:45:20 +0200

    ajout backoffice nomenclature dans un iframe

---
+++ frontend/src/app/nomenclatureModule/nomenclature.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+//import { AppConfig } from '@geonature_config/app.config';
+
+@Component({
+  selector: 'pnx-nomenclature-component',
+  templateUrl: 'nomenclature.component.html',
+  styleUrls: ['./nomenclature.component.scss']
+})
+export class NomenclatureComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
