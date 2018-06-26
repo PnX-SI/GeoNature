d8c4d87506b66c0357f7a8b8a93e7a5ba3239b60
amandine-sahl <amandine.sahl@gmail.com>
2018-06-26 10:20:40 +0200

    Suppression des fichiers de configs

--- contrib/occtax/frontend/app/occtax.config.ts
+++
@@ -1,9 +0,0 @@
-export const OccTaxConfig = {
-  'observers_txt': true,
-  'digital_proof_validator': true,
-  default_maplist_columns: [
-    {prop: 'taxons', name: 'Taxon'},
-    {prop: 'date_min', name: 'Date début'},
-    {prop: 'date_max', name: 'Date fin'},
-    ]
-};
