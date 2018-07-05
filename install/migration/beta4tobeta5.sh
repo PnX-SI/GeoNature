91ee7c33af44cc400f69827013287edc6a9f53ac
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-07-05 13:05:40 +0200

    Modif liés à la montée de version

--- install/migration/beta4tobeta5.sh
+++ install/migration/beta4tobeta5.sh
@@ -6,7 +6,6 @@ inter="$(dirname "$BASE_DIR")"
 geonature_dir="$(dirname "$inter")"
 
 
-mkdir $geonature_dir/external_modules
 mkdir $geonature_dir/var
 mkdir $geonature_dir/var/log
 
@@ -14,7 +13,7 @@ mkdir $geonature_dir/var/log
 ln -s $geonature_dir/contrib/occtax $geonature_dir/external_modules/occtax
 
 cp /etc/geonature/geonature_config.toml $geonature_dir/config/geonature_config.toml
-cp /etc/geonature/mods-enabled/occtax/conf_gn_module.toml $geonature_dir/external_modules/occtax/conf_gn_module.toml
+cp /etc/geonature/mods-enabled/occtax/conf_gn_module.toml $geonature_dir/external_modules/occtax/config/conf_gn_module.toml
 
 
 sudo rm -r /etc/geonature
