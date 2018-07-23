5e68427870740f68a1d1f072780f08a66b7641ff
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-07-23 15:05:06 +0200

    Correction gn_monitoring

--- backend/geonature/core/gn_monitoring/routes.py
+++ backend/geonature/core/gn_monitoring/routes.py
@@ -108,17 +108,13 @@ def get_site_areas(id_site):
         q = q.filter(LAreas.id_type == params['id_area_type'])
     if 'id_module' in params:
         q = q.filter(TBaseSites.applications.any(id_application=params['id_module']))
-   
 
     data = q.all()
 
     features = []
     for d in data:
-        feature = get_geojson_feature(d[1], 'id_area')
+        feature = get_geojson_feature(d[1])
         feature['properties'] = d[0].as_dict(True)
         feature['id'] = d[3]
         features.append(feature)
     return FeatureCollection(features)
-    
-
-   
