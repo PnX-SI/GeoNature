e71402c60814c5019cbcdddd0a8ea0a4a3b7a98d
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-08-08 13:38:09 +0200

    Renomage config.py en synthese_config.py + correction export shape

---
+++ backend/geonature/core/gn_synthese/synthese_config.py
@@ -0,0 +1,48 @@
+"""
+Default columns for the export in synthese
+"""
+
+DEFAULT_TAXONOMIC_COLUMNS = [
+    'cd_nom',
+    'cd_ref',
+    'nom_valide'
+]
+
+DEFAULT_SYNTHESE_COLUMNS = [
+    'id_synthese',
+    'unique_id_sinp',
+    'date_min',
+    'date_max',
+    'observers',
+    'altitude_min',
+    'altitude_max',
+    'count_min',
+    'count_max',
+    'sample_number_proof',
+    'digital_proof',
+    'non_digital_proof',
+    'comments'
+]
+
+DEFAULT_NOMENCLATURE_COLUMNS = [
+    'nat_obj_geo',
+    'grp_typ',
+    'obs_method',
+    'obs_technique',
+    'bio_status',
+    'bio_condition',
+    'naturalness',
+    'exist_proof',
+    'valid_status',
+    'diffusion_level',
+    'life_stage',
+    'sex',
+    'obj_count',
+    'type_count',
+    'sensitivity',
+    'observation_status',
+    'blurring',
+    'source_status',
+    'info_geo_type',
+    'determination_method'
+]
