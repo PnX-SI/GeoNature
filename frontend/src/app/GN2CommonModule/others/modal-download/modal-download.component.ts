d3d92665e9db89df79c0f68723e39f56c47f096b
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-08-29 11:35:58 +0200

    Amélioration du composant download

--- frontend/src/app/GN2CommonModule/others/modal-download/modal-download.component.ts
+++ frontend/src/app/GN2CommonModule/others/modal-download/modal-download.component.ts
@@ -1,33 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { HttpParams } from '@angular/common/http';
 
 @Component({
-    selector: 'pnx-modal-download',
-    templateUrl: 'modal-download.component.html',
-    styleUrls: ['./modal-download.component.scss'],
-
+  selector: 'pnx-modal-download',
+  templateUrl: 'modal-download.component.html',
+  styleUrls: ['./modal-download.component.scss']
 })
-
 export class ModalDownloadComponent implements OnInit {
+  @Input() pathDownload: string;
+  @Input() queryString: HttpParams;
+  @Input() exportFormat: Array<string>;
+  @Input() labelButton: string;
+  @Input() downloadMessage: string;
+  @Output() buttonClicked = new EventEmitter<any>();
 
-@Input ()pathDownload: string; 
-@Input ()exportFormat : Array<string>;  
+  constructor(private _modalService: NgbModal) {}
 
-    constructor (private _modalService: NgbModal,) { }
+  ngOnInit() {
+    this.labelButton = this.labelButton || 'Télécharger';
+    this.queryString = this.queryString || new HttpParams();
+  }
 
+  loadData(format) {
+    this.queryString = this.queryString.append('export_format', format);
+    document.location.href = this.pathDownload + this.queryString.toString();
+  }
 
-    ngOnInit() {
-        console.log("là", this.pathDownload);
-        console.log(this.exportFormat);
-        
-     }
-
-    
-    loadData(format){
-        document.location.href = this.pathDownload + '&export_format=' + format
-    }
-   
-    openIntesectionModal(content) {
-        this._modalService.open(content);
-      }
+  openModal(content) {
+    this._modalService.open(content);
+    this.buttonClicked.emit();
+  }
 }
