7ae2185cd8b80595e47953713fc8db36250d2a94
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2018-09-06 11:31:58 +0200

    OCCTAX sample data - Mise en forme et commentaires

--- contrib/occtax/data/sample_data.sql
+++ contrib/occtax/data/sample_data.sql
@@ -9,11 +9,47 @@ SET client_min_messages = warning;
 --DATAS--
 ---------
 
-INSERT INTO gn_meta.t_acquisition_frameworks (id_acquisition_framework, unique_acquisition_framework_id, acquisition_framework_name, acquisition_framework_desc, id_nomenclature_territorial_level, territory_desc, keywords, id_nomenclature_financing_type, target_description, ecologic_or_geologic_target, acquisition_framework_parent_id, is_parent, acquisition_framework_start_date, acquisition_framework_end_date, meta_create_date, meta_update_date) VALUES
-(1, '57b7d0f2-4183-4b7b-8f08-6e105d476dc5', 'Données d''observation de la faune, de la Flore et de la fonge du parc nationl des Ecrins','Données d''observation de la faune, de la Flore et de la fonge du parc nationl des Ecrins',ref_nomenclatures.get_id_nomenclature('NIVEAU_TERRITORIAL', '4')
-,'Territoire du parc national des Ecrins correspondant au massif alpin des Ecrins','Ecrins, parc national, faune, flore, fonge',ref_nomenclatures.get_id_nomenclature('TYPE_FINANCEMENT', '1'),'Tous les taxons',null,null,0,'1973-03-27', null,'2017-05-01 10:35:08', null)
+-- Insérer un cadre d'acquisition d'exemple
+
+INSERT INTO gn_meta.t_acquisition_frameworks (
+    id_acquisition_framework, 
+    unique_acquisition_framework_id, 
+    acquisition_framework_name, 
+    acquisition_framework_desc, 
+    id_nomenclature_territorial_level, 
+    territory_desc, 
+    keywords, 
+    id_nomenclature_financing_type, 
+    target_description, 
+    ecologic_or_geologic_target, 
+    acquisition_framework_parent_id, 
+    is_parent, 
+    acquisition_framework_start_date, 
+    acquisition_framework_end_date, 
+    meta_create_date, 
+    meta_update_date
+    ) VALUES (
+    1, 
+    '57b7d0f2-4183-4b7b-8f08-6e105d476dc5', 
+    'Données d''observation de la faune, de la Flore et de la fonge du Parc national des Ecrins',
+    'Données d''observation de la faune, de la Flore et de la fonge du Parc national des Ecrins',
+    ref_nomenclatures.get_id_nomenclature('NIVEAU_TERRITORIAL', '4'),
+    'Territoire du Parc national des Ecrins correspondant au massif alpin des Ecrins',
+    'Ecrins, parc national, faune, flore, fonge',
+    ref_nomenclatures.get_id_nomenclature('TYPE_FINANCEMENT', '1'),
+    'Tous les taxons',
+    null,
+    null,
+    0,
+    '1973-03-27',
+    null,
+    '2018-09-01 10:35:08',
+    null
+    )
 ;
 SELECT pg_catalog.setval('gn_meta.t_datasets_id_dataset_seq', 2, true);
+
+-- Insérer 2 jeux de données d'exemple
 
 INSERT INTO gn_meta.t_datasets (
     id_dataset,
@@ -44,7 +80,7 @@ INSERT INTO gn_meta.t_datasets (
     1,
     '4d331cae-65e4-4948-b0b2-a11bc5bb46c2',
      1,
-    'Conctat aléatoire tous règnes confondus',
+    'Contact aléatoire tous règnes confondus',
     'Contact aléatoire',
     'Observations aléatoires de la faune, de la flore ou de la fonge',
     ref_nomenclatures.get_id_nomenclature('DATA_TYP', '1'),
@@ -61,14 +97,14 @@ INSERT INTO gn_meta.t_datasets (
     ref_nomenclatures.get_id_nomenclature('STATUT_SOURCE', 'Te'),
     ref_nomenclatures.get_id_nomenclature('RESOURCE_TYP', '1'),
     true,
-    '2017-06-01 16:57:44.45879',
+    '2018-09-01 16:57:44.45879',
     null
     ),
     (
     2,
     'dadab32d-5f9e-4dba-aa1f-c06487d536e8',
     1,
-    'ATBI de la réserve intégrale de Lauvitel dans le Parc national des Ecrins',
+    'ATBI de la réserve intégrale du Lauvitel dans le Parc national des Ecrins',
     'ATBI Lauvitel',
     'Inventaire biologique généralisé sur la réserve du Lauvitel',
     ref_nomenclatures.get_id_nomenclature('DATA_TYP', '1'),
@@ -85,11 +121,13 @@ INSERT INTO gn_meta.t_datasets (
     ref_nomenclatures.get_id_nomenclature('STATUT_SOURCE', 'Te'),
     ref_nomenclatures.get_id_nomenclature('RESOURCE_TYP', '1'),
     true,
-    '2017-06-01 16:59:03.25687',
+    '2018-09-01 16:59:03.25687',
     null
     )
 ;
 SELECT pg_catalog.setval('gn_meta.t_acquisition_frameworks_id_acquisition_framework_seq', 2, true);
+
+-- Renseigner les tables de correspondance
 
 INSERT INTO gn_meta.cor_acquisition_framework_voletsinp (id_acquisition_framework, id_nomenclature_voletsinp) VALUES
 (1, ref_nomenclatures.get_id_nomenclature('VOLET_SINP', '1'))
@@ -130,14 +168,19 @@ INSERT INTO gn_meta.cor_dataset_protocol (id_dataset, id_protocol) VALUES
 ;
 SELECT pg_catalog.setval('gn_meta.sinp_datatype_protocols_id_protocol_seq', 2, true);
 
-INSERT INTO gn_synthese.t_sources (name_source, desc_source, entity_source_pk_field, url_source, target, picto_source, groupe_source, active) VALUES
-('occtax', 'Données issues du occtax aléatoire', 'pr_occtax.cor_counting_occtax.id_counting_occtax', '/occtax', NULL, NULL, 'CONTACT', true);
+-- Insérer une source exemple
 
+INSERT INTO gn_synthese.t_sources (name_source, desc_source, entity_source_pk_field, url_source, target, picto_source, groupe_source, active) VALUES
+('occtax', 'Données issues du module Occtax', 'pr_occtax.cor_counting_occtax.id_counting_occtax', '/occtax', NULL, NULL, 'CONTACT', true);
+
+-- Insérer 2 relevés d'exemple dans Occtax
 
 INSERT INTO pr_occtax.t_releves_occtax (id_releve_occtax,id_dataset,id_digitiser,observers_txt,id_nomenclature_obs_technique,id_nomenclature_grp_typ,date_min,date_max,hour_min,hour_max,altitude_min,altitude_max,meta_device_entry,comment,geom_local,geom_4326,precision) VALUES
 (1,1,1,'Obervateur test insert',ref_nomenclatures.get_id_nomenclature('TECHNIQUE_OBS', '133'),ref_nomenclatures.get_id_nomenclature('TYP_GRP', 'OBS'),'2017-01-01','2017-01-01','12:05:02','12:05:02',1500,1565,'web','Exemple test','01010000206A0800002E988D737BCC2D41ECFA38A659805841','0101000020E61000000000000000001A40CDCCCCCCCC6C4640',10)
 ,(2,1,1,'Obervateur test insert',ref_nomenclatures.get_id_nomenclature('TECHNIQUE_OBS', '133'),ref_nomenclatures.get_id_nomenclature('TYP_GRP', 'OBS'),'2017-01-08','2017-01-08','20:00:00','23:00:00',1600,1600,'web','Autre exemple test','01010000206A0800002E988D737BCC2D41ECFA38A659805841','0101000020E61000000000000000001A40CDCCCCCCCC6C4640',100);
 SELECT pg_catalog.setval('pr_occtax.t_releves_occtax_id_releve_occtax_seq', 3, true);
+
+-- Insérer 3 occurrences dans les 2 relevés Occtax
 
 INSERT INTO pr_occtax.t_occurrences_occtax  (
     id_occurrence_occtax,
@@ -225,13 +268,15 @@ VALUES
   'Troisieme test'
   );
 
-
 SELECT pg_catalog.setval('pr_occtax.t_occurrences_occtax_id_occurrence_occtax_seq', 4, true);
 
+-- Insérer 1 observateur pour chacun des 2 relevés Occtax
 
 INSERT INTO pr_occtax.cor_role_releves_occtax (id_releve_occtax, id_role) VALUES
 (1,1)
 ,(2,1);
+
+-- Insérer 3 dénombrements dans les 3 occurrences
 
 INSERT INTO  pr_occtax.cor_counting_occtax (
   id_counting_occtax,
