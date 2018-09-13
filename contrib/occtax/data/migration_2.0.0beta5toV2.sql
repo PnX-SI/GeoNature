225138f7ae4a1b40fb7353487d5f5c8432cca5d5
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-09-13 11:06:16 +0200

    ajout du id_digitiser dans les trigger d'occtax

--- contrib/occtax/data/migration_2.0.0beta5toV2.sql
+++ contrib/occtax/data/migration_2.0.0beta5toV2.sql
@@ -39,6 +39,7 @@ $BODY$
   COST 100;
 
 
+-- Fonction utilisée pour les triggers vers synthese
 CREATE OR REPLACE FUNCTION pr_occtax.insert_in_synthese(my_id_counting integer)
   RETURNS integer[] AS
   $BODY$
@@ -68,7 +69,10 @@ SELECT INTO releve * FROM pr_occtax.t_releves_occtax rel WHERE occurrence.id_rel
 SELECT INTO id_source s.id_source FROM gn_synthese.t_sources s WHERE lower(name_source) = 'occtax';
 
 -- Récupération du status de validation du counting dans la table t_validation
-SELECT INTO validation * FROM gn_commons.t_validations v WHERE uuid_attached_row = new_count.unique_id_sinp_occtax;
+SELECT INTO validation v.*, CONCAT(r.nom_role, r.prenom_role) as validator_full_name
+FROM gn_commons.t_validations v
+JOIN utilisateurs.t_roles r ON v.id_validator = r.id_role
+WHERE uuid_attached_row = new_count.unique_id_sinp_occtax;
 
 -- Récupération du status_source depuis le JDD
 SELECT INTO id_nomenclature_source_status d.id_nomenclature_source_status FROM gn_meta.t_datasets d WHERE id_dataset = releve.id_dataset;
@@ -121,13 +125,13 @@ altitude_max,
 the_geom_4326,
 the_geom_point,
 the_geom_local,
--- id_area, TODO
 date_min,
 date_max,
-id_validator,
+validator,
 validation_comment,
 observers,
 determiner,
+id_digitiser,
 id_nomenclature_determination_method,
 comments,
 last_action
@@ -140,51 +144,50 @@ VALUES(
   new_count.id_counting_occtax,
   releve.id_dataset,
   --nature de l'objet geo: id_nomenclature_geo_object_nature Le taxon observé est présent quelque part dans l'objet géographique - a ajouter dans default_nomenclature du schema occtax
-  'In',
-releve.id_nomenclature_grp_typ,
-occurrence.id_nomenclature_obs_meth,
-releve.id_nomenclature_obs_technique,
-occurrence.id_nomenclature_bio_status,
-occurrence.id_nomenclature_bio_condition,
-occurrence.id_nomenclature_naturalness,
-occurrence.id_nomenclature_exist_proof,
-  -- statut de validation récupérer à partir de gn_commons.t_validations
-validation.id_nomenclature_valid_status,
-occurrence.id_nomenclature_diffusion_level,
-new_count.id_nomenclature_life_stage,
-new_count.id_nomenclature_sex,
-new_count.id_nomenclature_obj_count,
-new_count.id_nomenclature_type_count,
-  -- id_nomenclature_sensitivity le trigger qui calcule la sensibilité doit remplir le champs niveau de sensibilité, qui n'est pas présent dans occtax ??
-  '0',
-occurrence.id_nomenclature_observation_status,
-occurrence.id_nomenclature_blurring,
--- status_source récupéré depuis le JDD
-id_nomenclature_source_status,
--- id_nomenclature_info_geo_type: type de rattachement = géoréferencement
-ref_nomenclatures.get_id_nomenclature('TYP_INF_GEO', '1')	,
-new_count.count_min,
-new_count.count_max,
-occurrence.cd_nom,
-occurrence.nom_cite,
-occurrence.meta_v_taxref,
-occurrence.sample_number_proof,
-occurrence.digital_proof,
-occurrence.non_digital_proof,
-releve.altitude_min,
-releve.altitude_max,
-releve.geom_4326,
-ST_CENTROID(releve.geom_4326),
-releve.geom_local,
-(to_char(releve.date_min, 'DD/MM/YYYY') || ' ' || to_char(releve.date_min, 'hh:mm:ss'))::timestamp,
-(to_char(releve.date_max, 'DD/MM/YYYY') || ' ' || to_char(releve.date_max, 'hh:mm:ss'))::timestamp,
-validation.id_validator,
-validation.validation_comment,
-COALESCE (observers.observers_name, releve.observers_txt),
-occurrence.determiner,
-occurrence.id_nomenclature_determination_method,
-CONCAT('Relevé : ',releve.comment, 'Occurrence: ', occurrence.comment),
-'I'
+  ref_nomenclatures.get_id_nomenclature('NAT_OBJ_GEO', 'In') ,
+  releve.id_nomenclature_grp_typ,
+  occurrence.id_nomenclature_obs_meth,
+  releve.id_nomenclature_obs_technique,
+  occurrence.id_nomenclature_bio_status,
+  occurrence.id_nomenclature_bio_condition,
+  occurrence.id_nomenclature_naturalness,
+  occurrence.id_nomenclature_exist_proof,
+    -- statut de validation récupérer à partir de gn_commons.t_validations
+  validation.id_nomenclature_valid_status,
+  occurrence.id_nomenclature_diffusion_level,
+  new_count.id_nomenclature_life_stage,
+  new_count.id_nomenclature_sex,
+  new_count.id_nomenclature_obj_count,
+  new_count.id_nomenclature_type_count,
+  occurrence.id_nomenclature_observation_status,
+  occurrence.id_nomenclature_blurring,
+  -- status_source récupéré depuis le JDD
+  id_nomenclature_source_status,
+  -- id_nomenclature_info_geo_type: type de rattachement = géoréferencement
+  ref_nomenclatures.get_id_nomenclature('TYP_INF_GEO', '1')	,
+  new_count.count_min,
+  new_count.count_max,
+  occurrence.cd_nom,
+  occurrence.nom_cite,
+  occurrence.meta_v_taxref,
+  occurrence.sample_number_proof,
+  occurrence.digital_proof,
+  occurrence.non_digital_proof,
+  releve.altitude_min,
+  releve.altitude_max,
+  releve.geom_4326,
+  ST_CENTROID(releve.geom_4326),
+  releve.geom_local,
+  (to_char(releve.date_min, 'DD/MM/YYYY') || ' ' || to_char(releve.date_min, 'hh:mm:ss'))::timestamp,
+  (to_char(releve.date_max, 'DD/MM/YYYY') || ' ' || to_char(releve.date_max, 'hh:mm:ss'))::timestamp,
+  validation.validator_full_name,
+  validation.validation_comment,
+  COALESCE (observers.observers_name, releve.observers_txt),
+  occurrence.determiner,
+  releve.id_digitiser,
+  occurrence.id_nomenclature_determination_method,
+  CONCAT('Relevé : ',releve.comment, 'Occurrence: ', occurrence.comment),
+  'I'
 );
 
   RETURN observers.observers_id ;
@@ -192,6 +195,7 @@ END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
+
 
 
 
@@ -391,8 +395,9 @@ BEGIN
       UPDATE gn_synthese.synthese SET
       id_dataset = NEW.id_dataset,
       observers = NEW.observers_txt,
-      cd_nomenclature_obs_technique = ref_nomenclatures.get_cd_nomenclature(NEW.id_nomenclature_obs_technique),
-      cd_nomenclature_grp_typ = ref_nomenclatures.get_cd_nomenclature(NEW.id_nomenclature_grp_typ),
+      id_digitiser = NEW.id_digitiser,
+      id_nomenclature_obs_technique = NEW.id_nomenclature_obs_technique,
+      id_nomenclature_grp_typ = NEW.id_nomenclature_grp_typ,
       date_min = (to_char(NEW.date_min, 'DD/MM/YYYY') || ' ' || COALESCE(to_char(NEW.hour_min, 'hh:mm:ss'), '00:00:00'))::timestamp,
       date_max = (to_char(NEW.date_max, 'DD/MM/YYYY') || ' ' || COALESCE(to_char(NEW.hour_max, 'hh:mm:ss'), '00:00:00'))::timestamp,
       altitude_min = NEW.altitude_min,
@@ -550,9 +555,8 @@ COST 100;
 ------------
 
 ALTER TABLE gn_meta.t_datasets
-ADD COLUMN active boolean;
+ADD COLUMN active boolean DEFAULT true;
 UPDATE gn_meta.t_datasets SET active = true; 
-
 
 ------------
 --TRIGGERS--
