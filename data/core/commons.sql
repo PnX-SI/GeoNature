86ff2eaa8b08d38146cd7b89ece89e83543fa35f
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2018-09-15 01:51:40 +0200

    SQL COMMONS - fonctions générique d'import de fichier csv

--- data/core/commons.sql
+++ data/core/commons.sql
@@ -232,6 +232,70 @@ END IF;
 END;
 $$;
 
+
+CREATE OR REPLACE FUNCTION gn_commons.load_csv_file(csv_file text, target_table text)
+RETURNS text LANGUAGE plpgsql AS $$
+
+--This function create a table and her structure according to the CSV structure and the passed name.
+--CSV content is loaded in. 
+--Then, the function tries to define the type of columns according to the content.
+--You must check and adapt result.
+--
+--USAGE (due to 'copy' function usage, use it with a superuser only).
+--SELECT gn_commons.load_csv_file('/path/to/file.csv', 'targetschema.targettable');
+DECLARE
+    col text; -- variable to keep the column name at each iteration
+    sname text; --destination schema name
+    tname text; --destination table name
+begin
+    create temp table import (line text) on commit drop;
+    --import all csv content
+    execute format('copy import from %L', csv_file);
+    -- if not blank, change the csv_temp_table name to the name given as parameter
+    IF length(target_table) > 0 THEN
+	--split schema.table to 2 strings
+	SELECT split_part(target_table, '.', 1) into sname;
+	SELECT split_part(target_table, '.', 2) into tname;
+	--if a schema name is given
+	IF(tname IS NOT NULL) THEN
+	    --drop if exists and create table with first line as columns name in given schema (before point in target_table string)
+	    EXECUTE format('DROP TABLE IF EXISTS %I.%I', sname, tname);
+	    EXECUTE format('create table %I.%I (%s);', 
+	        sname, tname, concat(replace(line, ';', ' text, '), ' text'))
+            from import limit 1;
+	    -- load data in target table
+	    EXECUTE format('copy %I.%I from %L WITH DELIMITER '';'' quote ''"'' csv header', sname, tname, csv_file);
+	--if no schema is given working with public schema
+	ELSE
+	    --drop if exists and create table with first line as columns name in public schema
+	    EXECUTE format('DROP TABLE IF EXISTS %I', target_table);
+	    EXECUTE format('create table %I (%s);', 
+	        target_table, concat(replace(line, ';', ' text, '), ' text'))
+            from import limit 1;
+            -- load data in target table
+	    EXECUTE format('copy %I from %L WITH DELIMITER '';'' quote ''"'' csv ', target_table, csv_file);
+        END IF;
+        --try to change convert numeric and date columns type. If error throw, do nothing, continue and keep 'text' type.
+        FOR col IN EXECUTE format('SELECT column_name FROM information_schema.columns WHERE table_schema  = %L AND table_name = %L', sname, tname)
+        LOOP
+	    BEGIN
+	        EXECUTE format('ALTER TABLE %I.%I ALTER COLUMN %s TYPE integer USING %s::integer', sname, tname, col, col);
+	        EXCEPTION WHEN OTHERS THEN 
+		    BEGIN
+		        EXECUTE format('ALTER TABLE %I.%I ALTER COLUMN %s TYPE real USING %s::real', sname, tname, col, col);
+		        EXCEPTION WHEN OTHERS THEN
+			    BEGIN
+			        EXECUTE format('ALTER TABLE %I.%I ALTER COLUMN %s TYPE date USING %s::date', sname, tname, col, col);
+			        EXCEPTION WHEN OTHERS THEN -- keep looping
+			    END;
+		    END;
+            END;
+        END LOOP;
+    END IF;
+    RETURN format('CREATE TABLE %I.%I FROM %L', sname, tname, csv_file);
+end $$;
+
+
 -------------
 --TABLES--
 -------------
