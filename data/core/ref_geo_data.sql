eec0815fb4c4111a16b69b128f14517a8d62beb5
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2018-09-14 10:37:03 +0200

    INSERT COMMUNE - Ne pas utiliser id_type en dur

--- data/core/ref_geo_data.sql
+++ data/core/ref_geo_data.sql
@@ -1,7 +1,7 @@
 SET search_path = ref_geo, pg_catalog;
 
 INSERT INTO l_areas (id_type, area_code, area_name, geom)
-SELECT 101 AS id_type, insee_com, nom_com, geom FROM temp_fr_municipalities;
+SELECT get_id_area_type('COM') AS id_type, insee_com, nom_com, geom FROM temp_fr_municipalities;
 
 TRUNCATE TABLE li_municipalities;
 INSERT INTO li_municipalities (id_municipality, id_area, status, insee_com, nom_com, insee_arr, nom_dep, insee_dep, nom_reg, insee_reg, code_epci)
