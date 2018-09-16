f95386d27b18df90e5b3f2e08550de53e7202a44
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2018-09-17 01:13:55 +0200

    SQL IMPORTS - préparation d'un mapping pour l'import

--- data/core/imports.sql
+++ data/core/imports.sql
@@ -1,1 +1,153 @@
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
 CREATE SCHEMA gn_imports;
+
+SET search_path = gn_commons, pg_catalog;
+SET default_with_oids = false;
+
+CREATE TABLE matching_tables
+(
+  id_matching_table serial NOT NULL,
+  source_schema text NOT NULL,
+  source_table text NOT NULL,
+  target_schema text NOT NULL,
+  target_table text NOT NULL,
+  matching_comments text,
+  CONSTRAINT pk_matching_tables PRIMARY KEY (id_matching_table)
+);
+
+CREATE TABLE matching_fields
+(
+  source_field text NOT NULL,
+  target_field text NOT NULL,
+  target_field_type text NOT NULL,
+  comments text,
+  id_matching_table integer NOT NULL,
+  CONSTRAINT pk_matching_fields PRIMARY KEY (source_field, target_field, id_matching_table)
+);
+
+CREATE TABLE matching_geoms
+(
+  id_matching_geom serial NOT NULL,	
+  source_x_field text,
+  source_y_field text,
+  source_geom_field text,
+  source_geom_format text,
+  source_srid integer,
+  target_geom_field text,
+  target_geom_srid integer,
+  geom_comments text,
+  id_matching_table integer NOT NULL,
+  CONSTRAINT pk_matching_synthese PRIMARY KEY (id_matching_geom)
+);
+
+
+----------------
+--FOREIGN KEYS--
+----------------
+ALTER TABLE ONLY matching_geoms
+    ADD CONSTRAINT fk_matching_geoms_matching_tables FOREIGN KEY (id_matching_table) REFERENCES matching_tables(id_matching_table) ON UPDATE CASCADE ON DELETE NO ACTION;
+ALTER TABLE ONLY matching_fields
+    ADD CONSTRAINT fk_matching_fields_matching_tables FOREIGN KEY (id_matching_table) REFERENCES matching_tables(id_matching_table) ON UPDATE CASCADE ON DELETE NO ACTION;
+
+
+-------------
+--FUNCTIONS--
+-------------
+CREATE OR REPLACE FUNCTION fct_prepare_import_query(
+  mysource_table text, 
+  mytarget_table text)  RETURNS text AS
+--This function prépare a sql query to insert data from a table to another.
+--Use the 3 matching tables in gn_imports schema.
+--USAGE : gn_imports.fct_prepare_import_query('table_source', 'table_cible');
+$BODY$
+DECLARE
+    insertfield text; --prepared field to insert
+    selectfield text; --prepared field in select clause
+    sqlimport text; --returned query as text
+    virgule text;
+    ssname text; --source schema name
+    stname text; --source table name
+    tsname text; --destination schema name
+    ttname text; --destination table name
+    r record;
+    g record;
+BEGIN
+    --test si la table source est fournie sinon on retourne un message d'erreur
+    IF length(mysource_table) > 0 THEN
+	--split schema.table n deux chaines
+	SELECT split_part(mysource_table, '.', 1) into ssname;
+	SELECT split_part(mysource_table, '.', 2) into stname;
+    ELSE
+        BEGIN
+            RAISE WARNING 'ERREUR : %', 'Vous devez passer en paramètre une table source et une table de destination.';
+        END;
+    END IF;
+    --test si la table destination est fournie sinon on retourne un message d'erreur
+    IF length(mytarget_table) > 0 THEN
+	--split schema.table en deux chaines
+	SELECT split_part(mytarget_table, '.', 1) into tsname;
+	SELECT split_part(mytarget_table, '.', 2) into ttname;
+    ELSE
+	BEGIN
+            RAISE WARNING 'ERREUR : %', 'Vous devez passer en paramètre une table source et une table de destination.';
+        END;
+    END IF;
+    --test si la table source et de destination existe et si un maping des champs a été préparé.
+    insertfield = format('INSERT INTO %I.%I(',tsname, ttname);
+    selectfield = 'SELECT ';
+    FOR r IN EXECUTE format('SELECT source_field, target_field, target_field_type 
+			     FROM gn_imports.matching_fields f
+			     JOIN gn_imports.matching_tables t ON t.id_matching_table = f.id_matching_table
+			     WHERE t.source_schema = %L 
+			     AND t.source_table = %L
+			     AND t.target_schema = %L
+			     AND t.target_table = %L'
+			     ,ssname, stname,tsname, ttname)
+    LOOP
+        insertfield := concat(insertfield, virgule, r.target_field);
+        selectfield := concat(selectfield, virgule, r.source_field,'::',r.target_field_type);
+        virgule := ', ';
+    END LOOP;
+    --gestion du geom avec la table gn_imports.matching_geoms
+    FOR g IN EXECUTE format('SELECT g.* FROM gn_imports.matching_geoms g
+			     JOIN gn_imports.matching_tables t ON t.id_matching_table = g.id_matching_table
+			     WHERE t.source_schema = %L 
+			     AND t.source_table = %L
+			     AND t.target_schema = %L
+			     AND t.target_table = %L'
+			     ,ssname, stname,tsname, ttname)
+    LOOP
+	--on test si un matching de geom est déclaré
+	IF g.id_matching_geom IS NOT NULL THEN
+	    --si oui on contruit le mapping
+	    IF((g.source_geom_format = 'xy') AND (g.source_x_field IS NOT NULL) AND (g.source_y_field IS NOT NULL)) THEN
+	        insertfield := concat(insertfield, virgule, g.target_geom_field);
+	        selectfield := concat(selectfield, virgule, 'ST_Transform(ST_GeomFromText(','''POINT(''|| ',g.source_x_field,' || ',''' ''',' || ',g.source_y_field,' ||'')''',', ',g.source_srid,'), ',g.target_geom_srid,')');
+	    ELSIF (g.source_geom_format = 'wkt' AND length(g.source_geom_field)>0) THEN
+	        --TODO
+	    ELSE
+	        BEGIN
+	            RAISE EXCEPTION 'ATTENTION %', 'Le format du champ "source_geom_format" dans la table "gn_imports.matching_geoms" 
+	            doit être "xy" ou "wkt" 
+	            ET les champs "source_x_field" et "source_y_field" doivent être complétés pour le format "xy"
+	            OU le champs "source_geom_field" doit être complété pour le format "wkt".';
+	        END;
+	    END IF;
+	END IF;
+    END LOOP;
+    --finalisation de la clause insert
+    insertfield := concat(insertfield, ')');
+    selectfield := concat(selectfield, ' FROM ', ssname, '.', stname);
+    --construction de la requête complète
+    sqlimport := concat(insertfield, ' ', selectfield, ';');
+    RETURN sqlimport;
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE
+  COST 100;
