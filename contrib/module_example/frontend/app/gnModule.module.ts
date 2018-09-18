d5f3e432db4982863173fa9dc746b0a8bc88ac91
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-09-18 14:51:56 +0200

    Module exemple plus complet

--- contrib/module_example/frontend/app/gnModule.module.ts
+++ contrib/module_example/frontend/app/gnModule.module.ts
@@ -1,22 +1,15 @@
-import { NgModule } from  '@angular/core'
-import { GN2CommonModule } from '@geonature_common/GN2Common.module';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { GN2CommonModule } from "@geonature_common/GN2Common.module";
+import { Routes, RouterModule } from "@angular/router";
+import { TestComponent } from "./components/test.component";
 
 // my module routing
-const routes: Routes = [
-  { path: '', component: myRootComponent },
-];
+const routes: Routes = [{ path: "", component: TestComponent }];
 
 @NgModule({
-  declarations: [
-     
-  ],
-  imports: [
-    GN2CommonModule,
-    RouterModule.forChild(routes),
-  ],
+  declarations: [TestComponent],
+  imports: [GN2CommonModule, RouterModule.forChild(routes)],
   providers: [],
   bootstrap: []
 })
-export class GeonatureModule { 
-}
+export class GeonatureModule {}
