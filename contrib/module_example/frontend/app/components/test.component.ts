47bd72aa0cd8375a72b71e817478b28cc2a34e32
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-09-18 14:51:56 +0200

    Module exemple plus complet

---
+++ contrib/module_example/frontend/app/components/test.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "test",
+  templateUrl: "test.component.html"
+})
+export class TestComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
