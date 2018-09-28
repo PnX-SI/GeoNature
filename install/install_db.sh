bd3bd6914649ac05bad7da35bce1a88f0716769c
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2018-09-28 16:16:15 +0200

    Install _db - installation des valeurs par défaut des nomenclatures

--- install/install_db.sh
+++ install/install_db.sh
@@ -292,10 +292,12 @@ then
         fi
 	      unzip /tmp/geonature/BDALTIV2_2-0_250M_ASC_LAMB93-IGN69_FRANCE_2017-06-21.zip -d /tmp/geonature
         #gdalwarp -t_srs EPSG:$srid_local /tmp/geonature/BDALTIV2_250M_FXX_0098_7150_MNT_LAMB93_IGN69.asc /tmp/geonature/dem.tif &>> var/log/install_db.log
-        export PGPASSWORD=$user_pg_pass;raster2pgsql -s $srid_local -c -C -I -M -d -t 100x100 /tmp/geonature/BDALTIV2_250M_FXX_0098_7150_MNT_LAMB93_IGN69.asc ref_geo.dem|psql -h $db_host -U $user_pg -d $db_name  &>> var/log/install_db.log
-    	  echo "Vectorisation of DEM raster. This may take a few minutes..."
+        export PGPASSWORD=$user_pg_pass;raster2pgsql -s $srid_local -c -C -I -M -d -t 5x5 /tmp/geonature/BDALTIV2_250M_FXX_0098_7150_MNT_LAMB93_IGN69.asc ref_geo.dem|psql -h $db_host -U $user_pg -d $db_name  &>> var/log/install_db.log
+    	#echo "Refresh DEM spatial index. This may take a few minutes..."
+        sudo -n -u postgres -s psql -d $db_name -c "REINDEX INDEX ref_geo.dem_st_convexhull_idx;" &>> var/log/install_db.log
+        #echo "Vectorisation of DEM raster. This may take a few minutes..."
         sudo -n -u postgres -s psql -d $db_name -c "INSERT INTO ref_geo.dem_vector (geom, val) SELECT (ST_DumpAsPolygons(rast)).* FROM ref_geo.dem;" &>> var/log/install_db.log
-        echo "Refresh DEM vector spatial index. This may take a few minutes..."
+        #echo "Refresh DEM vector spatial index. This may take a few minutes..."
         sudo -n -u postgres -s psql -d $db_name -c "REINDEX INDEX ref_geo.index_dem_vector_geom;" &>> var/log/install_db.log
     fi
 
@@ -329,6 +331,7 @@ then
     cp data/core/synthese.sql /tmp/geonature/synthese.sql
     sudo sed -i "s/MYLOCALSRID/$srid_local/g" /tmp/geonature/synthese.sql
     export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f /tmp/geonature/synthese.sql  &>> var/log/install_db.log
+    export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f data/core/synthese_default_values.sql  &>> var/log/install_db.log
 
     echo "Creating 'exports' schema..."
     echo "" &>> var/log/install_db.log
@@ -363,7 +366,6 @@ then
     then
         export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f data/core/meta_data.sql  &>> var/log/install_db.log
         export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f data/core/monitoring_data.sql  &>> var/log/install_db.log
-        export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f data/core/synthese_data.sql  &>> var/log/install_db.log
     fi
 
     if $install_default_dem
