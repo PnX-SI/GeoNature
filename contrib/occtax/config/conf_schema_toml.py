610aacffd52c7b89bed5a4dd22111635b6a7baa8
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2018-10-15 10:30:52 +0200

    Update conf_schema_toml.py

--- contrib/occtax/config/conf_schema_toml.py
+++ contrib/occtax/config/conf_schema_toml.py
@@ -1,5 +1,7 @@
 '''
    Spécification du schéma toml des paramètres de configurations
+   Fichier spécifiant les types des paramètres et leurs valeurs par défaut
+   Fichier à ne pas modifier. Paramètres surcouchables dans config/config_gn_module.tml
 '''
 
 from marshmallow import Schema, fields
