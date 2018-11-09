7698763cd2b2d170af9ff4e7e1efbf0916e5ba30
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2018-11-09 12:15:35 +0100

    Validation : trigger update synthese

--- data/core/commons.sql
+++ data/core/commons.sql
@@ -179,6 +179,23 @@ $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
 
+
+CREATE OR REPLACE FUNCTION fct_trg_update_synthese_validation_status()
+    RETURNS trigger AS
+$BODY$
+-- This trigger function update validation informations in corresponding row in synthese table
+BEGIN
+  UPDATE gn_synthese.synthese 
+  SET id_nomenclature_valid_status = NEW.id_nomenclature_valid_status,
+  validation_comment = NEW.validation_comment,
+  validator = (SELECT nom_role || ' ' || prenom_role FROM utilisateurs.t_roles WHERE id_role = NEW.id_validator)::text
+  WHERE unique_id_sinp = NEW.uuid_attached_row;
+RETURN NEW;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
 CREATE OR REPLACE FUNCTION get_default_parameter(myparamname text, myidorganisme integer DEFAULT 0)
   RETURNS text AS
 $BODY$
@@ -511,6 +528,12 @@ CREATE TRIGGER tri_log_changes_t_medias
   ON t_medias
   FOR EACH ROW
   EXECUTE PROCEDURE gn_commons.fct_trg_log_changes();
+
+CREATE TRIGGER tri_insert_synthese_update_validation_status
+  AFTER INSERT
+  ON t_validations
+  FOR EACH ROW
+  EXECUTE PROCEDURE gn_commons.fct_trg_update_synthese_validation_status();
 
 
 ---------
--- data/migrations/2.0.0rc3.1-to-2.0.0.sql
+++ data/migrations/2.0.0rc3.1-to-2.0.0.sql
@@ -48,6 +48,28 @@ $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
 
+CREATE OR REPLACE FUNCTION gn_commons.fct_trg_update_synthese_validation_status()
+    RETURNS trigger AS
+$BODY$
+-- This trigger function update validation informations in corresponding row in synthese table
+BEGIN
+  UPDATE gn_synthese.synthese 
+  SET id_nomenclature_valid_status = NEW.id_nomenclature_valid_status,
+  validation_comment = NEW.validation_comment,
+  validator = (SELECT nom_role || ' ' || prenom_role FROM utilisateurs.t_roles WHERE id_role = NEW.id_validator)::text
+  WHERE unique_id_sinp = NEW.uuid_attached_row;
+RETURN NEW;
+END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
+CREATE TRIGGER tri_insert_synthese_update_validation_status
+  AFTER INSERT
+  ON gn_commons.t_validations
+  FOR EACH ROW
+  EXECUTE PROCEDURE gn_commons.fct_trg_update_synthese_validation_status();
+
 
 --------
 --META--
