3febe464b76f3f126b112b9dbcc1028d45b6bc82
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-11-14 12:09:59 +0100

    update npm shrinkwrap

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from '@geonature/app.module';
+// import { AppModule } from '@geonature/app.module';
+import { AppModule } from './app/app.module';
 import { AppConfig } from './conf/app.config';
 
 if (AppConfig.FRONTEND.PROD_MOD) {
