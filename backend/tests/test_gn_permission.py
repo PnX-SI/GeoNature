372793e2ca2e8b8c79d76c0734e5b9183d70cc39
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-12-05 17:51:43 +0100

    test post cruved

--- backend/tests/test_gn_permission.py
+++ backend/tests/test_gn_permission.py
@@ -5,6 +5,8 @@ from flask import url_for, current_app, request
 
 from .bootstrap_test import app, json_of_response, get_token
 from pypnusershub.db.tools import InsufficientRightsError
+from geonature.core.gn_permissions.models import VUsersPermissions
+from geonature.utils.env import DB
 
 
 @pytest.mark.usefixtures('client_class')
@@ -69,4 +71,49 @@ class TestGnPermissions():
                     id_object=None
             )
         )
+
+    def test_post_cruved_scope_form(self):
+        token = get_token(self.client)
+        self.client.set_cookie('/', 'token', token)
+        # WARNING: wet set ID not code in the form !
+        data = {
+            'C': '4',
+            'R': '3',
+            'U': '3',
+            'V': '4',
+            'E': '2',
+            'D': '3',
+        }
+        response = self.client.post(
+            url_for(
+                'gn_permissions_backoffice.permission_form',
+                id_role=9,
+                id_module=3,
+                id_object=None
+            ),
+            data=data,
+            content_type='multipart/form-data'
+        )
+        assert response.status_code == 302
+
+        permissions = DB.session.query(
+            VUsersPermissions
+        ).filter_by(
+            id_role=9,
+            module_code='GEONATURE',
+            code_object='ALL',
+            code_filter_type='SCOPE'
+        ).all()
+
+        # check no multiple permission per action
+        assert len(permissions) == 6
+        cruved_dict = {perm.code_action: perm.value_filter for perm in permissions}
+        assert cruved_dict['C'] == '3'
+        assert cruved_dict['R'] == '2'
+        assert cruved_dict['U'] == '2'
+        assert cruved_dict['V'] == '3'
+        assert cruved_dict['E'] == '1'
+        assert cruved_dict['D'] == '2'
+
+
         
