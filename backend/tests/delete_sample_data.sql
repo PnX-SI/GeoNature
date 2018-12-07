f2ee6fc8767c399b12357ce15b71c7a7ba0fad1c
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-12-07 15:24:05 +0100

    delete sample data after test

--- backend/tests/delete_sample_data.sql
+++ backend/tests/delete_sample_data.sql
@@ -8,4 +8,6 @@ TRUNCATE gn_meta.t_acquisition_frameworks CASCADE;
 
 TRUNCATE gn_permissions.cor_role_action_filter_module_object CASCADE;
 
+TRUNCATE gn_synthese.synthese CASCADE;
+
 TRUNCATE utilisateurs.cor_role_app_profil CASCADE;
