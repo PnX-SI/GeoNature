f00525c1e5346fa39f9088fb80b91a0208c87e24
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2018-12-18 16:13:02 +0100

    Installation module: utilisation de gn_db

--- contrib/module_example/install_gn_module.py
+++ contrib/module_example/install_gn_module.py
@@ -7,9 +7,10 @@ ROOT_DIR = Path(__file__).absolute().parent
 
 def gnmodule_install_app(gn_db, gn_app):
     '''
-        Fonction principale permettant de réaliser les opérations d'installation du module : 
-            - Base de données
-            - Module (pour le moment rien)
+        Fonction principale permettant de réaliser les opérations d'installation du module
     '''
     with gn_app.app_context() :
+        # To run a SQL script use the gn_db parameter
+        #gn_db.session.execute(open(<my_sql_data.sql>, 'r').read())
+        #gn_db.session.commit()
         pass
