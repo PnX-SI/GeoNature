0328c47766dc70513ffa168a888cfd7eea3ae1e7
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2019-02-05 10:30:45 +0100

    correction script update + impact de suppression bib_noms

--- data/migrations/2.0.0rc4.2to2.0.0.sql
+++ data/migrations/2.0.0rc4.2to2.0.0.sql
@@ -1,3 +1,78 @@
+-- recréation de vue lié à la suppression de bib_noms
+
+DROP VIEW gn_synthese.v_tree_taxons_synthese;
+CREATE OR REPLACE VIEW gn_synthese.v_tree_taxons_synthese AS
+WITH 
+  cd_synthese AS(
+    SELECT DISTINCT cd_nom FROM gn_synthese.synthese
+  )
+	,taxon AS (
+    SELECT
+      t_1.cd_ref,
+      t_1.lb_nom AS nom_latin,
+      t_1.nom_vern AS nom_francais,
+      t_1.cd_nom,
+      t_1.id_rang,
+      t_1.regne,
+      t_1.phylum,
+      t_1.classe,
+      t_1.ordre,
+      t_1.famille,
+      t_1.lb_nom
+    FROM taxonomie.taxref t_1
+    JOIN cd_synthese s ON s.cd_nom = t_1.cd_nom
+  )
+  ,cd_regne AS (
+    SELECT DISTINCT taxref.cd_nom,
+      taxref.regne
+    FROM taxonomie.taxref
+    WHERE taxref.id_rang::text = 'KD'::text 
+    AND taxref.cd_nom = taxref.cd_ref
+  )
+SELECT
+  t.cd_ref,
+  t.nom_latin,
+  t.nom_francais,
+  t.id_regne,
+  t.nom_regne,
+  COALESCE(t.id_embranchement, t.id_regne) AS id_embranchement,
+  COALESCE(t.nom_embranchement, ' Sans embranchement dans taxref') AS nom_embranchement,
+  COALESCE(t.id_classe, t.id_embranchement) AS id_classe,
+  COALESCE(t.nom_classe, ' Sans classe dans taxref') AS nom_classe,
+  COALESCE(t.desc_classe, ' Sans classe dans taxref') AS desc_classe,
+  COALESCE(t.id_ordre, t.id_classe) AS id_ordre,
+  COALESCE(t.nom_ordre, ' Sans ordre dans taxref') AS nom_ordre,
+  COALESCE(t.id_famille, t.id_ordre) AS id_famille,
+  COALESCE(t.nom_famille, ' Sans famille dans taxref') AS nom_famille
+FROM ( 
+  SELECT DISTINCT
+    t_1.cd_ref,
+    t_1.nom_latin,
+    t_1.nom_francais,
+    ( SELECT DISTINCT r.cd_nom
+      FROM cd_regne r
+      WHERE r.regne = t_1.regne
+    ) AS id_regne,
+    t_1.regne AS nom_regne,
+    ph.cd_nom AS id_embranchement,
+    t_1.phylum AS nom_embranchement,
+    t_1.phylum AS desc_embranchement,
+    cl.cd_nom AS id_classe,
+    t_1.classe AS nom_classe,
+    t_1.classe AS desc_classe,
+    ord.cd_nom AS id_ordre,
+    t_1.ordre AS nom_ordre,
+    f.cd_nom AS id_famille,
+    t_1.famille AS nom_famille
+  FROM taxon t_1
+  LEFT JOIN taxonomie.taxref ph ON ph.id_rang = 'PH' AND ph.cd_nom = ph.cd_ref AND ph.lb_nom = t_1.phylum AND NOT t_1.phylum IS NULL
+  LEFT JOIN taxonomie.taxref cl ON cl.id_rang = 'CL' AND cl.cd_nom = cl.cd_ref AND cl.lb_nom = t_1.classe AND NOT t_1.classe IS NULL
+  LEFT JOIN taxonomie.taxref ord ON ord.id_rang = 'OR' AND ord.cd_nom = ord.cd_ref AND ord.lb_nom = t_1.ordre AND NOT t_1.ordre IS NULL
+  LEFT JOIN taxonomie.taxref f ON f.id_rang = 'FM' AND f.cd_nom = f.cd_ref AND f.lb_nom = t_1.famille AND f.phylum = t_1.phylum AND NOT t_1.famille IS NULL
+) t
+ORDER BY id_regne, id_embranchement, id_classe, id_ordre, id_famille;
+
+
 -- Migration du metadata comme un module à part
 
 INSERT INTO gn_commons.t_modules(module_code, module_label, module_picto, module_desc, module_path, module_target, active_frontend, active_backend) VALUES
@@ -119,22 +194,6 @@ $BODY$
     END IF;
   RETURN NULL;
   END;
-$BODY$
-  LANGUAGE plpgsql VOLATILE
-  COST 100;
-
-
-CREATE OR REPLACE FUNCTION taxonomie.trg_fct_refresh_nomfrancais_mv_taxref_list_forautocomplete()
-  RETURNS trigger AS
-$BODY$
-DECLARE
-BEGIN
-    UPDATE taxonomie.vm_taxref_list_forautocomplete v
-    SET search_name = concat(NEW.nom_francais, ' =  <i> ', t.nom_valide, '</i>', ' - [', t.id_rang, ' - ', t.cd_nom , ']')
-    FROM taxonomie.taxref t
-		WHERE v.cd_nom = NEW.cd_nom AND t.cd_nom = NEW.cd_nom;
-    RETURN NEW;
-END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
   COST 100;
