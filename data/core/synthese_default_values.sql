7c9c0dad53523e67010c86d4e78a22a2ce1814f6
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2019-03-18 09:20:36 +0100

    update default nomenclature synthese

--- data/core/synthese_default_values.sql
+++ data/core/synthese_default_values.sql
@@ -20,7 +20,7 @@ INSERT INTO defaults_nomenclatures_value (mnemonique_type, id_organism, regne, g
 ,('SEXE',0,0,0,ref_nomenclatures.get_id_nomenclature('SEXE','6'))
 ,('OBJ_DENBR',0,0,0,ref_nomenclatures.get_id_nomenclature('OBJ_DENBR','NSP'))
 ,('TYP_DENBR',0,0,0,ref_nomenclatures.get_id_nomenclature('TYP_DENBR','NSP'))
-,('STATUT_OBS',0,0,0,ref_nomenclatures.get_id_nomenclature('STATUT_OBS','NSP'))
+,('STATUT_OBS',0,0,0,ref_nomenclatures.get_id_nomenclature('STATUT_OBS','Pr'))
 ,('DEE_FLOU',0,0,0,ref_nomenclatures.get_id_nomenclature('DEE_FLOU','NON'))
 ,('TYP_GRP',0,0,0,ref_nomenclatures.get_id_nomenclature('TYP_GRP','NSP'))
 ,('TECHNIQUE_OBS',0,0,0,ref_nomenclatures.get_id_nomenclature('TECHNIQUE_OBS','133'))
