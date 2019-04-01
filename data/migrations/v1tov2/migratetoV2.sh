810dd4325634c8bbb25cb4525d247fd3480f70fb
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2019-04-01 13:44:19 +0200

    Ebauche de la migration V1 to V2

---
+++ data/migrations/v1tov2/migratetoV2.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+. migratetov2.ini
+. ../../../config/settings.ini
+
+#Sur le serveur de GeoNature V2 : création du lien FDW avec la base GeoNature1 
+sudo -n -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS postgres_fdw;" > ../../../var/log/migratetov2.log
+sudo chmod 777 . ../../../var/log/migratetov2.log
+sudo -n -u postgres -s psql -d $db_name -c "DROP SERVER IF EXISTS geonature1server CASCADE;" >> ../../../var/log/migratetov2.log
+sudo -n -u postgres -s psql -d $db_name -c "CREATE SERVER geonature1server FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host '$geonature1host', dbname '$geonature1db', port '$geonature1port');" >> ../../../var/log/migratetov2.log
+sudo -n -u postgres -s psql -d $db_name -c "CREATE USER MAPPING FOR $user_pg SERVER geonature1server OPTIONS (user '$geonature1user', password '$geonature1userpass');" >> ../../../var/log/migratetov2.log
+sudo -n -u postgres -s psql -d $db_name -c "ALTER SERVER geonature1server OWNER TO $user_pg;" >> ../../../var/log/migratetov2.log
+
+
+    if $install_sig_layers
+    then
+        echo "Get utilisateurs schema content from geontauredb1"
+        export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f users.sql  &>> ../../../var/log/migratetov2.log
+    fi
+
+
+# echo "Maintenant tu dois t'inspirer du script migratetov2.sql qui a été écrit pour le PNE."
+# echo "ce script établi des correspondances bib_programmes <-> t_acquisition_frameworks, bib_lots <-> t_datasets, bib_criteres_synthese et nomenclatures."
+# echo "Ces correspondances sont propres à chaque structures et ne peuvent pas être automatisées."
+
+#export PGPASSWORD='$user_pg_pass';psql -h $db_host -U $user_pg -d $db_name -f 'migratetov2.sql' >> ../../../var/log/migratetov2.log
+#export PGPASSWORD='$user_pg_pass';psql -h $db_host -U $user_pg -d $db_name -f 'v1_contactfaune.sql' >> ../../../var/log/migratetov2.log
+
