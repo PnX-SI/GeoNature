3e359fc9893c3a0f4033b378e90b010c58997a46
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2019-04-02 12:55:58 +0200

    WIP: contact faune to occtax [ci skip]

---
+++ data/migrations/v1tov2/contact_faune_to_occtax.sql
@@ -0,0 +1,116 @@
+ALTER TABLE pr_occtax.t_releves_occtax DISABLE TRIGGER USER;
+ALTER TABLE pr_occtax.t_occurrences_occtax DISABLE TRIGGER USER;
+
+CREATE TABLE v1_compat.cor_critere_contactfaune_v1_to_v2 (
+	pk_source integer,
+	entity_source character varying(100),
+	field_source character varying(50),
+	entity_target character varying(100),
+	field_target character varying(50),
+	id_type_nomenclature_cible integer,
+	id_nomenclature_cible integer,
+	commentaire text
+);
+
+
+INSERT INTO v1_compat.cor_critere_contactfaune_v1_to_v2
+SELECT * 
+FROM v1_compat.cor_synthese_v1_to_v2
+WHERE entity_source = 'v1_compat.bib_criteres_synthese' AND pk_source < 100 ;
+
+UPDATE v1_compat.cor_critere_contactfaune_v1_to_v2 SET entity_target = 'pr_occtax.t_occurrences_occtax';
+
+
+INSERT INTO pr_occtax.t_releves_occtax(
+            id_releve_occtax,
+            unique_id_sinp_grp,
+            id_dataset, 
+            -- technique d'obs non traité en l'état -> NSP
+            id_nomenclature_obs_technique, 
+            id_nomenclature_grp_typ, 
+            date_min, 
+            date_max, 
+            altitude_min, 
+            altitude_max, 
+            meta_device_entry, 
+            geom_local, 
+            geom_4326, 
+            "precision"
+        )
+WITH
+n24 AS (SELECT * FROM v1_compat.cor_synthese_v1_to_v2 WHERE id_type_nomenclature_cible = 24)
+SELECT 
+    id_cf AS id_releve_occtax,
+    uuid_generate_v4() AS unique_id_sinp_grp,
+    id_lot AS id_dataset,
+    ref_nomenclatures.get_id_nomenclature('TECHNIQUE_OBS','000000') AS id_nomenclature_obs_technique,
+    COALESCE(n24.id_nomenclature_cible, ref_nomenclatures.get_id_nomenclature('TYP_GRP','NSP')) 
+    dateobs AS date_min 
+    dateobs AS date_max
+    altitude_retenue AS altitude_min,
+    altitude_retenue AS altitude_max,
+    saisie_initiale AS meta_device_entry, 
+    the_geom_local AS geom_local
+    ST_TRANSFORM(the_geom_local, 4326) AS geom_4326,
+    50 AS precision
+FROM v1_compat.t_fiches_cf cf
+LEFT JOIN n24 ON cf.id_lot = n24.pk_source;
+
+
+
+
+INSERT INTO pr_occtax.t_occurrences_occtax(
+            id_occurrence_occtax,
+            unique_id_occurence_occtax, 
+            id_releve_occtax, 
+            id_nomenclature_obs_meth, 
+            id_nomenclature_bio_condition, 
+            id_nomenclature_bio_status, 
+            id_nomenclature_naturalness, 
+            id_nomenclature_exist_proof, 
+            id_nomenclature_diffusion_level, 
+            id_nomenclature_observation_status, 
+            id_nomenclature_blurring, 
+            id_nomenclature_source_status, 
+            determiner, 
+            id_nomenclature_determination_method, 
+            cd_nom, 
+            nom_cite, 
+            meta_v_taxref, 
+            sample_number_proof, 
+            digital_proof, 
+            non_digital_proof, 
+            comment
+        )
+    WITH 
+    n14 AS (SELECT * FROM v1_compat.cor_critere_contactfaune_v1_to_v2 WHERE id_type_nomenclature_cible = 14) ,
+    n7 AS (SELECT * FROM v1_compat.cor_critere_contactfaune_v1_to_v2 WHERE id_type_nomenclature_cible = 7),
+    n13 AS (SELECT * FROM v1_compat.cor_critere_contactfaune_v1_to_v2 WHERE id_type_nomenclature_cible = 13),
+    SELECT
+    id_releve_cf AS id_occurrence_occtax,
+    uuid_generate_v4() AS unique_id_occurence_occtax,
+    id_cf AS id_releve_occtax,
+    COALESCE(n24.id_nomenclature_cible, ref_nomenclatures.get_id_nomenclature('METH_OBS','21')) AS id_nomenclature_obs_meth,
+    COALESCE(n7.id_nomenclature_cible, ref_nomenclatures.get_id_nomenclature('ETA_BIO','0')) AS id_nomenclature_bio_condition,
+    COALESCE(n24.id_nomenclature_cible, ref_nomenclatures.get_id_nomenclature('STATUT_BIO','1')) AS id_nomenclature_bio_status,
+    ref_nomenclatures.get_id_nomenclature('NATURALITE','1') AS id_nomenclature_naturalness,
+    ref_nomenclatures.get_id_nomenclature('PREUVE_EXIST','2') AS id_nomenclature_exist_proof,
+    ref_nomenclatures.get_id_nomenclature('NIV_PRECIS','5') AS id_nomenclature_diffusion_level,
+    ref_nomenclatures.get_id_nomenclature('STATUT_OBS','Pr') AS id_nomenclature_observation_status,
+    ref_nomenclatures.get_id_nomenclature('DEE_FLOU','NON') AS id_nomenclature_blurring,
+    ref_nomenclatures.get_id_nomenclature('STATUT_SOURCE','Te') AS id_nomenclature_source_status,
+    -- determination = Non renseigné
+    NULL AS determiner,
+    ref_nomenclatures.get_id_nomenclature('METH_DETERMIN','1') AS id_nomenclature_source_status,
+    bib_noms.cd_nom AS cd_nom,
+    nom_taxon_saisi AS nom_cite,
+    'Taxref V11.0' AS meta_v_taxref,
+    NULL AS sample_number_proof,
+    NULL AS digital_proof, 
+    NULL AS non_digital_proof,
+    commentaire AS comment
+    FROM v1_compat.t_releves_cf cf
+    LEFT JOIN n14 ON n24.pk_source =  cf.id_critere_cf
+    LEFT JOIN n7 ON n7.pk_source =  cf.id_critere_cf
+    LEFT JOIN n13 ON n13.pk_source =  cf.id_critere_cf
+    JOIN taxonomie.bib_noms bib_noms ON bib_noms.id_nom = cf.id_nom;
