f30f22f34ba7a533b22753cfd4a94d6022b98afb
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2019-04-24 09:16:13 +0200

    migration ref_geo et unite_geo [ci skip]

--- data/migrations/v1tov2/my_organisme/mymigratetoV2.sh
+++ data/migrations/v1tov2/my_organisme/mymigratetoV2.sh
@@ -31,14 +31,5 @@ fi
 if $import_ref_geo
 then
     export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f myrefgeo.sql  &>> ../../../var/log/mymigratetov2.log
-    if [ ! -f '../../../tmp/geonature/communes_fr_admin_express_2019-01.zip' ]
-        then
-            wget  --cache=off http://geonature.fr/data/ign/communes_fr_admin_express_2019-01.zip -P ../../../tmp/geonature
-        else
-            echo "tmp/geonature/communes_fr_admin_express_2019-01.zip already exist"
-    fi
-        unzip ../../../tmp/geonature/communes_fr_admin_express_2019-01.zip -d ../../../tmp/geonature
-        sudo -n -u postgres -s psql -d $db_name -f ../../../tmp/geonature/fr_municipalities.sql &>> ../../../var/log/mymigratetov2.log
-    rm ../../../tmp/geonature/*.sql
 fi
 
