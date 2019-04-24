87782c7ac6cae5396cfaee5ee16987f925fa24cc
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2019-04-24 09:55:53 +0200

    ergo map-list occtax + fix hauteur relative bandeau [ci skip]

--- data/migrations/v1tov2/my_organisme/myrefgeo.sql
+++ data/migrations/v1tov2/my_organisme/myrefgeo.sql
@@ -1,18 +1,13 @@
--- TODO: pour supprimer les communes, il faut supprimer cor_synthese_area, et donc rejouer les triggers apres coup...
+-- libération des id pour les unités géo
+UPDATE ref_geo.l_areas
+SET id_area = id_area + 1000;
 
-DELETE FROM gn_synthese.cor_area_synthese;
-DELETE FROM ref_geo.l_areas WHERE id_type = 25;
-DELETE FROM ref_geo.li_municipalities;
-
-SELECT pg_catalog.setval('ref_geo.l_areas_id_area_seq', 1, true);
-
--- reinsertion des utités géo
+-- reinsertion des utités géo dans les id libérés
 INSERT INTO ref_geo.l_areas(
-            id_type, area_name, area_code, geom, centroid)
+            id_area, id_type, area_name, area_code, geom, centroid)
 SELECT 
-(SELECT id_type FROM ref_geo.bib_areas_types WHERE type_code = 'UG') AS id_type, id_unite_geo::text, id_unite_geo, the_geom, ST_Centroid(the_geom)
+id_unite_geo,(SELECT id_type FROM ref_geo.bib_areas_types WHERE type_code = 'UG') AS id_type, concat('unite_geoo', id_unite_geo::text), id_unite_geo, the_geom, ST_Centroid(the_geom)
 FROM v1_compat.l_unites_geo;
 
+-- maj de la sequence
 SELECT pg_catalog.setval('ref_geo.l_areas_id_area_seq', (SELECT max(id_area) FROM ref_geo.l_areas), true);
-
--- on insert les communes dans le sh
