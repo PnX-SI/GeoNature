60fbf6dff877ce53cc068510854cbd03a7e9343c
jbrieuclp <jbrieuclp@gmail.com>
2019-04-25 12:10:15 +0200

    correction du disabled des select

--- frontend/src/app/GN2CommonModule/form/genericForm.component.ts
+++ frontend/src/app/GN2CommonModule/form/genericForm.component.ts
@@ -29,11 +29,11 @@ export class GenericFormComponent implements OnInit, AfterViewInit, OnDestroy {
   constructor() {}
 
   ngOnInit() {
-    this.disabled ? this.parentFormControl.enable() : this.parentFormControl.disable();
     this.debounceTime = this.debounceTime || 0;
   }
 
   ngAfterViewInit() {
+    this.disabled ? this.parentFormControl.enable() : this.parentFormControl.disable();
     this.sub = this.parentFormControl.valueChanges
       .distinctUntilChanged()
       .debounceTime(this.debounceTime)
