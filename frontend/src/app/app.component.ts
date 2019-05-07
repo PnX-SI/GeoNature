704dc7233f1ac9582b2385527012c0a4ee0a231c
jbrieuclp <jbrieuclp@gmail.com>
2019-05-07 16:39:35 +0200

    deplacement de l'initialisation du translate

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from '@geonature/components/auth/auth.service';
+import { AppConfig } from '../conf/app.config';
 
 @Component({
   selector: 'pnx-root',
@@ -8,7 +9,14 @@ import { AuthService } from '@geonature/components/auth/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private _authService: AuthService) {}
+  constructor(
+  	private _authService: AuthService, 
+  	private translate: TranslateService
+  ) {
+  	translate.addLangs(['en', 'fr', 'cn']);
+    translate.setDefaultLang(AppConfig.DEFAULT_LANGUAGE);
+    translate.use(AppConfig.DEFAULT_LANGUAGE);
+  }
 
   ngOnInit() {}
 }
