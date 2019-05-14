261ab2b28e0cb279f3924591376d95f77a63ac66
jbrieuclp <jbrieuclp@gmail.com>
2019-05-14 17:14:48 +0200

    modif select areas pour permettre la recherche

--- frontend/src/app/GN2CommonModule/form/areas/areas.component.ts
+++ frontend/src/app/GN2CommonModule/form/areas/areas.component.ts
@@ -4,15 +4,20 @@ import { FormControl } from '@angular/forms';
 import { CommonService } from '@geonature_common/service/common.service';
 import { GenericFormComponent } from '@geonature_common/form/genericForm.component';
 import { AppConfig } from '@geonature_config/app.config';
+import { Subject, Observable, of, concat } from 'rxjs';
+import { distinctUntilChanged, debounceTime, switchMap, tap, catchError, map } from 'rxjs/operators'
 
 @Component({
   selector: 'pnx-areas',
   templateUrl: 'areas.component.html'
 })
 export class AreasComponent extends GenericFormComponent implements OnInit {
-  public areas: any;
   public cachedAreas: any;
   @Input() idTypes: Array<number>; // Areas id_type
+
+  areas_input$ = new Subject<string>();
+  areas: Observable<any>;
+  loading = false;
 
   constructor(
     private _dfs: DataFormService, 
@@ -22,45 +27,40 @@ export class AreasComponent extends GenericFormComponent implements OnInit {
   }
 
   ngOnInit() {
-    this._dfs.getAreas(this.idTypes).subscribe(data => {
-      this.cachedAreas = data;
-      this.formatAreas(data);
-    });
+    this.getAreas();
   }
+
+  getAreas() { 
+    this.areas = concat(
+        this._dfs.getAreas(this.idTypes).pipe(map(data=>this.formatAreas(data))), // default items
+        this.areas_input$.pipe(
+            debounceTime(200),
+            distinctUntilChanged(),
+            tap(() => this.loading = true),
+            switchMap(term => {
+              return term.length >= 2 ?
+                this._dfs.getAreas(this.idTypes, term).pipe(
+                  map(data=>this.formatAreas(data)),
+                  catchError(() => of([])), // empty list on error
+                  tap(() => this.loading = false)
+                ) : [];
+            }) 
+        )
+    );
+  }
+
   /**
    * Set the departement number if the id_type is municipalities
    * @param data
    */
   formatAreas(data) {
     if (data.length > 0 && data[0]['id_type'] === AppConfig.BDD.id_area_type_municipality) {
-      this.areas = data.map(element => {
+      return data.map(element => {
         element['area_name'] = `${element['area_name']} (${element.area_code.substr(0, 2)}) `;
         return element;
       });
-    } else {
-      this.areas = data;
-    }
-  }
+    } 
 
-  refreshAreas(area_name) {
-    // refresh area API call only when area_name >= 2 character
-    if (area_name && area_name.length >= 2) {
-      this._dfs.getAreas(this.idTypes, area_name).subscribe(
-        data => {
-          this.formatAreas(data);
-        },
-        err => {
-          if (err.status === 404) {
-            this.areas = [{ area_name: 'No data to display' }];
-          } else {
-            this.areas = [];
-            this._commonService.translateToaster('error', 'ErrorMessage');
-          }
-        }
-      );
-      // and reset areas when delete search or select a area
-    } else if (!area_name) {
-      this.areas = this.cachedAreas;
-    }
+    return data;
   }
 }
