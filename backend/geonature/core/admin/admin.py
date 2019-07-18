60d41773edb2b31577b2f34d82da1c632fa7b6dd
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2019-07-18 17:28:39 +0200

    up

--- backend/geonature/core/admin/admin.py
+++ backend/geonature/core/admin/admin.py
@@ -1,30 +1,10 @@
 from flask import current_app
 from flask_admin import Admin
 
-from pypnnomenclature.admin import (
-    BibNomenclaturesTypesAdminConfig,
-    BibNomenclaturesTypesAdmin,
-    TNomenclaturesAdminConfig,
-    TNomenclaturesAdmin,
-)
 
 admin = Admin(
     current_app,
     name="Backoffice d'administration de GeoNature",
     template_mode="bootstrap3",
     url="/admin",
-)
-
-admin.add_view(
-    BibNomenclaturesTypesAdminConfig(
-        BibNomenclaturesTypesAdmin, DB.session, name="Type de nomenclatures"
-    ),
-    category="Nomenclatures",
-)
-
-admin.add_view(
-    TNomenclaturesAdminConfig(
-        TNomenclaturesAdmin, DB.session, name="Items de nomenclatures"
-    ),
-    category="Nomenclatures",
 )
