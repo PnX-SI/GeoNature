c2209dd80af25cb5a252354a4ff2090ff09825c2
amandine.sahl <amandine.sahl@gmail.com>
2019-08-02 11:59:08 +0200

    Modification contraintes de check sur la sensibilité : NOT VALID

--- data/migrations/2.1.2to2.1.3.sql
+++ data/migrations/2.1.2to2.1.3.sql
@@ -4,3 +4,14 @@ ALTER TABLE gn_synthese.cor_area_synthese
   ADD CONSTRAINT fk_cor_area_synthese_id_synthese FOREIGN KEY (id_synthese)
       REFERENCES gn_synthese.synthese (id_synthese) MATCH SIMPLE
       ON UPDATE CASCADE ON DELETE CASCADE;
+
+-- Correction application différé des contraintes de check sur la nomenclature
+ALTER TABLE gn_sensitivity.cor_sensitivity_synthese DROP CONSTRAINT check_synthese_sensitivity;
+
+ALTER TABLE gn_sensitivity.cor_sensitivity_synthese
+  ADD CONSTRAINT check_synthese_sensitivity CHECK (ref_nomenclatures.check_nomenclature_type_by_mnemonique(id_nomenclature_sensitivity, 'SENSIBILITE'::character varying)) NOT VALID;
+
+ALTER TABLE gn_sensitivity.t_sensitivity_rules DROP CONSTRAINT check_t_sensitivity_rules_niv_precis;
+
+ALTER TABLE gn_sensitivity.t_sensitivity_rules
+  ADD CONSTRAINT check_t_sensitivity_rules_niv_precis CHECK (ref_nomenclatures.check_nomenclature_type_by_mnemonique(id_nomenclature_sensitivity, 'SENSIBILITE'::character varying)) NOT VALID;
