ba71b9794c1a9219f4a6a902f0e4878d5f4f7a6d
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2019-09-04 17:03:28 +0200

    centralidation du service dynamicform

--- frontend/src/app/GN2CommonModule/form/dynamic-form-generator/dynamic-form.service.ts
+++ frontend/src/app/GN2CommonModule/form/dynamic-form-generator/dynamic-form.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { arrayMinLengthValidator } from '@geonature/services/validators/validators';
 
 @Injectable()
 export class DynamicFormService {
   constructor() {}
 
   toFormGroup(formsDef: Array<any>) {
-    let group: any = {};
+    const group: any = {};
     formsDef.forEach(form => {
-      group[form.nom_attribut] = this.createControl(form);
+      group[form.attribut_name] = this.createControl(form);
     });
     return new FormGroup(group);
   }
@@ -16,9 +17,10 @@ export class DynamicFormService {
   createControl(formDef): AbstractControl {
     let abstractForm;
     if (formDef.type_widget === 'checkbox') {
-      abstractForm = formDef.required
-        ? new FormControl(null, Validators.required)
-        : new FormControl(null);
+      abstractForm = new FormControl(
+        new Array(),
+        formDef.required ? arrayMinLengthValidator(1) : null
+      );
     } else {
       abstractForm = formDef.required
         ? new FormControl(formDef.value || null, Validators.required)
