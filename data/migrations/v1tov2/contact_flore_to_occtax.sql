45b7fc1407655a6810a6af554ecbfd63c4dbf4b3
gildeluermoz <gil.deluermoz@ecrins-parcnational.fr>
2019-09-09 12:32:33 +0200

    MIGRATION - correction des séquences occtax

--- data/migrations/v1tov2/contact_flore_to_occtax.sql
+++ data/migrations/v1tov2/contact_flore_to_occtax.sql
@@ -233,6 +233,11 @@ id_cflore AS id_releve_occtax,
 id_role AS id_role
 FROM v1_compat.vm_cor_role_fiche_flore;
 
+-- mettre à jour les serial
+SELECT pg_catalog.setval('pr_occtax.t_occurrences_occtax_id_occurrence_occtax_seq', (SELECT max(id_occurrence_occtax)+1 FROM pr_occtax.t_occurrences_occtax), true);
+SELECT pg_catalog.setval('pr_occtax.t_releves_occtax_id_releve_occtax_seq', (SELECT max(id_releve_occtax)+1 FROM pr_occtax.t_releves_occtax), true);
+SELECT pg_catalog.setval('pr_occtax.cor_counting_occtax_id_counting_occtax_seq', (SELECT max(id_counting_occtax)+1 FROM pr_occtax.cor_counting_occtax), true);
+
 -- Suppression des VM
 DROP MATERIALIZED VIEW v1_compat.vm_t_fiches_cflore;
 DROP MATERIALIZED VIEW v1_compat.vm_t_releves_cflore;
