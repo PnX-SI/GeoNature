89ce53bf1587b58ccefd3f5ecd17ae3f2ccfea90
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2019-10-17 15:20:36 +0200

    1er map-list

--- contrib/gn_module_occ_hab/frontend/app/components/occhab-map-list.component.ts
+++ contrib/gn_module_occ_hab/frontend/app/components/occhab-map-list.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from "@angular/core";
 import { OcchabStoreService } from "../services/store.service";
+import { MapListService } from "@geonature_common/map-list/map-list.service";
+import { OccHabDataService } from "../services/data.service";
 
 @Component({
   selector: "pnx-occhab-map-list",
   templateUrl: "occhab-map-list.component.html"
 })
 export class OccHabMapListComponent implements OnInit {
-  constructor(public storeService: OcchabStoreService) {}
-  ngOnInit() {}
+  public displayedColumns = [
+    { name: "ID", prop: "id_station" },
+    { name: "Date", prop: "date_min" }
+  ];
+  // public tabColumns = this.displayedColumns.map(col => col.prop);
+  constructor(
+    public storeService: OcchabStoreService,
+    private _occHabDataService: OccHabDataService,
+    public mapListService: MapListService
+  ) {}
+  ngOnInit() {
+    this._occHabDataService.getStations().subscribe(data => {
+      // this.stations = data;
+      this.mapListService.geojsonData = data;
+      this.mapListService.loadTableData(data);
+    });
+  }
+
+  test() {
+    console.log("lAA");
+  }
 }
