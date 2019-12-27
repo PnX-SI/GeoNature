9c75074b53f35b0e02ab7b04ed1d1c3f6d72abc7
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2019-12-27 12:20:48 +0100

    Export synthese: ajout de colonnes et réordonnancement

--- data/core/synthese.sql
+++ data/core/synthese.sql
@@ -1051,6 +1051,9 @@ FROM gn_synthese.cor_area_taxon;
 -- Première version qui reste à affiner/étoffer
 CREATE OR REPLACE VIEW gn_synthese.v_synthese_taxon_for_export_view AS
  SELECT DISTINCT
+    ref.nom_valide,
+    ref.cd_ref,
+    ref.nom_vern,
     ref.group1_inpn,
     ref.group2_inpn,
     ref.regne,
@@ -1058,9 +1061,7 @@ CREATE OR REPLACE VIEW gn_synthese.v_synthese_taxon_for_export_view AS
     ref.classe,
     ref.ordre,
     ref.famille,
-    ref.id_rang,
-    ref.cd_ref,
-    ref.nom_valide
+    ref.id_rang
 FROM gn_synthese.synthese  s
 JOIN taxonomie.taxref t ON s.cd_nom = t.cd_nom
 JOIN taxonomie.taxref ref ON t.cd_ref = ref.cd_nom;
