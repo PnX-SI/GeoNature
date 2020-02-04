a2d697a8aeab9219b68b50e7e2e13a0cdd8203d0
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-02-04 14:49:57 +0100

    Correction commande npm inexistante

--- install/install_app.sh
+++ install/install_app.sh
@@ -145,7 +145,9 @@ echo "Installation de npm"
 cd ../frontend
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
- [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  
+source ~/.bashrc
 nvm install
 nvm use
 
