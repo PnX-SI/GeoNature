70f98c25d84032bc1d64ae07afe0a08e82793dfb
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-03-06 12:25:24 +0100

    commit GN build et gn2Commons build

---
+++ frontend/dist/custom/components/introduction/introduction.component.ts.sample
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { AppConfig } from '@geonature_config/app.config';
+
+@Component({
+  selector: 'pnx-introduction',
+  templateUrl: 'introduction.component.html'
+})
+export class IntroductionComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+
+
+}
---
+++ frontend/projects/gn2-common/src/public-api.ts
@@ -0,0 +1,26 @@
+/*
+ * Public API Surface of gn2-common
+ */
+
+export * from './lib/gn2-common.service';
+export * from './lib/gn2-common.component';
+export * from './lib/gn2-common.module';
+
+export * from './lib/form/acquisition-frameworks/acquisition-frameworks.component'
+export * from './lib/form/multiselect/multiselect.component';
+export * from './lib/form/genericForm.component';
+export * from './lib/form/disable-control.directive';
+export * from './lib/form/areas/areas.component';
+export * from './lib/form/areas-intersected/areas-intersected-modal.component';
+export * from './lib/form/datasets/datasets.component';
+export * from './lib/form/date/date.component';
+export * from './lib/form/dumb-select/dumb-select.component';
+export * from './lib/form/dynamic-form/dynamic-form.component';
+export * from './lib/form/municipalities/municipalities.component';
+export * from './lib/form/nomenclature/nomenclature.component';
+export * from './lib/form/observers/observers.component';
+export * from './lib/form/observers-text/observers-text.component';
+export * from './lib/form/autocomplete/autocomplete.component';
+export * from './lib/form/taxonomy/taxonomy.component';
+export * from './lib/form/dynamic-form-generator/dynamic-form-generator.component';
+
