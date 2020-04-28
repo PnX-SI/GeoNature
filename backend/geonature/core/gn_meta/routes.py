fa69466895531e8c1117c978250aa7c40fca9252
FloVollmer <vollmerflorent@gmail.com>
2020-04-28 17:33:44 +0200

    change dataset_name for dataset_shortname in filename

--- backend/geonature/core/gn_meta/routes.py
+++ backend/geonature/core/gn_meta/routes.py
@@ -255,7 +255,7 @@ def get_export_pdf_dataset(id_dataset, info_role):
 
     filename = 'jdd_{}_{}_{}.pdf'.format(
         id_dataset,
-        df["dataset_name"].replace(" ", "_"),
+        df["dataset_shortname"].replace(" ", "_"),
         dt.datetime.now().strftime("%d%m%Y_%H%M%S")
     )
 
