3bbc9418c3715c5adec728fed5453fc8524b639d
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-06-24 14:56:40 +0200

    correction scripts MAJ departements

--- data/migrations/2.4.0to2.4.1_insert_departments.sh
+++ data/migrations/2.4.0to2.4.1_insert_departments.sh
@@ -8,10 +8,12 @@ GEONATURE_ROOT=$(dirname $(dirname $(dirname $filepath)))
 wget  --cache=off http://geonature.fr/data/ign/departement_admin_express_2020-02.zip -P $GEONATURE_ROOT/tmp/geonature
 
 unzip $GEONATURE_ROOT/tmp/geonature/departement_admin_express_2020-02.zip -d $GEONATURE_ROOT/tmp/geonature
-
-sudo -n -u postgres -s psql -d $db_name -f $GEONATURE_ROOTtmp/geonature/fr_departements.sql &> $GEONATURE_ROOT/var/GEONATURE_ROOT/insert_departements.log
-sudo -n -u postgres -s psql -d $db_name -c "ALTER TABLE ref_geo.temp_fr_departements OWNER TO $user_pg;" &>> $myrootpath/var/log/insert_departements.log
-export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f $GEONATURE_ROOTdata/core/ref_geo_departements.sql &>> $myrootpath/var/log/insert_departements.log
+sudo ls
+sudo -u postgres -s psql -d $db_name -f $GEONATURE_ROOT/tmp/geonature/fr_departements.sql &> $GEONATURE_ROOT/var/log/insert_departements.log
+sudo -u postgres -s psql -d $db_name -c "ALTER TABLE ref_geo.temp_fr_departements OWNER TO $user_pg;" &>> $GEONATURE_ROOT/var/log/insert_departements.log
+export PGPASSWORD=$user_pg_pass;psql -h $db_host -U $user_pg -d $db_name -f $GEONATURE_ROOT/data/core/ref_geo_departements.sql &>> $GEONATURE_ROOT/var/log/insert_departements.log
 
 sudo -n -u postgres -s psql -d $db_name -c "DROP TABLE ref_geo.temp_fr_departements;" &>> $GEONATURE_ROOT/var/log/insert_departements.log
 
+rm $GEONATURE_ROOT/tmp/geonature/departement_admin_express_2020-02.zip
+rm $GEONATURE_ROOT/tmp/geonature/fr_departements.sql
