286668996774d9b6d2e24cf722e4a726224ee882
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2020-06-24 16:29:46 +0200

    Update 2.4.0to2.4.1.sql

--- data/migrations/2.4.0to2.4.1.sql
+++ data/migrations/2.4.0to2.4.1.sql
@@ -1,3 +1,4 @@
+-- Ajout d'une colonne "id_area_attachment" dans la table "gn_synthese.synthese" pour les occurrences sans géométrie précise
 ALTER TABLE gn_synthese.synthese ADD COLUMN id_area_attachment integer;
 ALTER TABLE ONLY gn_synthese.synthese
     ADD CONSTRAINT fk_synthese_id_area_attachment FOREIGN KEY (id_area_attachment) REFERENCES ref_geo.l_areas (id_area) ON UPDATE CASCADE;
@@ -8,14 +9,15 @@ COMMENT ON COLUMN gn_synthese.synthese.id_area_attachment
 ALTER TABLE gn_synthese.synthese
   ADD CONSTRAINT check_synthese_info_geo_type_id_area_attachment CHECK (NOT (ref_nomenclatures.get_cd_nomenclature(id_nomenclature_info_geo_type) = '2'  AND id_area_attachment IS NULL )) NOT VALID;
 
-
--- ajout d'une colonne geojson dans le ref_geo
+-- Ajout d'une colonne "geojson_4326" dans le ref_geo
 ALTER TABLE ref_geo.l_areas 
 ADD COLUMN geojson_4326 character varying;
 
+-- Calcul du champs "geojson_4326" à partir de la géométrie des zonages
 UPDATE ref_geo.l_areas
 SET geojson_4326 = public.ST_asgeojson(public.st_transform(geom, 4326));
 
+-- Création d'une fonction et d'un trigger calulant automatiquement le champs "geojson_4326" à partir de la géométrie des zonages
 CREATE OR REPLACE FUNCTION ref_geo.fct_tri_calculate_geojson() 
    RETURNS trigger AS
   $BODY$
