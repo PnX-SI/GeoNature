74f56938eb07a7fcca0a7290be6fc21bb91def27
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-06-24 10:53:59 +0200

    changement nom colonne + trigger

--- data/migrations/2.4.0to2.4.1.sql
+++ data/migrations/2.4.0to2.4.1.sql
@@ -9,9 +9,26 @@ ALTER TABLE gn_synthese.synthese
   ADD CONSTRAINT check_synthese_info_geo_type_id_area_attachment CHECK (NOT (ref_nomenclatures.get_cd_nomenclature(id_nomenclature_info_geo_type) = '2'  AND id_area_attachment IS NULL )) NOT VALID;
 
 
--- ajout d'une colonne 4326 dans le ref_geo
+-- ajout d'une colonne geojson dans le ref_geo
 ALTER TABLE ref_geo.l_areas 
-ADD COLUMN geojson character varying;
+ADD COLUMN geojson_4326 character varying;
 
 UPDATE ref_geo.l_areas
-SET geojson = public.ST_asgeojson(public.st_transform(geom, 4326));
+SET geojson_4326 = public.ST_asgeojson(public.st_transform(geom, 4326));
+
+CREATE OR REPLACE FUNCTION ref_geo.fct_tri_calculate_geojson() 
+   RETURNS trigger AS
+  $BODY$
+    BEGIN
+      NEW.geojson_4326 = public.ST_asgeojson(public.st_transform(NEW.geom, 4326));
+      RETURN NEW;
+    END;
+  $BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+
+DROP TRIGGER IF EXISTS tri_calculate_geojson ON ref_geo.l_areas;
+CREATE TRIGGER tri_calculate_geojson
+    BEFORE INSERT OR UPDATE OF geojson_4326 ON ref_geo.l_areas
+    FOR EACH ROW
+    EXECUTE PROCEDURE ref_geo.fct_tri_calculate_geojson();
