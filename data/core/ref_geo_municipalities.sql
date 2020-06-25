26859141bce2646641b9e57c4a6d3061337b4485
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-06-25 10:28:55 +0200

    set search path

--- data/core/ref_geo_municipalities.sql
+++ data/core/ref_geo_municipalities.sql
@@ -1,4 +1,4 @@
-SET search_path = ref_geo, pg_catalog;
+SET search_path = ref_geo, pg_catalog, public;
 
 INSERT INTO l_areas (id_type, area_code, area_name, geom, geojson_4326)
 SELECT get_id_area_type('COM') AS id_type, insee_com, nom_com, geom, geojson FROM temp_fr_municipalities
