8bfdc2bbbc9c20672b5d50e9bee313a4d36f9739
jbdesbas <jb.desbas@gmail.com>
2020-07-29 10:06:37 +0200

    fct_trg_meta_dates_change
    Modification de la fonction pour ne plus toucher au champs _meta_create_date_ (auparavant, si celui-ci était NULL il était rempalcé par now())
     #986

--- data/core/public.sql
+++ data/core/public.sql
@@ -15,13 +15,8 @@ CREATE OR REPLACE FUNCTION public.fct_trg_meta_dates_change()
   RETURNS trigger AS
 $BODY$
 begin
-        if(TG_OP = 'INSERT') THEN
-                NEW.meta_create_date = NOW();
-        ELSIF(TG_OP = 'UPDATE') THEN
+        IF(TG_OP = 'UPDATE') THEN
                 NEW.meta_update_date = NOW();
-                if(NEW.meta_create_date IS NULL) THEN
-                        NEW.meta_create_date = NOW();
-                END IF;
         end IF;
         return NEW;
 end;
