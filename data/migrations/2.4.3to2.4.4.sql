52b264567210730d6c34b95655cd90d0b708e61c
lpofredc <frederic.cloitre@lpo.fr>
2020-07-30 11:05:22 +0200

    Renforcement de l'unicité sur t_parameters lorsque id_organism is null

--- data/migrations/2.4.3to2.4.4.sql
+++ data/migrations/2.4.3to2.4.4.sql
@@ -80,3 +80,4 @@ COMMENT ON VIEW gn_commons.v_synthese_validation_forwebapp  IS 'Vue utilisée po
 -- Ajout d'une contrainte d'unicité sur la table gn_commons.t_parameters sur le duo de champs id_organism, parameter_name
 
 ALTER TABLE gn_commons.t_parameters ADD CONSTRAINT unique_t_parameters_id_organism_parameter_name UNIQUE (id_organism, parameter_name);
+CREATE UNIQUE INDEX i_unique_t_parameters_parameter_name_with_id_organism_null ON gn_commons.t_parameters (parameter_name) WHERE id_organism IS NULL;
