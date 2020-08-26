2a1c9275869a569f46962e05ea5431faaf54f228
alainlaupinmnhn <67423331+alainlaupinmnhn@users.noreply.github.com>
2020-08-26 13:23:34 +0200

    #787 Ordre menu

--- backend/geonature/core/gn_commons/models.py
+++ backend/geonature/core/gn_commons/models.py
@@ -71,6 +71,7 @@ class TModules(DB.Model):
     active_frontend = DB.Column(DB.Boolean)
     active_backend = DB.Column(DB.Boolean)
     module_doc_url = DB.Column(DB.Unicode)
+    order_number = DB.Column(DB.Integer)
 
 
 @serializable
