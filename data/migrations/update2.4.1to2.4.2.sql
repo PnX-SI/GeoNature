cda24c0c3e6cbb8a7a3360c492d70e075b570f29
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-09-15 16:21:19 +0200

    remove duplicate file

--- data/migrations/update2.4.1to2.4.2.sql
+++ data/migrations/update2.4.1to2.4.2.sql
@@ -18,3 +18,8 @@ ALTER TABLE ONLY gn_commons.t_places
 	
 ALTER TABLE ONLY gn_commons.t_places
   ADD CONSTRAINT fk_t_places_t_roles FOREIGN KEY (id_role) REFERENCES utilisateurs.t_roles(id_role) ON UPDATE CASCADE;
+
+-- Ajout d'une contrainte d'unicité sur la table gn_commons.t_parameters sur le duo de champs id_organism, parameter_name
+
+ALTER TABLE gn_commons.t_parameters ADD CONSTRAINT unique_t_parameters_id_organism_parameter_name UNIQUE (id_organism, parameter_name);
+CREATE UNIQUE INDEX i_unique_t_parameters_parameter_name_with_id_organism_null ON gn_commons.t_parameters (parameter_name) WHERE id_organism IS NULL;
