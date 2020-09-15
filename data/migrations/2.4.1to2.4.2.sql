f85b9ebfe8e8bf6a42600f2bdbc478fd8ac72865
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-09-15 16:26:45 +0200

    typo sql

--- data/migrations/2.4.1to2.4.2.sql
+++ data/migrations/2.4.1to2.4.2.sql
@@ -1301,7 +1301,7 @@ BEGIN
 			        FROM (
 			          SELECT
 			            id_acquisition_framework,
-			            id_nomenclature_objectif,
+			            id_nomenclature_objectif
 			          CASE
 			            WHEN ref_nomenclatures.get_cd_nomenclature(id_nomenclature_objectif) IN ('OLD_1', 'OLD_2', 'OLD_3', 'OLD_6') 
 			              THEN ref_nomenclatures.get_id_nomenclature('CA_OBJECTIFS', '8'),
@@ -1324,3 +1324,27 @@ END $$;
 
 DELETE FROM gn_meta.cor_acquisition_framework_objectif
 WHERE ref_nomenclatures.get_cd_nomenclature(id_nomenclature_objectif) IN ('OLD_1', 'OLD_2', 'OLD_3', 'OLD_4','OLD_5','OLD_6','OLD_7');
+
+
+
+
+
+/*MET 14/09/2020 Table t_places pour la fonctionnalité mes-lieux*/
+CREATE TABLE gn_commons.t_places
+(
+    id_place serial,
+    id_role integer NOT NULL,
+    place_name character varying(100),
+	place_geom geometry
+);
+
+COMMENT ON COLUMN gn_commons.t_places.id_place IS 'Clé primaire autoincrémente de la table t_places';
+COMMENT ON COLUMN gn_commons.t_places.id_role IS 'Clé étrangère vers la table utilisateurs.t_roles, chaque lieu est associé à un utilisateur';
+COMMENT ON COLUMN gn_commons.t_places.place_name IS 'Nom du lieu';
+COMMENT ON COLUMN gn_commons.t_places.place_geom IS 'Géométrie du lieu';
+
+ALTER TABLE ONLY gn_commons.t_places
+    ADD CONSTRAINT pk_t_places PRIMARY KEY (id_place);
+
+ALTER TABLE ONLY gn_commons.t_places
+  ADD CONSTRAINT fk_t_places_t_roles FOREIGN KEY (id_role) REFERENCES utilisateurs.t_roles(id_role) ON UPDATE CASCADE; 
