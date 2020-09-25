de265351c2231d3a851e559629ca4a3ef23a7750
flovollmer <vollmerflorent@gmail.com>
2020-09-25 17:54:34 +0200

    fix dataset deletion

--- backend/geonature/core/gn_meta/models.py
+++ backend/geonature/core/gn_meta/models.py
@@ -188,6 +188,14 @@ class CorDatasetTerritory(DB.Model):
     id_protocol = DB.Column(DB.Integer, ForeignKey("ref_nomenclatures.t_nomenclatures.id_nomenclature"))
 
 
+@serializable
+class CorModuleDataset(DB.Model):
+    __tablename__ = "cor_module_dataset"
+    __table_args__ = {"schema": "gn_commons", 'extend_existing': True}
+    id_module = DB.Column(DB.Integer, ForeignKey("gn_commons.t_modules.id_module"), primary_key=True)
+    id_dataset = DB.Column(DB.Integer, ForeignKey("gn_meta.t_datasets.id_dataset"), primary_key=True)
+
+
 class CruvedHelper(DB.Model):
     """
     Classe abstraite permettant d'ajouter des méthodes de
