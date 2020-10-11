440bae7e7633c4a2eac5dcee5dd4ce7bb2800540
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2020-10-11 15:21:34 +0200

    Doc frontend/pnx-taxa: add jsdoc

--- frontend/src/app/GN2CommonModule/form/taxa/taxa.component.ts
+++ frontend/src/app/GN2CommonModule/form/taxa/taxa.component.ts
@@ -3,33 +3,84 @@ import { DataFormService } from '../data-form.service';
 import { FormControl } from '@angular/forms';
 import { CommonService } from '@geonature_common/service/common.service';
 
+/**
+ * Ce composant a le même comportement que pnx-areas.
+ *
+ * Il encapsule [le composant pnx-multiselect]{@link MultiSelectComponent}
+ * et lui passe une liste de noms scientifiques supérieurs à un rang donné.
+ *
+ * La liste de noms scientifiques correspond aux caractères tapés dans la
+ * zone de recherche.
+ *
+ * Au maximum 20 noms sont affichés.
+ *
+ * @example
+ * <pnx-taxa
+ *   label="Mon libellé de champ"
+ *   rank="GN"
+ *   [parentFormControl]="form.controls.taxa"
+ *   [bindAllItem]="true"
+ *   [debounceTime]="400"
+ * >
+ * </pnx-taxa>
+ */
 @Component({
   selector: 'pnx-taxa',
   templateUrl: 'taxa.component.html'
 })
 export class TaxaComponent implements OnInit {
+  /** Contient un tableau d'objets. L'attribut "displayName" des objets
+   * sert à l'affichage du nom.
+   */
   public taxa: any;
-  public cachedTaxa: any;
-  // Taxa higher than this rank will be display.
+  /** Rang minimum des noms recherchés.
+   * Utiliser une valeur du champ "id_rang" de la table "bib_taxref_rangs".
+   *
+   * Par défaut, le rang défini correspond au Genre (=GN).
+   */
   @Input() rank: string = 'GN';
+  /** Intitulé du champ. */
   @Input() label: string;
-  @Input() searchBar = false;
+  /** Passer une instance de FormControl afin de pouvoir valider le
+   * formulaire et récupérer la valeur de ce champ.
+   */
   @Input() parentFormControl: FormControl;
+  /** Indique si tout l'objet correspondant à la sélection doit être
+   * passé au formControl (=true) ou seulement la valeur de la propriété
+   * "cd_nom" (=false).
+   *
+   * Par défaut, seul la valeur de "cd_nom" est transmise (=false).
+   */
   @Input() bindAllItem: false;
+  /** Temps en millisecondes avant lancement d'une recherche avec les
+   * caractères saisis dans la zone de recherche.
+   *
+   * Voir le composant [pnx-multiselect]{@link MultiSelectComponent} pour
+   * la valeur par défaut.
+   */
   @Input() debounceTime: number;
 
+  /** @ignore */
   constructor(
     private dataService: DataFormService,
     private commonService: CommonService
   ) {}
 
   ngOnInit() {
-    this.cachedTaxa = [];
     this.taxa = [];
   }
 
+  /** Relance un appel à l'API fournissant les noms scientifiques si le
+   * nombre de caractères saisies dans le zone de recherche est supérieur
+   * ou égal à 2.
+   *
+   * La liste de noms est réinitialisée quand un nom est sélectionné ou
+   * la zone de recherche vidée.
+   *
+   * @param taxon_name La chaine de caractère saisie dans la zone de
+   * recherche.
+  */
   refreshTaxaList(taxon_name) {
-    // Refresh taxa API call only when taxon_name >= 2 characters
     if (taxon_name && taxon_name.length >= 2) {
       this.dataService.getHigherTaxa(this.rank, taxon_name).subscribe(
         data => {
@@ -44,9 +95,8 @@ export class TaxaComponent implements OnInit {
           }
         }
       );
-    // Reset taxa when delete search or select a taxon
     } else if (!taxon_name) {
-      this.taxa = this.cachedTaxa;
+      this.taxa = [];
     }
   }
 }
