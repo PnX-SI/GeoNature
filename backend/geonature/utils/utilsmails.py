1b75b63069aa4a5e221bb10c09a9ee03f213d69c
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2020-10-22 15:13:06 +0200

    Feat backend/utilsmail: improve recipients syntax
    Ref #1098

--- backend/geonature/utils/utilsmails.py
+++ backend/geonature/utils/utilsmails.py
@@ -1,10 +1,10 @@
 # Fonctions génériques permettant l'envoie de mails
+import re
 
 from flask import current_app
 from flask_mail import Message
 
-from server import MAIL
-
+name_address_email_regex = re.compile(r"^([^<]+)<([^>]+)>$", re.IGNORECASE)
 
 def send_mail(recipients, subject, msg_html):
     """
@@ -12,22 +12,46 @@ def send_mail(recipients, subject, msg_html):
 
         .. :quickref:  Generic fonction for sending email
 
-        :query [str] recipients: List of recipients
+        :query str or [str] recipients: Recipients in comma 
+            separated string or in list. Syntax to use a label with email:
+            Label <email@my-domain.dot>
         :query str subject: Subjet of the mail
         :query str msg_html: Mail content in HTML
 
         **Returns:**
         .. void
     """
+    # Import here to permit use of other functions in config schema.
+    from server import MAIL
+
     if not MAIL:
         raise Exception("No configuration for email")
 
     with MAIL.connect() as conn:
-        mail_sender = current_app.config.get('MAIL_DEFAULT_SENDER') 
+        mail_sender = current_app.config.get("MAIL_DEFAULT_SENDER") 
         if not mail_sender:
             mail_sender = current_app.config["MAIL_USERNAME"]
-        msg = Message(subject, sender=mail_sender, recipients=recipients)
+
+        msg = Message(
+            subject,
+            sender=mail_sender,
+            recipients=clean_recipients(recipients)
+        )
 
         msg.html = msg_html
 
         conn.send(msg)
+
+
+def clean_recipients(recipients):
+    splited_recipients = recipients if type(recipients) is list else recipients.split(",")
+    trimed_recipients = list(map(str.strip, splited_recipients))
+    return list(map(split_name_address, trimed_recipients))
+
+
+def split_name_address(email):
+    name_address = email
+    match = name_address_email_regex.match(email)
+    if match:
+        name_address=(match.group(1).strip(), match.group(2).strip())
+    return name_address
