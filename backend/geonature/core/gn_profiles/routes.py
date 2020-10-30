0b0d6a79aab04798b736268ce050578ab5b94ff3
DonovanMaillard <donovan.maillard@gmail.com>
2020-10-30 10:25:40 +0100

    récupération branche ludovic

--- backend/geonature/core/gn_profiles/routes.py
+++ backend/geonature/core/gn_profiles/routes.py
@@ -1,6 +1,7 @@
 import json
 
 from flask import Blueprint, request
+from geoalchemy2.shape import to_shape
 from geojson import Feature
 from sqlalchemy.sql import func, text
 from utils_flask_sqla.response import json_resp
@@ -99,3 +100,73 @@ def get_profile(cd_ref):
     if data:
         return data.get_geofeature()
     return None
+    
+@routes.route("/get_observation_score/<cd_ref>", methods=["POST"])
+@json_resp
+def get_observation_score(cd_ref):
+    """ TODO : A Adapter lors de la prochaine version de la table vm_cor_taxon_phenologie """
+
+    filters = request.form
+
+    profile = (
+        DB.session.query(VmValidProfiles).filter(VmValidProfiles.cd_ref == cd_ref).one_or_none()
+    )
+
+    result = {}
+
+    """ Contrôle de la localisation """
+    if "geom" in filters:
+        check_geom = DB.session.query(
+            func.ST_Contains(
+                func.ST_Transform(profile.valid_distribution, 4326),
+                func.ST_SetSRID(func.ST_GeomFromGeoJSON(filters["geom"]), 4326),
+            )
+        ).one_or_none()
+
+        if check_geom is None:
+            result["controle_geom"] = {
+                "code_result": 0,
+                "Commentaire": "Il existe données valides pour ce taxon, mais il n'a jamais été observé dans cette zone.",
+            }
+    """ Contrôle de la localisation """
+
+    """ Contrôle des dates """
+    if "date_min" in filters:
+        q = q.filter(VmValidProfiles.first_valid_data < filters["date_min"]).one_or_none()
+
+    if "date_max" in filters:
+        q = q.filter(VmValidProfiles.last_valid_data > filters["date_max"])
+
+    if q.count() > 0:
+        result["controle_date"] = {
+            "code_result": 1,
+            "Commentaire": "Dans ce secteur, le taxon a déjà été observé à cette période",
+        }
+    else:
+        result["controle_date"] = {
+            "code_result": 0,
+            "Commentaire": "Dans ce secteur, le taxon n'a jamais été observé à cette période",
+        }
+    """ FIN Contrôle des dates """
+
+    """ Contrôle de l'altitude """
+    if "altitude_min" in filters:
+        q = q.filter(VmValidProfiles.altitude_min < filters["altitude_min"]).one_or_none()
+
+    if "altitude_max" in filters:
+        q = q.filter(VmValidProfiles.altitude_max > filters["altitude_max"]).one_or_none()
+
+    if q.count() > 0:
+        result["controle_altitude"] = {
+            "code_result": 1,
+            "Commentaire": "Dans ce secteur et à cette période, le taxon a déjà été observé à cette altitude",
+        }
+    else:
+        result["controle_altitude"] = {
+            "code_result": 0,
+            "Commentaire": "Dans ce secteur et à cette période, le taxon n'a jamais été observé à cette altitude",
+        }
+    """ FIN Contrôle l'altitude """
+
+    """ TODO : Contrôler le stade de vie """
+    return result
