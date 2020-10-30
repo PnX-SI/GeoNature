3c7ec542be773ab75806d4c2c0aa491d3220765a
DonovanMaillard <donovan.maillard@gmail.com>
2020-10-30 11:38:14 +0100

    Improve routes for occtax alerts

--- backend/geonature/core/gn_profiles/routes.py
+++ backend/geonature/core/gn_profiles/routes.py
@@ -77,100 +77,86 @@ def get_consistancy_data(id_synthese):
 
 @routes.route("/get_observation_score/<cd_ref>", methods=["POST"])
 @json_resp
-def get_observation_score():
-    """ Récupération des valeurs du POST"""
-    """filters = request.form"""
+def get_observation_score(cd_ref):
     filters = request.get_json()
     print(filters)
 
-    if "cd_nom" in filters:
-        """ Récupération du cd_ref """
-        cd_ref = DB.session.query(Taxref).get(filters["cd_nom"]).cd_ref
-
-        """ Récupération du paramètre "période" attribué au taxon """
-        sql = text("""select temporal_precision_days from gn_profiles.get_parameters(:cd_nom)""")
-        try:
-            temporal_precision_days = (
-                DB.engine.execute(sql, cd_nom=filters["cd_nom"]).fetchone().temporal_precision_days
-            )
-        except Exception as e:
-            DB.session.rollback()
-            raise
-
-        """for row in result:
-            temporal_precision_days = row[0]"""
-    else:
-        print("here i am")
+    # Récupération du profil du cd_ref
+    result = {}
+    profile = (
+        DB.session.query(VmValidProfiles).filter(VmValidProfiles.cd_ref == cd_ref).one_or_none()
+    )
+    if not profile:
         return None
 
-    """ Calcul de la période correspondant à la date """
+    # Récupération du paramètre "période" attribué au taxon
+    sql = text("""select temporal_precision_days from gn_profiles.get_parameters(:cd_ref)""")
+    temporal_precision_days = (
+        DB.engine.execute(sql, cd_ref=cd_ref).fetchone().temporal_precision_days
+    )
+
+    # Calcul de la période correspondant à la date
     if "date_min" in filters and "date_max" in filters:
-        """ 1- Création de la date au premier janvier de l'années en cours"""
-        current_year = datetime.datetime.now().year
-        first_january = datetime.datetime(current_year, 1, 1)
-
-        """ 2- Détermination de la période correspondant à date_min"""
         date_min = datetime.datetime.strptime(filters["date_min"], "%Y-%m-%d")
-        min_day_of_year = abs((date_min - first_january).days) + 1
-        min_periode = math.ceil(min_day_of_year / temporal_precision_days)
-
-        """ 3- Détermination de la période correspondant à date_max"""
         date_max = datetime.datetime.strptime(filters["date_max"], "%Y-%m-%d")
-        max_day_of_year = abs((date_max - first_january).days) + 1
-        max_periode = math.ceil(max_day_of_year / temporal_precision_days)
+        # Calcul du numéro du jour pour les dates min et max
+        doy_min = date_min.timetuple().tm_yday
+        doy_max = date_max.timetuple().tm_yday
+        # Si la précision temporelle de la donnée est suffisante, on calcule ses périodes phénologiques
+        if doy_max - doy_min < temporal_precision_days:
+            """ 2- Détermination de la période correspondant à date_min"""
+            min_periode = math.ceil(doy_min / temporal_precision_days)
+            """ 3- Détermination de la période correspondant à date_max"""
+            max_periode = math.ceil(doy_max / temporal_precision_days)
 
-    else:
-        return None
-
-    """ Récupération des altitudes """
+    # Récupération des altitudes
     if "altitude_min" in filters and "altitude_max" in filters:
         altitude_min = filters["altitude_min"]
         altitude_max = filters["altitude_max"]
-    else:
-        return None
 
-    result = {}
-    """ Contrôle de la localisation """
-    q_loc = DB.session.query(VmValidProfiles)
-    q_loc = q_loc.filter(VmValidProfiles.cd_ref == cd_ref)
-
+    # Check de la répartition
     if "geom" in filters:
-        q_loc = q_loc.filter(
-            func.ST_Transform(VmValidProfiles.valid_distribution, 4326).ST_Intersects(
-                func.ST_SetSRID(func.ST_GeomFromGeoJSON(json.dumps(filters["geom"])), 4326)
+        check_geom = DB.session.query(
+            func.ST_Contains(
+                func.ST_Transform(profile.valid_distribution, 4326),
+                func.ST_SetSRID(func.ST_GeomFromGeoJSON(json.dumps(filters["geom"])), 4326),
             )
-        )
+        ).one_or_none()
 
-        if q_loc.count() == 0:
-            result["result"] = {
+        if check_geom[0] is False:
+            result["controle_geom"] = {
                 "code_result": 0,
-                "Commentaire": "Le taxon n'a jamais été observé dans cette zone",
+                "Commentaire": f"Il existe {profile.count_valid_data} données valides pour ce taxon, mais celui-ci n'a jamais été observé dans cette zone.",
             }
+
         else:
-            """ Controle date et altitude """
-            q_pheno = DB.session.query(VmCorTaxonPhenology.id_nomenclature_life_stage).distinct()
-            q_pheno = q_pheno.filter(VmCorTaxonPhenology.cd_ref == cd_ref)
-            q_pheno = q_pheno.filter(VmCorTaxonPhenology.period.between(min_periode, max_periode))
-            q_pheno = q_pheno.filter(VmCorTaxonPhenology.calculated_altitude_min <= altitude_min)
-            q_pheno = q_pheno.filter(VmCorTaxonPhenology.calculated_altitude_max >= altitude_max)
+            result["controle_geom"] = {
+                "code_result": 1,
+                "Commentaire": "Répartition validée",
+            }
 
-            if q_pheno.count() > 0:
-                """Construction de la liste des stade de vie potentielle"""
-                l_lifestage = []
-                for row in q_pheno.all():
-                    l_lifestage.append(row.id_nomenclature_life_stage)
+    """ Controle date et altitude """
+    q_pheno = DB.session.query(VmCorTaxonPhenology.id_nomenclature_life_stage).distinct()
+    q_pheno = q_pheno.filter(VmCorTaxonPhenology.cd_ref == cd_ref)
+    q_pheno = q_pheno.filter(VmCorTaxonPhenology.period.between(min_periode, max_periode))
+    q_pheno = q_pheno.filter(VmCorTaxonPhenology.calculated_altitude_min <= altitude_min)
+    q_pheno = q_pheno.filter(VmCorTaxonPhenology.calculated_altitude_max >= altitude_max)
 
-                result["result"] = {
-                    "code_result": 1,
-                    "Commentaire": "Le taxon a déjà été observé à cette période et à cette altitude",
-                    "l_ids_lifestage": l_lifestage,
-                }
-            else:
-                result["result"] = {
-                    "code_result": 0,
-                    "Commentaire": "Le taxon n'a jamais été observé à cette période ou à cette altitude ou trop peu de données valides permettent de s'assurer de la conformité de cette observation",
-                }
+    if q_pheno.count() > 0:
+        """Construction de la liste des stade de vie potentielle"""
+        l_lifestage = []
+        for row in q_pheno.all():
+            l_lifestage.append(row.id_nomenclature_life_stage)
+
+        result["result"] = {
+            "code_result": 1,
+            "Commentaire": "Le taxon a déjà été observé à cette période et à cette altitude",
+            "l_ids_lifestage": l_lifestage,
+        }
     else:
-        return None
+        result["result"] = {
+            "code_result": 0,
+            "Commentaire": "Le taxon n'a jamais été observé à cette période ou à cette altitude ou trop peu de données valides permettent de s'assurer de la conformité de cette observation",
+        }
 
     return result
