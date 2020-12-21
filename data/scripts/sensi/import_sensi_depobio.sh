453a5178e9b444a3658698a5d0570986af67cecf
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2020-12-21 21:50:09 +0100

    Script Sensibilité DEPOBIO - Ajout de commentaires

--- data/scripts/sensi/import_sensi_depobio.sh
+++ data/scripts/sensi/import_sensi_depobio.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Script remplaçant les règles de sensibilité nationales et régionales intégrées dans GeoNature, par les règles départementales.
+# Utilisé pour DEPOBIO pour disposer des règles de sensibilité à jour, au niveau départemantal.
+
 rootdir=`pwd`/../../../
 . $rootdir/config/settings.ini 
 export PGPASSWORD=$user_pg_pass
@@ -7,8 +10,8 @@ rm /tmp/sensibles_to_inpn_20201218.csv
 wget --cache=off https://geonature.fr/data/inpn/sensitivity/ref_sensibilite_21122020.csv -P /tmp
 psql -h $db_host -U $user_pg -d $db_name -c " DROP TABLE IF EXISTS gn_sensitivity.tmp_rules"
 
+# Creation d'une table temporaire pour stocker les règles de sensibilité départementales
 psql -h $db_host -U $user_pg -d $db_name -c "
-
 CREATE TABLE gn_sensitivity.tmp_rules (
   cd_dept varchar,
   cd_nom integer,
@@ -22,13 +25,13 @@ CREATE TABLE gn_sensitivity.tmp_rules (
 )
 "
 
+# Copie du contenu du fichier CSV dans la table temporaire
 sudo -u postgres -s psql -d $db_name -c "
 COPY gn_sensitivity.tmp_rules FROM '/tmp/ref_sensibilite_21122020.csv' DELIMITER ';' CSV HEADER;
 "
 
-
+# Vider les tables existantes de GeoNature, puis les remplir avec le contenu de la table temporaire
 psql -h $db_host -U $user_pg -d $db_name -c "
-
 delete from gn_sensitivity.cor_sensitivity_area ;
 delete from gn_sensitivity.cor_sensitivity_criteria;
 delete from gn_sensitivity.t_sensitivity_rules;
@@ -39,8 +42,7 @@ cd_nom , nom_cite ,  id_nomenclature_sensitivity,  sensitivity_duration , sensit
 select t.cd_ref_v13, t.nom_cite , ref_nomenclatures.get_id_nomenclature('SENSIBILITE', codage::varchar)  , coalesce(t.duree, 10000) , 'Département', t.cd_dept, null, null, 'Fichier Solene issu BDC', true, null
 from gn_sensitivity.tmp_rules t;
 
-
--- insertion dans cor_area_sensi
+-- insertion dans cor_sensitivity_area
 
 INSERT INTO gn_sensitivity.cor_sensitivity_area
 SELECT DISTINCT id_sensitivity, id_area
