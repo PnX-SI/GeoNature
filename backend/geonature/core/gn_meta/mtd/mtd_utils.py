114b22cb3dbb23ba758df538cc16019836890827
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2021-01-07 11:35:01 +0100

    log added CA

--- backend/geonature/core/gn_meta/mtd/mtd_utils.py
+++ backend/geonature/core/gn_meta/mtd/mtd_utils.py
@@ -270,6 +270,9 @@ def post_jdd_from_user(id_user=None, id_organism=None):
 
 
 def import_all_dataset_af_and_actors():
+    file_handler = logging.FileHandler('/tmp/uuid_ca.txt')
+    file_handler.setLevel(logging.CRITICAL)
+    log.addHandler(file_handler)
     datasets = DB.session.query(TDatasets).all()
     for d in datasets:
         xml_jdd = get_jdd_by_uuid(str(d.unique_dataset_id))
@@ -277,71 +280,75 @@ def import_all_dataset_af_and_actors():
             ds_list = parse_jdd_xml(xml_jdd)
             if ds_list:
                 ds = ds_list[0]
-                # get user info from id_digitizer
                 inpn_user = get_user_from_id_inpn_ws(ds['id_digitizer'])
-                # insert user id digitizer
-                insert_user_and_org(inpn_user)
-                actors = ds.pop("actors")
-                # prevent to not fetch, post or merge the same acquisition framework multiple times
-                new_af = post_acquisition_framework(
-                    uuid=ds["uuid_acquisition_framework"],
-                    id_user=inpn_user.get('id'),
-                    id_organism=inpn_user.get('codeOrganisme'),
-                )
-                # get the id from the uuid
-                ds["id_acquisition_framework"] = new_af['id_acquisition_framework']
-                ds.pop("uuid_acquisition_framework")
-                # get the id of the dataset to check if exists
-                id_dataset = TDatasets.get_id(ds["unique_dataset_id"])
-                ds["id_dataset"] = id_dataset
-                # search nomenclature
-                ds_copy = copy(ds)
-                for key, value in ds_copy.items():
-                    if key.startswith("id_nomenclature"):
-                        if value is not None:
-                            ds[key] = func.ref_nomenclatures.get_id_nomenclature(
-                                NOMENCLATURE_MAPPING.get(key), value
-                            )
-                        else:
-                            ds.pop(key)
-                
-                #  set validable = true
-                ds["validable"] = True
-                dataset = TDatasets(**ds)
-                # if the dataset already exist
-                if id_dataset:
-                    # delete cor_ds_actor
-                    dataset.id_dataset = id_dataset
-
-                    delete_q = CorDatasetActor.__table__.delete().where(
-                        CorDatasetActor.id_dataset == id_dataset
+                # get user info from id_digitizer
+                if inpn_user:
+                    # insert user id digitizer
+                    insert_user_and_org(inpn_user)
+                    actors = ds.pop("actors")
+                    # prevent to not fetch, post or merge the same acquisition framework multiple times
+                    new_af = post_acquisition_framework(
+                        uuid=ds["uuid_acquisition_framework"],
+                        id_user=inpn_user.get('id'),
+                        id_organism=inpn_user.get('codeOrganisme'),
                     )
-                    DB.session.execute(delete_q)
-                    DB.session.commit()
+                    # get the id from the uuid
+                    ds["id_acquisition_framework"] = new_af['id_acquisition_framework']
+                    log.critical(new_af['id_acquisition_framework']+ ",")
+                    ds.pop("uuid_acquisition_framework")
+                    # get the id of the dataset to check if exists
+                    id_dataset = TDatasets.get_id(ds["unique_dataset_id"])
+                    ds["id_dataset"] = id_dataset
+                    # search nomenclature
+                    ds_copy = copy(ds)
+                    for key, value in ds_copy.items():
+                        if key.startswith("id_nomenclature"):
+                            if value is not None:
+                                ds[key] = func.ref_nomenclatures.get_id_nomenclature(
+                                    NOMENCLATURE_MAPPING.get(key), value
+                                )
+                            else:
+                                ds.pop(key)
+                    
+                    #  set validable = true
+                    ds["validable"] = True
+                    dataset = TDatasets(**ds)
+                    # if the dataset already exist
+                    if id_dataset:
+                        # delete cor_ds_actor
+                        dataset.id_dataset = id_dataset
 
-                    # create the correlation links
-                    create_cor_object_actors(actors, dataset)
-                    add_dataset_module(dataset)
-                    DB.session.merge(dataset)
+                        delete_q = CorDatasetActor.__table__.delete().where(
+                            CorDatasetActor.id_dataset == id_dataset
+                        )
+                        DB.session.execute(delete_q)
+                        DB.session.commit()
 
-                # its a new DS
+                        # create the correlation links
+                        create_cor_object_actors(actors, dataset)
+                        add_dataset_module(dataset)
+                        DB.session.merge(dataset)
+
+                    # its a new DS
+                    else:
+                        # set the dataset as activ
+                        dataset.active = True
+                        # create the correlation links
+                        create_cor_object_actors(actors, dataset)
+                        add_dataset_module(dataset)
+                        # Add the new DS
+                        DB.session.add(dataset)
+                    # try to commit
+                    try:
+                        DB.session.commit()
+                    # TODO catch db error ?
+                    except SQLAlchemyError as e:
+                        DB.session.flush()
+                        DB.session.rollback()
+                        error_msg = "Error posting a dataset\nTrace:\n{} \n\n ".format(e)
+                        print(error_msg)
                 else:
-                    # set the dataset as activ
-                    dataset.active = True
-                    # create the correlation links
-                    create_cor_object_actors(actors, dataset)
-                    add_dataset_module(dataset)
-                    # Add the new DS
-                    DB.session.add(dataset)
-                # try to commit
-                try:
-                    DB.session.commit()
-                # TODO catch db error ?
-                except SQLAlchemyError as e:
-                    DB.session.flush()
-                    DB.session.rollback()
-                    error_msg = "Error posting a dataset\nTrace:\n{} \n\n ".format(e)
-                    log.error(error_msg)
+                    print("NO USER FOUND")
             else:
                 "NO JDD IN XML ????"
         else:
