9abe2f799315733f9537b4e09027ed4095125fe7
joel.clement <joelclems@gmail.com>
2021-01-28 12:01:22 +0100

    gn_sensitivity - correction contraintes NOT VALID ref #1245

--- data/migrations/2.5.5to2.6.0.sql
+++ data/migrations/2.5.5to2.6.0.sql
@@ -683,3 +683,14 @@ SELECT cd_nom, id_area, nb_obs, last_date,
   ELSE 'red'
  END as color
 FROM gn_synthese.v_area_taxon;
+
+-- Correction application différé des contraintes de check sur la nomenclature
+ALTER TABLE gn_sensitivity.cor_sensitivity_synthese DROP CONSTRAINT check_synthese_sensitivity;
+
+ALTER TABLE gn_sensitivity.cor_sensitivity_synthese
+  ADD CONSTRAINT check_synthese_sensitivity CHECK (ref_nomenclatures.check_nomenclature_type_by_mnemonique(id_nomenclature_sensitivity, 'SENSIBILITE'::character varying)) NOT VALID;
+
+ALTER TABLE gn_sensitivity.t_sensitivity_rules DROP CONSTRAINT check_t_sensitivity_rules_niv_precis;
+
+ALTER TABLE gn_sensitivity.t_sensitivity_rules
+  ADD CONSTRAINT check_t_sensitivity_rules_niv_precis CHECK (ref_nomenclatures.check_nomenclature_type_by_mnemonique(id_nomenclature_sensitivity, 'SENSIBILITE'::character varying)) NOT VALID;
