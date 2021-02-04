febfc5e6389a4ec1c6f0b7f40b316a30ed3ad9e4
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2021-02-04 17:22:57 +0100

    Update 2.5.5to2.6.0-update-sensitivity.sql

--- data/migrations/2.5.5to2.6.0-update-sensitivity.sql
+++ data/migrations/2.5.5to2.6.0-update-sensitivity.sql
@@ -4,13 +4,14 @@
 -- Par défaut, seulement les règles nationales sont activées (voir table gn_sensitivity.t_sensitivity_rules)
 -- Après le calcul du niveau de sensibilité de chaque observation de la synthèse, leur niveau de diffusion est automatiquement calculé en fonction de leur niveau de sensibilité
 -- Seulement les observations dont le niveau de diffusion est nul voient leur niveau de sensibilité calculé automatiquement, pour ne pas écraser des niveaux de diffusion qui auraient été renseignés manuellement
+-- Voir https://github.com/PnX-SI/GeoNature/issues/413
 
 -- Désactiver trigger de mise à jour de la date de la synthèse, car sinon cela laisse penser que la donnée a été modifiée
 ALTER TABLE gn_synthese.synthese DISABLE TRIGGER tri_meta_dates_change_synthese;
 
 -- Calculer automatiquement le niveau de sensibilité des observations de la synthèse
 -- Attention cela va calculer automatiquement la sensibilité à partir des règles de sensibilité nationales du SINP
---      UNIQUEMENT POUR LES DONNÉES N'AYANT PAS DE SENSIBILITÉ DÉFINIE (c-a-d  id_nomenclature_sensitivity IS NULL)
+-- UNIQUEMENT POUR LES DONNÉES N'AYANT PAS DE SENSIBILITÉ DEJA DÉFINIE (c-a-d id_nomenclature_sensitivity IS NULL)
 
 UPDATE gn_synthese.synthese  SET id_nomenclature_sensitivity =  gn_sensitivity.get_id_nomenclature_sensitivity(
     date_min::date,
@@ -19,12 +20,9 @@ UPDATE gn_synthese.synthese  SET id_nomenclature_sensitivity =  gn_sensitivity.g
     ('{"STATUT_BIO": ' || id_nomenclature_bio_status::text || '}')::jsonb
 ) WHERE NOT id_nomenclature_sensitivity IS NULL;
 
-
-
 -- Calculer automatiquement le niveau de diffusion des observations de la synthèse
--- Attention cela va calculer automatiquement la sensibilité à partir de la sensibilté
---      UNIQUEMENT POUR LES DONNÉES N'AYANT PAS DE NIVRAU DE DIFFUSION DÉFINI (c-a-d  id_diffusion_level IS NULL)
-
+-- Attention cela va calculer automatiquement le niveau de diffusion à partir de la sensibilté
+-- UNIQUEMENT POUR LES DONNÉES N'AYANT PAS DE NIVEAU DE DIFFUSION DEJA DÉFINI (c-a-d id_diffusion_level IS NULL)
 
 WITH cor_diff_sens AS (
 	SELECT DISTINCT ref_nomenclatures.get_id_nomenclature(
@@ -44,6 +42,5 @@ FROM cor_diff_sens c
 WHERE c.id_sens = s.id_nomenclature_sensitivity
 	AND s.id_nomenclature_diffusion_level IS NULL;
 
---
 -- Réactiver le trigger de mise à jour des dates des observations de la synthèse
 ALTER TABLE gn_synthese.synthese ENABLE TRIGGER tri_meta_dates_change_synthese;
