6cd37408a95646a8ac35bdafddddd8c7f12853e5
Camille Monchicourt <camille.monchicourt@ecrins-parcnational.fr>
2021-02-15 17:50:24 +0100

    2.6.2to2.6.3 - Ajout paramètre manquant dans 2.6.0 fresh_install
    TODO @TheoLechemia, ne faire l'insert que si le paramètre n'existe pas déjà

--- data/migrations/2.6.2to2.6.3.sql
+++ data/migrations/2.6.2to2.6.3.sql
@@ -1,9 +1,9 @@
 -- Update script from GeoNature 2.6.2 to 2.6.3
 
 BEGIN;
-   ------------------------------------
-   -- ADD MISSING UNIQUE CONSTRAINTS --
-   ------------------------------------
+   ----------------------------------------------
+   -- REF_GEO - ADD MISSING UNIQUE CONSTRAINTS --
+   ----------------------------------------------
 
    CREATE UNIQUE INDEX IF NOT EXISTS i_unique_l_areas_id_type_area_code ON ref_geo.l_areas (id_type, area_code);
    ALTER TABLE ONLY ref_geo.l_areas DROP CONSTRAINT IF EXISTS unique_l_areas_id_type_area_code;
@@ -14,6 +14,14 @@ BEGIN;
    ALTER TABLE ONLY ref_geo.bib_areas_types
         ADD CONSTRAINT unique_bib_areas_types_type_code UNIQUE (type_code);
     
+
+   -- !!! TODO !!! A ne faire que si le paramètre n'existe pas déjà dans la table...
+   -- Oubli de la 2.6.0 - A faire seulement sur une nouvelle installation faite avec la 2.6.0, 2.6.1 ou 2.6.2
+   -- où il manquait ce paramètre fait en update2.5.5to2.6.0
+   INSERT INTO gn_commons.t_parameters
+   (id_organism, parameter_name, parameter_desc, parameter_value, parameter_extra_value)
+   VALUES(0, 'ref_sensi_version', 'Version du referentiel de sensibilité', 'Referentiel de sensibilite taxref v13 2020', '');
+
 
 COMMIT;
 
