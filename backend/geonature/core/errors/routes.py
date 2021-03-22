990d35cf6a7a7f3be508be9af08dbcaa90d889cc
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2021-03-22 16:33:42 +0100

    refactor of errors handling
    this commit ensure that all errors produce a json with code, name and descriptions keys.

--- backend/geonature/core/errors/routes.py
+++ backend/geonature/core/errors/routes.py
@@ -1,62 +1,12 @@
-import logging
 from urllib.parse import urlparse
 
-from flask import current_app, jsonify, Response, request, json, redirect
-from werkzeug.exceptions import Unauthorized, HTTPException
+from flask import current_app, request, json, redirect
+from werkzeug.exceptions import Unauthorized, InternalServerError, HTTPException
 from werkzeug.urls import url_encode
 
-from pypnusershub.db.tools import InsufficientRightsError
-from sqlalchemy.exc import SQLAlchemyError
 
-from utils_flask_sqla.response import json_resp
-from utils_flask_sqla.errors import UtilsSqlaError
-
-from geonature.utils.env import DB
-from geonature.utils.errors import GeonatureApiError
-
-
-log = logging.getLogger(__name__)
-gunicorn_error_logger = logging.getLogger("gunicorn.error")
-
-
-@current_app.errorhandler(500)
-@json_resp
-def internal_error(error):  # pylint: disable=W0613
-    gunicorn_error_logger.error(error)
-    return {"message": "internal server error"}, 500
-
-
-@current_app.errorhandler(SQLAlchemyError)
-@json_resp
-def sqlalchemy_error(error):  # pylint: disable=W0613
-    gunicorn_error_logger.error(error)
-    return {"message": "internal server error"}, 500
-
-
-@current_app.errorhandler(GeonatureApiError)
-def geonature_api_error(error):
-    gunicorn_error_logger.error(error.to_dict())
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
-
-
-@current_app.errorhandler(UtilsSqlaError)
-def utils_flask_sql_error(error):
-    gunicorn_error_logger.error(error.to_dict())
-    response = jsonify(error.to_dict())
-    response.status_code = error.status_code
-    return response
-
-
-@current_app.errorhandler(InsufficientRightsError)
-def geonature_insuffisant_rights_error(error):
-    gunicorn_error_logger.error(error)
-    response = jsonify(str(error))
-    response.status_code = 403
-    return response
-
-
+# Unauthorized means disconnected
+# (logged but not allowed to perform an action = Forbidden)
 @current_app.errorhandler(Unauthorized)
 def handle_unauthenticated_request(e):
     if request.accept_mimetypes.best == 'application/json':
@@ -82,7 +32,7 @@ def handle_unauthenticated_request(e):
 
 
 @current_app.errorhandler(HTTPException)
-def handle_exception(e):
+def handle_http_exception(e):
     response = e.get_response()
     if request.accept_mimetypes.best == 'application/json':
         response.data = json.dumps({
@@ -92,3 +42,35 @@ def handle_exception(e):
         })
         response.content_type = 'application/json'
     return response
+
+
+@current_app.errorhandler(InternalServerError)
+def handle_internal_server_error(e):
+    # original excepion is set when the InternalServerError have been raised by catched
+    # non-HTTP exception (PROPAGATE_EXCEPTIONS=False, default value when DEBUG=False)
+    original = getattr(e, "original_exception", None)
+    # the original exception may contains sensitive information (e.g. db pwd)
+    # so we return it as description only in debug mode
+    if original and current_app.debug:
+        description = str(original)
+    else:
+        description = e.description
+    response = e.get_response()
+    if request.accept_mimetypes.best == 'application/json':
+        response.data = json.dumps({
+            'code': e.code,
+            'name': e.name,
+            'description': description,
+        })
+    return response
+
+@current_app.errorhandler(Exception)
+def handle_exception(e):
+    if request.accept_mimetypes.best == 'application/json':
+        server_error = InternalServerError(original_exception=e)
+        return handle_internal_server_error(server_error)
+    else:
+        # If PROPAGATE_EXCEPTIONS=True (default value in debug mode),
+        # this will produce a debug page with the full backtrace.
+        # Otherwise, an InternalServerError will be produced by flask.
+        raise e
