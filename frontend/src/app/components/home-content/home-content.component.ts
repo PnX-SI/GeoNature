56dcfeb5f2dee03868cb56a48034da7f04f90b71
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2021-03-24 22:59:08 +0100

    Feat home-content: use local storage for stats
    Close #1320.

--- frontend/src/app/components/home-content/home-content.component.ts
+++ frontend/src/app/components/home-content/home-content.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { AppConfig } from '../../../conf/app.config';
+
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+
 import { MapService } from '@geonature_common/map/map.service';
-import { SideNavService } from '../sidenav-items/sidenav-service';
 import { SyntheseDataService } from '@geonature_common/form/synthese-form/synthese-data.service';
+
+import { AppConfig } from '../../../conf/app.config';
+import { SideNavService } from '../sidenav-items/sidenav-service';
 import { GlobalSubService } from '../../services/global-sub.service';
 import { ModuleService } from '../../services/module.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'pnx-home-content',
@@ -17,16 +22,21 @@ export class HomeContentComponent implements OnInit {
   public appConfig: any;
   public lastObs: any;
   public generalStat: any;
+  public locale: string;
+  public destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(
     private _SideNavService: SideNavService,
     private _syntheseApi: SyntheseDataService,
     private _globalSub: GlobalSubService,
     private _mapService: MapService,
-    private _moduleService: ModuleService
+    private _moduleService: ModuleService,
+    private translateService: TranslateService,
   ) {}
 
   ngOnInit() {
+    this.getI18nLocale();
+
     this._moduleService.moduleSub
     .filter(m => m!== null)
     .subscribe(m => {
@@ -35,10 +45,7 @@ export class HomeContentComponent implements OnInit {
       });
       gn_module.module_label = 'Accueil'
       this._globalSub.currentModuleSubject.next(gn_module);
-      
-      
     })
-    
     this._SideNavService.sidenav.open();
     this.appConfig = AppConfig;
 
@@ -49,7 +56,37 @@ export class HomeContentComponent implements OnInit {
     }
 
     if (AppConfig.FRONTEND.DISPLAY_STAT_BLOC) {
-      // Get general stats
+      this.computeStatsBloc();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
+
+  private computeStatsBloc() {
+    // Get general stats from Local Storage if exists
+    let needToRefreshStats = true;
+    let statsSerialized = localStorage.getItem('homePage.stats');
+    if (statsSerialized && JSON.parse(statsSerialized)) {
+      let stats = JSON.parse(statsSerialized);
+      this.generalStat = stats;
+
+      // Compute refresh need
+      const currentDatetime = new Date();
+      const cacheEndDatetime = new Date(stats.createdDate);
+      const milliSecondsTtl = (AppConfig.FRONTEND.STAT_BLOC_TTL * 1000)
+      const futureTimestamp = cacheEndDatetime.getTime() + milliSecondsTtl;
+      cacheEndDatetime.setTime(futureTimestamp);
+
+      if (currentDatetime.getTime() < cacheEndDatetime.getTime()) {
+        needToRefreshStats = false;
+      }
+    }
+
+    if (needToRefreshStats) {
+      // Get general stats from Server
       this._syntheseApi
         .getSyntheseGeneralStat()
         .map(stat => {
@@ -62,11 +99,22 @@ export class HomeContentComponent implements OnInit {
           }
           return stat;
         })
-        .subscribe(result => {
-          this.generalStat = result;
+        .subscribe(stats => {
+          stats['createdDate'] = new Date().toUTCString();
+          localStorage.setItem('homePage.stats', JSON.stringify(stats));
+          this.generalStat = stats;
         });
     }
+  }
 
+  private getI18nLocale() {
+    this.locale = this.translateService.currentLang;
+    // don't forget to unsubscribe!
+    this.translateService.onLangChange
+      .takeUntil(this.destroy$)
+      .subscribe((langChangeEvent: LangChangeEvent) => {
+        this.locale = langChangeEvent.lang;
+      });
   }
 
   onEachFeature(feature, layer) {
