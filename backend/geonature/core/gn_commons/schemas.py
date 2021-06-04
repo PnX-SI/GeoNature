cab5fb29ffb73e5cb046381f08c9c19c756477ca
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2021-06-04 10:13:54 +0200

    OCCTAX: fix bug on POST with media + reorder media schema in commons

--- backend/geonature/core/gn_commons/schemas.py
+++ backend/geonature/core/gn_commons/schemas.py
@@ -1,5 +1,7 @@
+from marshmallow import pre_load
+
 from geonature.utils.env import MA 
-from geonature.core.gn_commons.models import TModules
+from geonature.core.gn_commons.models import TModules, TMedias
 
 class ModuleSchema(MA.SQLAlchemyAutoSchema):
     class Meta:
@@ -18,3 +20,16 @@ class ModuleSchema(MA.SQLAlchemyAutoSchema):
             "module_doc_url",
             "module_order",
         )
+
+
+class MediaSchema(MA.SQLAlchemyAutoSchema):
+    class Meta:
+        model = TMedias
+        load_instance = True
+        include_fk = True
+
+    @pre_load
+    def make_media(self, data, **kwargs):
+        if data.get("id_media") is None:
+            data.pop("id_media", None)
+        return data
