02be84155fb336ef2fdc08209da25d39a613277d
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2021-06-30 18:09:02 +0200

    correction trigger

--- data/migrations/2.6.2to2.7.0.sql
+++ data/migrations/2.6.2to2.7.0.sql
@@ -337,41 +337,43 @@ BEGIN;
   $BODY$;
 
   CREATE OR REPLACE FUNCTION pr_occtax.fct_tri_synthese_update_occ()
-      RETURNS trigger
-      LANGUAGE 'plpgsql'
-      VOLATILE
-      COST 100
-  AS $BODY$  DECLARE
-    BEGIN
-      UPDATE gn_synthese.synthese SET
-        id_nomenclature_obs_technique = NEW.id_nomenclature_obs_technique,
-        id_nomenclature_bio_condition = NEW.id_nomenclature_bio_condition,
-        id_nomenclature_bio_status = NEW.id_nomenclature_bio_status,
-        id_nomenclature_naturalness = NEW.id_nomenclature_naturalness,
-        id_nomenclature_exist_proof = NEW.id_nomenclature_exist_proof,
-        id_nomenclature_diffusion_level = NEW.id_nomenclature_diffusion_level,
-        id_nomenclature_observation_status = NEW.id_nomenclature_observation_status,
-        id_nomenclature_blurring = NEW.id_nomenclature_blurring,
-        id_nomenclature_source_status = NEW.id_nomenclature_source_status,
-        determiner = NEW.determiner,
-        id_nomenclature_determination_method = NEW.id_nomenclature_determination_method,
-        id_nomenclature_behaviour = id_nomenclature_behaviour,
-        cd_nom = NEW.cd_nom,
-        nom_cite = NEW.nom_cite,
-        meta_v_taxref = NEW.meta_v_taxref,
-        sample_number_proof = NEW.sample_number_proof,
-        digital_proof = NEW.digital_proof,
-        non_digital_proof = NEW.non_digital_proof,
-        comment_description = NEW.comment,
-        last_action = 'U',
-      additional_data = pr_occtax.t_releves_occtax.additional_fields || NEW.additional_fields || pr_occtax.cor_counting_occtax.additional_fields
-    FROM pr_occtax.t_releves_occtax 
-    JOIN pr_occtax.cor_counting_occtax ON NEW.id_occurrence_occtax = pr_occtax.cor_counting_occtax.id_occurrence_occtax
-      WHERE unique_id_sinp = pr_occtax.cor_counting_occtax.unique_id_sinp_occtax;
-    
-      RETURN NULL;
-    END;
-    $BODY$;
+    RETURNS trigger
+AS $BODY$  declare
+    counting RECORD;
+    releve_add_fields jsonb;
+  begin
+	 select * into counting from pr_occtax.cor_counting_occtax c where id_occurrence_occtax = new.id_occurrence_occtax;
+	 select r.additional_fields into releve_add_fields from pr_occtax.t_releves_occtax r where id_releve_occtax = new.id_releve_occtax;
+    UPDATE gn_synthese.synthese SET
+      id_nomenclature_obs_technique = NEW.id_nomenclature_obs_technique,
+      id_nomenclature_bio_condition = NEW.id_nomenclature_bio_condition,
+      id_nomenclature_bio_status = NEW.id_nomenclature_bio_status,
+      id_nomenclature_naturalness = NEW.id_nomenclature_naturalness,
+      id_nomenclature_exist_proof = NEW.id_nomenclature_exist_proof,
+      id_nomenclature_diffusion_level = NEW.id_nomenclature_diffusion_level,
+      id_nomenclature_observation_status = NEW.id_nomenclature_observation_status,
+      id_nomenclature_blurring = NEW.id_nomenclature_blurring,
+      id_nomenclature_source_status = NEW.id_nomenclature_source_status,
+      determiner = NEW.determiner,
+      id_nomenclature_determination_method = NEW.id_nomenclature_determination_method,
+      id_nomenclature_behaviour = id_nomenclature_behaviour,
+      cd_nom = NEW.cd_nom,
+      nom_cite = NEW.nom_cite,
+      meta_v_taxref = NEW.meta_v_taxref,
+      sample_number_proof = NEW.sample_number_proof,
+      digital_proof = NEW.digital_proof,
+      non_digital_proof = NEW.non_digital_proof,
+      comment_description = NEW.comment,
+      last_action = 'U',
+	  --CHAMPS ADDITIONNELS OCCTAX
+	  additional_data =  releve_add_fields || NEW.additional_fields || counting.additional_fields
+    WHERE unique_id_sinp = counting.unique_id_sinp_occtax;
+	
+    RETURN NULL;
+  END;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
 
 CREATE OR REPLACE FUNCTION pr_occtax.fct_tri_synthese_update_releve()
   RETURNS trigger AS
