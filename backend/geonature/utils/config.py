9a1dd093c39eacba4b8a47fba83f87cfcac3850d
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2021-08-02 15:24:43 +0200

    update database ci config

--- backend/geonature/utils/config.py
+++ backend/geonature/utils/config.py
@@ -12,5 +12,6 @@ from geonature.utils.env import DEFAULT_CONFIG_FILE
 config_path = os.environ.get("GEONATURE_CONFIG_FILE", DEFAULT_CONFIG_FILE)
 config_backend = load_and_validate_toml(config_path, GnPySchemaConf)
 config_frontend = load_and_validate_toml(config_path, GnGeneralSchemaConf)
-config_environ = { k: v for k, v in os.environ.items() if k.startswith('GEONATURE_') }
+# Note: v.removeprefix('GEONATURE_') Python 3.9+
+config_environ = { k: v[len('GEONATURE_'):] for k, v in os.environ.items() if k.startswith('GEONATURE_') }
 config = ChainMap(config_environ, config_frontend, config_backend)
