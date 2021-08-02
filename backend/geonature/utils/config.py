02af0561e511c93b8f6234534c0cac5b69375a56
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2021-08-02 15:47:44 +0200

    fix config from environment variable

--- backend/geonature/utils/config.py
+++ backend/geonature/utils/config.py
@@ -13,5 +13,5 @@ config_path = os.environ.get("GEONATURE_CONFIG_FILE", DEFAULT_CONFIG_FILE)
 config_backend = load_and_validate_toml(config_path, GnPySchemaConf)
 config_frontend = load_and_validate_toml(config_path, GnGeneralSchemaConf)
 # Note: v.removeprefix('GEONATURE_') Python 3.9+
-config_environ = { k: v[len('GEONATURE_'):] for k, v in os.environ.items() if k.startswith('GEONATURE_') }
+config_environ = { k[len('GEONATURE_'):]: v for k, v in os.environ.items() if k.startswith('GEONATURE_') }
 config = ChainMap(config_environ, config_frontend, config_backend)
