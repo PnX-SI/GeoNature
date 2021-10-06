15a9693114317eb2bd6426caf80590d21fab7c0e
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2021-10-06 09:35:02 +0200

    create db: set -e

--- install/02_create_db.sh
+++ install/02_create_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-set -o pipefail
+set -eo pipefail
 
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -52,14 +52,14 @@ write_log "Check GeoNature database user '$user_pg' exists…"
 user_pg_exists=$(sudo -u postgres -s psql -t -c "SELECT COUNT(1) FROM pg_catalog.pg_roles WHERE  rolname = '${user_pg}';")
 if [ ${user_pg_exists} -eq 0 ]; then
   write_log "Create GeoNature database user…"
-  sudo -u postgres -s psql -c "CREATE ROLE $user_pg WITH LOGIN PASSWORD '$user_pg_pass';" |& tee -a "${LOG_FILE}" || exit 1
+  sudo -u postgres -s psql -c "CREATE ROLE $user_pg WITH LOGIN PASSWORD '$user_pg_pass';" |& tee -a "${LOG_FILE}"
 fi
 
 write_log "Creating GeoNature database..."
-sudo -u postgres -s createdb -O $user_pg $db_name -T template0 -E UTF-8 -l $my_local |& tee -a "${LOG_FILE}" || exit 1
+sudo -u postgres -s createdb -O $user_pg $db_name -T template0 -E UTF-8 -l $my_local |& tee -a "${LOG_FILE}"
 
 write_log "Adding default PostGIS extension"
-sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS postgis;" |& tee -a "${LOG_FILE}" || exit 1
+sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS postgis;" |& tee -a "${LOG_FILE}"
 
 write_log "Extracting PostGIS version"
 postgis_full_version=$(sudo -u postgres -s psql -d "${db_name}" -c "SELECT PostGIS_Version();")
@@ -71,22 +71,22 @@ write_log "Adding Raster PostGIS extension if necessary"
 postgis_required_version="3.0"
 if [[ "$(printf '%s\n' "${postgis_required_version}" "${postgis_short_version}" | sort -V | head -n1)" = "${postgis_required_version}" ]]; then
     write_log "PostGIS version greater than or equal to ${postgis_required_version} --> adding Raster extension"
-    sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS postgis_raster;" |& tee -a "${LOG_FILE}" || exit 1
+    sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS postgis_raster;" |& tee -a "${LOG_FILE}"
 else
     write_log "PostGIS version lower than ${postgis_required_version} --> do nothing"
 fi
 
 write_log "Adding other use PostgreSQL extensions"
-sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS hstore;" |& tee -a "${LOG_FILE}" || exit 1
-sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog; COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';" |& tee -a "${LOG_FILE}" || exit 1
-sudo -u postgres -s psql -d $db_name -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";' |& tee -a "${LOG_FILE}" || exit 1
-sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA pg_catalog;" |& tee -a "${LOG_FILE}" || exit 1
+sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS hstore;" |& tee -a "${LOG_FILE}"
+sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog; COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';" |& tee -a "${LOG_FILE}"
+sudo -u postgres -s psql -d $db_name -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";' |& tee -a "${LOG_FILE}"
+sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA pg_catalog;" |& tee -a "${LOG_FILE}"
 
 
 # Mise en place de la structure de la BDD et des données permettant son fonctionnement avec l'application
 write_log 'GRANT access to GeoNature user to necessary tables...'
 for table in geometry_columns geography_columns spatial_ref_sys; do
-  sudo -u postgres -s psql -d $db_name -c "GRANT SELECT ON TABLE ${table} TO ${user_pg}" |& tee -a "${LOG_FILE}" || exit 1
+  sudo -u postgres -s psql -d $db_name -c "GRANT SELECT ON TABLE ${table} TO ${user_pg}" |& tee -a "${LOG_FILE}"
 done
 
 
@@ -94,10 +94,6 @@ done
 # Create database schema #
 ##########################
 
-
-function gn_psql() {
-  PGPASSWORD=$user_pg_pass psql -h $db_host -U $user_pg -d $db_name -v ON_ERROR_STOP=ON $* |& tee -a "${LOG_FILE}" || exit 1
-}
 
 for branch in geonature utilisateurs nomenclatures taxonomie nomenclatures_taxonomie habitats ref_geo; do
     geonature db upgrade $branch@head -x data-directory=tmp/ -x local-srid=$srid_local
@@ -107,30 +103,30 @@ done
 if [ "$add_sample_data" = true ];
 then
     write_log "Inserting sample datasets..."
-    geonature db upgrade geonature-samples@head |& tee -a "${LOG_FILE}" || exit 1
+    geonature db upgrade geonature-samples@head |& tee -a "${LOG_FILE}"
 
     write_log "Inserting sample dataset of taxons for taxonomic schema..."
-    geonature db upgrade taxonomie_taxons_example@head |& tee -a "${LOG_FILE}" || exit 1
+    geonature db upgrade taxonomie_taxons_example@head |& tee -a "${LOG_FILE}"
 fi
 
 if [ "$install_sig_layers" = true ];
 then
-    geonature db upgrade ref_geo_fr_departments -x data-directory=tmp |& tee -a "${LOG_FILE}" || exit 1
-    geonature db upgrade ref_geo_fr_municipalities -x data-directory=tmp |& tee -a "${LOG_FILE}" || exit 1
+    geonature db upgrade ref_geo_fr_departments -x data-directory=tmp |& tee -a "${LOG_FILE}"
+    geonature db upgrade ref_geo_fr_municipalities -x data-directory=tmp |& tee -a "${LOG_FILE}"
 fi
 
 if [ "$install_grid_layer" = true ];
 then
-    geonature db upgrade ref_geo_inpn_grids_1 -x data-directory=tmp |& tee -a "${LOG_FILE}" || exit 1
-    geonature db upgrade ref_geo_inpn_grids_5 -x data-directory=tmp |& tee -a "${LOG_FILE}" || exit 1
-    geonature db upgrade ref_geo_inpn_grids_10 -x data-directory=tmp |& tee -a "${LOG_FILE}" || exit 1
+    geonature db upgrade ref_geo_inpn_grids_1 -x data-directory=tmp |& tee -a "${LOG_FILE}"
+    geonature db upgrade ref_geo_inpn_grids_5 -x data-directory=tmp |& tee -a "${LOG_FILE}"
+    geonature db upgrade ref_geo_inpn_grids_10 -x data-directory=tmp |& tee -a "${LOG_FILE}"
 fi
 
 if  [ "$install_default_dem" = true ];
 then
-    geonature db upgrade ign_bd_alti@head -x local-srid=$srid_local -x data-directory=tmp |& tee -a "${LOG_FILE}" || exit 1
+    geonature db upgrade ign_bd_alti@head -x local-srid=$srid_local -x data-directory=tmp |& tee -a "${LOG_FILE}"
     if [ "$vectorise_dem" = true ];
     then
-        geonature db upgrade ign_bd_alti_vector@head -x data-directory=tmp |& tee -a "${LOG_FILE}" || exit 1
+        geonature db upgrade ign_bd_alti_vector@head -x data-directory=tmp |& tee -a "${LOG_FILE}"
     fi
 fi
