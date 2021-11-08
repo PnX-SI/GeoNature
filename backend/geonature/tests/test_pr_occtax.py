54764d1353eaaba27b976cd4d8389622497e646a
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2021-11-08 12:09:20 +0100

    Update tests

--- backend/geonature/tests/test_pr_occtax.py
+++ backend/geonature/tests/test_pr_occtax.py
@@ -5,6 +5,7 @@ from werkzeug.exceptions import Forbidden
 
 from .utils import set_logged_user_cookie
 from .fixtures import *
+from . import temporary_transaction
 
 occtax = pytest.importorskip("occtax")
 
