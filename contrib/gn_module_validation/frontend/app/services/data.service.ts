e126a586bec9fae1d8c1ecd96c8db429593228c2
Antoine Cecchi <antoine.cecchi@mnhn.fr>
2021-12-02 11:48:42 +0100

    Remove unused util library

--- contrib/gn_module_validation/frontend/app/services/data.service.ts
+++ contrib/gn_module_validation/frontend/app/services/data.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
-import { isArray } from "util";
 import { AppConfig } from "@geonature_config/app.config";
 import { CommonService } from "@geonature_common/service/common.service";
 import {Nomenclature} from "@geonature_common/interfaces";
@@ -14,18 +13,6 @@ export class ValidationDataService {
     private _commonService: CommonService
   ) { }
 
-  // TODO REMOVE : Unused
-  buildQueryUrl(params): HttpParams {
-    let queryUrl = new HttpParams();
-    for (let key in params) {
-      if (isArray(params[key])) {
-        queryUrl = queryUrl.set(key, params[key]);
-      } else {
-        queryUrl = queryUrl.set(key, params[key]);
-      }
-    }
-    return queryUrl;
-  }
 
 
   getSyntheseData(params) {
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -28,5 +28,6 @@ export class AppComponent implements OnInit {
     translate.use(AppConfig.DEFAULT_LANGUAGE);
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+  }
 }
