b608dec0825ba45fb86690729a924954aa7998d6
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2021-12-15 14:21:36 +0100

    message on data displayed in taxa sheet

--- frontend/src/app/syntheseModule/taxon-sheet/taxon-sheet.component.ts
+++ frontend/src/app/syntheseModule/taxon-sheet/taxon-sheet.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataFormService } from '@geonature_common/form/data-form.service';
 import { AppConfig } from '@geonature_config/app.config';
+import { CommonService } from "@geonature_common/service/common.service";
 
 
 @Component({
@@ -15,7 +16,7 @@ export class TaxonSheetComponent implements OnInit {
     public profile: any;
     public profilArea: any;
     public mediaUrl: any;
-    constructor(private _route: ActivatedRoute, private _ds: DataFormService) { }
+    constructor(private _route: ActivatedRoute, private _ds: DataFormService, private _commonService: CommonService) { }
 
     ngOnInit() {
         this._route.params.subscribe(params => {
@@ -26,19 +27,25 @@ export class TaxonSheetComponent implements OnInit {
                     this._ds.getProfile(taxon.cd_ref).subscribe(profil => {
                         this.profile = profil.properties;
                         this.profilArea = profil;
-                        console.log(profil);
                         this._ds.getTaxonAttributsAndMedia(taxon.cd_ref).subscribe(
                             taxonAttrAndMedias => {
                                 const media = taxonAttrAndMedias.medias.find(m => m.id_type == AppConfig.TAXHUB.ID_TYPE_MAIN_PHOTO);
                                 if(media) {
                                     this.mediaUrl = `${AppConfig.API_TAXHUB}tmedias/thumbnail/${media.id_media}?h=300&w300`;
                                 }
-                                // background-image: url(https://taxhub.ecrins-parcnational.fr/api/tmedias/thumbnail/2013?h=150&w=400)
 
                             }
                         )
 
-                    })
+                    },
+                    (errors) => {
+                        console.log(errors);
+                        if(errors.status == 404) {
+                            this._commonService.regularToaster("warning", "Aucune donnée pour ce taxon")
+                        }
+                        
+                    }
+                    )
                 });
             }
         })
