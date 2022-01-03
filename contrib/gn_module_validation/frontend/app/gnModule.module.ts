f36a23a7dc0a1eb2464155511fda02321c5e8855
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2022-01-03 18:03:39 +0100

    feat(i18n): add translations owned by Validation (#1570)
    Co-authored-by: Jean-Pascal Milcent <jp.milcent@cbn-alpin.fr>
    
    Resolve #1570.

--- contrib/gn_module_validation/frontend/app/gnModule.module.ts
+++ contrib/gn_module_validation/frontend/app/gnModule.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { GN2CommonModule } from '@geonature_common/GN2Common.module';
 import { Routes, RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TreeModule } from '@circlon/angular-tree-component';
+import { TranslateService, TranslateModule, TranslateLoader } from '@ngx-translate/core';
 
 import { SharedSyntheseModule } from '@geonature/shared/syntheseSharedModule/synthese-shared.module';
 
@@ -21,6 +23,14 @@ import { SyntheseDataService } from '@geonature_common/form/synthese-form/synthe
 import { SyntheseFormService } from '@geonature_common/form/synthese-form/synthese-form.service';
 import { TaxonAdvancedStoreService } from '@geonature_common/form/synthese-form/advanced-form/synthese-advanced-form-store.service';
 import { ValidationService } from './services/validation.service';
+import { ConfigService } from '@geonature/services/config.service';
+import { CustomTranslateLoader } from '@geonature/shared/translate/custom-loader';
+import { I18nService } from '@geonature/shared/translate/i18n-service';
+
+export function createTranslateLoader(http: HttpClient, config: ConfigService) {
+  return new CustomTranslateLoader(http, config, { moduleName: 'validation' });
+}
+
 // my module routing
 const routes: Routes = [
   { path: '', component: ValidationComponent },
@@ -41,6 +51,14 @@ const routes: Routes = [
     GN2CommonModule,
     SharedSyntheseModule,
     RouterModule.forChild(routes),
+    TranslateModule.forChild({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient, ConfigService],
+      },
+      isolate: true,
+    }),
     CommonModule,
     NgbModule,
     TreeModule,
@@ -56,4 +74,13 @@ const routes: Routes = [
 
   bootstrap: [],
 })
-export class GeonatureModule {}
+export class GeonatureModule {
+  constructor(
+    private translateService: TranslateService,
+    private i18nService: I18nService
+  ) {
+    // Workaround to force translation loaded for LazyModule.
+    // See: https://github.com/ngx-translate/core/issues/1302
+    this.i18nService.initializeModuleTranslateService(this.translateService);
+  }
+}
