aaa6c85fd3aa19c6496372a31bcb3aa844160b78
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2022-01-06 10:21:49 +0100

    exclude altitude null from profile

--- data/core/profiles.sql
+++ data/core/profiles.sql
@@ -264,13 +264,17 @@ WITH excluded_live_stage AS (
      CROSS JOIN LATERAL gn_profiles.get_parameters(s.cd_nom) p(cd_ref, spatial_precision, temporal_precision_days, active_life_stage, distance))
   WHERE ((p.spatial_precision IS NOT NULL) AND (
 	  public.st_maxdistance(public.st_centroid(s.the_geom_local), s.the_geom_local) < (p.spatial_precision)::double precision
-	  ) AND (
+	  )
+    AND s.altitude_max IS NOT NULL AND s.altitude_min IS NOT NULL
+    AND (
 		  s.id_nomenclature_valid_status IN ( SELECT (regexp_split_to_table(t_parameters.value, ','::text))::integer AS regexp_split_to_table
            FROM gn_profiles.t_parameters
           WHERE ((t_parameters.name)::text = 'id_valid_status_for_profiles'::text))
-		) AND ((t.id_rang)::text IN ( SELECT regexp_split_to_table(t_parameters.value, ','::text) AS regexp_split_to_table
+		) 
+    AND ((t.id_rang)::text IN ( SELECT regexp_split_to_table(t_parameters.value, ','::text) AS regexp_split_to_table
            FROM gn_profiles.t_parameters
-          WHERE ((t_parameters.name)::text = 'id_rang_for_profiles'::text))));
+          WHERE ((t_parameters.name)::text = 'id_rang_for_profiles'::text))
+        ));
 
 COMMENT ON VIEW gn_profiles.v_synthese_for_profiles IS 'View containing synthese data feeding profiles calculation.
  cd_ref, date_min, date_max, the_geom_local, altitude_min, altitude_max and
