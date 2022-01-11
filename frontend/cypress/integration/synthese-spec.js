04ca563d745ef25760a857ca9c753ee21c9a4756
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2022-01-11 18:23:16 +0100

    bump ng-select

---
+++ frontend/cypress/integration/synthese-spec.js
@@ -0,0 +1,19 @@
+describe("Geonature connection", () => {
+    beforeEach(() => {
+      cy.visit("http://127.0.0.1:4200");
+      cy.get("#login").type("admin");
+      cy.get("#cdk-step-content-0-0 > form > div:nth-child(2) > input").type(
+        "admin"
+      );
+      cy.get("#cdk-step-content-0-0 > form > button").click();
+    });
+
+    it("should display synthese window", () => {
+        // access to synthese
+        cy.visit("http://127.0.0.1:4200/#/synthese")
+        // check there are elements in the list
+        cy.get("datatable-scroller").children('datatable-row-wrapper');
+        
+    });
+
+});
