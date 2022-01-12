b5cf9d874ff98fd3efc0700cb2566342ff7bc8a9
jbrieuclp <jbrieuclp@gmail.com>
2022-01-12 19:04:48 +0100

    merge fix observers

--- contrib/occtax/frontend/app/occtax-form/releve/releve.service.ts
+++ contrib/occtax/frontend/app/occtax-form/releve/releve.service.ts
@@ -211,7 +211,7 @@ export class OcctaxFormReleveService {
                    additionnalFieldsObservable
                 );
         }),
-        map(([releve, additional_fields]) => {          
+        map(([releve, additional_fields]) => { 
           additional_fields.forEach(field => {
             //Formattage des dates
             if(field.type_widget == "date"){
@@ -233,9 +233,15 @@ export class OcctaxFormReleveService {
         tap(([releve, additional_fields]) => this.additionalFieldsForm = additional_fields),
         //map for return releve data only
         map(([releve, additional_fields]) => releve),
+        //transform Array[observers] to Array[id_role] before patchForm
+        map((releve) => {
+          const releve_data = Object.assign({}, releve);
+          releve_data.observers = releve_data.observers.map(obs => obs.id_role);
+          return releve_data;
+        }),
       )
-      .subscribe((releve) => {            
-        this.propertiesForm.patchValue(releve)
+      .subscribe((releve_data) => {            
+        this.propertiesForm.patchValue(releve_data);
       });
 
 
@@ -438,11 +444,6 @@ export class OcctaxFormReleveService {
     value.properties.date_max = this.dateParser.format(
       value.properties.date_max
     );
-    if (!ModuleConfig.observers_txt) {
-      value.properties.observers = value.properties.observers.map(
-        (observer) => observer.id_role
-      );
-    }
     /* Champs additionnels - formatter les dates et les nomenclatures */
     this.additionalFieldsForm.forEach((fieldForm: any) => {
       if(fieldForm.type_widget == "date"){
