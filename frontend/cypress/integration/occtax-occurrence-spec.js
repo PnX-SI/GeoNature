d603e776314ec60bd5adacde0e2eecae8efb094c
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2022-01-14 09:43:42 +0100

    uncomment test

--- frontend/cypress/integration/occtax-occurrence-spec.js
+++ frontend/cypress/integration/occtax-occurrence-spec.js
@@ -10,28 +10,28 @@ describe("Occtax occurrence creation", () => {
       cy.get("#cdk-step-content-0-0 > form > button").click();
     });
 
-    it("should create occurrence",  async () => {
+    it("should create occurrence", () => {
         cy.visit('http://127.0.0.1:4200/#/occtax/form/1/taxons');
         // check button sumbit is disabled
-        // const submitBtn = cy.get("[data-qa='occurrence-add-btn'][disabled='true']");
-        // // get taxon input which must be as required : .ng-invalid
-        // const taxonInput = cy.get("input[data-qa='taxonomy-form-input'].ng-invalid");
-        // taxonInput.type("canis lupus")
-        // const results = cy.get("ngb-typeahead-window")
-        // const firstTaxon = results.first().click()
-        // console.log(results);
-        // const nomValideResult = cy.get("[data-qa='occurrence-nom-valide']");
-        // nomValideResult.contains("Canis lupus Linnaeus, 1758")
+        const submitBtn = cy.get("[data-qa='occurrence-add-btn'][disabled='true']");
+        // get taxon input which must be as required : .ng-invalid
+        const taxonInput = cy.get("input[data-qa='taxonomy-form-input'].ng-invalid");
+        taxonInput.type("canis lupus")
+        const results = cy.get("ngb-typeahead-window")
+        const firstTaxon = results.first().click()
+        console.log(results);
+        const nomValideResult = cy.get("[data-qa='occurrence-nom-valide']");
+        nomValideResult.contains("Canis lupus Linnaeus, 1758")
 
-        // // check the button add occurrence is focused
-        // cy.focused()
-        // .invoke('attr', 'data-qa')
-        // .should('eq', 'occurrence-add-btn')
+        // check the button add occurrence is focused
+        cy.focused()
+        .invoke('attr', 'data-qa')
+        .should('eq', 'occurrence-add-btn')
 
-        // // chek autocompletion on min/max
-        // const count_min = await promisify(cy.get("[data-qa='counting-count-min']"))
-        // console.log(count_min);
-        // const count_max = await promisify(cy.get("[data-qa='counting-count-max']"))
+        // chek autocompletion on min/max
+        const count_min = cy.get("[data-qa='counting-count-min']")
+        console.log(count_min);
+        const count_max = cy.get("[data-qa='counting-count-max']")
 
 
         });
--- frontend/cypress/integration/occtax-spec.js
+++ frontend/cypress/integration/occtax-spec.js
@@ -9,14 +9,14 @@ describe("Geonature connection", () => {
     });
 
     it("should create relevé", () => {
-        // cy.get(
-        //   '[href="http://127.0.0.1:4200/#/occtax"] > .sidenav-item > .mat-list-item-content > .module-name'
-        // ).click();
-        // cy.get(".button-success > .mat-button-wrapper").click();
-        // cy.get(".leaflet-container").click();
-        // const plus_button = cy.get(
-        //   "body > pnx-root > pnx-nav-home > mat-sidenav-container > mat-sidenav-content > div > div > pnx-occtax-form > div > div > div.occtax-form-content.ng-star-inserted > div > div.col-xl-6.col-lg-7.col-sm-6 > pnx-occtax-form-map > pnx-map > div > div.leaflet-container.leaflet-touch.leaflet-fade-anim.leaflet-grab.leaflet-touch-drag.leaflet-touch-zoom > div.leaflet-control-container > div.leaflet-top.leaflet-right > div.leaflet-control-zoom.leaflet-bar.leaflet-control > a.leaflet-control-zoom-in"
-        // );
+        cy.get(
+          '[href="http://127.0.0.1:4200/#/occtax"] > .sidenav-item > .mat-list-item-content > .module-name'
+        ).click();
+        cy.get(".button-success > .mat-button-wrapper").click();
+        cy.get(".leaflet-container").click();
+        const plus_button = cy.get(
+          "body > pnx-root > pnx-nav-home > mat-sidenav-container > mat-sidenav-content > div > div > pnx-occtax-form > div > div > div.occtax-form-content.ng-star-inserted > div > div.col-xl-6.col-lg-7.col-sm-6 > pnx-occtax-form-map > pnx-map > div > div.leaflet-container.leaflet-touch.leaflet-fade-anim.leaflet-grab.leaflet-touch-drag.leaflet-touch-zoom > div.leaflet-control-container > div.leaflet-top.leaflet-right > div.leaflet-control-zoom.leaflet-bar.leaflet-control > a.leaflet-control-zoom-in"
+        );
       });
 
 });
