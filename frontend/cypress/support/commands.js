a8ee38fbc228c223176b401bf93e13d92c3127b9
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2022-01-19 12:12:52 +0100

    add login/logout utility commands

--- frontend/cypress/support/commands.js
+++ frontend/cypress/support/commands.js
@@ -1,25 +1,17 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("geonatureLogin", () => {
+    cy.visit("/");
+    cy.get("#login").type("admin");
+    cy.get("#cdk-step-content-0-0 > form > div:nth-child(2) > input").type(
+      "admin"
+    );
+    cy.get("#cdk-step-content-0-0 > form > button").click();
+    Cypress.Cookies.defaults({
+        preserve: 'token',
+      })
+});
+
+Cypress.Commands.add("geonatureLogout", () => {
+    cy.clearCookie('token') // clear the 'authId' cookie
+});
+
