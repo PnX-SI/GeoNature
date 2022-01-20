adb0df77fd0ab42a67a5aec0ae576531261a3f08
Antoine Cecchi <antoine.cecchi@mnhn.fr>
2022-01-20 18:25:23 +0100

    Add token id to requests

--- backend/geonature/utils/logs.py
+++ backend/geonature/utils/logs.py
@@ -3,6 +3,14 @@ import warnings
 import smtplib
 import logging
 from logging.handlers import SMTPHandler
+from .request import request_id
+
+
+class RequestIdFilter(logging.Filter):
+
+    def filter(self, record):
+        record.request_id = request_id() if flask.has_request_context() else ''
+        return True
 
 
 def config_loggers(config):
@@ -13,6 +21,7 @@ def config_loggers(config):
     root_logger = logging.getLogger()
     root_logger.addHandler(logging.StreamHandler())
     root_logger.setLevel(config["SERVER"]["LOG_LEVEL"])
+    root_logger.addFilter(RequestIdFilter())
     if config["MAIL_ON_ERROR"] and config["MAIL_CONFIG"]:
         MAIL_CONFIG = config["MAIL_CONFIG"]
         mail_handler = SMTPHandler(
@@ -30,5 +39,3 @@ def config_loggers(config):
     else:
         gunicorn_error_logger = logging.getLogger("gunicorn.error")
         root_logger.handlers.extend(gunicorn_error_logger.handlers)
-
-
