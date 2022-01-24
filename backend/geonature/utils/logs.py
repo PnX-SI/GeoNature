e91f388c2d05edaafbaf1c4de9f069d0e72551b0
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2022-01-24 19:45:00 +0100

    remote SSLSMTPHandler
    pass secure=() to SMTPHandler instead to trigger starttls

--- backend/geonature/utils/logs.py
+++ backend/geonature/utils/logs.py
@@ -5,39 +5,6 @@ import logging
 from logging.handlers import SMTPHandler
 
 
-# custom class to send email in SSL and with non ascii character
-class SSLSMTPHandler(SMTPHandler):
-    """ Custom class to emit email log with SSL """
-
-    def emit(self, record):
-        """
-        Emit a record.
-        """
-        try:
-            from email.mime.text import MIMEText
-            from email.utils import formatdate
-
-            port = self.mailport
-            if not port:
-                port = smtplib.SMTP_PORT
-            smtp = smtplib.SMTP_SSL(self.mailhost, port)
-            msg = self.format(record)
-            message = MIMEText(msg, _charset="utf-8")
-
-            message.add_header("Subject", self.getSubject(record))
-            message.add_header("From", self.fromaddr)
-            message.add_header("To", ",".join(self.toaddrs))
-            message.add_header("Date", formatdate())
-
-            if self.username:
-                smtp.login(self.username, self.password)
-            smtp.sendmail(self.fromaddr, self.toaddrs, message.as_string())
-            smtp.quit()
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except:
-            self.handleError(record)
-
 def config_loggers(config):
     """
         Configuration des niveaux de logging/warnings 
@@ -48,12 +15,13 @@ def config_loggers(config):
     root_logger.setLevel(config["SERVER"]["LOG_LEVEL"])
     if config["MAIL_ON_ERROR"] and config["MAIL_CONFIG"]:
         MAIL_CONFIG = config["MAIL_CONFIG"]
-        mail_handler = SSLSMTPHandler(
+        mail_handler = SMTPHandler(
             mailhost=(MAIL_CONFIG["MAIL_SERVER"], MAIL_CONFIG["MAIL_PORT"]),
             fromaddr=MAIL_CONFIG["MAIL_USERNAME"],
             toaddrs=MAIL_CONFIG["ERROR_MAIL_TO"],
             subject="GeoNature error",
             credentials=(MAIL_CONFIG["MAIL_USERNAME"], MAIL_CONFIG["MAIL_PASSWORD"]),
+            secure=(),
         )
         mail_handler.setLevel(logging.ERROR)
         root_logger.addHandler(mail_handler)
