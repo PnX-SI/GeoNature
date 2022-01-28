ed3f1b7409720e593beee1c28b579d83f9862e38
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2022-01-28 10:53:41 +0100

    Feat pnx-taxa: use ng-select instead of pnx-multiselect

--- frontend/src/app/GN2CommonModule/form/taxa/taxa.component.ts
+++ frontend/src/app/GN2CommonModule/form/taxa/taxa.component.ts
@@ -1,26 +1,45 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { DataFormService } from '../data-form.service';
-import { FormControl } from '@angular/forms';
-import { CommonService } from '@geonature_common/service/common.service';
+
+import { Observable, of, Subject, concat } from 'rxjs';
+import {
+  distinctUntilChanged,
+  debounceTime,
+  switchMap,
+  tap,
+  catchError,
+} from 'rxjs/operators';
+
+import { DataFormService } from '@geonature_common/form/data-form.service';
+import { GenericFormComponent } from '@geonature_common/form/genericForm.component';
+
 
 /**
- * Ce composant a le même comportement que pnx-areas.
+ * Ce composant a le même comportement que {@link AreasComponent|pnx-areas}.
  *
- * Il encapsule [le composant pnx-multiselect]{@link MultiSelectComponent}
- * et lui passe une liste de noms scientifiques supérieurs à un rang donné.
+ * Il encapsule ng-select et utilise un observable contenant une liste de
+ * noms scientifiques supérieurs à un rang donné (`rank`).
  *
  * La liste de noms scientifiques correspond aux caractères tapés dans la
- * zone de recherche.
+ * zone de recherche. L'affichage de la liste ne se déclenche qu'à partir de la
+ * saisie du second caractères. Au maximum 20 noms sont affichés.
  *
- * Au maximum 20 noms sont affichés.
+ * Ce composant retourne une liste de code de noms scientifiques (`cd_nom`)
+ * dans la variable `parentFormControl` correspondant aux sélections de l'utilisateur.
+ *
+ * En mode mise à jour, il est nécessaire de fournir dans `parentFormControl` une liste
+ * d'objets. Ces objets doivent contenir à minima 2 attributs :
+ *  - un attribut correspondant à la valeur de l'input `valueFieldName` et
+ * contenant l'"identifiant" d'un nom.
+ *  - un attribut `displayName` contenant l'intitulé du nom scientifique
+ * à afficher.
+ * Le composant se charge de transformer le contenu de l'input `parentFormControl`
+ * pour qu'il contienne en sortie seulement une liste d'"identifiant" (`valueFieldName`).
  *
  * @example
  * <pnx-taxa
  *   label="Mon libellé de champ"
  *   rank="GN"
  *   [parentFormControl]="form.controls.taxa"
- *   [bindAllItem]="true"
- *   [debounceTime]="400"
  * >
  * </pnx-taxa>
  */
@@ -28,82 +47,72 @@ import { CommonService } from '@geonature_common/service/common.service';
   selector: 'pnx-taxa',
   templateUrl: 'taxa.component.html'
 })
-export class TaxaComponent implements OnInit {
-  /** Contient un tableau d'objets. L'attribut "displayName" des objets
-   * sert à l'affichage du nom.
-   */
-  public taxa: any;
-  /** Rang minimum des noms recherchés.
-   * Utiliser une valeur du champ "id_rang" de la table "bib_taxref_rangs".
-   *
+export class TaxaComponent extends GenericFormComponent implements OnInit {
+  taxa: Observable<any>;
+  /**
+   * Rang minimum des noms recherchés.
+   * Utiliser une valeur du champ "`id_rang`" de la table "`bib_taxref_rangs`".
    * Par défaut, le rang défini correspond au Genre (=GN).
    */
   @Input() rank: string = 'GN';
-  /** Intitulé du champ. */
-  @Input() label: string;
-  /** Passer une instance de FormControl afin de pouvoir valider le
-   * formulaire et récupérer la valeur de ce champ.
+  /**
+   * Nom du champ à utiliser pour déterminer la valeur à utiliser pour le
+   * contenu du `FormControl`.
+   * Si vous souhaitez forcer le maintient dans `parentFormControl` d'un
+   * tableau d'objets comprenant le champ identifiant et le champ pour
+   * l'affichage (`area_name`), vous pouvez l'indiquer en passant une
+   * valeur `null` à cet attribut.
    */
-  @Input() parentFormControl: FormControl;
-  /** Indique si tout l'objet correspondant à la sélection doit être
-   * passé au formControl (=true) ou seulement la valeur de la propriété
-   * "cd_nom" (=false).
-   *
-   * Par défaut, seul la valeur de "cd_nom" est transmise (=false).
+  @Input() valueFieldName: string = 'cd_nom';
+  /**
+   * Fonction de comparaison entre les élements sélectionnés et les éléments
+   * affichés dans la liste des options.
+   * @param item Valeur présente dans la liste d'options.
+   * @param selected Valeur présente dans le `FormControl`.
+   * @returns
    */
-  @Input() bindAllItem: false;
-  /** Temps en millisecondes avant lancement d'une recherche avec les
-   * caractères saisis dans la zone de recherche.
-   *
-   * Voir le composant [pnx-multiselect]{@link MultiSelectComponent} pour
-   * la valeur par défaut.
-   */
-  @Input() debounceTime: number;
-  /** Désactive le composant. */
-  @Input() disabled: boolean = false;
+  @Input() compareWith = (item, selected) => item[this.valueFieldName] === selected;
+  taxaInput$ = new Subject<string>();
+  loading = false;
+  private defaultItems = [];
 
-  /** @ignore */
-  constructor(
-    private dataService: DataFormService,
-    private commonService: CommonService
-  ) {}
+  constructor(private dataService: DataFormService) {
+    super();
+  }
 
   ngOnInit() {
-    this.taxa = [];
+    this.updateParentFormControl();
+    this.getTaxa();
+  }
 
-    // In update mode
+  private updateParentFormControl() {
+    // Replace objects by valueFieldName values
     if (this.parentFormControl.value) {
-      this.taxa = this.parentFormControl.value;
+      this.defaultItems = this.parentFormControl.value;
+      this.parentFormControl.setValue(this.defaultItems.map(item => item[this.valueFieldName]));
     }
   }
 
   /** Relance un appel à l'API fournissant les noms scientifiques si le
    * nombre de caractères saisies dans le zone de recherche est supérieur
    * ou égal à 2.
-   *
-   * La liste de noms est réinitialisée quand un nom est sélectionné ou
-   * la zone de recherche vidée.
-   *
-   * @param taxon_name La chaine de caractère saisie dans la zone de
-   * recherche.
-  */
-  refreshTaxaList(taxon_name) {
-    if (taxon_name && taxon_name.length >= 2) {
-      this.dataService.getHigherTaxa(this.rank, taxon_name).subscribe(
-        data => {
-          this.taxa = data;
-        },
-        err => {
-          if (err.status === 404) {
-            this.taxa = [{ displayName: 'No data to display' }];
-          } else {
-            this.taxa = [];
-            this.commonService.translateToaster('error', 'ErrorMessage');
-          }
-        }
-      );
-    } else if (!taxon_name) {
-      this.taxa = [];
-    }
+   */
+  getTaxa() {
+    this.taxa = concat(
+      of(this.defaultItems), // Load items for update mode
+      this.taxaInput$.pipe(
+        debounceTime(200),
+        distinctUntilChanged(),
+        tap(() => (this.loading = true)),
+        switchMap(term => {
+          return term && term.length >= 2
+            ? this.dataService.getHigherTaxa(this.rank, term).pipe(
+                catchError(() => of([])) // empty list on error
+              )
+            : of([]);
+        }),
+        tap(() => (this.loading = false))
+      )
+    );
   }
 }
