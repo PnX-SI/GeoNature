133e5d1a6a1ebb0354d926454b469f58a0c19927
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2022-01-31 13:59:12 +0100

    Add id_module in t_source

---
+++ backend/geonature/migrations/versions/31f4fab360c1_add_id_module_in_gn_commons_t_source.py
@@ -0,0 +1,44 @@
+"""Add id_module in gn_commons.t_source
+
+Revision ID: 31f4fab360c1
+Revises: 30edd97ae582
+Create Date: 2022-01-31 12:15:41.714849
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '31f4fab360c1'
+down_revision = '30edd97ae582'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute(
+    """
+        ALTER TABLE gn_synthese.t_sources 
+        ADD COLUMN id_module integer;
+        ALTER TABLE gn_synthese.t_sources 
+        ADD CONSTRAINT fk_t_sources_id_module FOREIGN KEY (id_module)
+        REFERENCES gn_commons.t_modules(id_module) ON UPDATE CASCADE ON DELETE CASCADE
+    """
+    )
+    op.execute(
+    """
+        UPDATE gn_synthese.t_sources
+        SET id_module = (SELECT id_module FROM gn_commons.t_modules WHERE module_code = 'OCCTAX')
+        WHERE name_source ILIKE 'Occtax'
+    """
+    )
+
+
+def downgrade():
+    op.execute(
+    """
+        AlTER TABLE gn_synthese.t_sources 
+        DROP COLUMN id_module 
+    """
+    )
