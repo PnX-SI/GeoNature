a32074c2f51978188e5b60c8d9e1f79395fea2de
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2022-02-01 13:55:22 +0100

    fix down revision

--- contrib/occtax/backend/occtax/migrations/86d48653a07b_add_id_module_in_pr_occtax_t_releves.py
+++ contrib/occtax/backend/occtax/migrations/86d48653a07b_add_id_module_in_pr_occtax_t_releves.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '86d48653a07b'
-down_revision = '0d89c5978fa2'
+down_revision = '944072911ff7'
 branch_labels = None
 depends_on = None
 
