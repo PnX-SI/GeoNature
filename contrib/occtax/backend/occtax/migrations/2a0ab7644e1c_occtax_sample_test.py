6ed48db8dccbcce23b1d2a0d933f8eae91ea0569
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2022-02-01 15:57:33 +0100

    update sample data for occtax tests

--- contrib/occtax/backend/occtax/migrations/2a0ab7644e1c_occtax_sample_test.py
+++ contrib/occtax/backend/occtax/migrations/2a0ab7644e1c_occtax_sample_test.py
@@ -23,12 +23,7 @@ depends_on = (
 
 def upgrade():
     operations = importlib.resources.read_text('occtax.migrations.data', 'sample_data_test.sql')
-    # op.execute("SAVEPOINT datasets")
-    # with op.get_bind().begin_nested() as s:
     op.execute(operations)
-        # s.commit()
-        
-    # op.execute("RELEASE SAVEPOINT datasets")
     
 
 def downgrade():
@@ -102,3 +97,24 @@ def downgrade():
     DELETE FROM gn_meta.t_acquisition_frameworks af
     WHERE af.unique_acquisition_framework_id = '57b7d0f2-4183-4b7b-8f08-6e105d476dc5'
     """)
+
+    op.execute(
+        """
+         DELETE FROM gn_synthese.synthese 
+        WHERE id_source = (SELECT id_source FROM gn_synthese.t_sources WHERE id_module = (
+            SELECT id_module FROM gn_commons.t_modules WHERE module_code = 'OCCTAX_DS'
+        )
+        )
+        """
+    )
+    op.execute(
+        """
+         DELETE FROM gn_commons.cor_module_dataset
+         WHERE id_module = (SELECT id_module FROM gn_commons.t_modules WHERE module_code = 'OCCTAX_DS') 
+        """
+    )
+    op.execute(
+        """
+        DELETE FROM gn_commons.t_modules WHERE module_code = 'OCCTAX_DS'
+        """
+    )
