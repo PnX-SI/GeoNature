d67f6cf99aa679d32c5b60cb65fee35fa36f15b6
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2022-02-15 17:59:06 +0100

    Test backend/synthese: fix observation_count_per_column
    Previous test not work when database contains real data in
    gn_synthese.synthese.

--- backend/geonature/tests/test_synthese.py
+++ backend/geonature/tests/test_synthese.py
@@ -106,7 +106,7 @@ class TestSynthese:
 
     def test_get_synthese_data_cruved(self, app, users, synthese_data, datasets):
         set_logged_user_cookie(self.client, users['self_user'])
-        
+
         response = self.client.get(url_for("gn_synthese.get_observations_for_web"), query_string={'limit': 100})
         data = response.get_json()
 
@@ -297,7 +297,7 @@ class TestSynthese:
         response = self.client.get(url_for('gn_synthese.get_observation_count'))
 
         assert response.json >= nb_observations
-    
+
     def test_get_observation_count_id_dataset(self, synthese_data, datasets, unexisted_id):
         id_dataset = datasets['own_dataset'].id_dataset
         nb_observations = len(synthese_data)
@@ -319,7 +319,7 @@ class TestSynthese:
 
         assert response.status_code == 200
         assert response.json["type"] in ["Point", "Polygon"]
-    
+
     def test_get_bbox_id_dataset(self, synthese_data, datasets, unexisted_id):
         id_dataset = datasets['own_dataset'].id_dataset
         # In synthese, all entries are located at the same point
@@ -338,7 +338,7 @@ class TestSynthese:
                                          query_string={'id_dataset': unexisted_id})
         assert response_empty.status_code == 204
         assert response_empty.get_data(as_text=True) == ''
-    
+
     def test_observation_count_per_column(self, synthese_data):
         column_name_dataset = 'id_dataset'
         column_name_cd_nom = 'cd_nom'
@@ -346,16 +346,36 @@ class TestSynthese:
         response_dataset = self.client.get(url_for('gn_synthese.observation_count_per_column', column=column_name_dataset))
         response_cd_nom = self.client.get(url_for('gn_synthese.observation_count_per_column', column=column_name_cd_nom))
 
-        id_datasets = [synt.id_dataset for synt in synthese_data]
-        id_dataset_set = set(id_datasets)
+        datasets_count = {}
+        for synt in synthese_data:
+            if synt.id_dataset not in datasets_count:
+                datasets_count[synt.id_dataset] = {'count': 1, 'id_dataset': synt.id_dataset}
+            else:
+                datasets_count[synt.id_dataset]['count'] += 1
+
+        cd_nom_count = {}
+        for synt in synthese_data:
+            if synt.cd_nom not in cd_nom_count:
+                cd_nom_count[synt.cd_nom] = {'count': 1, 'cd_nom': synt.cd_nom}
+            else:
+                cd_nom_count[synt.cd_nom]['count'] += 1
+
         resp_json = response_dataset.json
-        assert len(resp_json) == len(id_dataset_set)
-        assert [resp['count'] for resp in resp_json] == [id_datasets.count(ds) for ds in id_dataset_set]
-       
+        assert resp_json
+        for test_dataset in datasets_count.values():
+            assert test_dataset['id_dataset'] in [item['id_dataset'] for item in resp_json]
+            for item in resp_json:
+                if item['id_dataset'] == test_dataset['id_dataset']:
+                    assert item['count'] == test_dataset['count']
+
         resp_json = response_cd_nom.json
-        assert len(resp_json) == len(set(synt.cd_nom for synt in synthese_data))
-        assert response_cd_nom.json
-    
+        assert resp_json
+        for test_cd_nom in cd_nom_count.values():
+            assert test_cd_nom['cd_nom'] in [item['cd_nom'] for item in resp_json]
+            for item in resp_json:
+                if item['cd_nom'] == test_cd_nom['cd_nom']:
+                    assert item['count'] >= test_cd_nom['count']
+
     def test_get_autocomplete_taxons_synthese(self, synthese_data):
         seach_name = synthese_data[0].nom_cite
 
@@ -364,4 +384,4 @@ class TestSynthese:
 
         assert response.status_code == 200
         assert response.json[0]['cd_nom'] == synthese_data[0].cd_nom
-        
+
