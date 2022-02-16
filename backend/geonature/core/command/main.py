24cead47d0a11acc14d93bc956f3e2e76015b472
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2022-02-16 18:08:41 +0100

    db status command now support alembic branches

--- backend/geonature/core/command/main.py
+++ backend/geonature/core/command/main.py
@@ -4,6 +4,8 @@
 
 import logging
 from os import environ
+from collections import ChainMap, deque
+from io import StringIO
 
 import click
 from flask.cli import run_command
@@ -198,52 +200,100 @@ def status(directory, x_arg):
     config = migrate.get_config(directory, x_arg)
     script = ScriptDirectory.from_config(config)
     migration_context = MigrationContext.configure(db.session.connection())
+
     current_heads = migration_context.get_current_heads()
-    unknown_heads = []
-    for head in current_heads:
-        try:
-            script.get_revision(head)
-        except alembic.util.exc.CommandError:
-            unknown_heads.append(head)
-    if unknown_heads:
-        current_heads = set()
-    else:
-        current_heads = script.get_all_current(current_heads)
-    outdated = False
+    applied_rev = set(script.iterate_revisions(current_heads, 'base'))
+
     bases = [ script.get_revision(base) for base in script.get_bases() ]
-    for rev in sorted(bases, key=lambda rev: next(iter(rev.branch_labels))):
-        branch, = rev.branch_labels
-        rev_list = [rev]
-        while rev.nextrev:
-            nextrev, = rev.nextrev
-            rev = script.get_revision(nextrev)
-            rev_list.append(rev)
-        enabled_branch = set(rev_list) & current_heads
-        fg = 'white' if enabled_branch else None
-        if enabled_branch:
-            if rev_list[-1] in current_heads:
-                fg = 'white'
-                check = ' ✓'
+    heads = [ script.get_revision(head) for head in script.get_heads() ]
+
+    outdated = False
+    for branch_base in sorted(bases, key=lambda rev: next(iter(rev.branch_labels))):
+        output = StringIO()
+        branch, = branch_base.branch_labels
+        levels = { branch_base: 0 }
+        branch_outdated = False
+        seen = set()
+        todo = deque()
+        todo.append(branch_base)
+        while todo:
+            rev = todo.pop()
+            seen.add(rev)
+            i = levels[rev]
+            if rev.is_branch_point:
+                for j, nextrev in enumerate(rev.nextrev):
+                    nextrev = script.get_revision(nextrev)
+                    if j == 0:
+                        symbol = ' ' * i + '┣'
+                        _i = i
+                        levels[nextrev] = _i
+                    else:
+                        __i = max(levels.values()) + 1
+                        symbol += '━' * (__i-_i-1)
+                        if j == len(rev.nextrev)-1:
+                            symbol += '┓'
+                        else:
+                            symbol += '┳'
+                        _i = __i
+                        levels[nextrev] = _i
+                    todo.append(nextrev)
+            elif rev.is_merge_point:
+                down_revision = { script.get_revision(r) for r in rev.down_revision }
+                if not down_revision.issubset(seen):
+                    continue
+                for j, downrev in enumerate(sorted(down_revision, key=lambda rev: levels[rev])):
+                    if j == 0:
+                        _i = levels[downrev]
+                        symbol = ' ' * _i
+                        if rev in heads:
+                            symbol += '┗'
+                        else:
+                            symbol += '┣'
+                    else:
+                        __i = levels[downrev]
+                        symbol += '━' * (__i-_i-1)
+                        _i = __i
+                        if j == len(rev.down_revision)-1:
+                            symbol += '┛'
+                        else:
+                            symbol += '┻'
             else:
-                fg = 'red'
-                check = ' ×'
+                symbol = ' ' * i
+                if rev in heads and rev in bases:
+                    symbol += '─'
+                elif rev in heads:
+                    symbol += '┸'
+                elif rev in bases:
+                    symbol += '┰'
+                else:
+                    symbol += '┃'
+
+            if not rev.is_branch_point and rev.nextrev:
+                nextrev, = rev.nextrev
+                nextrev = script.get_revision(nextrev)
+                levels[nextrev] = i
+                todo.append(nextrev)
+
+            check = 'x' if rev in applied_rev else ' '
+            if branch_base in applied_rev and rev in applied_rev:
+                fg = 'white'
+            elif branch_base in applied_rev:
                 outdated = True
+                branch_outdated = True
+                fg = 'red'
+            else:
+                fg = None
+            print(click.style(f"  [{check}] {symbol} {rev.revision} {rev.doc}", fg=fg), file=output)
+
+        if branch_base in applied_rev:
+            fg = 'white'
+            mark = ' '
+            mark += click.style('×', fg='red') if branch_outdated else click.style('✓', fg='green')
         else:
             fg = None
-            check = ''
-        click.secho(f"[{branch}{check}]", bold=True, fg=fg)
-        applied = enabled_branch
-        for i, rev in enumerate(rev_list, 1):
-            fg = 'white' if applied else 'red' if enabled_branch else None
-            check = 'x' if applied else ' '
-            click.secho(f"  [{check}] {i:04d} ({rev.revision}) {rev.doc}", fg=fg)
-            if rev in current_heads:
-                applied = False
-    if unknown_heads:
-       click.secho("⚠ UNABLE TO CHECK DATABASE STATUS ⚠", bold=True, fg="red")
-       click.secho("These revisions are present in alembic version table "
-                   "but their script was not found:", bold=True, fg="red")
-       for unknown_head in unknown_heads:
-           click.secho(f"  · {unknown_head}", bold=True, fg="red")
-    elif outdated:
+            mark = ''
+        click.echo(click.style(f"[{branch}", bold=True, fg=fg) + mark + click.style("]", bold=True, fg=fg))
+        click.echo(output.getvalue(), nl=False)
+
+    if outdated:
         click.secho("Some branches are outdated, you can upgrade with: geonature db autoupgrade", fg="red")
