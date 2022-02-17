6cbfe01fb27279702a4ee24caf88ce2b5c932f5b
Antoine Cecchi <antoine.cecchi@mnhn.fr>
2022-02-17 12:01:04 +0100

    Merge geonature alembic branches

---
+++ backend/geonature/migrations/versions/8f136bbcc341_merge_blurring.py
@@ -0,0 +1,24 @@
+"""merge blurring
+
+Revision ID: 8f136bbcc341
+Revises: ac08dcf3f27b
+Create Date: 2022-02-17 10:59:56.115924
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8f136bbcc341'
+down_revision = 'ac08dcf3f27b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
