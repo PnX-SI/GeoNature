70e37d08025bad81d9b16a3353b9035eb21c0612
gaetanbrl <gaetan.bruel@jdev.fr>
2022-03-03 16:28:31 +0100

    First basic UI #1731 - send message and display comments by idSynthese

--- frontend/src/app/shared/discussionCardModule/discussion-card.component.ts
+++ frontend/src/app/shared/discussionCardModule/discussion-card.component.ts
@@ -1,44 +1,82 @@
 import { Component, OnInit, OnChanges, Input } from '@angular/core';
-import { Observable } from 'rxjs';
 import { AppConfig } from '@geonature_config/app.config';
-import { DataFormService } from '@geonature_common/form/data-form.service';
 import { AuthService, User } from '@geonature/components/auth/auth.service';
-import { ModuleService } from '@geonature/services/module.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { GlobalSubService } from "@geonature/services/global-sub.service";
+import { SyntheseDataService } from '@geonature_common/form/synthese-form/synthese-data.service';
+import { CommonService } from "@geonature_common/service/common.service";
+import { pickBy, isEqual, isEmpty } from "lodash";
 
 @Component({
   selector: 'pnx-discussion-card',
   templateUrl: 'discussion-card.component.html',
   styleUrls: ['discussion-card.component.scss']
 })
-
+  
+  
 export class DiscussionCardComponent implements OnInit, OnChanges {
   @Input() idSynthese: number;
-  public comment: any;
+  public commentForm: FormGroup;
+  public open = false;
   public currentUser: User;
-  public cruved: any;
+  public moduleId: number;
+  public discussions: any;
   constructor(
     private _authService: AuthService,
-    private _gnDataService: DataFormService,
-    private _moduleService: ModuleService
-  ) { }
-
-  addComment() {
-    this.comment = {
-      date: new Date().toISOString(),
-      user: 'Jean'
-    }
+    private _formBuilder: FormBuilder,
+    private globalSubService: GlobalSubService,
+    private _commonService: CommonService,
+    private _syntheseDataService: SyntheseDataService
+  ) {
+    this.commentForm = this._formBuilder
+    .group({
+      user: [],
+      content: ['', Validators.required],
+      module: [],
+      item: []
+    });
   }
+
+  handleSubmitComment() {
+    const userInfos = pickBy(this?.currentUser, function(value, key) {
+      return ['id_role', 'prenom_role', 'nom_role'].includes(key);
+    })
+    this.commentForm.get('user').setValue(userInfos);
+    this.commentForm.get('content').setValue({ comment: this.commentForm.get('content').value });
+    this.commentForm.get('item').setValue(this.idSynthese);
+    this._syntheseDataService.createDiscussions(this.commentForm.value).subscribe(data => {
+      this._commonService.regularToaster(
+        "success",
+        "Commentaire sauvegardé !"
+      );
+      this.openCloseComment();
+      this.getAllDiscussions();
+      
+    });
+  }
+
+  openCloseComment() {
+    this.open = !this.open;
+    if (!this.open) this.commentForm.reset();
+  }
+
   ngOnInit() {
-    console.log("INIT");
+    this.open = false;
     this.currentUser = this._authService.getCurrentUser();
-    this.cruved = this._gnDataService.getCruved(['6']);
-    console.log(this.currentUser);
-    console.log(this.cruved);
-    let synthese_module = this._moduleService.getModule('VALIDATION');
-    console.log(synthese_module);
+    this.getAllDiscussions();
+  }
+
+  getAllDiscussions() {
+    this._syntheseDataService.getDiscussions(this.idSynthese).subscribe(data => {
+      this.discussions = data;
+    });
   }
 
   ngOnChanges() {
-    console.log("CHANGES");
+    this.globalSubService.currentModuleSub.subscribe(module => {
+      if (module) {
+        this.commentForm.get('module').setValue(module.id_module);
+      }
+    });
   }
 }
