dc1a5398f834659d41659d40576dba76c4c88063
Adrien-Pajot <pajot.adrien@wanadoo.fr>
2022-03-22 11:52:02 +0100

    test: create test on home page

---
+++ frontend/cypress/integration/homepage-spec.js
@@ -0,0 +1,38 @@
+import promisify from 'cypress-promise';
+
+
+describe("Testing metadata", () => {
+
+  before(() => {
+    cy.geonatureLogout();
+    cy.geonatureLogin();
+  });
+
+  it.only('should close and open the menu', () => {
+    cy.get("#app-toolbar > button.mat-focus-indicator.mat-tooltip-trigger.mat-elevation-z1.mr-2.mat-icon-button.mat-button-base").click()
+    cy.wait(500)
+    cy.get("#app-sidenav").should('have.css', 'visibility').and('match', /hidden/)
+    cy.get("#app-toolbar > button.mat-focus-indicator.mat-tooltip-trigger.mat-elevation-z1.mr-2.mat-icon-button.mat-button-base").click()
+    cy.wait(500)
+    cy.get("#app-sidenav").should('have.css', 'visibility').and('match', /visible/)
+  })
+
+  it('should display synthese page', () => {
+    cy.get('body > pnx-root > pnx-nav-home > mat-sidenav-container > mat-sidenav-content > div > div > pnx-home-content > div > div.row > div.col-6.ng-star-inserted > div > div.panel-heading > button').click({force: true})
+    cy.url().should('include', 'synthese') 
+  })
+
+  it('back to home', () => {
+    cy.get('#app-sidenav > div > pnx-sidenav-items > mat-card').click()
+  })
+
+  it('open documentation', () => {
+    cy.get('#app-toolbar > a').click()
+  })
+
+  it('disconnect', () => {
+    cy.get('#app-toolbar > button.mat-focus-indicator.mat-tooltip-trigger.mx-2.mat-elevation-z1.mat-icon-button.mat-button-base').click()
+    cy.get('#cdk-step-content-0-0 > form > button')
+  })
+
+})
