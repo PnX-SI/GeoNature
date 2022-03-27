233aae9997be59f73baf11ccd29aa0589d1f42b9
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2022-03-27 23:50:23 +0200

    Feat fontend: disable profiles map when data blurring enable

--- frontend/src/app/syntheseModule/taxon-sheet/taxon-sheet.component.ts
+++ frontend/src/app/syntheseModule/taxon-sheet/taxon-sheet.component.ts
@@ -6,49 +6,51 @@ import { CommonService } from "@geonature_common/service/common.service";
 
 
 @Component({
-    selector: 'pnx-taxon-sheet',
-    templateUrl: 'taxon-sheet.component.html',
-    styleUrls: ['taxon-sheet.component.scss']
+  selector: 'pnx-taxon-sheet',
+  templateUrl: 'taxon-sheet.component.html',
+  styleUrls: ['taxon-sheet.component.scss'],
 })
-
 export class TaxonSheetComponent implements OnInit {
-    public taxon: any;
-    public profile: any;
-    public profilArea: any;
-    public mediaUrl: any;
-    constructor(private _route: ActivatedRoute, private _ds: DataFormService, private _commonService: CommonService) { }
+  public taxon: any;
+  public profile: any;
+  public profilArea: any;
+  public mediaUrl: any;
+  public dataBlurringEnable = AppConfig.DATA_BLURRING.ENABLE_DATA_BLURRING;
 
-    ngOnInit() {
-        this._route.params.subscribe(params => {
-            const cdNom = params["cd_nom"];
-            if(cdNom) {
-                this._ds.getTaxonInfo(cdNom).subscribe(taxon => {
-                    this.taxon = taxon;
-                    this._ds.getProfile(taxon.cd_ref).subscribe(profil => {
-                        this.profile = profil.properties;
-                        this.profilArea = profil;
-                        this._ds.getTaxonAttributsAndMedia(taxon.cd_ref).subscribe(
-                            taxonAttrAndMedias => {
-                                const media = taxonAttrAndMedias.medias.find(m => m.id_type == AppConfig["TAXHUB"]["ID_TYPE_MAIN_PHOTO"]);
-                                if(media) {
-                                    this.mediaUrl = `${AppConfig.API_TAXHUB}/tmedias/thumbnail/${media.id_media}?h=300&w300`;
-                                }
+  constructor(
+    private _route: ActivatedRoute,
+    private _ds: DataFormService,
+    private _commonService: CommonService
+  ) {}
 
-                            }
-                        )
-
-                    },
-                    (errors) => {
-                        console.log(errors);
-                        if(errors.status == 404) {
-                            this._commonService.regularToaster("warning", "Aucune donnée pour ce taxon")
-                        }
-
-                    }
-                    )
-                });
+  ngOnInit() {
+    this._route.params.subscribe((params) => {
+      const cdNom = params['cd_nom'];
+      if (cdNom) {
+        this._ds.getTaxonInfo(cdNom).subscribe((taxon) => {
+          this.taxon = taxon;
+          this._ds.getProfile(taxon.cd_ref).subscribe(
+            (profil) => {
+              this.profile = profil.properties;
+              this.profilArea = profil;
+              this._ds.getTaxonAttributsAndMedia(taxon.cd_ref).subscribe((taxonAttrAndMedias) => {
+                const media = taxonAttrAndMedias.medias.find(
+                  (m) => m.id_type == AppConfig['TAXHUB']['ID_TYPE_MAIN_PHOTO']
+                );
+                if (media) {
+                  this.mediaUrl = `${AppConfig.API_TAXHUB}/tmedias/thumbnail/${media.id_media}?h=300&w300`;
+                }
+              });
+            },
+            (errors) => {
+              console.log(errors);
+              if (errors.status == 404) {
+                this._commonService.regularToaster('warning', 'Aucune donnée pour ce taxon');
+              }
             }
-        })
-     }
-
+          );
+        });
+      }
+    });
+  }
 }
