7e657770967a62cb4ffeb98a5d4bb8780d60180b
hypsug0 <fcloitre@dbchiro.org>
2022-04-06 16:25:47 +0200

    fix: reformat code

--- backend/geonature/core/gn_synthese/routes.py
+++ backend/geonature/core/gn_synthese/routes.py
@@ -1182,6 +1182,7 @@ def delete_report(id_report):
 
 
 if config["SYNTHESE"]["LOG_API"]:
+
     @routes.route("/log", methods=["get"])
     @permissions.check_cruved_scope("R", True)
     @json_resp
@@ -1198,7 +1199,7 @@ if config["SYNTHESE"]["LOG_API"]:
         dict
             log action list
         """
-        
+
         limit = request.args.get("limit", default=1000, type=int)
         offset = request.args.get("offset", default=0, type=int)
 
@@ -1210,12 +1211,7 @@ if config["SYNTHESE"]["LOG_API"]:
         filters = {f: args.get(f) for f in args}
 
         query = GenericQuery(
-            DB,
-            'v_log_synthese',
-            'gn_synthese',
-            filters=filters,
-            limit=limit,
-            offset=offset
+            DB, "v_log_synthese", "gn_synthese", filters=filters, limit=limit, offset=offset
         )
 
         data = query.return_query()
