d162cc1012a97d2ca1f5cb1f84a3a4982afdace7
Adrien-Pajot <pajot.adrien@wanadoo.fr>
2022-05-24 17:09:17 +0200

    refactor: search for the jdd to remove

--- frontend/cypress/integration/metadata-spec.js
+++ frontend/cypress/integration/metadata-spec.js
@@ -159,14 +159,15 @@ describe("Testing metadata", () => {
 
 	it('should delete the new "jeux de données"', async () => {
     cy.get('[data-qa="pnx-metadata-search"]').clear()
-    cy.get('[data-qa="pnx-metadata-refresh"]').click()
-    cy.wait(2000)
+    cy.get('[data-qa="pnx-metadata-search"]').type(newJdd.name)
 		cy.get('[data-qa="pnx-metadata-acq-framework-header-0"]').click()
 		const myCadreAcq = await promisify(cy.get('[data-qa="pnx-metadata-acq-framework-header-0"]'))
-    const id = myCadreAcq[0].parentElement.childNodes[1].firstChild.firstChild.childNodes[1].childNodes[2].childNodes[0].innerText
+    const id = myCadreAcq[0].parentElement.childNodes[1].firstChild.firstChild.childNodes[1].childNodes[0].childNodes[0].innerText
 		cy.get('[data-qa="pnx-metadata-jdd-nb-obs-'+id+'"]').contains("0")
 		cy.get('[data-qa="pnx-metadata-jdd-delete-'+id+'"]').click()
 		cy.get('[data-qa="confirmation-dialog-yes"]').click()
+    cy.get('[data-qa="pnx-metadata-search"]').clear()
+    cy.get('[data-qa="pnx-metadata-refresh"]').click()
 	})
 
 	it('should display data of the "cadre d\'acquisition"', async () => {
