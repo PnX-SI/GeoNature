93d7b643ffa6943f664820bbfabe0d55ec9957cd
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2022-08-31 13:53:09 +0200

    make sure db object is unique

--- src/ref_geo/env.py
+++ src/ref_geo/env.py
@@ -5,12 +5,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 db_path = environ.get('FLASK_SQLALCHEMY_DB')
-if db_path:
+if db_path and db_path != f"{__name__}.db":
     db_module_name, db_object_name = db_path.rsplit('.', 1)
     db_module = import_module(db_module_name)
     db = getattr(db_module, db_object_name)
 else:
     db = SQLAlchemy()
+    environ["FLASK_SQLALCHEMY_DB"] = f"{__name__}.db"
 
 
 __all__ = ['db']
