1e0b1292f47b790dabdd9b3f016e74b05e590434
joel.clement <joelclems@gmail.com>
2022-09-26 09:41:04 +0200

    fix/occtax-date-display ref #2005

--- contrib/occtax/frontend/app/occtax-form/occtax-form.service.ts
+++ contrib/occtax/frontend/app/occtax-form/occtax-form.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from "@angular/core";
 import { FormControl } from "@angular/forms";
 import { BehaviorSubject, Observable, of } from "rxjs";
-import { filter, tap, skip, mergeMap, distinctUntilChanged, switchMap } from "rxjs/operators";
+import {
+  filter,
+  tap,
+  skip,
+  mergeMap,
+  distinctUntilChanged,
+  switchMap,
+} from "rxjs/operators";
 
 import { AppConfig } from "@geonature_config/app.config";
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Router } from "@angular/router";
 import { AuthService, User } from "@geonature/components/auth/auth.service";
 import { CommonService } from "@geonature_common/service/common.service";
-import { DataFormService } from "@geonature_common/form/data-form.service"
+import { DataFormService } from "@geonature_common/form/data-form.service";
 
 import { OcctaxDataService } from "../services/occtax-data.service";
 
@@ -24,19 +31,18 @@ export class OcctaxFormService {
   public editionMode: BehaviorSubject<boolean> = new BehaviorSubject(false); // boolean to check if its editionMode
   public chainRecording: boolean = false; // boolean to check if chain the recording is activate
   public stayOnFormInterface = new FormControl(false);
-  public currentIdDataset:any;
+  public currentIdDataset: any;
   public previousReleve = null;
   // public globalOccurrenceAddFields: Array<any>= [];
-  public globalCountingAddFields: Array<any>= [];
+  public globalCountingAddFields: Array<any> = [];
   // public datasetOccurrenceAddFields: Array<any>= [];
-  public datasetCountingAddFields: Array<any>= [];
+  public datasetCountingAddFields: Array<any> = [];
   public idTaxonList: number;
   public currentTab: "releve" | "taxons";
 
   get observerFromCurrentUser() {
-    return 
+    return;
   }
-
 
   constructor(
     private _http: HttpClient,
@@ -44,16 +50,16 @@ export class OcctaxFormService {
     private _auth: AuthService,
     private _commonService: CommonService,
     private _dataS: OcctaxDataService,
-    private dataFormService: DataFormService,
-  ) {        
+    private dataFormService: DataFormService
+  ) {
     this.currentUser = this._auth.getCurrentUser();
 
     //observation de l'URL et recuperation du relevé si édition id !== null
     this.id_releve_occtax
       .pipe(
         skip(1), // skip initilization value (null)
-        tap((id) =>{           
-          if(id == null) {            
+        tap((id) => {
+          if (id == null) {
             this.editionMode.next(false);
           } else {
             this.editionMode.next(true);
@@ -64,19 +70,18 @@ export class OcctaxFormService {
         switchMap((id) => this._dataS.getOneReleve(id))
       )
       .subscribe(
-        (data) => {        
+        (data) => {
           this.occtaxData.next(data);
           // set taxa list
-          if(data.releve.properties.dataset.id_taxa_list) {
+          if (data.releve.properties.dataset.id_taxa_list) {
             this.idTaxonList = data.releve.properties.dataset.id_taxa_list;
           }
-          
         },
         (error) => {
           this._commonService.translateToaster("error", "Releve.DoesNotExist");
           this._router.navigate(["occtax/form"]);
         }
-    );
+      );
   }
 
   getDefaultValues(idOrg?: number, regne?: string, group2_inpn?: string) {
@@ -98,17 +103,21 @@ export class OcctaxFormService {
     );
   }
 
-  getAdditionnalFields(object_code: Array<string>, idDataset?: string): Observable<any> {          
-    return this.dataFormService.getadditionalFields({
-      'id_dataset':  idDataset || "null",
-      'module_code': ['OCCTAX'],
-      'object_code': object_code
-    }).catch((error) => {  
-      console.error("error while get addional fields", error);
-      return of([])
-    })
+  getAdditionnalFields(
+    object_code: Array<string>,
+    idDataset?: string
+  ): Observable<any> {
+    return this.dataFormService
+      .getadditionalFields({
+        id_dataset: idDataset || "null",
+        module_code: ["OCCTAX"],
+        object_code: object_code,
+      })
+      .catch((error) => {
+        console.error("error while get addional fields", error);
+        return of([]);
+      });
   }
-
 
   onEditReleve(id) {
     this._router.navigate(["occtax/form", id]);
@@ -127,12 +136,12 @@ export class OcctaxFormService {
   }
 
   addOccurrenceData(occurrence): void {
-    let occtaxData = this.occtaxData.getValue();    
+    let occtaxData = this.occtaxData.getValue();
 
     if (!occtaxData.releve.properties.t_occurrences_occtax) {
       occtaxData.releve.properties.t_occurrences_occtax = [];
     }
-    occtaxData.releve.properties.t_occurrences_occtax.push(occurrence);    
+    occtaxData.releve.properties.t_occurrences_occtax.push(occurrence);
     this.occtaxData.next(occtaxData);
   }
 
@@ -161,15 +170,14 @@ export class OcctaxFormService {
     this.addOccurrenceData(occurrence);
   }
 
-  replaceReleveData(releve): void {    
+  replaceReleveData(releve): void {
     let occtaxData = this.occtaxData.getValue();
     occtaxData.releve = releve;
     this.occtaxData.next(occtaxData);
   }
 
-
   formatDate(strDate) {
-    const date = new Date(strDate);
+    const date = new Date(strDate + "00:00:00");
     return {
       year: date.getFullYear(),
       month: date.getMonth() + 1,
@@ -178,16 +186,15 @@ export class OcctaxFormService {
   }
 
   clearFormerAdditonnalFields(globalAddFields, formerDatasetAddFields) {
-      // copy object in order to not modify him directly (he is linked to an input)
-      let inter = Object.assign([], globalAddFields);
-      // remove formde dataset additional field from globalAddfield
-      inter = globalAddFields.filter(globalField => {
-        return !formerDatasetAddFields.map(f => f.id_field).includes(globalField.id_field)
-      })
+    // copy object in order to not modify him directly (he is linked to an input)
+    let inter = Object.assign([], globalAddFields);
+    // remove formde dataset additional field from globalAddfield
+    inter = globalAddFields.filter((globalField) => {
+      return !formerDatasetAddFields
+        .map((f) => f.id_field)
+        .includes(globalField.id_field);
+    });
 
-      return inter;
+    return inter;
   }
-
-
-
 }
