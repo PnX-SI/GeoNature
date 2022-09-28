b41cdf6cff925cb8ec135f0d085ef5494d0623e2
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2022-09-28 14:57:07 +0200

    chore(front): remove unused code and logs

--- frontend/src/app/metadataModule/services/metadata.service.ts
+++ frontend/src/app/metadataModule/services/metadata.service.ts
@@ -91,12 +91,6 @@ export class MetadataService {
       tap(() => (this.isLoading = false)),
       map((val) => {
         this._datasetNbObs = val.datasetNbObs;
-        //val: {afs: CA[], datasetNbObs: {id_dataset: number, count: number}[]}
-        //boucle sur les CA pour attribuer le nombre de données au JDD et création de la clé datasetsTemp
-        // for (let i = 0; i < val.afs.length; i++) {
-        //   this.setDsObservationCount(val.afs[i]['t_datasets'], val.datasetNbObs);
-        // }
-        //renvoie uniquement les CA
         return val.afs;
       })
     );
@@ -129,43 +123,12 @@ export class MetadataService {
     );
   }
 
-  /**
-   *  Filtre les éléments CA et JDD selon la valeur de la barre de recherche
-   **/
-  private _filterAcquisitionFrameworks(filterValue) {
-    return this.acquisitionFrameworks.filter((af) => {
-      //recherche des cadres d'acquisition qui matchent
-      if (
-        af.id_acquisition_framework == filterValue ||
-        this._removeAccentAndLower(af.unique_acquisition_framework_id) == filterValue ||
-        this._removeAccentAndLower(af.acquisition_framework_name).includes(filterValue) ||
-        this._removeAccentAndLower(af.acquisition_framework_start_date) == filterValue
-      ) {
-        return true;
-      }
-      //Sinon on filtre les JDD qui matchent eventuellement.
-      if (af.t_datasets) {
-        af.datasetsTemp = af.t_datasets.filter((ds) => {
-          return (
-            ds.id_dataset == filterValue ||
-            this._removeAccentAndLower(ds.dataset_name).includes(filterValue) ||
-            this._removeAccentAndLower(ds.unique_dataset_id) == filterValue ||
-            this._removeAccentAndLower(ds.meta_create_date) == filterValue
-          );
-        });
-        return af.datasetsTemp.length; //On envoie ce test pour garder le CA si un JDD a matché
-      }
-      return false;
-    });
-  }
-
   addDatasetToAcquisitionFramework(id_af: number) {
     // current_af is either the af corresponding to id_af
     // or undefined
     const current_af = this.acquisitionFrameworks.filter(
       (af) => af.id_acquisition_framework === id_af
     )[0];
-    console.log(current_af);
     if (current_af !== undefined && !('datasetsTemp' in current_af)) {
       this.dataFormService
         .getDatasets({ id_acquisition_frameworks: [id_af] })
@@ -173,7 +136,6 @@ export class MetadataService {
         .then((datasets) => {
           this.setDsObservationCount(datasets, this._datasetNbObs);
           current_af.datasetsTemp = datasets;
-          console.log(current_af);
         });
     }
   }
@@ -183,14 +145,6 @@ export class MetadataService {
       let idx = dsNbObs.findIndex((e) => e.id_dataset == ds.id_dataset);
       ds.observation_count = idx > -1 ? dsNbObs[idx]['count'] : 0;
     });
-  }
-
-  private _removeAccentAndLower(value): string {
-    return String(value)
-      .toLocaleLowerCase()
-      .trim()
-      .normalize('NFD')
-      .replace(/[\u0300-\u036f]/g, '');
   }
 
   formatFormValue(formValue) {
