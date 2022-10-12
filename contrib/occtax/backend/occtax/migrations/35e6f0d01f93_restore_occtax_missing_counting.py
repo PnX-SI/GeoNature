b3b41a8b8c9b9e973cd9fa1e4dadc8f37f2651a2
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2022-10-12 15:09:56 +0200

    remove trigger

--- contrib/occtax/backend/occtax/migrations/35e6f0d01f93_restore_occtax_missing_counting.py
+++ contrib/occtax/backend/occtax/migrations/35e6f0d01f93_restore_occtax_missing_counting.py
@@ -7,17 +7,18 @@ Create Date: 2022-01-07 15:46:58.634720
 """
 from alembic import op
 import sqlalchemy as sa
- 
- 
+
+
 # revision identifiers, used by Alembic.
-revision = '35e6f0d01f93'
-down_revision = '944072911ff7'
+revision = "35e6f0d01f93"
+down_revision = "944072911ff7"
 branch_labels = None
 depends_on = None
- 
- 
+
+
 def upgrade():
-    op.execute("""
+    op.execute(
+        """
         WITH
             occ_id (id_occ) as (
                 SELECT DISTINCT o.id_occurrence_occtax
@@ -67,9 +68,11 @@ def upgrade():
             (table_content->>'count_max')::integer,
             (table_content->>'additional_fields')::jsonb
         FROM restauration_occ
-    """)
- 
-    op.execute("""
+    """
+    )
+
+    op.execute(
+        """
         WITH 
         observers (id_releve_occtax, observers) as (
             SELECT r.id_releve_occtax, array_to_string(array_agg(concat_ws(' ', obs.nom_role, obs.prenom_role)), ', ')
@@ -132,172 +135,9 @@ def upgrade():
         INNER JOIN pr_occtax.t_releves_occtax rel ON occ.id_releve_occtax = rel.id_releve_occtax
         LEFT JOIN observers ON rel.id_releve_occtax = observers.id_releve_occtax
         WHERE synthese.unique_id_sinp = counting.unique_id_sinp_occtax AND synthese.cd_nom <> occ.cd_nom;
-    """)
- 
-    op.execute("""
-        DROP FUNCTION IF EXISTS pr_occtax.insert_in_synthese(integer);
- 
-        CREATE OR REPLACE FUNCTION pr_occtax.insert_in_synthese(
-            my_id_counting integer)
-            RETURNS integer[]
-            LANGUAGE 'plpgsql'
-            COST 100
-            VOLATILE PARALLEL UNSAFE
-        AS $BODY$
-          DECLARE
-          new_count RECORD;
-          occurrence RECORD;
-          releve RECORD;
-          id_source integer;
-          id_module integer;
-          id_nomenclature_source_status integer;
-          myobservers RECORD;
-          id_role_loop integer;
- 
-          BEGIN
-          --recupération du counting à partir de son ID
-          SELECT INTO new_count * FROM pr_occtax.cor_counting_occtax WHERE id_counting_occtax = my_id_counting;
- 
-          -- Récupération de l'occurrence
-          SELECT INTO occurrence * FROM pr_occtax.t_occurrences_occtax occ WHERE occ.id_occurrence_occtax = new_count.id_occurrence_occtax;
- 
-          -- Récupération du relevé
-          SELECT INTO releve * FROM pr_occtax.t_releves_occtax rel WHERE occurrence.id_releve_occtax = rel.id_releve_occtax;
- 
-          -- Récupération de la source
-          SELECT INTO id_source s.id_source FROM gn_synthese.t_sources s WHERE name_source ILIKE 'occtax';
- 
-          -- Récupération de l'id_module
-          SELECT INTO id_module gn_commons.get_id_module_bycode('OCCTAX');
- 
-          -- Récupération du status_source depuis le JDD
-          SELECT INTO id_nomenclature_source_status d.id_nomenclature_source_status FROM gn_meta.t_datasets d WHERE id_dataset = releve.id_dataset;
- 
-          --Récupération et formatage des observateurs
-          SELECT INTO myobservers array_to_string(array_agg(concat_ws(' ', rol.nom_role, rol.prenom_role)), ', ') AS observers_name,
-          array_agg(rol.id_role) AS observers_id
-          FROM pr_occtax.cor_role_releves_occtax cor
-          JOIN utilisateurs.t_roles rol ON rol.id_role = cor.id_role
-          WHERE cor.id_releve_occtax = releve.id_releve_occtax;
- 
-          -- insertion dans la synthese
-          INSERT INTO gn_synthese.synthese (
-          unique_id_sinp,
-          unique_id_sinp_grp,
-          id_source,
-          entity_source_pk_value,
-          id_dataset,
-          id_module,
-          id_nomenclature_geo_object_nature,
-          id_nomenclature_grp_typ,
-          grp_method,
-          id_nomenclature_obs_technique,
-          id_nomenclature_bio_status,
-          id_nomenclature_bio_condition,
-          id_nomenclature_naturalness,
-          id_nomenclature_exist_proof,
-          id_nomenclature_diffusion_level,
-          id_nomenclature_life_stage,
-          id_nomenclature_sex,
-          id_nomenclature_obj_count,
-          id_nomenclature_type_count,
-          id_nomenclature_observation_status,
-          id_nomenclature_blurring,
-          id_nomenclature_source_status,
-          id_nomenclature_info_geo_type,
-          id_nomenclature_behaviour,
-          count_min,
-          count_max,
-          cd_nom,
-          cd_hab,
-          nom_cite,
-          meta_v_taxref,
-          sample_number_proof,
-          digital_proof,
-          non_digital_proof,
-          altitude_min,
-          altitude_max,
-          depth_min,
-          depth_max,
-          place_name,
-          precision,
-          the_geom_4326,
-          the_geom_point,
-          the_geom_local,
-          date_min,
-          date_max,
-          observers,
-          determiner,
-          id_digitiser,
-          id_nomenclature_determination_method,
-          comment_context,
-          comment_description,
-          last_action,
-          additional_data
-          )
-          VALUES(
-            new_count.unique_id_sinp_occtax,
-            releve.unique_id_sinp_grp,
-            id_source,
-            new_count.id_counting_occtax,
-            releve.id_dataset,
-            id_module,
-            releve.id_nomenclature_geo_object_nature,
-            releve.id_nomenclature_grp_typ,
-            releve.grp_method,
-            occurrence.id_nomenclature_obs_technique,
-            occurrence.id_nomenclature_bio_status,
-            occurrence.id_nomenclature_bio_condition,
-            occurrence.id_nomenclature_naturalness,
-            occurrence.id_nomenclature_exist_proof,
-            occurrence.id_nomenclature_diffusion_level,
-            new_count.id_nomenclature_life_stage,
-            new_count.id_nomenclature_sex,
-            new_count.id_nomenclature_obj_count,
-            new_count.id_nomenclature_type_count,
-            occurrence.id_nomenclature_observation_status,
-            occurrence.id_nomenclature_blurring,
-            -- status_source récupéré depuis le JDD
-            id_nomenclature_source_status,
-            -- id_nomenclature_info_geo_type: type de rattachement = non saisissable: georeferencement
-            ref_nomenclatures.get_id_nomenclature('TYP_INF_GEO', '1'),
-            occurrence.id_nomenclature_behaviour,
-            new_count.count_min,
-            new_count.count_max,
-            occurrence.cd_nom,
-            releve.cd_hab,
-            occurrence.nom_cite,
-            occurrence.meta_v_taxref,
-            occurrence.sample_number_proof,
-            occurrence.digital_proof,
-            occurrence.non_digital_proof,
-            releve.altitude_min,
-            releve.altitude_max,
-            releve.depth_min,
-            releve.depth_max,
-            releve.place_name,
-            releve.precision,
-            releve.geom_4326,
-            ST_CENTROID(releve.geom_4326),
-            releve.geom_local,
-            date_trunc('day',releve.date_min)+COALESCE(releve.hour_min,'00:00:00'::time),
-            date_trunc('day',releve.date_max)+COALESCE(releve.hour_max,'00:00:00'::time),
-            COALESCE (myobservers.observers_name, releve.observers_txt),
-            occurrence.determiner,
-            releve.id_digitiser,
-            occurrence.id_nomenclature_determination_method,
-            releve.comment,
-            occurrence.comment,
-            'I',
-            COALESCE(releve.additional_fields, '{}'::jsonb) || COALESCE(occurrence.additional_fields, '{}'::jsonb) || COALESCE(new_count.additional_fields, '{}'::jsonb)
-          );
- 
-            RETURN myobservers.observers_id ;
-          END;
-           
-        $BODY$;
-    """)
- 
- 
+    """
+    )
+
+
 def downgrade():
     pass
