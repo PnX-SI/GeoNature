8704ee2b10d193c4fab1394eca410a88c2f41866
Antoine Cecchi <antoine.cecchi@mnhn.fr>
2022-10-14 18:33:59 +0200

    Dynamic routin backbone

--- backend/geonature/core/command/create_gn_module.py
+++ backend/geonature/core/command/create_gn_module.py
@@ -47,7 +47,6 @@ from geonature.utils.gn_module_import import (
     remove_application_db,
     create_module_config,
     copy_in_external_mods,
-    frontend_routes_templating,
     MSG_OK,
 )
 from geonature.utils.module import get_module_config_path
@@ -135,8 +134,6 @@ def install_packaged_gn_module(module_path, module_code, build):
     install_frontend_dependencies(os.path.abspath(module_path))
     # generation du fichier tsconfig.app.json
     tsconfig_app_templating(app=current_app)
-    # generation du routing du frontend
-    frontend_routes_templating(app=current_app)
     # generation du fichier de configuration du frontend
     create_module_config(current_app, module_code, build=False)
     if build:
