fb182ee709a7d172ff4f043a7f845d09438bda36
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2022-10-18 15:46:55 +0200

    update trigger - add dynamic id_module

---
+++ contrib/occtax/backend/occtax/migrations/023b0be41829_add_id_module_in_pr_occtax_t_releves.py
@@ -0,0 +1,39 @@
+"""add id_module in pr_occtax.t_releves
+
+Revision ID: 023b0be41829
+Revises: 944072911ff7
+Create Date: 2022-04-06 16:19:58.971694
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "023b0be41829"
+down_revision = "944072911ff7"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute(
+        """
+            ALTER TABLE pr_occtax.t_releves_occtax
+            ADD column id_module integer;
+            ALTER TABLE ONLY pr_occtax.t_releves_occtax 
+                ADD CONSTRAINT fk_id_module FOREIGN KEY (id_module) 
+                REFERENCES gn_commons.t_modules (id_module) ON UPDATE CASCADE; 
+            UPDATE pr_occtax.t_releves_occtax
+            SET id_module = (SELECT id_module FROM gn_commons.t_modules WHERE module_code = 'OCCTAX');
+        """
+    )
+
+
+def downgrade():
+    op.execute(
+        """
+            ALTER TABLE pr_occtax.t_releves_occtax
+            DROP column id_module;
+        """
+    )
