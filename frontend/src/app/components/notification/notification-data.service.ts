b18c8b49676187cdb6b7ef5fa27ea9b262985663
Pierre Jego <pierre.jego@jdev.fr>
2022-10-19 10:41:00 +0000

    Update frontend with new routes

--- frontend/src/app/components/notification/notification-data.service.ts
+++ frontend/src/app/components/notification/notification-data.service.ts
@@ -10,18 +10,42 @@ import {
 
 import { AppConfig } from '../../../conf/app.config';
 
+export interface NotificationCard {
+  id_notification: string;
+  title: string;
+  content: string;
+  url: string;
+  code_status: string;
+  creation_date: string;
+}
+
 @Injectable()
 export class NotificationDataService {
   constructor(private _api: HttpClient) {}
 
+  // Create notification
+  createNotification(data) {
+    return this._api.put(`${AppConfig.API_ENDPOINT}/notifications/notification`, data, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json'),
+    });
+  }
+
   // returns notifications content for this user
   getNotifications() {
-    return this._api.get(`${AppConfig.API_ENDPOINT}/notifications`);
+    return this._api.get<NotificationCard[]>(
+      `${AppConfig.API_ENDPOINT}/notifications/notifications`
+    );
   }
 
   // returns number of notification for this user
-  getNotificationsNumber(): number {
-    console.log(this._api.get(`${AppConfig.API_ENDPOINT}/notificationsNumber`));
-    return 2; //this._api.get(`${AppConfig.API_ENDPOINT}/notificationsNumber`);
+  getNotificationsNumber(): any {
+    console.log('Call notification count');
+    return this._api.get<any>(`${AppConfig.API_ENDPOINT}/notifications/count`);
+  }
+
+  updateNotification(data: any) {
+    return this._api.post(`${AppConfig.API_ENDPOINT}/notifications/notification`, data, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json'),
+    });
   }
 }
