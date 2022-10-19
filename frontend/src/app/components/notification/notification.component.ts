c5a6bd0ea560381372180118e2dfe57a342d4917
Pierre Jego <pierre.jego@jdev.fr>
2022-10-19 10:41:00 +0000

    Update frontend with new routes

--- frontend/src/app/components/notification/notification.component.ts
+++ frontend/src/app/components/notification/notification.component.ts
@@ -1,124 +1,47 @@
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ChangeDetectorRef, AfterViewInit } from '@angular/core';
 import { AppConfig } from '@geonature_config/app.config';
 import { AuthService, User } from '@geonature/components/auth/auth.service';
 import { NotificationDataService } from '@geonature/components/notification/notification-data.service';
+import { NotificationCard } from '@geonature/components/notification/notification-data.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { Observable } from 'rxjs';
 import { MatTableDataSource } from '@angular/material/table';
 
-export interface NotificationCard {
-  title: string;
-  content: string;
-  url: string;
-  code_status: string;
-  creation_date: string;
-}
 
-const NOTIFICATION_DATA: NotificationCard[] = [
+const NOTIFICATION_DATA_JSON = 
   {
+    category: "VALIDATION-1",
     title: 'Modification du statut',
-    code_status: 'unread',
     content: 'Le statut de l\'observation a été modifié ( Invalide )',
-    url: 'http://geonature.jdev.fr/geonature/#/validation/occurrence/2',
-    creation_date: '09:32 28/09/2022'
-  },
-  {
-    title: 'Modification du statut',
-    code_status: 'unread',
-    content: 'Le statut de l\'observation a été modifié ( Probable )',
-    url: 'http://geonature.jdev.fr/geonature/#/validation/occurrence/2',
-    creation_date: '09:28 28/09/2022'
-  },
-  {
-    title: 'Modification du statut',
-    code_status: 'read',
-    content: 'Le statut de l\'observation a été modifié ( Douteux )',
-    url: 'http://geonature.jdev.fr/geonature/#/validation/occurrence/2',
-    creation_date: '09:12 10/09/2022'
-  },
-  {
-    title: 'Modification du statut',
-    code_status: 'read',
-    content: 'Le statut de l\'observation a été modifié ( Invalide )',
-    url: 'http://geonature.jdev.fr/geonature/#/validation/occurrence/2',
-    creation_date: '07:55 10/09/2022'
-  },
-  {
-    title: 'Modification du statut',
-    code_status: 'read',
-    content: 'Le statut de l\'observation a été modifié',
-    url: 'http://geonature.jdev.fr/geonature/#/validation/occurrence/2',
-    creation_date: '17:19 28/08/2022'
-  },
-  {
-    title: 'Modification du statut',
-    code_status: 'read',
-    content: 'Le statut de l\'observation a été modifié',
-    url: 'http://geonature.jdev.fr/geonature/#/validation/occurrence/2',
-    creation_date: '10:49 26/08/2022'
-  },
-  {
-    title: 'Modification du statut',
-    code_status: 'read',
-    content: 'Le statut de l\'observation a été modifié',
-    url: 'http://geonature.jdev.fr/geonature/#/validation/occurrence/2',
-    creation_date: '11:28 01/07/2022'
-  },
-  {
-    title: 'Modification du statut',
-    code_status: 'read',
-    content: 'Le statut de l\'observation a été modifié',
-    url: 'http://geonature.jdev.fr/geonature/#/validation/occurrence/2',
-    creation_date: '11:28 01/07/2022'
-  },
-  {
-    title: 'Modification du statut',
-    code_status: 'read',
-    content: 'Le statut de l\'observation a été modifié',
-    url: 'http://geonature.jdev.fr/geonature/#/validation/occurrence/2',
-    creation_date: '11:28 01/07/2022'
-  },
-  {
-    title: 'Modification du statut',
-    code_status: 'read',
-    content: 'Le statut de l\'observation a été modifié',
-    url: 'http://geonature.jdev.fr/geonature/#/validation/occurrence/2',
-    creation_date: '11:28 01/07/2022'
-  },
-  {
-    title: 'Modification du statut',
-    code_status: 'read',
-    content: 'Le statut de l\'observation a été modifié',
-    url: 'http://geonature.jdev.fr/geonature/#/validation/occurrence/2',
-    creation_date: '11:28 01/07/2022'
-  },
-];
-
+    url: 'http://geonature.jdev.fr/geonature/#/validation/occurrence/2'
+  };
 
 @Component({
   selector: 'pnx-notification',
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.scss']
 })
-export class NotificationComponent implements OnInit, OnDestroy {
+export class NotificationComponent implements OnInit, OnDestroy, AfterViewInit  {
 
   public currentUser: User;
-  public notifications: any;
   
   @ViewChild(MatPaginator) paginator: MatPaginator;
   obs: Observable<any>;
-  dataSource: MatTableDataSource<NotificationCard> = new MatTableDataSource<NotificationCard>(NOTIFICATION_DATA);
+  dataSource: MatTableDataSource<NotificationCard>= new MatTableDataSource<NotificationCard>(); 
+  
   constructor(
     public authService: AuthService,
     private notificationDataService : NotificationDataService) {
     
-   }
+  }
 
   ngOnInit(): void {
 
+    // check for user information (must be connected for this feature)
     this.currentUser = this.authService.getCurrentUser();
     this.obs = this.dataSource.connect();
-    //this.getNotifications();
+    this.getNotifications();
+    this.createNotification(NOTIFICATION_DATA_JSON);
   }
 
   /**
@@ -126,25 +49,37 @@ export class NotificationComponent implements OnInit, OnDestroy {
    */
    getNotifications() {
       this.notificationDataService.getNotifications().subscribe((response) => {
-      this.setNotifications(response);
+        console.log(response);
+        this.dataSource.data = response;
     });
 
   }
 
-  /**
-   *  Sort notification by creation_date
-   * @param data 
-   */
-  setNotifications(data) {
-    let listEl = data.length ? data : [];
-    
-    this.notifications = listEl;
+  updateNotificationStatus(data) {
+      this.notificationDataService.updateNotification(data).subscribe((response) => {
+    });
   }
 
+  createNotification(data) {
+    this.notificationDataService.createNotification(data).subscribe((response) => {
+  });
+}
   ngOnDestroy() {
     if (this.dataSource) {
       this.dataSource.disconnect();
     }
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  //function with param item object
+  OnMatCardClickEvent(notification:NotificationCard){
+    // update status 
+    this.updateNotificationStatus(notification)
+    // open given url
+    window.open(notification.url, '_self');
   }
 
 }
