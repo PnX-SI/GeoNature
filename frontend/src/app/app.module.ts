fcf19293f7c13f6aa3e30da51d3b1e4bed24150c
Pierre Jego <pierre.jego@jdev.fr>
2022-10-25 13:03:36 +0000

    add subscribe notification page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 import { NavHomeComponent } from './components/nav-home/nav-home.component';
 import { LoginModule } from './modules/login/login.module';
 import { NotificationComponent } from './components/notification/notification.component';
+import { RulesComponent } from './components/notification/rules/rules.component';
 
 // Custom component (footer, presentation etc...)
 import { FooterComponent } from '../custom/components/footer/footer.component';
@@ -112,6 +113,7 @@ export function getModulesAndInitRouting(injector) {
     FooterComponent,
     IntroductionComponent,
     NotificationComponent,
+    RulesComponent,
   ],
   providers: [
     AuthService,
