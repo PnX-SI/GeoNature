2498eeeb884886446ea9656112e5a4bd7156275b
Pierre Jego <pierre.jego@jdev.fr>
2022-10-26 10:13:19 +0000

    update notification number in badge on click

--- frontend/src/app/components/nav-home/nav-home.component.ts
+++ frontend/src/app/components/nav-home/nav-home.component.ts
@@ -108,6 +108,11 @@ export class NavHomeComponent implements OnInit, OnDestroy {
     });
   }
 
+  openNotification() {
+    this.updateNotificationCount();
+    this.router.navigate(['/notification']);
+  }
+
   ngOnDestroy() {
     // Prevent memory leak by unsubscribing
     this.subscription.unsubscribe();
