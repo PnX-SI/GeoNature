d80a26dd0cdde39d073cf5645b81b80793ad87b0
Pierre Jego <pierre.jego@jdev.fr>
2022-10-26 06:37:28 +0000

    Change display for notification and remove used information

--- frontend/src/app/components/notification/notification-data.service.ts
+++ frontend/src/app/components/notification/notification-data.service.ts
@@ -1,56 +1,110 @@
 
 import { Injectable } from '@angular/core';
 import {
-    HttpClient,
-    HttpParams,
-    HttpHeaders,
-    HttpEventType,
-    HttpErrorResponse,
-    HttpEvent,
+  HttpClient,
+  HttpParams,
+  HttpHeaders,
+  HttpEventType,
+  HttpErrorResponse,
+  HttpEvent,
 } from '@angular/common/http';
 
 import { AppConfig } from '../../../conf/app.config';
 
+/** Interface to display notifications */
 export interface NotificationCard {
-    id_notification: string;
-    title: string;
-    content: string;
-    url: string;
-    code_status: string;
-    creation_date: string;
-  }
+  id_notification: string;
+  title: string;
+  content: string;
+  url: string;
+  code_status: string;
+  creation_date: string;
+}
+
+/** Interface to serialise method rules */
+export interface MethodRules {
+  code_notification_method: string;
+  label_notification_method: string;
+  description_notification_method: string;
+}
+
+/** Interface to serialise categorie rules */
+export interface CategoriesRules {
+  code_notification_category: string;
+  label_notification_category: string;
+  description_notification_category: string;
+}
+
+/** Interface to serialise categorie rules */
+export interface Rules {
+  code_notification_category: string;
+  code_notification_method: string;
+}
 
 @Injectable()
 export class NotificationDataService {
 
-    constructor(private _api: HttpClient) {
-    }
+  constructor(private _api: HttpClient) {
+  }
 
-    // Create notification
-    createNotification(data) {
-        return this._api.put(`${AppConfig.API_ENDPOINT}/notifications/notification`,
-          data, {
-          headers: new HttpHeaders().set('Content-Type', 'application/json')
-        });
-      }
+  // Create notification via API
+  // Could be used for notification on frond environnement
+  createNotification(data) {
+    return this._api.put(`${AppConfig.API_ENDPOINT}/notifications/notification`,
+      data, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    });
+  }
 
-    // returns notifications content for this user
-    getNotifications() {
-        return this._api.get<NotificationCard[]>(`${AppConfig.API_ENDPOINT}/notifications/notifications`);
-    }
+  // returns notifications content for this user
+  getNotifications() {
+    return this._api.get<NotificationCard[]>(`${AppConfig.API_ENDPOINT}/notifications/notifications`);
+  }
 
-    // returns number of notification for this user
-    getNotificationsNumber():any {  
-        console.log("Call notification count");
-        return this._api.get<any>(`${AppConfig.API_ENDPOINT}/notifications/count`);
-    }
+  // returns number of notification for this user
+  getNotificationsNumber(): any {
+    console.log("Call notification count");
+    return this._api.get<any>(`${AppConfig.API_ENDPOINT}/notifications/count`);
+  }
 
-    updateNotification(data: any) {
-        return this._api.post(`${AppConfig.API_ENDPOINT}/notifications/notification`,
-          data, {
-          headers: new HttpHeaders().set('Content-Type', 'application/json')
-        });
-    }
+  // update notification status
+  updateNotification(data: any) {
+    return this._api.post(`${AppConfig.API_ENDPOINT}/notifications/notification`,
+      data, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    });
+  }
+
+  // Create rules
+  createRule(data) {
+    return this._api.put(`${AppConfig.API_ENDPOINT}/notifications/rule`,
+      data, {
+      headers: new HttpHeaders().set('Content-Type', 'application/json')
+    });
+  }
+
+  // returns all rules for current user
+  getRules() {
+    return this._api.get<Rules[]>(`${AppConfig.API_ENDPOINT}/notifications/rules`);
+  }
+
+  // returns notifications content for this user
+  getRulesCategories() {
+    return this._api.get<CategoriesRules[]>(`${AppConfig.API_ENDPOINT}/notifications/categories`);
+  }
+
+  // returns notifications content for this user
+  getRulesMethods() {
+    return this._api.get<MethodRules[]>(`${AppConfig.API_ENDPOINT}/notifications/methods`);
+  }
+
+  deleteRules() {
+    return this._api.delete<any>(`${AppConfig.API_ENDPOINT}/notifications/rules`);
+  }
+
+  deleteRule(data: Rules) {
+    return this._api.delete<any>(`${AppConfig.API_ENDPOINT}/notifications/rules`);
+  }
 
 }
 
--- frontend/src/app/components/notification/notification.component.ts
+++ frontend/src/app/components/notification/notification.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, OnDestroy, ViewChild, ChangeDetectorRef, AfterViewInit } from '@angular/core';
 import { AppConfig } from '@geonature_config/app.config';
-import { AuthService, User } from '@geonature/components/auth/auth.service';
 import { NotificationDataService } from '@geonature/components/notification/notification-data.service';
 import { NotificationCard } from '@geonature/components/notification/notification-data.service';
 import { MatPaginator } from '@angular/material/paginator';
@@ -12,24 +11,19 @@ import { MatTableDataSource } from '@angular/material/table';
   templateUrl: './notification.component.html',
   styleUrls: ['./notification.component.scss']
 })
-export class NotificationComponent implements OnInit, OnDestroy, AfterViewInit  {
+export class NotificationComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  public currentUser: User;
-  
+
   @ViewChild(MatPaginator) paginator: MatPaginator;
   obs: Observable<any>;
-  dataSource: MatTableDataSource<NotificationCard>= new MatTableDataSource<NotificationCard>(); 
-  
+  dataSource: MatTableDataSource<NotificationCard> = new MatTableDataSource<NotificationCard>();
+
   constructor(
-    public authService: AuthService,
-    private notificationDataService : NotificationDataService) {
-    
+    private notificationDataService: NotificationDataService) {
+
   }
 
   ngOnInit(): void {
-
-    // check for user information (must be connected for this feature)
-    this.currentUser = this.authService.getCurrentUser();
     this.obs = this.dataSource.connect();
     this.getNotifications();
   }
@@ -37,17 +31,18 @@ export class NotificationComponent implements OnInit, OnDestroy, AfterViewInit  
   /**
    * get all notifications for current user
    */
-   getNotifications() {
-      this.notificationDataService.getNotifications().subscribe((response) => {
-        console.log(response);
-        this.dataSource.data = response;
+  getNotifications() {
+    this.notificationDataService.getNotifications().subscribe((response) => {
+      this.dataSource.data = response;
     });
-
   }
 
-  updateNotificationStatus(data) {
+  updateNotificationStatus(data:NotificationCard) {
+    // Only update status if need
+    if(data.code_status == "UNREAD"){
       this.notificationDataService.updateNotification(data).subscribe((response) => {
-    });
+      });
+    }
   }
 
   ngOnDestroy() {
@@ -61,7 +56,7 @@ export class NotificationComponent implements OnInit, OnDestroy, AfterViewInit  
   }
 
   //function with param item object
-  OnMatCardClickEvent(notification:NotificationCard){
+  OnMatCardClickEvent(notification: NotificationCard) {
     // update status 
     this.updateNotificationStatus(notification)
     // open given url
