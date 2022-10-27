cde62e0a876d08edeff9b1ad051e2248658123ab
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2022-10-27 21:44:42 +0200

    remove list_frontend_enabled_modules()

--- backend/geonature/utils/module.py
+++ backend/geonature/utils/module.py
@@ -2,8 +2,6 @@ import os
 from pathlib import Path
 from pkg_resources import load_entry_point, get_entry_info, iter_entry_points
 
-from flask import current_app
-
 from geonature.utils.utilstoml import load_and_validate_toml
 from geonature.utils.env import CONFIG_FILE, GN_EXTERNAL_MODULE
 from geonature.core.gn_commons.models import TModules
@@ -32,18 +30,3 @@ def get_dist_from_code(module_code):
         if module_code == entry_point.load():
             return entry_point.dist
     raise Exception(f"Module with code {module_code} not installed in venv")
-
-
-def list_frontend_enabled_modules():
-    """
-    yield module_config
-    for frontend-enabled modules in gn_commons.t_modules
-    """
-    enabled_modules = TModules.query.filter_by(active_frontend=True).all()
-    for module_object in enabled_modules:
-        # ignore internal module (i.e. without symlink in external module directory)
-        if not Path(GN_EXTERNAL_MODULE / module_object.module_code.lower()).exists():
-            continue
-        if module_object.module_code in current_app.config["DISABLED_MODULES"]:
-            continue
-        yield module_object
