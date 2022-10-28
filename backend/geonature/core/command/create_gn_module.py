8e7ffa05d53259bc6f716691861a6f491fc3c721
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2022-10-28 12:21:46 +0200

    add command upgrade-modules

--- backend/geonature/core/command/create_gn_module.py
+++ backend/geonature/core/command/create_gn_module.py
@@ -1,42 +1,25 @@
-"""
-    Fonctions permettant d'ajouter un module tiers à GN
-    Ce module ne doit en aucun cas faire appel à des models ou au coeur de geonature
-    dans les imports d'entête de fichier pour garantir un bon fonctionnement des fonctions
-    d'administration de l'application GeoNature (génération des fichiers de configuration, des
-    fichiers de routing du frontend etc...). Ces dernières doivent pouvoir fonctionner même si
-    un paquet PIP du requirement GeoNature n'a pas été bien installé
-"""
-
 import os
 import sys
-import logging
 import subprocess
 import site
 import importlib
 from pathlib import Path
 import pkg_resources
-from pkg_resources import load_entry_point, get_entry_info
-import sqlalchemy.orm.exc as sa_exc
+from pkg_resources import iter_entry_points
 
 import click
 from click import ClickException
-from flask import current_app
-from flask_migrate import upgrade as db_upgrade
 
-from geonature.utils.env import db, ROOT_DIR
-from geonature.utils.module import get_dist_from_code
+from geonature.utils.env import ROOT_DIR
+from geonature.utils.module import get_dist_from_code, module_db_upgrade
 
 from geonature.core.command.main import main
+from geonature.utils.config import config
 from geonature.utils.command import (
     install_frontend_dependencies,
     create_frontend_module_config,
     build_frontend,
 )
-from geonature.utils.module import get_module_config_path
-from geonature.core.gn_commons.models import TModules
-
-
-log = logging.getLogger(__name__)
 
 
 @main.command()
@@ -44,7 +27,7 @@ log = logging.getLogger(__name__)
 @click.argument("module_code")
 @click.option("--build", type=bool, required=False, default=True)
 def install_gn_module(module_path, module_code, build):
-    # install python package and dependencies
+    click.echo("Installation du backend…")
     subprocess.run(f"pip install -e '{module_path}'", shell=True, check=True)
 
     # refresh list of entry points
@@ -55,45 +38,10 @@ def install_gn_module(module_path, module_code, build):
     # load python package
     module_dist = get_dist_from_code(module_code)
     if not module_dist:
-        raise ClickException(f"Unable to load module with code {module_code}")
+        raise ClickException(f"Aucun module ayant pour code {module_code} n’a été trouvé")
 
-    # add module to database
-    try:
-        module_picto = load_entry_point(module_dist, "gn_module", "picto")
-    except ImportError:
-        module_picto = "fa-puzzle-piece"
-    try:
-        module_type = load_entry_point(module_dist, "gn_module", "type")
-    except ImportError:
-        module_type = None
-    try:
-        module_object = TModules.query.filter_by(module_code=module_code).one()
-        module_object.module_picto = module_picto
-        db.session.merge(module_object)
-    except sa_exc.NoResultFound:
-        module_object = TModules(
-            type=module_type,
-            module_code=module_code,
-            module_label=module_code.capitalize(),
-            module_path=module_code.lower(),
-            module_target="_self",
-            module_picto=module_picto,
-            active_frontend=True,
-            active_backend=True,
-            ng_module=module_code.lower(),
-        )
-        db.session.add(module_object)
-    db.session.commit()
-
-    info = get_entry_info(module_dist, "gn_module", "migrations")
-    if info is not None:
-        try:
-            alembic_branch = load_entry_point(module_dist, "gn_module", "alembic_branch")
-        except ImportError:
-            alembic_branch = module_code.lower()
-        db_upgrade(revision=alembic_branch + "@head")
-    else:
-        log.info("Module do not provide any migration files, skipping database upgrade.")
+    click.echo("Installation de la basse de données…")
+    module_db_upgrade(module_dist)
 
     # symlink module in exernal module directory
     module_frontend_path = os.path.realpath(f"{module_path}/frontend")
@@ -118,3 +66,18 @@ def install_gn_module(module_path, module_code, build):
         click.echo("Rebuild du frontend …")
         build_frontend()
         click.secho("Rebuild du frontend terminé.", fg="green")
+
+
+@main.command()
+@click.argument("module_codes", metavar="[MODULE_CODE]...", nargs=-1)
+def upgrade_modules(module_codes):
+    for module_code_entry in iter_entry_points("gn_module", "code"):
+        module_code = module_code_entry.resolve()
+        if module_codes and module_code not in module_codes:
+            continue
+        if module_code in config["DISABLED_MODULES"]:
+            click.echo(f"Omission du module {module_code} (déactivé)")
+            continue
+        click.echo(f"Mise-à-jour du module {module_code}…")
+        module_dist = module_code_entry.dist
+        module_db_upgrade(module_dist)
--- backend/geonature/utils/module.py
+++ backend/geonature/utils/module.py
@@ -2,8 +2,10 @@ import os
 from pathlib import Path
 from pkg_resources import load_entry_point, get_entry_info, iter_entry_points
 
+from flask_migrate import upgrade as db_upgrade
+
 from geonature.utils.utilstoml import load_and_validate_toml
-from geonature.utils.env import CONFIG_FILE
+from geonature.utils.env import db, CONFIG_FILE
 from geonature.core.gn_commons.models import TModules
 
 
@@ -36,3 +38,38 @@ def get_dist_from_code(module_code):
         if module_code == entry_point.load():
             return entry_point.dist
     raise Exception(f"Module with code {module_code} not installed in venv")
+
+
+def module_db_upgrade(module_dist):
+    module_code = module_dist.load_entry_point("gn_module", "code")
+    module = TModules.query.filter_by(module_code=module_code).one_or_none()
+    if module is None:
+        # add module to database
+        try:
+            module_picto = module_dist.load_entry_point("gn_module", "picto")
+        except ImportError:
+            module_picto = "fa-puzzle-piece"
+        try:
+            module_type = module_dist.load_entry_point("gn_module", "type")
+        except ImportError:
+            module_type = None
+        module = TModules(
+            type=module_type,
+            module_code=module_code,
+            module_label=module_code.capitalize(),
+            module_path=module_code.lower(),
+            module_target="_self",
+            module_picto=module_picto,
+            active_frontend=True,
+            active_backend=True,
+            ng_module=module_code.lower(),
+        )
+        db.session.add(module)
+        db.session.commit()
+
+    if "migrations" in module_dist.get_entry_map("gn_module"):
+        try:
+            alembic_branch = module_dist.load_entry_point("gn_module", "alembic_branch")
+        except ImportError:
+            alembic_branch = module_code.lower()
+        db_upgrade(revision=alembic_branch + "@head")
