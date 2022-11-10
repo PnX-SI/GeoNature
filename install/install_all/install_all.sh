1558971f188e50569f5af3c878fc2a46011db47d
gaetanbrl <gaetan.bruel@jdev.fr>
2022-11-10 17:34:38 +0100

    xvfb for dev mode only

--- install/install_all/install_all.sh
+++ install/install_all/install_all.sh
@@ -57,8 +57,11 @@ echo "############### Installation des paquets systèmes ###############"
 
 # Installing required environment for GeoNature and TaxHub
 echo "Installation de l'environnement logiciel..."
-sudo apt-get install -y unzip git postgresql postgis python3-pip python3-venv python3-dev libpq-dev libgdal-dev libffi-dev libpangocairo-1.0-0 apache2 redis xvfb || exit 1
+sudo apt-get install -y unzip git postgresql postgis python3-pip python3-venv python3-dev libpq-dev libgdal-dev libffi-dev libpangocairo-1.0-0 apache2 redis || exit 1
 
+if [ "${mode}" = "dev" ]; then
+    sudo apt-get install -y xvfb || exit 1
+fi
 
 # Apache configuration
 sudo a2enmod rewrite || exit 1
