5d81a4b9ceb39109518d8b359517d39ebd91a3a0
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2023-01-11 17:30:35 +0100

    CruvedSchemaMixin: add documentation

--- backend/geonature/utils/schema.py
+++ backend/geonature/utils/schema.py
@@ -4,6 +4,15 @@ from geonature.core.gn_permissions.tools import get_scopes_by_action
 
 
 class CruvedSchemaMixin:
+    """
+    This mixin add a cruved field which serialize to a dict "{action: boolean}".
+        example: {"C": False, "R": True, "U": True, "V": False, "E": True, "D": False}
+    The schema must have a __module_code__ property (and optionally a __object_code__property)
+    to indicate from which permissions must be verified.
+    The model must have an has_instance_permission method which take the scope and retrurn a boolean.
+    The cruved field is excluded by default and may be added to serialization with only=["+cruved"].
+    """
+
     cruved = fields.Method("get_cruved", metadata={"exclude": True})
 
     def get_cruved(self, obj):
