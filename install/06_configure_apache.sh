2d1fbb04ebed0e5a3ddd1b3b90b7aea433c8ceba
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2023-01-13 20:38:16 +0100

    enable apache gzip compression on api

--- install/06_configure_apache.sh
+++ install/06_configure_apache.sh
@@ -10,4 +10,5 @@ echo "Configuration Apache"
 envsubst '${GEONATURE_DIR}' < "${BASE_DIR}/install/assets/geonature_apache.conf" | sudo tee /etc/apache2/conf-available/geonature.conf || exit 1
 sudo a2enmod proxy || exit 1
 sudo a2enmod proxy_http || exit 1
-# you may need to restart apache2 if proxy & proxy_http was not already enabled
+sudo a2enmod deflate || exit 1
+# you may need to restart apache2 if proxy, proxy_http or deflate was not already enabled
