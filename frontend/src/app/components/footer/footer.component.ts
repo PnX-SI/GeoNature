53f16cd60bbb3bc54825558ce97fef682bb6ed36
bastyen <bastien.alvez@makina-corpus.com>
2023-01-27 14:46:17 +0100

    use config on home page

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from '@geonature/services/config.service';
 
 @Component({
   selector: 'pnx-footer',
   templateUrl: 'footer.component.html',
 })
 export class FooterComponent implements OnInit {
-  constructor() {}
+  constructor(public config: ConfigService) {}
 
   ngOnInit() {}
 }
