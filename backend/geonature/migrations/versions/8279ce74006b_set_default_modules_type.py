20b08076401bb1485a90e250be10f7bfa33e5e01
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2023-02-22 13:18:11 +0100

    set default modules type to 'base'
    This ensure compatibility with SQLALchemy 1.4.

---
+++ backend/geonature/migrations/versions/8279ce74006b_set_default_modules_type.py
@@ -0,0 +1,56 @@
+"""set modules default type
+
+Revision ID: 8279ce74006b
+Revises: 5d65f9c93a32
+Create Date: 2023-02-22 12:47:26.727855
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "8279ce74006b"
+down_revision = "5d65f9c93a32"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute(
+        """
+        UPDATE
+            gn_commons.t_modules
+        SET
+            type = 'base'
+        WHERE
+            type IS NULL
+        """
+    )
+    op.alter_column(
+        schema="gn_commons",
+        table_name="t_modules",
+        column_name="type",
+        nullable=False,
+        server_default="base",
+    )
+
+
+def downgrade():
+    op.alter_column(
+        schema="gn_commons",
+        table_name="t_modules",
+        column_name="type",
+        nullable=True,
+        server_default=None,
+    )
+    op.execute(
+        """
+        UPDATE
+            gn_commons.t_modules
+        SET
+            type = NULL
+        WHERE
+            type = 'base'
+        """
+    )
