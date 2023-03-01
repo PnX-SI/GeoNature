750a75a86f36222fa6c43f0944e35c65af62ede2
Pierre LD <contact@pierreledorze.pro>
2023-03-01 13:42:31 +0100

    Amélioration V1
    Retour a la V1,
    fonctionnelle dans synthese, occtax et création site pour monitoring.
    pas de lib supplémentaire nécessaire

--- frontend/src/app/GN2CommonModule/map/geolocation/geolocation.component.ts
+++ frontend/src/app/GN2CommonModule/map/geolocation/geolocation.component.ts
@@ -1,68 +1,75 @@
 import { Component, OnInit, OnChanges, ViewChild, ElementRef } from '@angular/core';
 import { Map } from 'leaflet';
+
 import { MapService } from '../map.service';
-import { MapListService } from '../../map-list/map-list.service';
 import { AppConfig } from '@geonature_config/app.config';
 import { CommonService } from '../../service/common.service';
-import { LocateControl }  from '@librairies/leaflet-locatecontrol';
+
 import * as L from 'leaflet';
-import 'leaflet-locatecontrol';
 
 @Component({
   selector: 'pnx-geolocation-button',
   templateUrl: 'geolocation.component.html'
 })
-export class GeolocationComponent implements OnInit, control {
+export class GeolocationComponent {
   @ViewChild('map') public mapElement: ElementRef;
   map: L.Map;
 
-  constructor(
-    public mapservice: MapService,
-    public commonService: CommonService,
-    private _mapListServive: MapListService
-  ) {
-  }
+  constructor(public mapservice: MapService, private _commonService: CommonService) {}
+
+
 
   ngOnInit() {
     this.map = this.mapservice.map;
     this.setMarkerGeolocation();
   }
 
-  setMarkerGeolocation() {
-    // Defaults
-    L.control.locate().addTo(this.map);
+  setMarkerGeolocation()  {
+    // Marker
+    const MarkerGeolocation = this.mapservice.addCustomLegend(
+      'topleft',
+      'markerGeolocation',
+      'url(assets/images/icons8-where-34.png)'
+    );
+    this.map.addControl(new MarkerGeolocation());
+    // custom the marker
+    // document.getElementById('markerGeolocation').style.backgroundColor = '#edcb44';
+    document.getElementById('markerGeolocation').style.backgroundPosition = ' 0px center';
+  
+    L.DomEvent.disableClickPropagation(document.getElementById('markerGeolocation'));
+    document.getElementById('markerGeolocation').onclick = () => {
+      this.getCurrentLocation();
+    };
+  }
 
-    // Locate Control
-    var lc = L.control.locate({
-      position: 'topleft',
-      drawCircle: true,
-      follow: true,
-      setView: true,
-      keepCurrentZoomLevel: false,
-      markerStyle: {
-        weight: 1,
-        opacity: 0.8,
-        fillOpacity: 0.8
-      },
-      circleStyle: {
-        weight: 1,
-        clickable: false
-      },
-      icon: 'fa fa-location-arrow',
-      metric: true,
-      strings: {
-        title: 'Show me where I am',
-        popup: 'You are within {distance} {unit} from this point',
-        outsideMapBoundsMsg: 'You seem located outside the boundaries of the map'
-      },
-      locateOptions: {
-        maxZoom: 18,
-        watch: true,
-        enableHighAccuracy: true,
-        maximumAge: 10000,
-        timeout: 10000
-      }
-    }).addTo(this.map);
+
+  getCurrentLocation() {
+
+    // bug : non affichage du marker si usage de 'url(assets/images/icons8-where-34.png)'
+    // je pense, relié a https://github.com/PnX-SI/GeoNature/tree/feat/leaflet-marker
+    var mobileIcon = L.icon({
+      iconUrl: 'https://geonature.bretagne-vivante-dev.org/assets/images/icons8-street-view-50.png',
+      iconSize:     [50, 50] // size of the icon
+    })
+  
+    // amélioration possible a faire  
+    var DesktopIcon = L.icon({
+      iconUrl: 'https://geonature.bretagne-vivante-dev.org/assets/images/icons8-street-view-50.png',
+      iconSize:     [50, 50] // size of the icon
+    })
+
+    // ajouter if pour le type de device et modifier l'icone marker en fonction
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        console.log(`Latitude : ${latitude}, Longitude : ${longitude}`);
+        this.map.setView([latitude, longitude], 18); // zoom max sur la localisation
+        L.marker([latitude, longitude], {icon: mobileIcon}).addTo(this.map); // ajout d'un marker
+      });
+    } else {
+      console.error("La géolocalisation n'est pas prise en charge par votre navigateur.");
+    }
   }
 
   ngAfterViewInit() {
