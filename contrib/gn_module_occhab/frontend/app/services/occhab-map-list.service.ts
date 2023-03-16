aae8fb9f0cfb3bb0e93dc730ea007845539fdbd2
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2023-03-16 17:33:03 +0100

    Validation: fix height and profile note color

--- contrib/gn_module_occhab/frontend/app/services/occhab-map-list.service.ts
+++ contrib/gn_module_occhab/frontend/app/services/occhab-map-list.service.ts
@@ -1,19 +1,31 @@
-import { Injectable } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
-import { MapListService } from '@geonature_common/map-list/map-list.service';
-import { OccHabDataService } from '../services/data.service';
-import * as moment from 'moment';
+import { Injectable } from "@angular/core";
+import { UntypedFormBuilder, UntypedFormGroup } from "@angular/forms";
+import { MapListService } from "@geonature_common/map-list/map-list.service";
+import { OccHabDataService } from "../services/data.service";
+import * as moment from "moment";
 
 @Injectable()
 export class OccHabMapListService {
   public searchForm: UntypedFormGroup;
   public mapListService: MapListService;
-  constructor(private _fb: UntypedFormBuilder, private _occHabDataService: OccHabDataService) {
+  constructor(
+    private _fb: UntypedFormBuilder,
+    private _occHabDataService: OccHabDataService
+  ) {
     this.searchForm = this._fb.group({
       id_dataset: null,
       date_low: null,
       date_up: null,
       habitat: null,
     });
+  }
+  nbOfFilter() {
+    let result = 0;
+    Object.keys(this.searchForm.value).forEach((key) => {
+      if (this.searchForm.value[key]) {
+        result = result + 1;
+      }
+    });
+    return result;
   }
 }
