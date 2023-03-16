eed985970b175f8ac1a1b5de43ed89f931e08f41
jbrieuclp <jbrieuclp@gmail.com>
2023-03-16 10:54:22 +0100

    change user form

--- frontend/src/app/userModule/user.component.ts
+++ frontend/src/app/userModule/user.component.ts
@@ -10,28 +10,25 @@ import { DataFormService } from '@geonature_common/form/data-form.service';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
-export class UserComponent implements OnInit, AfterViewInit {
-
-  private roleForm: UntypedFormGroup;
-
-  form: UntypedFormGroup;
-  additionalFieldsForm: Array<any>;
+export class UserComponent implements OnInit {
+  form: FormGroup;
+  // dynamicFormGroup: FormGroup;
+  // public FORM_CONFIG = AppConfig.ACCOUNT_MANAGEMENT.ACCOUNT_FORM;
 
   constructor(
     private authService: AuthService,
-    private fb: UntypedFormBuilder,
-    private userService: UserDataService,
-    private dataService: DataFormService
-  ) {}
+    private roleFormService: RoleFormService,
+    private userService: UserDataService
+  ) { }
 
   ngOnInit() {
     //recupération des infos custom depuis la config de GN
     this.additionalFieldsForm = [...AppConfig.ACCOUNT_MANAGEMENT.ACCOUNT_FORM]
-                                    .map(form_element => {
-                                      //on desactive les elements customs
-                                      form_element['disable'] = true;
-                                      return form_element;
-                                    });
+      .map(form_element => {
+        //on desactive les elements customs
+        form_element['disable'] = true;
+        return form_element;
+      });
     this.initForm();
     this.form.disable();
   }
@@ -39,27 +36,26 @@ export class UserComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     //patch du formulaire à partir des infos de l'utilisateur connecté
     this.dataService.getRole(this.authService.getCurrentUser().id_role)
-      .subscribe((user) => this.form.patchValue(user));      
+      .subscribe((user) => this.form.patchValue(user));
   }
 
   initForm() {
-    this.form = this.fb.group({
-      identifiant: ['', Validators.required],
-      nom_role: ['', Validators.required],
-      prenom_role: ['', Validators.required],
-      email: [
-        '',
-        [Validators.pattern('^[a-z0-9._-]+@[a-z0-9._-]{2,}.[a-z]{2,4}$'), Validators.required],
-      ],
-      remarques: ['', null],
-      champs_addi: this.fb.group({})
-    });
+    this.form = this.getForm(this.authService.getCurrentUser().id_role);
+    // this.dynamicFormGroup = this.fb.group({});
+  }
+
+  getForm(role: number): UntypedFormGroup {
+    return this.roleFormService.getForm(role);
   }
 
   save() {
     if (this.form.valid) {
-      this.userService.putRole(this.form.value)
-        .subscribe((res) => this.form.disable());
+      // const finalForm = Object.assign({}, this.form.value);
+      // // concatenate two forms
+      // if (AppConfig.ACCOUNT_MANAGEMENT.ACCOUNT_FORM.length > 0) {
+      //   finalForm['champs_addi'] = this.dynamicFormGroup.value;
+      // }
+      this.userService.putRole(this.form.value).subscribe((res) => this.form.disable());
     }
   }
 
