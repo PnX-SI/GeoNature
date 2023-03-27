83376135f9c4c0eb5334a11b05803223586980d3
Mathieu ROUDAUT <mroudaut@creamontblanc.org>
2023-03-27 16:26:12 +0200

    feat(install): allow remote geonature db servers

--- install/03_create_db.sh
+++ install/03_create_db.sh
@@ -26,6 +26,13 @@ fi
 mkdir -p tmp
 mkdir -p var/log
 
+if [ "${db_host}" != "localhost" ] && [ "${db_host}" != "127.0.0.1" ]; then
+    export $PGHOST=$db_host
+    export $PGPORT=$db_port
+    export $PGDATABASE=$remote_super_user_database_name
+    export $PGUSER=$remote_super_user_name
+    export $PGPASSWORD=$remote_super_user_pass
+fi
 
 if database_exists "${db_name}"; then
     if $drop_apps_db; then
@@ -34,10 +41,10 @@ if database_exists "${db_name}"; then
             "FROM pg_stat_activity "
             "WHERE pg_stat_activity.datname = '${db_name}' "
             "AND pid <> pg_backend_pid() ;")
-        sudo -u "postgres" -s psql -d "postgres" -c "${query[*]}"
+        sudo -E -u "postgres" -s psql -d "postgres" -c "${query[*]}"
 
         echo "Drop database..."
-        sudo -u "postgres" -s dropdb "${db_name}"
+        sudo -E -u "postgres" -s dropdb "${db_name}"
     else
         echo "Database exists but the settings file indicates that we don't have to drop it."
         exit 0
@@ -49,20 +56,20 @@ fi
 ###################
 
 write_log "Check GeoNature database user '$user_pg' exists…"
-user_pg_exists=$(sudo -u postgres -s psql -t -c "SELECT COUNT(1) FROM pg_catalog.pg_roles WHERE  rolname = '${user_pg}';")
+user_pg_exists=$(sudo -E -u postgres -s psql -t -c "SELECT COUNT(1) FROM pg_catalog.pg_roles WHERE  rolname = '${user_pg}';")
 if [ ${user_pg_exists} -eq 0 ]; then
   write_log "Create GeoNature database user…"
-  sudo -u postgres -s psql -c "CREATE ROLE $user_pg WITH LOGIN PASSWORD '$user_pg_pass';" |& tee -a "${LOG_FILE}"
+  sudo -E -u postgres -s psql -c "CREATE ROLE $user_pg WITH LOGIN PASSWORD '$user_pg_pass';" |& tee -a "${LOG_FILE}"
 fi
 
 write_log "Creating GeoNature database..."
-sudo -u postgres -s createdb -O $user_pg $db_name -T template0 -E UTF-8 -l $my_local |& tee -a "${LOG_FILE}"
+sudo -E -u postgres -s createdb -O $user_pg $db_name -T template0 -E UTF-8 -l $my_local |& tee -a "${LOG_FILE}"
 
 write_log "Adding default PostGIS extension"
-sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS postgis;" |& tee -a "${LOG_FILE}"
+sudo -E -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS postgis;" |& tee -a "${LOG_FILE}"
 
 write_log "Extracting PostGIS version"
-postgis_full_version=$(sudo -u postgres -s psql -d "${db_name}" -c "SELECT PostGIS_Version();")
+postgis_full_version=$(sudo -E -u postgres -s psql -d "${db_name}" -c "SELECT PostGIS_Version();")
 postgis_short_version=$(echo "${postgis_full_version}" | sed -n 's/^\s*\([0-9]*\.[0-9]*\)\s.*/\1/p')
 write_log "PostGIS full version:\n ${postgis_full_version}"
 write_log  "PostGIS short version extract: '${postgis_short_version}'"
@@ -71,23 +78,23 @@ write_log "Adding Raster PostGIS extension if necessary"
 postgis_required_version="3.0"
 if [[ "$(printf '%s\n' "${postgis_required_version}" "${postgis_short_version}" | sort -V | head -n1)" = "${postgis_required_version}" ]]; then
     write_log "PostGIS version greater than or equal to ${postgis_required_version} --> adding Raster extension"
-    sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS postgis_raster;" |& tee -a "${LOG_FILE}"
+    sudo -E -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS postgis_raster;" |& tee -a "${LOG_FILE}"
 else
     write_log "PostGIS version lower than ${postgis_required_version} --> do nothing"
 fi
 
 write_log "Adding other use PostgreSQL extensions"
-sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS hstore;" |& tee -a "${LOG_FILE}"
-sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog; COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';" |& tee -a "${LOG_FILE}"
-sudo -u postgres -s psql -d $db_name -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";' |& tee -a "${LOG_FILE}"
-sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA pg_catalog;" |& tee -a "${LOG_FILE}"
-sudo -u postgres -s psql -d $db_name -c 'CREATE EXTENSION IF NOT EXISTS "unaccent";' |& tee -a "${LOG_FILE}"
+sudo -E -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS hstore;" |& tee -a "${LOG_FILE}"
+sudo -E -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog; COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';" |& tee -a "${LOG_FILE}"
+sudo -E -u postgres -s psql -d $db_name -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";' |& tee -a "${LOG_FILE}"
+sudo -E -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA pg_catalog;" |& tee -a "${LOG_FILE}"
+sudo -E -u postgres -s psql -d $db_name -c 'CREATE EXTENSION IF NOT EXISTS "unaccent";' |& tee -a "${LOG_FILE}"
 
 
 # Mise en place de la structure de la BDD et des données permettant son fonctionnement avec l'application
 write_log 'GRANT access to GeoNature user to necessary tables...'
 for table in geometry_columns geography_columns spatial_ref_sys; do
-  sudo -u postgres -s psql -d $db_name -c "GRANT SELECT ON TABLE ${table} TO ${user_pg}" |& tee -a "${LOG_FILE}"
+  sudo -E -u postgres -s psql -d $db_name -c "GRANT SELECT ON TABLE ${table} TO ${user_pg}" |& tee -a "${LOG_FILE}"
 done
 
 
