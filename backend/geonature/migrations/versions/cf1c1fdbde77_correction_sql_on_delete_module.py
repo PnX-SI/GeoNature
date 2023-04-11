ca38508da13b945bc6d6ffe6d9aba5f086242831
VincentCauchois <vincent.cauchois@mnhn.fr>
2023-04-11 17:11:27 +0200

    Make the following corrections : - Adding 'ON UPDATE CASCADE' for new FK 'fk_cor_r_a_f_m_o_id_module' - Keeping original 'ON UPDATE' rules for modified FK - Add the operations for the migration downgrade

--- backend/geonature/migrations/versions/cf1c1fdbde77_correction_sql_on_delete_module.py
+++ backend/geonature/migrations/versions/cf1c1fdbde77_correction_sql_on_delete_module.py
@@ -10,43 +10,126 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'cf1c1fdbde77'
-down_revision = '9e9218653d6c'
+revision = "cf1c1fdbde77"
+down_revision = "9e9218653d6c"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
+    # Removing rows in 'cor_role_action_filter_module_object' which have non-valid values for 'id_module' field :
+    #   values that are not in 'gn_commons.t_modules'
     op.execute(
         """
-        DELETE FROM gn_permissions.cor_role_action_filter_module_object cr WHERE NOT EXISTS (SELECT NULL FROM gn_commons.t_modules m WHERE cr.id_module = m.id_module); 
+        DELETE
+        FROM
+        	gn_permissions.cor_role_action_filter_module_object cr
+        WHERE
+        	NOT EXISTS (
+        	SELECT
+        		NULL
+        	FROM
+        		gn_commons.t_modules m
+        	WHERE
+        		cr.id_module = m.id_module); 
     """
     )
+
+    # Adding FK 'fk_cor_r_a_f_m_o_id_module'
     op.execute(
         """
-        ALTER TABLE gn_permissions.cor_role_action_filter_module_object DROP CONSTRAINT IF EXISTS fk_cor_r_a_f_m_o_id_module;
-        ALTER TABLE gn_permissions.cor_role_action_filter_module_object ADD CONSTRAINT fk_cor_r_a_f_m_o_id_module FOREIGN KEY (id_module) REFERENCES gn_commons.t_modules (id_module) ON DELETE CASCADE;
+        ALTER TABLE gn_permissions.cor_role_action_filter_module_object 
+        ADD CONSTRAINT fk_cor_r_a_f_m_o_id_module FOREIGN KEY (id_module) 
+        REFERENCES gn_commons.t_modules (id_module) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE; 
     """
     )
+
+    # Modifying FK 'fk_t_base_visits_id_module'
     op.execute(
         """
-        ALTER TABLE gn_monitoring.t_base_visits DROP CONSTRAINT IF EXISTS fk_t_base_visits_id_module;
-        ALTER TABLE gn_monitoring.t_base_visits ADD CONSTRAINT fk_t_base_visits_id_module FOREIGN KEY (id_module) REFERENCES gn_commons.t_modules (id_module) ON DELETE NO ACTION;
+        ALTER TABLE gn_monitoring.t_base_visits 
+        DROP CONSTRAINT fk_t_base_visits_id_module;
+        ALTER TABLE gn_monitoring.t_base_visits 
+        ADD CONSTRAINT fk_t_base_visits_id_module FOREIGN KEY (id_module) 
+        REFERENCES gn_commons.t_modules (id_module) 
+        ON DELETE NO ACTION -- previously 'ON DELETE CASCADE'
+        ON UPDATE CASCADE;  -- previously 'ON UPDATE CASCADE'
     """
     )
+
+    # Modifying FK 'fk_cor_filter_module_id_module'
     op.execute(
         """
-        ALTER TABLE gn_permissions.cor_filter_type_module DROP CONSTRAINT IF EXISTS fk_cor_filter_module_id_module;
-        ALTER TABLE gn_permissions.cor_filter_type_module ADD CONSTRAINT fk_cor_filter_module_id_module FOREIGN KEY (id_module) REFERENCES gn_commons.t_modules (id_module) ON DELETE CASCADE;
+        ALTER TABLE gn_permissions.cor_filter_type_module 
+        DROP CONSTRAINT fk_cor_filter_module_id_module;
+        ALTER TABLE gn_permissions.cor_filter_type_module 
+        ADD CONSTRAINT fk_cor_filter_module_id_module FOREIGN KEY (id_module) 
+        REFERENCES gn_commons.t_modules (id_module) 
+        ON DELETE CASCADE   -- previously 'ON DELETE NO ACTION'
+        ON UPDATE CASCADE;  -- previously 'ON UPDATE CASCADE'
     """
     )
+
+    # Modifying FK 'fk_cor_site_module_id_module'
     op.execute(
         """
-        ALTER TABLE gn_monitoring.cor_site_module DROP CONSTRAINT IF EXISTS fk_cor_site_module_id_module;
-        ALTER TABLE gn_monitoring.cor_site_module ADD CONSTRAINT fk_cor_site_module_id_module FOREIGN KEY (id_module) REFERENCES gn_commons.t_modules (id_module) ON UPDATE CASCADE ON DELETE CASCADE;
+        ALTER TABLE gn_monitoring.cor_site_module 
+        DROP CONSTRAINT fk_cor_site_module_id_module;
+        ALTER TABLE gn_monitoring.cor_site_module 
+        ADD CONSTRAINT fk_cor_site_module_id_module FOREIGN KEY (id_module) 
+        REFERENCES gn_commons.t_modules (id_module) 
+        ON DELETE CASCADE   -- previously 'ON DELETE NO ACTION'
+        ON UPDATE CASCADE;  -- previously 'ON DELETE NO ACTION'
     """
     )
 
 
 def downgrade():
-    pass
+    # Removing FK 'fk_cor_r_a_f_m_o_id_module'
+    op.execute(
+        """
+        ALTER TABLE gn_permissions.cor_role_action_filter_module_object 
+        DROP CONSTRAINT fk_cor_r_a_f_m_o_id_module;
+    """
+    )
+
+    # Modifying back FK 'fk_t_base_visits_id_module'
+    op.execute(
+        """
+        ALTER TABLE gn_monitoring.t_base_visits 
+        DROP CONSTRAINT fk_t_base_visits_id_module;
+        ALTER TABLE gn_monitoring.t_base_visits 
+        ADD CONSTRAINT fk_t_base_visits_id_module FOREIGN KEY (id_module) 
+        REFERENCES gn_commons.t_modules (id_module) 
+        ON DELETE CASCADE
+        ON UPDATE CASCADE;
+    """
+    )
+
+    # Modifying back FK 'fk_cor_filter_module_id_module'
+    op.execute(
+        """
+        ALTER TABLE gn_permissions.cor_filter_type_module 
+        DROP CONSTRAINT fk_cor_filter_module_id_module;
+        ALTER TABLE gn_permissions.cor_filter_type_module 
+        ADD CONSTRAINT fk_cor_filter_module_id_module FOREIGN KEY (id_module) 
+        REFERENCES gn_commons.t_modules (id_module) 
+        ON DELETE NO ACTION
+        ON UPDATE CASCADE;
+    """
+    )
+
+    # Modifying back FK 'fk_cor_site_module_id_module'
+    op.execute(
+        """
+        ALTER TABLE gn_monitoring.cor_site_module 
+        DROP CONSTRAINT fk_cor_site_module_id_module;
+        ALTER TABLE gn_monitoring.cor_site_module 
+        ADD CONSTRAINT fk_cor_site_module_id_module FOREIGN KEY (id_module) 
+        REFERENCES gn_commons.t_modules (id_module) 
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION;
+    """
+    )
