716f32c2f69b785b477153c1ed3db59e8b74491f
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2023-05-15 11:45:25 +0200

    improve permissions admin with user/group access

--- backend/geonature/core/gn_permissions/admin.py
+++ backend/geonature/core/gn_permissions/admin.py
@@ -1,6 +1,7 @@
-from flask import has_app_context, Markup
+from flask import url_for, has_app_context, Markup, request
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.contrib.sqla.filters import FilterEqual
+from sqlalchemy.orm import contains_eager, joinedload
 
 from geonature.utils.env import db
 from geonature.core.admin.admin import admin
@@ -12,24 +13,13 @@ from geonature.core.gn_permissions.models import (
     Permission,
     PermissionAvailable,
 )
+from geonature.core.gn_permissions.tools import get_permissions
 from geonature.core.gn_commons.models.base import TModules
 
 from pypnusershub.db.models import User
 
 
-class ObjectAdmin(CruvedProtectedMixin, ModelView):
-    module_code = "ADMIN"
-    object_code = "PERMISSIONS"
-
-    column_list = ("code_object", "description_object", "modules")
-    column_labels = {
-        "code_object": "Code",
-        "description_object": "Description",
-    }
-
-    can_create = False
-    can_edit = False
-    can_delete = False
+### Filters
 
 
 class RoleFilter(DynamicOptionsMixin, FilterEqual):
@@ -70,6 +60,9 @@ class ScopeFilter(DynamicOptionsMixin, FilterEqual):
             yield from [(a.value, a.label) for a in PermScope.query.all()]
 
 
+### Formatters
+
+
 def filters_formatter(v, c, m, p):
     filters = []
     if m.scope:
@@ -77,6 +70,188 @@ def filters_formatter(v, c, m, p):
     if m.sensitivity_filter:
         filters.append("Données non sensible")
     return Markup("<ul>" + "".join(["<li>{}</li>".format(f) for f in filters]) + "</ul>")
+
+
+def modules_formatter(view, context, model, name):
+    modules = [
+        '<a href="{}">{}</a>'.format(
+            url_for("tmodules.details_view", id=module.id_module),
+            Markup.escape(module.module_code),
+        )
+        for module in model.modules
+    ]
+    return Markup(", ".join(modules))
+
+
+def groups_formatter(view, context, model, name):
+    groups = [
+        '<a href="{}">{}</a>'.format(
+            url_for("permissions/group.details_view", id=group.id_role), Markup.escape(group)
+        )
+        for group in model.groups
+    ]
+    return Markup(", ".join(groups))
+
+
+def role_formatter(view, context, model, name):
+    role = model.role
+    if role.groupe:
+        url = url_for("permissions/group.details_view", id=role.id_role)
+        nom = "<b>{}</b>".format(Markup.escape(role.nom_role))
+    else:
+        url = url_for("permissions/user.details_view", id=role.id_role)
+        nom = Markup.escape(role.nom_complet)
+    return Markup('<a href="{}">{}</a>'.format(url, nom))
+
+
+def permissions_formatter(view, context, model, name):
+    available_permissions = (
+        PermissionAvailable.query.join(PermissionAvailable.module)
+        .join(PermissionAvailable.object)
+        .join(PermissionAvailable.action)
+        .options(
+            contains_eager(PermissionAvailable.module),
+            contains_eager(PermissionAvailable.object),
+            contains_eager(PermissionAvailable.action),
+        )
+        .order_by(TModules.module_code, PermObject.id_object, PermAction.id_action)
+        .all()
+    )
+
+    o = '<table class="table">'
+    columns = ["Module", "Object", "Action", "Label"]
+    if model.groupe:
+        return_url = url_for("permissions/group.details_view", id=model.id_role)
+        columns += ["Permissions"]
+    else:
+        return_url = url_for("permissions/user.details_view", id=model.id_role)
+        columns += ["Permissions personnelles", "Permissions effectives"]
+    o += "<thead><tr>" + "".join([f"<th>{col}</th>" for col in columns]) + "</tr></thead>"
+    o += "<tbody>"
+    for ap in available_permissions:
+        own_permissions = list(
+            filter(
+                lambda p: p.module == ap.module
+                and p.object == ap.object
+                and p.action == ap.action,
+                model.permissions,
+            )
+        )
+        permissions = [(own_permissions, True)]
+        if not model.groupe:
+            effective_permissions = list(
+                get_permissions(
+                    id_role=model.id_role,
+                    module_code=ap.module.module_code,
+                    object_code=ap.object.code_object,
+                    action_code=ap.action.code_action,
+                )
+            )
+            permissions.append((effective_permissions, False))
+        o += "<tr>"
+        o += "".join(
+            [
+                f"<td>{col}</td>"
+                for col in [
+                    ap.module.module_code,
+                    ap.object.code_object,
+                    ap.action.code_action,
+                    ap.label,
+                ]
+            ]
+        )
+        for perms, managable in permissions:
+            o += "<td>"
+            if perms:
+                o += '<table class="table table-bordered table-sm" style="border-collapse: separate; border-spacing:0 8px;">'
+                for perm in perms:
+                    flts = perm.filters
+                    o += "<tr>"
+                    if not flts:
+                        o += '<td class="table-success">'
+                    else:
+                        o += '<td class="table-info">'
+                    o += """<div class="row"><div class="col">"""
+                    if not flts:
+                        o += """<i class="fa fa-check" aria-hidden="true"></i>"""
+                    else:
+                        o += "Restrictions :"
+                        o += """<ul class="list-group">"""
+                        for flt in flts:
+                            o += f"""<li class="list-group-item">{flt}</li>"""
+                        o += "</ul>"
+                    o += """</div></div>"""
+                    if managable:
+                        o += """<div class="row"><div class="col text-right">"""
+                        edit_url = url_for(
+                            "permissions/permission.edit_view",
+                            id=perm.id_permission,
+                            url=return_url,
+                        )
+                        delete_url = url_for(
+                            "permissions/permission.delete_view",
+                            id=perm.id_permission,
+                            url=return_url,
+                        )
+                        o += (
+                            f"""<form method="post" action="{delete_url}">"""
+                            f"""<a class="btn btn-primary btn-sm" href="{edit_url}">"""
+                            """<i class="fa fa-pencil" aria-hidden="true"></i>"""
+                            """</a>"""
+                            """<button class="btn btn-danger btn-sm" onclick="return faHelpers.safeConfirm('Supprimer cette permission ?');">"""
+                            """<i class="fa fa-trash" aria-hidden="true"></i>"""
+                            "</button>"
+                            "</form>"
+                        )
+                        o += """</div></div>"""
+                    o += "</td></tr>"
+                o += "</table>"
+            if managable:
+                add_url = url_for(
+                    "permissions/permission.create_view",
+                    id_role=model.id_role,
+                    module_code=ap.module.module_code,
+                    code_object=ap.object.code_object,
+                    code_action=ap.action.code_action,
+                    url=return_url,
+                )
+                o += (
+                    f"""<a class="btn btn-success btn-sm float-right" href="{add_url}">"""
+                    """<i class="fa fa-plus" aria-hidden="true"></i>"""
+                    """</a>"""
+                )
+            o += "</td>"
+        o += "</tr>"
+    o += "</tbody>"
+    o += "</table>"
+    return Markup(o)
+
+
+def permissions_count_formatter(view, context, model, name):
+    url = url_for("permissions/permission.index_view", flt1_rle_equals=model.id_role)
+    return Markup(f'<a href="{url}">{len(model.permissions)}</a>')
+
+
+### ModelViews
+
+
+class ObjectAdmin(CruvedProtectedMixin, ModelView):
+    module_code = "ADMIN"
+    object_code = "PERMISSIONS"
+
+    column_list = ("code_object", "description_object", "modules")
+    column_labels = {
+        "code_object": "Code",
+        "description_object": "Description",
+    }
+    column_default_sort = "id_object"
+    column_formatters = {
+        "modules": modules_formatter,
+    }
+
+    can_create = False
+    can_edit = False
+    can_delete = False
 
 
 class PermissionAdmin(CruvedProtectedMixin, ModelView):
@@ -95,9 +270,7 @@ class PermissionAdmin(CruvedProtectedMixin, ModelView):
     }
     column_searchable_list = ("role.identifiant", "role.nom_complet")
     column_formatters = {
-        "role": lambda v, c, m, p: Markup("<b>{}</b>".format(Markup.escape(m.role.nom_role)))
-        if m.role.groupe
-        else m.role.nom_complet,
+        "role": role_formatter,
         "module": lambda v, c, m, p: m.module.module_code,
         "object": lambda v, c, m, p: m.object.code_object,
         "label": lambda v, c, m, p: m.availability.label if m.availability else None,
@@ -123,7 +296,30 @@ class PermissionAdmin(CruvedProtectedMixin, ModelView):
         ("object.code_object", True),
         ("action.code_action", True),
     ]
-    form_columns = ("role", "module", "object", "action", "scope", "sensitivity_filter")
+    form_columns = ("role", "availability", "scope", "sensitivity_filter")
+
+    def get_query(self):
+        return super().get_query().join(Permission.availability)
+
+    def create_form(self):
+        form = super().create_form()
+        if request.method == "GET":
+            # Set default values from request.args
+            if "id_role" in request.args:
+                form.role.data = User.query.get(request.args.get("id_role", type=int))
+            if {"module_code", "code_object", "code_action"}.issubset(request.args.keys()):
+                form.availability.data = (
+                    PermissionAvailable.query.join(PermissionAvailable.module)
+                    .join(PermissionAvailable.object)
+                    .join(PermissionAvailable.action)
+                    .filter(
+                        TModules.module_code == request.args.get("module_code"),
+                        PermObject.code_object == request.args.get("code_object"),
+                        PermAction.code_action == request.args.get("code_action"),
+                    )
+                    .one_or_none()
+                )
+        return form
 
 
 class PermissionAvailableAdmin(CruvedProtectedMixin, ModelView):
@@ -142,12 +338,102 @@ class PermissionAvailableAdmin(CruvedProtectedMixin, ModelView):
     }
 
 
+class RolePermAdmin(CruvedProtectedMixin, ModelView):
+    module_code = "ADMIN"
+    object_code = "PERMISSIONS"
+
+    can_create = False
+    can_edit = False
+    can_delete = False
+    can_export = False
+    can_view_details = True
+
+    column_select_related_list = ("permissions",)
+    column_labels = {
+        "nom_role": "Nom",
+        "prenom_role": "Prénom",
+        "groups": "Groupes",
+        "permissions": "Permissions",
+        "permissions_count": "Nombre de permissions",
+    }
+    column_formatters = {
+        "groups": groups_formatter,
+        "permissions_count": permissions_count_formatter,
+    }
+    column_formatters_detail = {
+        "groups": groups_formatter,
+        "permissions": permissions_formatter,
+        "permissions_count": permissions_count_formatter,
+    }
+
+
+class GroupPermAdmin(RolePermAdmin):
+    column_list = (
+        "nom_role",
+        "permissions_count",
+    )
+    column_details_list = ("nom_role", "permissions_count", "permissions")
+
+    def get_query(self):
+        # TODO: filter_by_app
+        return super().get_query().filter_by(groupe=True)
+
+
+class UserPermAdmin(RolePermAdmin):
+    column_list = (
+        "identifiant",
+        "nom_role",
+        "prenom_role",
+        "groups",
+        "permissions_count",
+    )
+    column_labels = {
+        **RolePermAdmin.column_labels,
+        "permissions_count": "Nombre de permissions non héritées",
+    }
+    column_details_list = (
+        "identifiant",
+        "nom_role",
+        "prenom_role",
+        "groups",
+        "permissions_count",
+        "permissions",
+    )
+
+    def get_query(self):
+        # TODO: filter_by_app
+        return super().get_query().filter_by(groupe=False)
+
+
+admin.add_view(
+    GroupPermAdmin(
+        User,
+        db.session,
+        name="Groupes",
+        category="Permissions",
+        endpoint="permissions/group",
+    )
+)
+
+
+admin.add_view(
+    UserPermAdmin(
+        User,
+        db.session,
+        name="Utilisateurs",
+        category="Permissions",
+        endpoint="permissions/user",
+    )
+)
+
+
 admin.add_view(
     ObjectAdmin(
         PermObject,
         db.session,
         name="Objets",
         category="Permissions",
+        endpoint="permissions/object",
     )
 )
 
@@ -156,8 +442,9 @@ admin.add_view(
     PermissionAdmin(
         Permission,
         db.session,
-        name="Permissions",
+        name="Toutes les permissions",
         category="Permissions",
+        endpoint="permissions/permission",
     )
 )
 
@@ -168,5 +455,6 @@ admin.add_view(
         db.session,
         name="Permissions disponibles",
         category="Permissions",
+        endpoint="permissions/availablepermission",
     )
 )
