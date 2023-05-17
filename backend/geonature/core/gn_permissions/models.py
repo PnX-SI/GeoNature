ae6a2173226f60d53b1113b5206b6b844a0eccbb
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2023-05-17 19:24:27 +0200

    improve permissions admin

--- backend/geonature/core/gn_permissions/models.py
+++ backend/geonature/core/gn_permissions/models.py
@@ -1,11 +1,18 @@
 """
 Models of gn_permissions schema
 """
+from packaging import version
 
 import sqlalchemy as sa
 from sqlalchemy import ForeignKey
 from sqlalchemy.sql import select
-from sqlalchemy.orm import foreign
+from sqlalchemy.orm import foreign, joinedload, contains_eager
+import flask_sqlalchemy
+
+if version.parse(flask_sqlalchemy.__version__) >= version.parse("3"):
+    from flask_sqlalchemy.query import Query
+else:
+    from flask_sqlalchemy import BaseQuery as Query
 
 from utils_flask_sqla.serializers import serializable
 from pypnusershub.db.models import User
@@ -84,9 +91,42 @@ class PermObject(db.Model):
 TObjects = PermObject
 
 
+def _nice_order(model, qs):
+    from geonature.core.gn_commons.models import TModules
+
+    return (
+        qs.join(model.module)
+        .join(model.object)
+        .join(model.action)
+        .options(
+            contains_eager(model.module),
+            contains_eager(model.object),
+            contains_eager(model.action),
+        )
+        .order_by(
+            TModules.module_code,
+            # ensure ALL at first:
+            sa.case([(PermObject.code_object == "ALL", "1")], else_=PermObject.code_object),
+            model.id_action,
+        )
+    )
+
+
+class PermissionAvailableQuery(Query):
+    def nice_order(self):
+        return _nice_order(PermissionAvailable, self)
+
+
+class PermissionQuery(Query):
+    def nice_order(self):
+        return _nice_order(Permission, self)
+
+
 class PermissionAvailable(db.Model):
     __tablename__ = "t_permissions_available"
     __table_args__ = {"schema": "gn_permissions"}
+    query_class = PermissionAvailableQuery
+
     id_module = db.Column(
         db.Integer, ForeignKey("gn_commons.t_modules.id_module"), primary_key=True
     )
@@ -116,7 +156,12 @@ class PermissionAvailable(db.Model):
         return [k for k, v in self.filters_fields.items() if getattr(self, v.name)]
 
     def __str__(self):
-        return self.label
+        s = self.module.module_label
+        if self.object.code_object != "ALL":
+            object_label = self.object.code_object.title().replace("_", " ")
+            s += f" | {object_label}"
+        s += f" | {self.label}"
+        return s
 
 
 class PermFilter:
@@ -139,6 +184,8 @@ class PermFilter:
 class Permission(db.Model):
     __tablename__ = "t_permissions"
     __table_args__ = {"schema": "gn_permissions"}
+    query_class = PermissionQuery
+
     id_permission = db.Column(db.Integer, primary_key=True)
     id_role = db.Column(db.Integer, ForeignKey("utilisateurs.t_roles.id_role"))
     id_action = db.Column(db.Integer, ForeignKey(PermAction.id_action))
