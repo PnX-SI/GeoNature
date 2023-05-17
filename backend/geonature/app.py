3aa414eee8cfcb5f228e63e801a2196ee144d1ae
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2023-05-17 14:14:57 +0200

    optimize permissions with caching

--- backend/geonature/app.py
+++ backend/geonature/app.py
@@ -158,7 +158,8 @@ def create_app(with_external_mods=True):
             g.current_user = user_from_token(request.cookies["token"]).role
         except (KeyError, UnreadableAccessRightsError, AccessRightsExpiredError):
             g.current_user = None
-        g.permissions_by_action = {}
+        g._permissions_by_user = {}
+        g._permissions = {}
 
     if config.get("SENTRY_DSN"):
         from sentry_sdk import set_tag, set_user
