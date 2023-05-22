a264b4a9e0bdc007cf2610db5f24f85431250142
joelclems <40370682+joelclems@users.noreply.github.com>
2023-05-22 18:39:20 +0200

    split script install db (#2545)

--- install/03_create_db.sh
+++ install/03_create_db.sh
@@ -58,30 +58,8 @@ fi
 write_log "Creating GeoNature database..."
 sudo -u postgres -s createdb -O $user_pg $db_name -T template0 -E UTF-8 -l $my_local |& tee -a "${LOG_FILE}"
 
-write_log "Adding default PostGIS extension"
-sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS postgis;" |& tee -a "${LOG_FILE}"
-
-write_log "Extracting PostGIS version"
-postgis_full_version=$(sudo -u postgres -s psql -d "${db_name}" -c "SELECT PostGIS_Version();")
-postgis_short_version=$(echo "${postgis_full_version}" | sed -n 's/^\s*\([0-9]*\.[0-9]*\)\s.*/\1/p')
-write_log "PostGIS full version:\n ${postgis_full_version}"
-write_log  "PostGIS short version extract: '${postgis_short_version}'"
-
-write_log "Adding Raster PostGIS extension if necessary"
-postgis_required_version="3.0"
-if [[ "$(printf '%s\n' "${postgis_required_version}" "${postgis_short_version}" | sort -V | head -n1)" = "${postgis_required_version}" ]]; then
-    write_log "PostGIS version greater than or equal to ${postgis_required_version} --> adding Raster extension"
-    sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS postgis_raster;" |& tee -a "${LOG_FILE}"
-else
-    write_log "PostGIS version lower than ${postgis_required_version} --> do nothing"
-fi
-
-write_log "Adding other use PostgreSQL extensions"
-sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS hstore;" |& tee -a "${LOG_FILE}"
-sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog; COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';" |& tee -a "${LOG_FILE}"
-sudo -u postgres -s psql -d $db_name -c 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";' |& tee -a "${LOG_FILE}"
-sudo -u postgres -s psql -d $db_name -c "CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA pg_catalog;" |& tee -a "${LOG_FILE}"
-sudo -u postgres -s psql -d $db_name -c 'CREATE EXTENSION IF NOT EXISTS "unaccent";' |& tee -a "${LOG_FILE}"
+write_log "Adding PostgreSQL extensions"
+sudo -u postgres -s psql -d $db_name -f "${SCRIPT_DIR}/assets/db/add_pg_extensions.sql" |& tee -a "${LOG_FILE}"
 
 
 # Mise en place de la structure de la BDD et des données permettant son fonctionnement avec l'application
@@ -100,58 +78,4 @@ if [[ "${MODE}" == "dev" ]]; then
     mkdir -p "${DATA_DIRECTORY}"
 fi
 
-geonature db upgrade geonature@head -x local-srid=$srid_local |& tee -a "${LOG_FILE}"
-geonature db autoupgrade -x local-srid=$srid_local |& tee -a "${LOG_FILE}"
-
-geonature db exec "DO 'BEGIN ASSERT EXISTS (SELECT 1 FROM taxonomie.taxref); END'" 2>/dev/null || if [ "$install_bdc_statuts" = true ];
-then
-    geonature db upgrade ref_geo_fr_departments@head |& tee -a "${LOG_FILE}"
-    geonature taxref import-v16
-else
-    geonature taxref import-v16 --skip-bdc-statuts
-fi
-geonature db upgrade nomenclatures_taxonomie_data@head
-
-# Installation des données exemples
-if [ "$add_sample_data" = true ];
-then
-    write_log "Inserting sample datasets..."
-    geonature db upgrade geonature-samples@head |& tee -a "${LOG_FILE}"
-fi
-
-if [ "$install_sig_layers" = true ];
-then
-    geonature db upgrade ref_geo_fr_departments@head |& tee -a "${LOG_FILE}"
-    geonature db upgrade ref_geo_fr_municipalities@head |& tee -a "${LOG_FILE}"
-    geonature db upgrade ref_geo_fr_regions@head |& tee -a "${LOG_FILE}"
-fi
-
-if [ "$install_grid_layer" = true ];
-then
-    geonature db upgrade ref_geo_inpn_grids_1@head |& tee -a "${LOG_FILE}"
-    geonature db upgrade ref_geo_inpn_grids_5@head |& tee -a "${LOG_FILE}"
-    geonature db upgrade ref_geo_inpn_grids_10@head |& tee -a "${LOG_FILE}"
-fi
-
-if [ "$install_ref_sensitivity" = true ];
-then
-    geonature db upgrade ref_geo_fr_departments@head |& tee -a "${LOG_FILE}"
-    geonature sensitivity add-referential \
-            --source-name "Référentiel sensibilité TAXREF v16 20230203" \
-            --url https://geonature.fr/data/inpn/sensitivity/RefSensibiliteV16_20230203.zip \
-            --zipfile RefSensibiliteV16_20230203.zip \
-            --csvfile RefSensibiliteV16_20230203/RefSensibilite_16.csv  \
-            --encoding=iso-8859-15 |& tee -a "${LOG_FILE}"
-    geonature sensitivity refresh-rules-cache |& tee -a "${LOG_FILE}"
-fi
-
-if  [ "$install_default_dem" = true ];
-then
-    geonature db upgrade ign_bd_alti@head -x local-srid=$srid_local |& tee -a "${LOG_FILE}"
-    if [ "$vectorise_dem" = true ];
-    then
-        geonature db upgrade ign_bd_alti_vector@head |& tee -a "${LOG_FILE}"
-    fi
-fi
-
-geonature db autoupgrade -x local-srid=$srid_local |& tee -a "${LOG_FILE}"
+. ${SCRIPT_DIR}/03b_populate_db.sh |& tee -a "${LOG_FILE}"
