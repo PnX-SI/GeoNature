864896af5c800e4fd8555b38ab7d22c38be11ce1
Andria Capai <andria_capai@natural-solutions.eu>
2023-07-21 15:53:52 +0200

    feat: add group3 inpn into synthese view export
    Since the Taxref v14 or v15 , group3_inpn is available
    
    Creation of revision in order to add this column to
    v_synthese_for_export and v_synthese_taxon_for_export_view
    
    Reviewed-by: andriacap

---
+++ backend/geonature/migrations/versions/9f4db1786c22_add_group3_inpn_to_synthese_views.py
@@ -0,0 +1,30 @@
+"""add group3 inpn to synthese views
+
+Revision ID: 9f4db1786c22
+Revises: f1dd984bff97
+Create Date: 2023-07-21 14:15:23.311469
+
+"""
+import importlib
+from alembic import op
+from sqlalchemy.sql import text
+
+# revision identifiers, used by Alembic.
+revision = '9f4db1786c22'
+down_revision = 'f1dd984bff97'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    conn = op.get_bind()
+    path = "geonature.migrations.data.core"
+    filename = "synthese_view_export_add_group3_inpn.sql"
+    conn.execute(text(importlib.resources.read_text(path, filename)))
+
+
+def downgrade():
+    conn = op.get_bind()
+    path = "geonature.migrations.data.core"
+    filename = "synthese_view_export_remove_group3_inpn.sql"
+    conn.execute(text(importlib.resources.read_text(path, filename)))
