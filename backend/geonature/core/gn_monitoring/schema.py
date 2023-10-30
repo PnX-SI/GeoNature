ebeccdad05cecbbc7201da035f1019479cb4150a
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2023-10-30 12:38:23 +0100

    feat(api): add Individuals Marshmallow schema

---
+++ backend/geonature/core/gn_monitoring/schema.py
@@ -0,0 +1,15 @@
+from geonature.utils.env import MA
+
+from geonature.core.gn_monitoring.models import TIndividuals
+from pypnnomenclature.schemas import NomenclatureSchema
+from pypnusershub.schemas import UserSchema
+
+
+class TIndividualsSchema(MA.SQLAlchemyAutoSchema):
+    class Meta:
+        model = TIndividuals
+        include_fk = True
+        load_instance = True
+
+    nomenclature_sex = MA.Nested(NomenclatureSchema, dump_only=True)
+    digitiser = MA.Nested(UserSchema, dump_only=True)
