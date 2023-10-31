d65d9a9ee8e6cae2d33244b620a40c1fe119c19a
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2023-10-31 17:23:30 +0100

    feat(front): add create component for individuals

---
+++ frontend/src/app/GN2CommonModule/form/individuals/create/individuals-create.component.ts
@@ -0,0 +1,53 @@
+import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { Taxon } from '@geonature_common/form/taxonomy/taxonomy.component';
+import { NgbTypeaheadSelectItemEvent } from '@ng-bootstrap/ng-bootstrap';
+import { IndividualsService } from '../individuals.service';
+import { Individual } from '../interfaces';
+import { throwError } from 'rxjs';
+@Component({
+  selector: 'pnx-individuals-create',
+  templateUrl: './individuals-create.component.html',
+  styleUrls: ['./individuals-create.component.scss'],
+})
+export class IndividualsCreateComponent {
+  @Input() idModule: null | number = null;
+  @Output() individualEvent = new EventEmitter<Individual>();
+  @Output() cancelEvent = new EventEmitter();
+
+  form = new FormGroup({
+    individual_name: new FormControl<string>('', {
+      validators: [Validators.required],
+    }),
+    id_nomenclature_sex: new FormControl<number | null>(null),
+    cd_nom: new FormControl<number | null>(null, {
+      validators: [Validators.required],
+    }),
+    // Normally we could avoid providing a form to taxonomy
+    // widget, but it needs it and affecting the entire taxon
+    // to the form control. Creating a temp one to fix this problem
+    cd_nom_temp: new FormControl<number | null>(null, {
+      validators: [Validators.required],
+    }),
+    comment: new FormControl<string>(''),
+  });
+
+  constructor(private _individualsService: IndividualsService) {}
+
+  taxonSelected(value: NgbTypeaheadSelectItemEvent<Taxon>) {
+    this.form.patchValue({ cd_nom: value.item.cd_nom });
+  }
+
+  createIndividual() {
+    const value = this.form.getRawValue();
+    delete value.cd_nom_temp;
+    this._individualsService
+      .postIndividual(value as Individual, this.idModule)
+      .subscribe((value) => this.individualEvent.emit(value));
+  }
+
+  cancelCreate() {
+    this.cancelEvent.emit();
+  }
+}
