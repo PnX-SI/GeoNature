02f9853d4b901f517614f177ae23ab9e01c13593
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2023-10-31 15:01:02 +0100

    feat(api): now possible to specify module
    When creating an individual
    Do not load modules when listing individuals
    Exclude modules from the schema

--- backend/geonature/core/gn_monitoring/schema.py
+++ backend/geonature/core/gn_monitoring/schema.py
@@ -1,5 +1,7 @@
-from geonature.utils.env import MA
+from marshmallow import fields
 
+from geonature.core.gn_commons.schemas import ModuleSchema
+from geonature.utils.env import MA
 from geonature.core.gn_monitoring.models import TIndividuals
 from pypnnomenclature.schemas import NomenclatureSchema
 from pypnusershub.schemas import UserSchema
@@ -13,3 +15,4 @@ class TIndividualsSchema(MA.SQLAlchemyAutoSchema):
 
     nomenclature_sex = MA.Nested(NomenclatureSchema, dump_only=True)
     digitiser = MA.Nested(UserSchema, dump_only=True)
+    modules = fields.List(MA.Nested(ModuleSchema, dump_only=True))
