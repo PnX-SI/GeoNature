b4699e8bd42b6896d89db14452229b7057004e7b
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2023-11-10 18:26:47 +0100

    feat(front): possibility to provide cdNom & idList
    To the widget.
    Workaroung to make cdNom work: deactivate taxon widget when cdNom is provided

--- frontend/src/app/GN2CommonModule/form/individuals/create/individuals-create.component.ts
+++ frontend/src/app/GN2CommonModule/form/individuals/create/individuals-create.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter, Input } from '@angular/core';
+import { Component, Output, EventEmitter, Input, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { Taxon } from '@geonature_common/form/taxonomy/taxonomy.component';
@@ -11,29 +11,41 @@ import { throwError } from 'rxjs';
   templateUrl: './individuals-create.component.html',
   styleUrls: ['./individuals-create.component.scss'],
 })
-export class IndividualsCreateComponent {
+export class IndividualsCreateComponent implements OnInit {
   @Input() idModule: null | number = null;
+  @Input() idList: null | string = null;
+  @Input() cdNom: null | number = null;
   @Output() individualEvent = new EventEmitter<Individual>();
   @Output() cancelEvent = new EventEmitter();
 
-  form = new FormGroup({
-    individual_name: new FormControl<string>('', {
-      validators: [Validators.required],
-    }),
-    id_nomenclature_sex: new FormControl<number | null>(null),
-    cd_nom: new FormControl<number | null>(null, {
-      validators: [Validators.required],
-    }),
-    // Normally we could avoid providing a form to taxonomy
-    // widget, but it needs it and affecting the entire taxon
-    // to the form control. Creating a temp one to fix this problem
-    cd_nom_temp: new FormControl<number | null>(null, {
-      validators: [Validators.required],
-    }),
-    comment: new FormControl<string>(''),
-  });
+  form: FormGroup<{
+    individual_name: FormControl<string>;
+    id_nomenclature_sex: FormControl<number | null>;
+    cd_nom: FormControl<number | null>;
+    cd_nom_temp: FormControl<number | null>;
+    comment: FormControl<string>;
+  }>;
 
   constructor(private _individualsService: IndividualsService) {}
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      individual_name: new FormControl<string>('', {
+        validators: [Validators.required],
+      }),
+      id_nomenclature_sex: new FormControl<number | null>(null),
+      cd_nom: new FormControl<number | null>(this.cdNom, {
+        validators: [Validators.required],
+      }),
+      // Normally we could avoid providing a form to taxonomy
+      // widget, but it needs it and affecting the entire taxon
+      // to the form control. Creating a temp one to fix this problem
+      cd_nom_temp: new FormControl<number | null>(this.cdNom, {
+        validators: [Validators.required],
+      }),
+      comment: new FormControl<string>(''),
+    });
+  }
 
   taxonSelected(value: NgbTypeaheadSelectItemEvent<Taxon>) {
     this.form.patchValue({ cd_nom: value.item.cd_nom });
