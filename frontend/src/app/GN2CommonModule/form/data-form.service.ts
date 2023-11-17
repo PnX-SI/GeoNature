354b89823a2e673fb7a15084599957f44ae43f62
Andria Capai <andria_capai@natural-solutions.eu>
2023-11-17 12:33:25 +0100

    style: apply prettier again

--- frontend/src/app/GN2CommonModule/form/data-form.service.ts
+++ frontend/src/app/GN2CommonModule/form/data-form.service.ts
@@ -26,7 +26,10 @@ export const FormatMapMime = new Map([
 @Injectable()
 export class DataFormService {
   private _blob: Blob;
-  constructor(private _http: HttpClient, public config: ConfigService) {}
+  constructor(
+    private _http: HttpClient,
+    public config: ConfigService
+  ) {}
 
   getNomenclature(
     codeNomenclatureType: string,
