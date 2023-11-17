369add3951e803cc5ccc275f2356992fb3bd7466
Andria Capai <andria_capai@natural-solutions.eu>
2023-11-17 16:08:10 +0100

    feat: add group inpn to synthese_for_web_app
    Reviewed-by: andriacap

--- backend/geonature/tests/test_synthese.py
+++ backend/geonature/tests/test_synthese.py
@@ -677,6 +677,7 @@ class TestSynthese:
             '"nom_vern"',
             '"group1_inpn"',
             '"group2_inpn"',
+            '"group3_inpn"',
             '"regne"',
             '"phylum"',
             '"classe"',
