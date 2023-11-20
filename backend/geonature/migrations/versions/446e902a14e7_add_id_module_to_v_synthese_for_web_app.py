72791fe49cd87c1de3ab94f0c8067585247fe6e3
Andria Capai <andria_capai@natural-solutions.eu>
2023-11-20 12:14:22 +0100

    feat: add custom op Alembic
    As recommended here by alembic https://alembic.sqlalchemy.org/en/latest/cookbook.html#the-replaceable-object-structure
    
    Reviewed-by: andriacap

--- backend/geonature/migrations/versions/446e902a14e7_add_id_module_to_v_synthese_for_web_app.py
+++ backend/geonature/migrations/versions/446e902a14e7_add_id_module_to_v_synthese_for_web_app.py
@@ -9,6 +9,7 @@ import importlib
 
 from alembic import op
 from sqlalchemy.sql import text
+from geonature.utils import alembic_utils
 
 # revision identifiers, used by Alembic.
 revision = "446e902a14e7"
@@ -17,15 +18,23 @@ branch_labels = None
 depends_on = None
 
 
+view_name = "gn_synthese.v_synthese_for_web_app"
+
+path_synthese = "geonature.migrations.data.core.gn_synthese"
+init_filename = "initial_v_synthese_for_web_app_v1.0.0.sql"
+sql_text = text(importlib.resources.read_text(path_synthese, init_filename))
+v_synthese_for_web_app_init = alembic_utils.ReplaceableObject(view_name, sql_text)
+
+filename = "v_synthese_for_web_app_add_id_module_v1.0.1.sql"
+sql_text = text(importlib.resources.read_text(path_synthese, filename))
+v_synthese_for_web_app = alembic_utils.ReplaceableObject(view_name, sql_text)
+
+
 def upgrade():
-    conn = op.get_bind()
-    path = "geonature.migrations.data.core.gn_synthese"
-    filename = "v_synthese_for_web_app_add_id_module_v1.0.1.sql"
-    conn.execute(text(importlib.resources.read_text(path, filename)))
+    op.drop_view(v_synthese_for_web_app_init)
+    op.create_view(v_synthese_for_web_app)
 
 
 def downgrade():
-    conn = op.get_bind()
-    path = "geonature.migrations.data.core.gn_synthese"
-    filename = "initial_v_synthese_for_web_app_v1.0.0.sql"
-    conn.execute(text(importlib.resources.read_text(path, filename)))
+    op.drop_view(v_synthese_for_web_app)
+    op.create_view(v_synthese_for_web_app_init)
