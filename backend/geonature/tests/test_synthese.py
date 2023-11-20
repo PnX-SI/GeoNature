534951a4a1dcaec6ab48080511762ad9260ebb30
Andria Capai <andria_capai@natural-solutions.eu>
2023-11-20 12:30:37 +0100

    fix: test backend
    Reviewed-by: andriacap

--- backend/geonature/tests/test_synthese.py
+++ backend/geonature/tests/test_synthese.py
@@ -719,7 +719,6 @@ class TestSynthese:
             '"nom_vern"',
             '"group1_inpn"',
             '"group2_inpn"',
-            '"group3_inpn"',
             '"regne"',
             '"phylum"',
             '"classe"',
