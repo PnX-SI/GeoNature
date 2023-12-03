c4833289a7d13f516adc971c3e5b3d81de1722b3
Élie Bouttier <elie@bouttier.eu>
2023-12-03 17:52:08 +0100

    import schema

---
+++ backend/geonature/migrations/versions/92f0083cf735_import_schema.py
@@ -0,0 +1,25 @@
+"""import schema
+
+Revision ID: 92f0083cf735
+Revises: 446e902a14e7
+Create Date: 2023-11-07 16:06:36.745188
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "92f0083cf735"
+down_revision = "d99a7c22cc3c"
+branch_labels = None
+depends_on = ("2b0b3bd0248c",)
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
