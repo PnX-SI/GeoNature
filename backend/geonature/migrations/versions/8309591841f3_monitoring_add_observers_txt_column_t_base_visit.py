b41886f09f218f844b660c0e4e5cd0709f516c12
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2023-12-11 17:06:39 +0100

    [MONITORING - site] - db and model evolution to make site cross module - some fixes on GN2CommonModule forms

---
+++ backend/geonature/migrations/versions/8309591841f3_monitoring_add_observers_txt_column_t_base_visit.py
@@ -0,0 +1,38 @@
+"""[monitoring] add_observers_txt_column_t_base_visit
+
+Revision ID: 8309591841f3
+Revises: 7b6a578eccd7
+Create Date: 2023-10-06 11:07:43.532623
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "8309591841f3"
+down_revision = "7b6a578eccd7"
+branch_labels = None
+depends_on = None
+
+
+monitorings_schema = "gn_monitoring"
+table = "t_base_visits"
+column = "observers_txt"
+
+
+def upgrade():
+    op.add_column(
+        table,
+        sa.Column(
+            column,
+            sa.Text(),
+            nullable=True,
+        ),
+        schema=monitorings_schema,
+    )
+
+
+def downgrade():
+    op.drop_column(table, column, schema=monitorings_schema)
