6bc14c579934edf3c5e5c5862c446032a61b93b2
Élie Bouttier <elie@bouttier.eu>
2023-12-18 10:39:03 +0100

    perf: optimize sensitive obs fixture

--- backend/geonature/tests/fixtures.py
+++ backend/geonature/tests/fixtures.py
@@ -26,7 +26,13 @@ from geonature.core.gn_meta.models import (
     CorDatasetActor,
     CorAcquisitionFrameworkActor,
 )
-from geonature.core.gn_synthese.models import TSources, Synthese, TReport, BibReportsTypes
+from geonature.core.gn_synthese.models import (
+    TSources,
+    Synthese,
+    TReport,
+    BibReportsTypes,
+    corAreaSynthese,
+)
 from geonature.core.sensitivity.models import SensitivityRule, cor_sensitivity_area
 
 from pypnusershub.db.models import (
@@ -44,7 +50,7 @@ from apptax.taxonomie.models import (
     bdc_statut_cor_text_area,
     TaxrefBdcStatutText,
 )
-from ref_geo.models import LAreas
+from ref_geo.models import LAreas, BibAreasTypes
 
 from utils_flask_sqla.tests.utils import JSONClient
 
@@ -399,7 +405,9 @@ def sources_modules(modules):
     return sources
 
 
-def create_synthese(geom, taxon, user, dataset, source, uuid, cor_observers, **kwargs):
+def create_synthese(
+    geom, taxon, user, dataset, source, uuid=func.uuid_generate_v4(), cor_observers=[], **kwargs
+):
     now = datetime.datetime.now()
 
     return Synthese(
@@ -413,7 +421,7 @@ def create_synthese(geom, taxon, user, dataset, source, uuid, cor_observers, **k
         cd_hab=3,
         the_geom_4326=geom,
         the_geom_point=geom,
-        the_geom_local=func.st_transform(geom, 2154),
+        the_geom_local=func.ST_Transform(geom, 2154),  # FIXME
         date_min=now,
         date_max=now,
         cor_observers=cor_observers,
@@ -465,187 +473,120 @@ def synthese_data(app, users, datasets, source, sources_modules):
 def synthese_sensitive_data(app, users, datasets, source):
     data = {}
 
-    # Retrieve all the taxa with a protection status, and the corresponding areas
-    cte_taxa_area_with_status = (
-        select(TaxrefBdcStatutTaxon.cd_nom, LAreas.id_area)
-        .select_from(TaxrefBdcStatutTaxon, LAreas)
-        .join(
-            TaxrefBdcStatutCorTextValues,
-            TaxrefBdcStatutTaxon.id_value_text == TaxrefBdcStatutCorTextValues.id_value_text,
-        )
-        .join(
-            bdc_statut_cor_text_area,
-            LAreas.id_area == bdc_statut_cor_text_area.c.id_area,
-        )
-        .join(
-            TaxrefBdcStatutText, bdc_statut_cor_text_area.c.id_text == TaxrefBdcStatutText.id_text
-        )
-        .where(bdc_statut_cor_text_area.c.id_text == TaxrefBdcStatutCorTextValues.id_text)
-        .where(TaxrefBdcStatutText.enable == True)
-    ).cte("taxa_with_status")
+    sensitive_area, sensitive_area_centroid = db.session.execute(
+        sa.select(LAreas, func.ST_Centroid(LAreas.geom_4326)).where(
+            LAreas.area_type.has(BibAreasTypes.type_code == "DEP"),
+            LAreas.area_code == "03",
+        )  # Allier
+    ).one()
+    unsensitive_area, unsensitive_area_centroid = db.session.execute(
+        sa.select(LAreas, func.ST_Centroid(LAreas.geom_4326)).where(
+            LAreas.area_type.has(BibAreasTypes.type_code == "DEP"),
+            LAreas.area_code == "01",
+        )  # Ain
+    ).one()
 
-    # Retrieve all the taxa with a sensitivity rule, and the corresponding areas
-    cte_taxa_area_with_sensitivity = (
-        select(SensitivityRule.cd_nom, cor_sensitivity_area.c.id_area)
-        .select_from(SensitivityRule)
-        .join(cor_sensitivity_area, SensitivityRule.id == cor_sensitivity_area.c.id_sensitivity)
-        .where(SensitivityRule.active == True)
-    ).cte("taxa_with_sensitivity")
-
-    # Retrieve a cd_nom and point that fit both a sensitivity rule and a protection status
-    sensitive_protected_cd_nom, sensitive_protected_id_area = db.session.execute(
-        select(
-            cte_taxa_area_with_status.c.cd_nom,
-            cte_taxa_area_with_status.c.id_area,
-        )
-        .join(
-            cte_taxa_area_with_sensitivity,
-            sa.and_(
-                cte_taxa_area_with_status.c.cd_nom == cte_taxa_area_with_sensitivity.c.cd_nom,
-                cte_taxa_area_with_status.c.id_area == cte_taxa_area_with_sensitivity.c.id_area,
-            ),
-        )
-        .limit(1)
-    ).first()
+    sensitive_protected_taxon = db.session.execute(
+        select(Taxref).filter_by(cd_nom=139)
+    ).scalar_one()  # Triton crété
     sensitivity_rule = db.session.scalars(
         select(SensitivityRule)
         .where(
-            SensitivityRule.cd_nom == sensitive_protected_cd_nom,
-            SensitivityRule.areas.any(LAreas.id_area == sensitive_protected_id_area),
+            SensitivityRule.cd_nom == sensitive_protected_taxon.cd_nom,
+            SensitivityRule.areas.any(LAreas.id_area == sensitive_area.id_area),
+            SensitivityRule.nomenclature_sensitivity.has(
+                TNomenclatures.cd_nomenclature == "2"
+            ),  # diffusion à la maille 10
         )
         .limit(1)
     ).first()
-    sensitive_protected_area = db.session.scalars(
-        select(LAreas).where(LAreas.id_area == sensitive_protected_id_area).limit(1)
-    ).first()
-    # Get one point inside the area : the centroid (assuming the area is convex)
-    sensitive_protected_point = db.session.scalars(
-        select(func.ST_Centroid(func.ST_Transform(sensitive_protected_area.geom, 4326)))
-    ).first()
-    # Add a criteria to the sensitivity rule if needed
-    id_nomenclature_bio_status = None
-    id_type_nomenclature_bio_status = (
-        db.session.execute(
-            select(BibNomenclaturesTypes).where(BibNomenclaturesTypes.mnemonique == "STATUT_BIO")
-        )
-        .scalar_one()
-        .id_type
-    )
-    id_nomenclature_behaviour = None
-    id_type_nomenclature_behaviour = (
-        db.session.execute(
-            select(BibNomenclaturesTypes).where(
-                BibNomenclaturesTypes.mnemonique == "OCC_COMPORTEMENT"
-            )
-        )
-        .scalar_one()
-        .id_type
-    )
-    # Get one criteria for the sensitivity rule if needed
-    list_criterias_for_sensitivity_rule = sensitivity_rule.criterias
-    if list_criterias_for_sensitivity_rule:
-        one_criteria_for_sensitive_rule = list_criterias_for_sensitivity_rule[0]
-        id_type_criteria_for_sensitive_rule = one_criteria_for_sensitive_rule.id_type
-        if id_type_criteria_for_sensitive_rule == id_type_nomenclature_bio_status:
-            id_nomenclature_bio_status = one_criteria_for_sensitive_rule.id_nomenclature
-        elif id_type_criteria_for_sensitive_rule == id_type_nomenclature_behaviour:
-            id_nomenclature_behaviour = one_criteria_for_sensitive_rule.id_nomenclature
+    assert sensitivity_rule, "Le référentiel de sensibilité ne convient pas aux tests"
+    assert (
+        sensitivity_rule.criterias == []
+    ), "Le référentiel de sensibilité ne convient pas aux tests"
 
-    # Retrieve a cd_nom and point that fit a protection status but no sensitivity rule
-    protected_not_sensitive_cd_nom, protected_not_sensitive_id_area = db.session.execute(
-        select(cte_taxa_area_with_status.c.cd_nom, cte_taxa_area_with_status.c.id_area)
+    unsensitive_protected_taxon = db.session.execute(
+        select(Taxref).filter_by(cd_nom=64357)
+    ).scalar_one()  # Datte de mer
+    sensitivity_rule = db.session.scalars(
+        select(SensitivityRule)
         .where(
-            cte_taxa_area_with_status.c.cd_nom.notin_([cte_taxa_area_with_sensitivity.c.cd_nom])
+            SensitivityRule.cd_nom == sensitive_protected_taxon.cd_nom,
+            SensitivityRule.areas.any(LAreas.id_area == unsensitive_area.id_area),
         )
         .limit(1)
     ).first()
-    protected_not_sensitive_area = db.session.scalars(
-        select(LAreas).where(LAreas.id_area == protected_not_sensitive_id_area).limit(1)
-    ).first()
-    # Get one point inside the area : the centroid (assuming the area is convex)
-    protected_not_sensitive_point = db.session.scalars(
-        select(func.ST_Centroid(func.ST_Transform(protected_not_sensitive_area.geom, 4326)))
-    ).first()
+    assert sensitivity_rule is None, "Le référentiel de sensibilité ne convient pas aux tests"
 
-    with db.session.begin_nested():
-        for name, cd_nom, point, ds, comment_description in [
-            (
-                "obs_sensitive_protected",
-                sensitive_protected_cd_nom,
-                sensitive_protected_point,
-                datasets["own_dataset"],
-                "obs_sensitive_protected",
-            ),
-            (
-                "obs_protected_not_sensitive",
-                protected_not_sensitive_cd_nom,
-                protected_not_sensitive_point,
-                datasets["own_dataset"],
-                "obs_protected_not_sensitive",
-            ),
-            (
-                "obs_sensitive_protected_2",
-                sensitive_protected_cd_nom,
-                sensitive_protected_point,
-                datasets["associate_2_dataset_sensitive"],
-                "obs_sensitive_protected_2",
-            ),
-        ]:
-            unique_id_sinp = func.uuid_generate_v4()
-            geom = point
-            taxon = db.session.execute(select(Taxref).filter_by(cd_nom=cd_nom)).scalar_one()
-            kwargs = {}
-            if id_nomenclature_bio_status:
-                kwargs["id_nomenclature_bio_status"] = id_nomenclature_bio_status
-            elif id_nomenclature_behaviour:
-                kwargs["id_nomenclature_behaviour"] = id_nomenclature_behaviour
-            kwargs["comment_description"] = comment_description
-            s = create_synthese(
-                geom, taxon, users["self_user"], ds, source, unique_id_sinp, [], **kwargs
-            )
-            db.session.add(s)
-            data[name] = s
+    for name, taxon, geom, ds in [
+        (
+            "obs_sensitive_protected",
+            sensitive_protected_taxon,
+            sensitive_area_centroid,
+            datasets["own_dataset"],
+        ),
+        (
+            "obs_protected_not_sensitive",
+            unsensitive_protected_taxon,
+            unsensitive_area_centroid,
+            datasets["own_dataset"],
+        ),
+        (
+            "obs_sensitive_protected_2",
+            sensitive_protected_taxon,
+            sensitive_area_centroid,
+            datasets["associate_2_dataset_sensitive"],
+        ),
+    ]:
+        s = create_synthese(
+            geom,
+            taxon,
+            users["self_user"],
+            ds,
+            source,
+            comment_description=name,
+        )
+        db.session.add(s)
+        data[name] = s
 
-        # Assert that obs_sensitive_protected is a sensitive observation
-    id_nomenclature_not_sensitive = (
-        db.session.execute(
-            select(TNomenclatures)
-            .where(
-                TNomenclatures.nomenclature_type.has(
-                    BibNomenclaturesTypes.mnemonique == "SENSIBILITE"
+    # retrieves sensitive nomenclatures computed by trigger
+    db.session.flush()
+    for s in data.values():
+        db.session.refresh(s)
+
+    assert data["obs_sensitive_protected"].nomenclature_sensitivity.cd_nomenclature == "2"
+    assert data["obs_sensitive_protected_2"].nomenclature_sensitivity.cd_nomenclature == "2"
+    assert data["obs_protected_not_sensitive"].nomenclature_sensitivity.cd_nomenclature == "0"
+
+    for s in data.values():
+        assert db.session.execute(
+            sa.exists(
+                sa.select(sa.literal(1))
+                .select_from(TaxrefBdcStatutTaxon)
+                .join(
+                    TaxrefBdcStatutCorTextValues,
+                    TaxrefBdcStatutCorTextValues.id_value_text
+                    == TaxrefBdcStatutTaxon.id_value_text,
                 )
-            )
-            .where(TNomenclatures.cd_nomenclature == "4")
-        ).scalar_one()
-    ).id_nomenclature
-
-    db.session.scalars(
-        select(Synthese).where(Synthese.cd_nom == sensitive_protected_cd_nom)
-    ).first().id_nomenclature_sensitivity != id_nomenclature_not_sensitive
-
-    # Assert that obs_protected_not_sensitive is not a sensitive observation
-    db.session.scalars(
-        select(Synthese).where(Synthese.cd_nom == protected_not_sensitive_cd_nom)
-    ).first().id_nomenclature_sensitivity == id_nomenclature_not_sensitive
-
-    ## Assert that obs_sensitive_protected and obs_protected_not_sensitive are protected observation
-    def assert_observation_is_protected(name_observation):
-        observation_synthese = data[name_observation]
-        list_areas_observation = [area.id_area for area in observation_synthese.areas]
-        cd_nom = observation_synthese.cd_nom
-        list_id_areas_with_status_for_cd_nom = [
-            tuple_id_area
-            for tuple_id_area in db.session.scalars(
-                select(cte_taxa_area_with_status.c.id_area).where(
-                    cte_taxa_area_with_status.c.cd_nom == cd_nom
+                .join(
+                    TaxrefBdcStatutText,
+                    TaxrefBdcStatutText.id_text == TaxrefBdcStatutCorTextValues.id_text,
                 )
-            ).all()
-        ]
-        # assert that intersection of the two lists is not empty
-        assert set(list_areas_observation).intersection(set(list_id_areas_with_status_for_cd_nom))
-
-    assert_observation_is_protected("obs_sensitive_protected")
-    assert_observation_is_protected("obs_protected_not_sensitive")
+                .join(
+                    bdc_statut_cor_text_area,
+                    bdc_statut_cor_text_area.c.id_text == TaxrefBdcStatutText.id_text,
+                )
+                .join(
+                    LAreas,
+                    LAreas.id_area == bdc_statut_cor_text_area.c.id_area,
+                )
+                .join(corAreaSynthese, corAreaSynthese.c.id_area == LAreas.id_area)
+                .join(Synthese, Synthese.id_synthese == corAreaSynthese.c.id_synthese)
+                .where(Synthese.id_synthese == s.id_synthese)
+                .where(TaxrefBdcStatutTaxon.cd_nom == s.cd_nom)
+                .where(TaxrefBdcStatutText.enable == True)
+            ).select()
+        ).scalar()
 
     return data
 
