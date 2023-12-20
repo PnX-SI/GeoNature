9df80b767c9dc06b1a34e5531ebc2a79306a8725
Pierre Narcisi <pierre.narcisi@mnhn.fr>
2023-12-20 10:28:50 +0100

    feat(import) moving import frontend into the core

---
+++ frontend/src/app/modules/imports/components/import_process/decode-file-step/decode-file-step.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { DataService } from "../../../services/data.service";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { ImportProcessService } from "../import-process.service";
+import { Step } from "../../../models/enums.model";
+import { Import } from "../../../models/import.model";
+import { Observable } from "rxjs";
+import { finalize } from 'rxjs/operators';
+import { ConfigService } from '@geonature/services/config.service';
+
+@Component({
+  selector: "decode-file-step",
+  styleUrls: ["decode-file-step.component.scss"],
+  templateUrl: "decode-file-step.component.html"
+})
+export class DecodeFileStepComponent implements OnInit {
+  public step: Step;
+  public importData: Import;
+  public paramsForm: FormGroup;
+  public isRequestPending: boolean = false; // spinner
+
+  constructor(
+    private fb: FormBuilder,
+    private ds: DataService,
+    private importProcessService: ImportProcessService,
+    private route: ActivatedRoute,
+    public config: ConfigService
+  ) {
+    this.paramsForm = this.fb.group({
+      encoding: [null, Validators.required],
+      format: [null, Validators.required],
+      srid: [null, Validators.required],
+      separator: [null, Validators.required],
+    });
+  }
+
+  ngOnInit() {
+    this.step = this.route.snapshot.data.step;
+    this.importData = this.importProcessService.getImportData();
+    if (this.importData.encoding) {
+      this.paramsForm.patchValue({ encoding: this.importData.encoding });
+    } else if (this.importData.detected_encoding) {
+      this.paramsForm.patchValue({ encoding: this.importData.detected_encoding });
+    }
+    if (this.importData.format_source_file) {
+      this.paramsForm.patchValue({ format: this.importData.format_source_file });
+    } else if (this.importData.detected_format) {
+      this.paramsForm.patchValue({ format: this.importData.detected_format });
+    }
+    if (this.importData.srid) {
+      this.paramsForm.patchValue({ srid: this.importData.srid });
+    }
+    if (this.importData.separator) {
+        this.paramsForm.patchValue({separator: this.importData.separator})
+    } else if (this.importData.detected_separator) {
+      this.paramsForm.patchValue({ separator: this.importData.detected_separator });
+    }
+  }
+
+  onPreviousStep() {
+    this.importProcessService.navigateToPreviousStep(this.step);
+  }
+
+  isNextStepAvailable() {
+    return this.paramsForm.valid;
+  }
+  onSaveData(decode=0) :Observable<Import> {
+      return  this.ds.decodeFile( this.importData.id_import,
+          this.paramsForm.value, decode)
+  }
+  onSubmit() {
+    if (this.paramsForm.pristine && this.importData.step > Step.Decode) {
+        this.importProcessService.navigateToNextStep(this.step);
+        return;
+    }
+    this.isRequestPending = true;
+    this.onSaveData(1).pipe(
+        finalize(() => {
+            this.isRequestPending = false;
+        }),
+    ).subscribe(
+        res => {
+            this.importProcessService.setImportData(res);
+            this.importProcessService.navigateToLastStep();
+        },
+    );
+  }
+}
---
+++ frontend/src/app/modules/imports/components/import_process/import-process.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute, ActivatedRouteSnapshot, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { Step } from '../../models/enums.model';
+
+@Component({
+	selector: 'import-process',
+	styleUrls: ['import-process.component.scss'],
+	templateUrl: 'import-process.component.html'
+})
+export class ImportProcessComponent {
+  public step: Step;
+  public stepComponent;
+
+  constructor(
+      private route: ActivatedRoute,
+      private router: Router,
+  ) {
+    this.router.routeReuseStrategy.shouldReuseRoute = (future: ActivatedRouteSnapshot, current: ActivatedRouteSnapshot) => {
+      if (future.routeConfig === current.routeConfig) {
+        if (current.parent && current.parent.component === ImportProcessComponent) {
+          // reset components on id_import changes
+          return future.params.id_import == current.params.id_import;
+        } else {
+          return true;
+        }
+      } else {
+          return false;
+      }
+    };
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+    ).subscribe(() => {
+      this.step = this.route.snapshot.firstChild.data.step;
+    });
+  }
+
+  onActivate(stepComponent) {
+    this.stepComponent = stepComponent;
+  }
+}
