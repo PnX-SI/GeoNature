1031ec0bc65e58b333c7a8b474bb07a6f52e3a66
Pierre Narcisi <pierre.narcisi@mnhn.fr>
2023-12-20 10:28:50 +0100

    feat(import) moving import frontend into the core

---
+++ frontend/src/app/modules/imports/components/import_process/footer-stepper/footer-stepper.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { Router } from "@angular/router";
+import { DataService } from "../../../services/data.service";
+import { ImportProcessService } from "../import-process.service";
+import { isObservable } from "rxjs";
+import { Import } from "../../../models/import.model";
+import { ConfigService } from '@geonature/services/config.service';
+
+@Component({
+  selector: "footer-stepper",
+  styleUrls: ["footer-stepper.component.scss"],
+  templateUrl: "footer-stepper.component.html"
+})
+export class FooterStepperComponent implements OnInit {
+  @Input() stepComponent;
+
+  constructor(
+    private _router: Router,
+    private _ds: DataService,
+    private importProcessService: ImportProcessService,
+    public config: ConfigService
+  ) { }
+
+  ngOnInit() { }
+
+  deleteImport() {
+    let importData: Import | null = this.importProcessService.getImportData();
+    if (importData) {
+      this._ds.deleteImport(importData.id_import).subscribe(
+        () => { this.leaveImport(); }
+      );
+    } else {
+      this.leaveImport();
+    }
+  }
+
+  saveAndLeaveImport() {
+    if (this.stepComponent.onSaveData !== undefined) {
+      let ret = this.stepComponent.onSaveData();
+      if (isObservable(ret)) {
+        ret.subscribe(() => {
+          this.leaveImport();
+        });
+      } else {
+        this.leaveImport();
+      }
+    } else {
+      this.leaveImport();
+    }
+  }
+
+  leaveImport() {
+    this.importProcessService.resetImportData();
+    this._router.navigate([`${this.config.IMPORT.MODULE_URL}`]);
+  }
+}
---
+++ frontend/src/app/modules/imports/components/import_process/import-process.service.ts
@@ -0,0 +1,92 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Step } from '../../models/enums.model';
+import { Import } from '../../models/import.model';
+import { ConfigService } from '@geonature/services/config.service';
+
+@Injectable()
+export class ImportProcessService {
+    private importData: Import | null = null;
+
+constructor(
+    private router: Router,
+    public config: ConfigService
+) {}
+
+  setImportData(importData: Import) {
+    this.importData  = importData;
+  }
+
+  getImportData(): Import | null {
+    return this.importData;
+  }
+
+  getLastAvailableStep(): Step {
+    let lastAvailableStep = Step.Import;
+    if (!this.importData.full_file_name) {
+      lastAvailableStep = Step.Upload;
+    } else if (!this.importData.columns || !this.importData.columns.length) {
+      lastAvailableStep = Step.Decode;
+    } else if (!this.importData.loaded) {
+      lastAvailableStep = Step.FieldMapping;
+    } else if (!this.importData.contentmapping) {
+      lastAvailableStep = Step.ContentMapping;
+    }
+    return lastAvailableStep;
+  }
+
+  resetImportData() {
+    this.importData = null;
+  }
+
+  getRouterLinkForStep(step: Step) {
+    if (this.importData == null) return null;
+    let stepName = Step[step].toLowerCase();
+    let importId: number = this.importData.id_import;
+    return [this.config.IMPORT.MODULE_URL, 'process', importId, stepName];
+  }
+
+  navigateToStep(step: Step) {
+    this.router.navigate(this.getRouterLinkForStep(step));
+  }
+
+  // If some steps must be skipped, implement it here
+  getPreviousStep(step: Step): Step {
+    let previousStep = step - 1;
+    if (!this.config.IMPORT.ALLOW_VALUE_MAPPING && previousStep === Step.ContentMapping) {
+      previousStep -= 1;
+    }
+    return previousStep;
+  }
+
+  // If some steps must be skipped, implement it here
+  getNextStep(step: Step): Step {
+    let nextStep = step + 1;
+    if (!this.config.IMPORT.ALLOW_VALUE_MAPPING && nextStep === Step.ContentMapping) {
+      nextStep += 1;
+    }
+    return nextStep;
+  }
+
+  navigateToPreviousStep(step: Step) {
+    this.navigateToStep(this.getPreviousStep(step));
+  }
+
+  navigateToNextStep(step: Step) {
+    this.navigateToStep(this.getNextStep(step));
+  }
+
+  navigateToLastStep() {
+    this.navigateToStep(this.getLastAvailableStep());
+  }
+
+  beginProcess(datasetId: number) {
+    const link = [this.config.IMPORT.MODULE_URL, 'process', Step[Step.Upload].toLowerCase()];
+	   this.router.navigate(link, { queryParams: { datasetId: datasetId } });
+  }
+
+  continueProcess(importData: Import) {
+    this.importData = importData;
+    this.navigateToStep(this.getLastAvailableStep());
+  }
+}
