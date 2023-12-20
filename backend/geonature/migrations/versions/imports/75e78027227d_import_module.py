909009a3d6224417c395cce367cd313f3b458892
Pierre Narcisi <pierre.narcisi@mnhn.fr>
2023-12-20 16:27:54 +0100

    Feat(import) remove ng_module from import

--- backend/geonature/migrations/versions/imports/75e78027227d_import_module.py
+++ backend/geonature/migrations/versions/imports/75e78027227d_import_module.py
@@ -31,8 +31,7 @@ def upgrade():
                 module_path,
                 module_target,
                 active_frontend,
-                active_backend,
-                ng_module  -- FIXME
+                active_backend
             )
         VALUES (
             'IMPORT',
@@ -41,8 +40,7 @@ def upgrade():
             'import',
             '_self',
             TRUE,
-            TRUE,
-            'import' -- FIXME
+            TRUE
         )
         """
     )
