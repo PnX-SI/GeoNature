056a5c81c450e2ca55573b4852fde16493249e11
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2023-12-21 19:12:49 +0100

    feat(api): add marking schema & relationship

--- backend/geonature/core/gn_monitoring/schema.py
+++ backend/geonature/core/gn_monitoring/schema.py
@@ -2,10 +2,18 @@ from marshmallow import fields
 
 from geonature.core.gn_commons.schemas import ModuleSchema
 from geonature.utils.env import MA
-from geonature.core.gn_monitoring.models import TIndividuals
+from geonature.core.gn_monitoring.models import TIndividuals, TMarkingEvent
 from pypnnomenclature.schemas import NomenclatureSchema
 from pypnusershub.schemas import UserSchema
 
+
+class TMarkingEventSchema(MA.SQLAlchemyAutoSchema):
+    class Meta:
+        model = TMarkingEvent
+        include_fk = True
+        load_instance = True
+
+    operator = MA.Nested(UserSchema, dump_only=True)
 
 class TIndividualsSchema(MA.SQLAlchemyAutoSchema):
     class Meta:
@@ -16,3 +24,4 @@ class TIndividualsSchema(MA.SQLAlchemyAutoSchema):
     nomenclature_sex = MA.Nested(NomenclatureSchema, dump_only=True)
     digitiser = MA.Nested(UserSchema, dump_only=True)
     modules = fields.List(MA.Nested(ModuleSchema, dump_only=True))
+    markings = fields.List(MA.Nested(TMarkingEventSchema, dump_only=True))
