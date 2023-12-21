757322a12289946c94ab78e8723019c8c1d1c91f
Élie Bouttier <elie@bouttier.eu>
2023-12-21 12:58:43 +0100

    lint(import) apply prettier to imported code

--- frontend/src/app/modules/imports/models/mapping.model.ts
+++ frontend/src/app/modules/imports/models/mapping.model.ts
@@ -1,34 +1,31 @@
-import { Step } from "./enums.model";
-import { Cruved } from "./cruved.model";
+import { Step } from './enums.model';
+import { Cruved } from './cruved.model';
 
 interface Mapping {
-    id: number;
-    label: string;
-    type: string;
-    active: boolean;
-    public: boolean;
-    cruved: Cruved;
+  id: number;
+  label: string;
+  type: string;
+  active: boolean;
+  public: boolean;
+  cruved: Cruved;
 }
 
 export interface FieldMappingValues {
-    [propName: string]: string | string[];
+  [propName: string]: string | string[];
 }
-
 
 export interface FieldMapping extends Mapping {
-    values: FieldMappingValues;
+  values: FieldMappingValues;
 }
-
 
 interface ContentMappingNomenclatureValues {
-    [propName: string]: string;  // source value: target cd_nomenclature
+  [propName: string]: string; // source value: target cd_nomenclature
 }
 
-
 export interface ContentMappingValues {
-    [mnemonique: string]: ContentMappingNomenclatureValues;
+  [mnemonique: string]: ContentMappingNomenclatureValues;
 }
 
 export interface ContentMapping extends Mapping {
-    values: ContentMappingValues;
+  values: ContentMappingValues;
 }
