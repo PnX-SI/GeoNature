93a7659b112b91f7c08f554a0fb80808dcb6181e
Élie Bouttier <elie@bouttier.eu>
2023-12-21 12:58:43 +0100

    lint(import) apply prettier to imported code

--- frontend/src/app/modules/imports/components/import_errors/import_errors.component.ts
+++ frontend/src/app/modules/imports/components/import_errors/import_errors.component.ts
@@ -1,37 +1,37 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, ActivatedRoute } from "@angular/router";
-import { ImportProcessService } from "../import_process/import-process.service";
+import { Router, ActivatedRoute } from '@angular/router';
+import { ImportProcessService } from '../import_process/import-process.service';
 
 import { Import, ImportError } from '../../models/import.model';
 import { DataService } from '../../services/data.service';
 
 @Component({
-    selector: 'pnx-import-errors',
-    templateUrl: 'import_errors.component.html',
-    styleUrls: ["import_errors.component.scss"],
-
+  selector: 'pnx-import-errors',
+  templateUrl: 'import_errors.component.html',
+  styleUrls: ['import_errors.component.scss'],
 })
-
 export class ImportErrorsComponent implements OnInit {
-    public importData: Import;
-    public importErrors: Array<ImportError> = null;
-    public importWarnings: Array<ImportError> = null;
+  public importData: Import;
+  public importErrors: Array<ImportError> = null;
+  public importWarnings: Array<ImportError> = null;
 
-    constructor(
-        private _router: Router,
-        private _route: ActivatedRoute,
-        private _ds: DataService,
-    ) {
-      _router.routeReuseStrategy.shouldReuseRoute = () => false; // reset component on importId change
-    }
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _ds: DataService
+  ) {
+    _router.routeReuseStrategy.shouldReuseRoute = () => false; // reset component on importId change
+  }
 
-    ngOnInit() {
-      this.importData = this._route.snapshot.data.importData;
-      this._ds.getImportErrors(this.importData.id_import).subscribe(
-        errors => {
-            this.importErrors = errors.filter(err => {return err.type.level === "ERROR" });
-            this.importWarnings = errors.filter(err => {return err.type.level === "WARNING"})
-            },
-      );
-    }
+  ngOnInit() {
+    this.importData = this._route.snapshot.data.importData;
+    this._ds.getImportErrors(this.importData.id_import).subscribe((errors) => {
+      this.importErrors = errors.filter((err) => {
+        return err.type.level === 'ERROR';
+      });
+      this.importWarnings = errors.filter((err) => {
+        return err.type.level === 'WARNING';
+      });
+    });
+  }
 }
