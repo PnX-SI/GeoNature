a0e85a8301d8743a99718506715a6f90382883b3
Élie Bouttier <elie@bouttier.eu>
2024-01-09 02:23:55 +0100

    fix(celery): move session.remove() in app context

--- backend/geonature/tasks/__init__.py
+++ backend/geonature/tasks/__init__.py
@@ -1,9 +1,1 @@
-from celery.signals import task_postrun
 
-from geonature.utils.env import db
-from geonature.utils.celery import celery_app
-
-
-@task_postrun.connect
-def close_session(*args, **kwargs):
-    db.session.remove()
