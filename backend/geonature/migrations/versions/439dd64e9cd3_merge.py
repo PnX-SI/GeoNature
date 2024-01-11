13358b11d22ac6192af2e161c7ea7488bab1382c
Élie Bouttier <elie@bouttier.eu>
2024-01-11 17:49:32 +0100

    add merge revision following rebase on develop

---
+++ backend/geonature/migrations/versions/439dd64e9cd3_merge.py
@@ -0,0 +1,24 @@
+"""merge
+
+Revision ID: 439dd64e9cd3
+Revises: 92f0083cf735
+Create Date: 2024-01-11 17:40:15.162675
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "439dd64e9cd3"
+down_revision = ("92f0083cf735", "5a2c9c65129f")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
