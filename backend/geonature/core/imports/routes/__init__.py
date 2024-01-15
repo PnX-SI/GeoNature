ef59a981bfc68264748688f43b4233ed4d1aa0b7
Pierre Narcisi <pierre.narcisi@mnhn.fr>
2024-01-15 10:50:53 +0100

    Feat(import) fetch module in destination for datasets

--- backend/geonature/core/imports/routes/__init__.py
+++ backend/geonature/core/imports/routes/__init__.py
@@ -1,8 +1,10 @@
 from geonature.core.gn_permissions.decorators import login_required
 
 from geonature.core.imports.models import Destination
+from sqlalchemy.orm import joinedload
 from geonature.core.imports.schemas import DestinationSchema
 from geonature.core.imports.blueprint import blueprint
+from geonature.utils.env import db
 
 
 @blueprint.route("/destinations/", methods=["GET"])
@@ -12,3 +14,15 @@ def list_destinations():
     destinations = Destination.query.all()
     # FIXME: filter with C permissions?
     return schema.dump(destinations, many=True)
+
+
+@blueprint.route("/destination/<destinationCode>", methods=["GET"])
+@login_required
+def get_destination(destinationCode):
+    schema = DestinationSchema(only=["module"])
+    destination = db.session.execute(
+        db.select(Destination)
+        .options(joinedload("module"))
+        .where(Destination.code == destinationCode)
+    ).scalar_one_or_none()
+    return schema.dump(destination)
