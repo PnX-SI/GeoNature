d6db822b0ba973b06c009e894603678c7364a5fe
Élie Bouttier <elie@bouttier.eu>
2024-01-25 15:17:28 +0100

    fix(tests/import): fixtures

--- backend/geonature/tests/imports/fixtures.py
+++ backend/geonature/tests/imports/fixtures.py
@@ -20,7 +20,7 @@ def default_destination(app):
         if (
             app.url_map.is_endpoint_expecting(endpoint, "destination")
             and "destination" not in values
-            and g.default_destination
+            and hasattr(g, "default_destination")
         ):
             values["destination"] = g.default_destination.code
 
