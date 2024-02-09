64aff5c4a8a12959eaeae4434c60fb70788be8f2
Jacobe2169 <jacques.fize@ecrins-parcnational.fr>
2024-02-09 13:15:52 +0100

    fix migration

--- backend/geonature/migrations/versions/9f4db1786c22_add_group3_inpn_to_synthese_views.py
+++ backend/geonature/migrations/versions/9f4db1786c22_add_group3_inpn_to_synthese_views.py
@@ -11,7 +11,7 @@ from sqlalchemy.sql import text
 
 # revision identifiers, used by Alembic.
 revision = "9f4db1786c22"
-down_revision = "446e902a14e7"
+down_revision = "5a2c9c65129f"
 branch_labels = None
 depends_on = ("c4415009f164",)  # Taxref v15 db structure
 
