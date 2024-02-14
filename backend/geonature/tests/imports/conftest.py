2d9da5b949b6ee101fd9885e6e9eaa82cfbfafd5
Jacobe2169 <jacques.fize@ecrins-parcnational.fr>
2024-02-14 11:19:55 +0100

    fix backend test

--- backend/geonature/tests/imports/conftest.py
+++ backend/geonature/tests/imports/conftest.py
@@ -1,6 +1,8 @@
+import pytest
+
 from geonature.tests.fixtures import *
-from geonature.tests.fixtures import app, _session, users
 from pypnusershub.tests.fixtures import teardown_logout_user
+
 from .fixtures import *
 
 
