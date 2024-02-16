d81efc570a6b99559e7dfe53913f839cd9283017
Jacobe2169 <jacques.fize@ecrins-parcnational.fr>
2024-02-16 11:27:09 +0100

    debug test for public access + add docstring to `test_users_login.py`

--- backend/geonature/tests/test_users_login.py
+++ backend/geonature/tests/test_users_login.py
@@ -15,8 +15,21 @@ from .utils import logged_user_headers
 class TestUsersLogin:
     @pytest.fixture
     def user(self, app):
+        """
+        Create a user for testing.
+
+        Parameters
+        ----------
+        app : pytest.FixtureDef
+            geonature app fixture
+
+        Returns
+        -------
+        pytest.FixtureDef
+            fake user fixture
+        """
         with db.session.begin_nested():
-            user = User(groupe=False, active=True, identifiant="user", password="password")
+            user = User(groupe=False, active=True, identifiant="bob", password="password")
             db.session.add(user)
             application = db.session.scalars(
                 select(Application).filter_by(code_application=app.config["CODE_APPLICATION"])
@@ -31,11 +44,33 @@ class TestUsersLogin:
         return user
 
     def test_current_user(self, app, user):
+        """
+        Test if the desired user logged in is the one available in the `g.current_user` global variable.
+
+        Parameters
+        ----------
+        app : pytest.FixtureDef
+            geonature app fixture
+        users : pytest.FixtureDef
+            user fixture
+        """
         with app.test_request_context(headers=logged_user_headers(user)):
             app.preprocess_request()
             assert g.current_user == user
 
     def test_public_user(self, app, user, monkeypatch):
+        """
+        Test if the public access is working
+
+        Parameters
+        ----------
+        app : pytest.FixtureDef
+            geonature app fixture
+        users : pytest.FixtureDef
+            user fixture
+        monkeypatch : pytest.FixtureDef
+            pytest fixture to patch/set environment variables
+        """
         monkeypatch.setitem(app.config, "PUBLIC_ACCESS_USERNAME", user.identifiant)
         response = self.client.post(url_for("auth.public_login"))
         assert response.status_code == 200
