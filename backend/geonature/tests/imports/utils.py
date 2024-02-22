df3a653df2ccfd7cbbbc6649be31c65b0a1dca73
Élie Bouttier <elie@bouttier.eu>
2024-02-22 14:58:33 +0100

    fix(import): entity & errors
    - small fix regarding report of entity in sql checks
    - replace unique constraint on (id_import, id_error, column_error) by two partial unique indexes:
      - (id_import, id_error, column_error) WHEN id_entity IS NULL
      - (id_import, id_entity, id_error, column_error) WHEN id_entity IS NOT NULL
    
    tests(import): assert errors entity

--- backend/geonature/tests/imports/utils.py
+++ backend/geonature/tests/imports/utils.py
@@ -5,13 +5,30 @@ from geonature.utils.env import db
 from geonature.core.imports.models import ImportUserErrorType
 
 
-def assert_import_errors(imprt, expected_errors):
+def assert_import_errors(imprt, expected_errors, entity_code=None):
+    """
+    expected_errors must be a set of 4-elements tuples:
+        (error_name: str, entity_code: str, error_column:str , error_rows: frozenset<int>)
+    if entity_code is set, expected_errors must be a set of 3-elements tuples:
+        (error_name: str, error_column:str , error_rows: frozenset<int>)
+    """
+    if entity_code is not None:
+        expected_errors = {
+            (error_name, entity_code, error_column, error_rows)
+            for error_name, error_column, error_rows in expected_errors
+        }
     errors = {
-        (error.type.name, error.column, frozenset(error.rows or [])) for error in imprt.errors
+        (
+            error.type.name,
+            error.entity.code if error.entity else None,
+            error.column,
+            frozenset(error.rows or []),
+        )
+        for error in imprt.errors
     }
     assert errors == expected_errors
     expected_erroneous_rows = set()
-    for error_type, _, rows in expected_errors:
+    for error_type, _, _, rows in expected_errors:
         error_type = ImportUserErrorType.query.filter_by(name=error_type).one()
         if error_type.level == "ERROR":
             expected_erroneous_rows |= set(rows)
