9c5d7b5edc55d28be55b04e95c022e2338b3010f
Jacobe2169 <jacques.fize@ecrins-parcnational.fr>
2024-02-29 15:50:24 +0100

    debug import list filters

--- frontend/src/app/modules/imports/components/import_list/import-list.component.ts
+++ frontend/src/app/modules/imports/components/import_list/import-list.component.ts
@@ -28,14 +28,17 @@ export class ImportListComponent implements OnInit {
   public total: number;
   public offset: number;
   public limit: number;
-  public search_string: string = '';
+
   public sort: string;
   public dir: string;
   public runningImport: Array<number> = [];
   public inErrorImport: Array<number> = [];
   public checkingImport: Array<number> = [];
   private fetchTimeout: any;
-  private destCode: string = '';
+
+  /* Filter value storage */
+  private destinationCode: string;
+  public searchString: string = '';
 
   constructor(
     public _cruvedStore: CruvedStoreService,
@@ -46,36 +49,54 @@ export class ImportListComponent implements OnInit {
     private importProcessService: ImportProcessService,
     public _csvExport: CsvExportService,
     public config: ConfigService
-  ) {}
+  ) { }
 
+  /**
+   * Initialize the import list
+   */
   ngOnInit() {
-    this.onImportList(1);
+    this.onImportList();
+
     this.fetchTimeout = setTimeout(() => {
       this.updateImports();
     }, 15000);
+
+    // Delay the query to retrieve imports from the database
+    this.search.valueChanges.subscribe((value) => {
+      setTimeout(() => {
+        if (value == this.search.value) {
+          this.updateSearchQuery();
+        }
+      }, 500);
+    });
   }
 
+  /**
+   * Destroy the timeout
+   */
   ngOnDestroy() {
     clearTimeout(this.fetchTimeout);
     this._ds.getImportList({}).subscribe().unsubscribe();
   }
 
-  updateFilter(val: any) {
-    const value = val.toString().toLowerCase().trim();
-    this.search_string = value;
-    this.onImportList(1);
-
-    // listes des colonnes selon lesquelles filtrer
+  /**
+   * Update the searchString attribute and refresh the import list view
+   */
+  updateSearchQuery() {
+    this.searchString = this.search.value;
+    this.onImportList();
   }
 
-  updateDest(destCode: string) {
-    this.destCode = destCode;
-    this.onImportList(1);
-  }
 
-  private onImportList(page) {
+  /**
+   * Refresh the import list based on the page number and value in the filters
+   * @param {object} [params={}]
+   */
+  private onImportList(params: object = {}) {
+    let default_params = { page: 1, search: this.searchString }
+    default_params = { ...default_params, ...params };
     this._ds
-      .getImportList({ page: page, search: this.search_string }, this.destCode)
+      .getImportList(default_params, this.selectDestinationForm.value)
       .subscribe((res) => {
         this.history = res['imports'];
         this.getImportsStatus();
@@ -85,42 +106,46 @@ export class ImportListComponent implements OnInit {
         this.total = res['count'];
         this.limit = res['limit'];
         this.offset = res['offset'];
+        this.fetchTimeout = setTimeout(() => {
+          this.updateImports();
+        }, 15000);
       });
   }
+
+  /**
+   * Return import status
+   */
   private getImportsStatus() {
-    this.history.forEach((h) => {
-      if (h.task_id !== null && h.task_progress !== null) {
-        if (h.task_progress == -1) {
-          this.inErrorImport.push(h.id_import);
-        } else if (h.processed) {
-          this.runningImport.push(h.id_import);
+
+    this.history.forEach((hist) => {
+      if (hist.task_id !== null && hist.task_progress !== null) {
+        if (hist.task_progress == -1) {
+          this.inErrorImport.push(hist.id_import);
+        } else if (hist.processed) {
+          this.runningImport.push(hist.id_import);
         } else {
-          this.checkingImport.push(h.id_import);
+          this.checkingImport.push(hist.id_import);
         }
       }
     });
   }
+
   private resetImportInfos() {
     this.checkingImport = this.inErrorImport = this.runningImport = [];
   }
+
+  
   private updateImports() {
-    let params = { page: this.offset + 1, search: this.search_string };
+    let params = { page: this.offset + 1, search: this.searchString };
     if (this.sort) {
       params['sort'] = this.sort;
     }
     if (this.dir) {
       params['sort_dir'] = this.dir;
     }
-    this._ds.getImportList(params).subscribe((res) => {
-      this.history = res['imports'];
-      this.checkingImport = [];
-      this.getImportsStatus();
-      this.filteredHistory = this.history;
-      this.fetchTimeout = setTimeout(() => {
-        this.updateImports();
-      }, 15000);
-    });
+    this.onImportList(params);
   }
+  
   onFinishImport(data: Import) {
     clearTimeout(this.fetchTimeout);
     this.importProcessService.continueProcess(data);
@@ -145,41 +170,20 @@ export class ImportListComponent implements OnInit {
 
   onSort(e) {
     let sort = e.sorts[0];
-    let params = { page: 1, search: this.search_string, sort: sort.prop, sort_dir: sort.dir };
-    this._ds.getImportList(params).subscribe((res) => {
-      this.history = res['imports'];
-      this.filteredHistory = this.history;
-      this.empty = res.length == 0;
-      this.total = res['count'];
-      this.limit = res['limit'];
-      this.offset = res['offset'];
-      this.sort = sort.prop;
-      this.dir = sort.dir;
-    });
+    let params = { page: 1, search: this.searchString, sort: sort.prop, sort_dir: sort.dir };
+    this.sort = sort.prop;
+    this.dir = sort.dir;
+    this.onImportList(params)
   }
   setPage(e) {
-    let params = { page: e.offset + 1, search: this.search_string };
+    let params = { page: e.offset + 1, search: this.searchString };
     if (this.sort) {
       params['sort'] = this.sort;
     }
     if (this.dir) {
       params['sort_dir'] = this.dir;
     }
-    this._ds.getImportList(params).subscribe(
-      (res) => {
-        this.history = res['imports'];
-        this.filteredHistory = this.history;
-        this.empty = res.length == 0;
-        this.total = res['count'];
-        this.limit = res['limit'];
-        this.offset = res['offset'];
-      },
-      (error) => {
-        if (error.status === 404) {
-          this._commonService.regularToaster('warning', 'Aucun import trouvé');
-        }
-      }
-    );
+    this.onImportList(params);
   }
   getTooltip(row, tooltipType) {
     if (!row?.cruved?.U) {
