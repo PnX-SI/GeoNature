0fcd5f53883992dbb08d61b63c3a8b28be01fbf7
Jacobe2169 <jacques.fize@ecrins-parcnational.fr>
2024-03-01 15:09:51 +0100

    add Module ts interface + fix bad redirect of `Accueil` button in HomePage

---
+++ frontend/src/app/models/module.model.ts
@@ -0,0 +1,8 @@
+export interface Module{
+    id_module: number;
+    module_label: string;
+    module_picto?: string;
+    module_path:string;
+    module_doc_url?: string;
+    module_url?: string;
+}
