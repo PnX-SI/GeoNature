f44abbe1df33cc9e7c98a0820afbe6241826282b
Jacobe2169 <jacques.fize@ecrins-parcnational.fr>
2024-03-04 15:25:43 +0100

    add performance for occtax

---
+++ backend/geonature/tests/benchmarks/test_benchmark_occtax.py
@@ -0,0 +1,27 @@
+import logging
+import pytest
+from geonature.tests.benchmarks import *
+from geonature.tests.test_pr_occhab import stations
+
+from .benchmark_generator import BenchmarkTest, CLater
+
+logging.basicConfig()
+logger = logging.getLogger("logger-name")
+logger.setLevel(logging.DEBUG)
+
+from .utils import CLIENT_GET, CLIENT_POST
+
+
+@pytest.mark.usefixtures("client_class", "temporary_transaction")
+class TestBenchmarkOcctax:
+
+    test_occtax_list_releves_restricted = BenchmarkTest(
+        CLIENT_GET,
+        [CLater("""url_for("pr_occtax.getReleves")""")],
+        dict(user_profile="user_restricted_occhab", fixtures=[]),
+    )()
+    test_occtax_list_releves_unrestricted = BenchmarkTest(
+        CLIENT_GET,
+        [CLater("""url_for("pr_occtax.getReleves")""")],
+        dict(user_profile="admin_user", fixtures=[]),
+    )()
