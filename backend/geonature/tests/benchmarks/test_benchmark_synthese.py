cdc660c795a8d63aa1dca89eca62f64cb03bc327
Jacobe2169 <jacques.fize@ecrins-parcnational.fr>
2024-03-04 15:25:59 +0100

    add performance test in synthese with blurring

--- backend/geonature/tests/benchmarks/test_benchmark_synthese.py
+++ backend/geonature/tests/benchmarks/test_benchmark_synthese.py
@@ -27,6 +27,7 @@ class TestBenchmarkSynthese:
             json=benchmark_synthese_intersection_data_test_bbox,
         ),
     )()
+
     test_synthese_with_geometry_complex_poly = BenchmarkTest(
         CLIENT_POST,
         [CLater("""url_for("gn_synthese.get_observations_for_web")""")],
@@ -65,6 +66,58 @@ class TestBenchmarkSynthese:
         [CLater("""url_for("gn_synthese.get_observations_for_web")""")],
         dict(
             user_profile="admin_user",
+            json=benchmark_synthese_with_tree_taxon,
+        ),
+    )()
+
+    ### WITH BLURRING
+    test_synthese_with_geometry_bbox_with_blurring = BenchmarkTest(
+        CLIENT_POST,
+        [CLater("""url_for("gn_synthese.get_observations_for_web")""")],
+        dict(
+            user_profile="user_with_blurring",
+            json=benchmark_synthese_intersection_data_test_bbox,
+        ),
+    )()
+
+    test_synthese_with_geometry_complex_poly_with_blurring = BenchmarkTest(
+        CLIENT_POST,
+        [CLater("""url_for("gn_synthese.get_observations_for_web")""")],
+        dict(
+            user_profile="user_with_blurring",
+            json=benchmark_synthese_intersection_data_test_complex_polygon,
+        ),
+    )()
+    test_synthese_with_commune_with_blurring = BenchmarkTest(
+        CLIENT_POST,
+        [CLater("""url_for("gn_synthese.get_observations_for_web")""")],
+        dict(
+            user_profile="user_with_blurring",
+            json=CLater("benchmark_data.benchmark_synthese_intersection_data_test_commune()"),
+        ),
+    )()
+
+    test_synthese_with_departement_with_blurring = BenchmarkTest(
+        CLIENT_POST,
+        [CLater("""url_for("gn_synthese.get_observations_for_web")""")],
+        dict(
+            user_profile="user_with_blurring",
+            json=CLater("benchmark_data.benchmark_synthese_intersection_data_test_departement()"),
+        ),
+    )()
+    test_synthese_with_region_with_blurring = BenchmarkTest(
+        CLIENT_POST,
+        [CLater("""url_for("gn_synthese.get_observations_for_web")""")],
+        dict(
+            user_profile="user_with_blurring",
+            json=CLater("benchmark_data.benchmark_synthese_intersection_data_test_region()"),
+        ),
+    )()
+    test_synthese_with_up_tree_taxon_with_blurring = BenchmarkTest(
+        CLIENT_POST,
+        [CLater("""url_for("gn_synthese.get_observations_for_web")""")],
+        dict(
+            user_profile="user_with_blurring",
             json=benchmark_synthese_with_tree_taxon,
         ),
     )()
