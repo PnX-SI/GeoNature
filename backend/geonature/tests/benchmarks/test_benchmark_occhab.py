ba5c1a1a3a6ed26108adf1ce93ad7a4a94605f26
Jacobe2169 <jacques.fize@ecrins-parcnational.fr>
2024-03-04 15:25:28 +0100

    add performance test of occhab

--- backend/geonature/tests/benchmarks/test_benchmark_occhab.py
+++ backend/geonature/tests/benchmarks/test_benchmark_occhab.py
@@ -15,8 +15,20 @@ from .utils import CLIENT_GET, CLIENT_POST
 @pytest.mark.usefixtures("client_class", "temporary_transaction")
 class TestBenchmarkOcchab:
 
-    test_get_station = BenchmarkTest(
+    test_occhab_get_station = BenchmarkTest(
         CLIENT_GET,
         [CLater("""url_for("occhab.get_station", id_station=8)""")],
         dict(user_profile="user", fixtures=[stations]),
+    )()
+
+    test_occhab_list_stations = BenchmarkTest(
+        CLIENT_GET,
+        [CLater("""url_for("occhab.list_stations")""")],
+        dict(user_profile="admin_user", fixtures=[]),
+    )()
+
+    test_occhab_list_stations_restricted = BenchmarkTest(
+        CLIENT_GET,
+        [CLater("""url_for("occhab.list_stations")""")],
+        dict(user_profile="user_restricted_occhab", fixtures=[]),
     )()
