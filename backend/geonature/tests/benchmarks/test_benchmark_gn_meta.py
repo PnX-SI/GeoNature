1588b894eecf84b9637c61edf24aa585e7fb8ed8
Jacobe2169 <jacques.fize@ecrins-parcnational.fr>
2024-03-07 16:36:54 +0100

    add benchmark test for gn_meta

---
+++ backend/geonature/tests/benchmarks/test_benchmark_gn_meta.py
@@ -0,0 +1,28 @@
+import logging
+import pytest
+from geonature.tests.benchmarks import *
+from geonature.tests.test_pr_occhab import stations
+
+from .benchmark_generator import BenchmarkTest, CLater
+
+logging.basicConfig()
+logger = logging.getLogger("logger-name")
+logger.setLevel(logging.DEBUG)
+
+from .utils import CLIENT_GET, CLIENT_POST
+
+
+@pytest.mark.benchmark(group="gn_meta")
+@pytest.mark.usefixtures("client_class", "temporary_transaction")
+class TestBenchmarkGnMeta:
+
+    test_list_acquisition_frameworks = BenchmarkTest(
+        CLIENT_GET,
+        [CLater("""url_for("gn_meta.get_acquisition_frameworks_list")""")],
+        dict(user_profile="admin_user", fixtures=[]),
+    )()
+    test_list_datasets = BenchmarkTest(
+        CLIENT_GET,
+        [CLater("""url_for("gn_meta.get_datasets")""")],
+        dict(user_profile="admin_user", fixtures=[]),
+    )()
