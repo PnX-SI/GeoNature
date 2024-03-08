0c9cf4fd12f1d24ed42c68890ccb6192dad094d4
Jacobe2169 <jacques.fize@ecrins-parcnational.fr>
2024-03-08 11:11:55 +0100

    translate to sqla op

--- contrib/occtax/backend/occtax/migrations/43ecf0836b4a_invert_count_default.py
+++ contrib/occtax/backend/occtax/migrations/43ecf0836b4a_invert_count_default.py
@@ -5,6 +5,7 @@ Revises: e170d1902137
 Create Date: 2024-01-23 09:36:40.189931
 
 """
+
 from alembic import op
 import sqlalchemy as sa
 
@@ -17,18 +18,30 @@ depends_on = None
 
 
 def upgrade():
-    op.execute(
-        """
-
-        ALTER TABLE pr_occtax.cor_counting_occtax
-        ALTER COLUMN id_nomenclature_sex SET DEFAULT pr_occtax.get_default_nomenclature_value('SEXE');
-
-        ALTER TABLE pr_occtax.cor_counting_occtax  
-        ALTER COLUMN id_nomenclature_obj_count SET DEFAULT pr_occtax.get_default_nomenclature_value('OBJ_DENBR');
-
-        """
+    op.alter_column(
+        "cor_counting_occtax",
+        "id_nomenclature_sex",
+        server_default=sa.func.pr_occtax.get_default_nomenclature_value("SEXE"),
+        schema="pr_occtax",
+    )
+    op.alter_column(
+        "cor_counting_occtax",
+        "id_nomenclature_obj_count",
+        server_default=sa.func.pr_occtax.get_default_nomenclature_value("OBJ_DENBR"),
+        schema="pr_occtax",
     )
 
 
 def downgrade():
-    pass
+    op.alter_column(
+        "cor_counting_occtax",
+        "id_nomenclature_sex",
+        server_default=sa.func.pr_occtax.get_default_nomenclature_value("OBJ_DENBR"),
+        schema="pr_occtax",
+    )
+    op.alter_column(
+        "cor_counting_occtax",
+        "id_nomenclature_obj_count",
+        server_default=sa.func.pr_occtax.get_default_nomenclature_value("SEXE"),
+        schema="pr_occtax",
+    )
