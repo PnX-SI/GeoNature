5c0a6082f8fb887e8f181a59049c7e08b155b630
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-03-18 14:37:40 +0100

    add CruvedWithScope interface + function to parse the latter to boolean (Cruved interface)

--- frontend/src/app/modules/imports/models/cruved.model.ts
+++ frontend/src/app/modules/imports/models/cruved.model.ts
@@ -6,3 +6,23 @@ export interface Cruved {
   E: boolean;
   D: boolean;
 }
+
+export interface CruvedWithScope {
+  C: number;
+  R: number;
+  U: number;
+  V: number;
+  E: number;
+  D: number;
+}
+
+export function toBooleanCruved(cruved: CruvedWithScope,compare_function:Function = (c)=> c>0): Cruved {
+  return Object.assign(
+    {},
+    ...Object.entries(
+      cruved as CruvedWithScope
+    ).map(([key, value]) => ({
+      [key]: value > 0,
+    }))
+  );
+}
