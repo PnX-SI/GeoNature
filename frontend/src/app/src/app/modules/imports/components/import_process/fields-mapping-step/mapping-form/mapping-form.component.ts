cf243f818049f4563256795aac8c010d3d50da83
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-03-20 13:56:38 +0100

    (mapping, import) add new components

---
+++ frontend/src/app/src/app/modules/imports/components/import_process/fields-mapping-step/mapping-form/mapping-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pnx-mapping-form',
+  templateUrl: './mapping-form.component.html',
+  styleUrls: ['./mapping-form.component.scss']
+})
+export class MappingFormComponent {
+
+}
