fe5d8575b8859061a6303a9fc4ddcf4e18f3c0ac
Pierre Narcisi <pierre.narcisi@mnhn.fr>
2024-03-20 16:27:45 +0100

    Feat(mapping) adding mapping-themes

--- frontend/src/app/modules/imports/components/import_process/fields-mapping-step/mapping-theme/mapping-theme.component.ts
+++ frontend/src/app/modules/imports/components/import_process/fields-mapping-step/mapping-theme/mapping-theme.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Field } from '@geonature/modules/imports/models/mapping.model';
 
 @Component({
   selector: 'pnx-mapping-theme',
   templateUrl: './mapping-theme.component.html',
-  styleUrls: ['./mapping-theme.component.scss']
+  styleUrls: ['./mapping-theme.component.scss'],
 })
-export class MappingThemeComponent {
+export class MappingThemeComponent implements OnInit {
+  @Input() fieldsData: Field[];
+  @Input() themeLabel: string;
+  @Input() sourceFields: any;
+  @Input() mappingFormControl: FormGroup;
 
+  ngOnInit() {
+    console.log(this.sourceFields);
+    console.log('HAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA');
+  }
 }
