8478621cf263f192515c59e008ef78c2d6efea8a
Pierre Narcisi <pierre.narcisi@mnhn.fr>
2024-03-26 09:19:02 +0100

    Feat(mapping) start working on mapping selection

--- frontend/src/app/modules/imports/components/import_process/fields-mapping-step/mapping-selection/mapping-selection.component.ts
+++ frontend/src/app/modules/imports/components/import_process/fields-mapping-step/mapping-selection/mapping-selection.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FieldMapping } from 'external_modules/WA/app/models/mapping.model';
 
 @Component({
   selector: 'pnx-mapping-selection',
   templateUrl: './mapping-selection.component.html',
-  styleUrls: ['./mapping-selection.component.scss']
+  styleUrls: ['./mapping-selection.component.scss'],
 })
 export class MappingSelectionComponent {
-
+  public userFieldMappings: Array<FieldMapping>;
 }
