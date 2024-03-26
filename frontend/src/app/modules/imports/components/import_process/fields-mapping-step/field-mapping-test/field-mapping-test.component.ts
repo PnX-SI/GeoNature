0c2d45af126bad4a957f16b545fe058bd7489888
Pierre Narcisi <pierre.narcisi@mnhn.fr>
2024-03-26 09:19:02 +0100

    Feat(mapping) start working on mapping selection

--- frontend/src/app/modules/imports/components/import_process/fields-mapping-step/field-mapping-test/field-mapping-test.component.ts
+++ frontend/src/app/modules/imports/components/import_process/fields-mapping-step/field-mapping-test/field-mapping-test.component.ts
@@ -5,20 +5,20 @@ import { FieldMappingService } from '@geonature/modules/imports/services/mapping
 @Component({
   selector: 'pnx-field-mapping-test',
   templateUrl: './field-mapping-test.component.html',
-  styleUrls: ['./field-mapping-test.component.scss']
+  styleUrls: ['./field-mapping-test.component.scss'],
 })
 export class FieldMappingTestComponent implements OnInit {
   public targetFields;
-  public sourceFields:Array<string> = [];
-  public isReady:boolean=false;
+  public sourceFields: Array<string> = [];
+  public isReady: boolean = false;
   constructor(public _fieldMappingService: FieldMappingService) {}
 
   ngOnInit() {
-    
-    this._fieldMappingService.retrieveData().subscribe(({ fieldMappings, targetFields, sourceFields })=>{
+    this._fieldMappingService.data.subscribe(({ fieldMappings, targetFields, sourceFields }) => {
+      if (!fieldMappings) return;
       this._fieldMappingService.parseData({ fieldMappings, targetFields, sourceFields });
       this.targetFields = this._fieldMappingService.getTargetFieldsData();
-      this.sourceFields = this._fieldMappingService.getSourceFieldsData()
+      this.sourceFields = this._fieldMappingService.getSourceFieldsData();
       this._fieldMappingService.initForm();
       this._fieldMappingService.populateMappingForm();
       this.isReady = true;
