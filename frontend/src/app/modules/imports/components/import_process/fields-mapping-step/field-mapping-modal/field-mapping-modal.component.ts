1b93b82f0119bead210adb0704cef122af533977
Pierre Narcisi <pierre.narcisi@mnhn.fr>
2024-04-11 10:48:41 +0200

    Fix rebase
    feat(fix) not assigned mapping

--- frontend/src/app/modules/imports/components/import_process/fields-mapping-step/field-mapping-modal/field-mapping-modal.component.ts
+++ frontend/src/app/modules/imports/components/import_process/fields-mapping-step/field-mapping-modal/field-mapping-modal.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
+import { FieldMappingService } from '@geonature/modules/imports/services/mappings/field-mapping.service';
+import { ConfigService } from '@geonature/services/config.service';
+import { ImportProcessService } from '../../import-process.service';
+import { ImportDataService } from '@geonature/modules/imports/services/data.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'pnx-field-mapping-modal',
   templateUrl: './field-mapping-modal.component.html',
-  styleUrls: ['./field-mapping-modal.component.scss']
+  styleUrls: ['./field-mapping-modal.component.scss'],
 })
 export class FieldMappingModalComponent {
+  @Input() updateAvailable: boolean;
+  @ViewChild('saveMappingModal') saveMappingModal;
 
+  public modalCreateMappingForm = new FormControl('');
+  constructor(
+    private _fm: FieldMappingService,
+    private config: ConfigService,
+    private _importProcessService: ImportProcessService,
+    private _importDataService: ImportDataService,
+    private _modalService: NgbModal
+  ) {}
+
+  /**
+   * Open the modal
+   */
+  open() {
+    this._modalService.open(this.saveMappingModal, { size: 'lg' });
+  }
+
+  updateMapping() {
+    // this.spinner = true;
+    let name = '';
+    // if (this.modalCreateMappingForm.value != this.fieldMappingForm.value.label) {
+    //   name = this.modalCreateMappingForm.value;
+    // }
+    // this._importDataService
+    //   .updateFieldMapping(this.fieldMappingForm.value.id, this.getFieldMappingValues(), name)
+    //   .pipe()
+    //   .subscribe(
+    //     () => {
+    //       this.processNextStep();
+    //     },
+    //     () => {
+    //       this.spinner = false;
+    //     }
+    //   );
+  }
 }
--- frontend/src/app/modules/imports/components/import_process/fields-mapping-step/mapping-entity/mapping-entity.component.ts
+++ frontend/src/app/modules/imports/components/import_process/fields-mapping-step/mapping-entity/mapping-entity.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pnx-mapping-entity',
   templateUrl: './mapping-entity.component.html',
-  styleUrls: ['./mapping-entity.component.scss']
+  styleUrls: ['./mapping-entity.component.scss'],
 })
-export class MappingEntityComponent {
-
-}
+export class MappingEntityComponent {}
