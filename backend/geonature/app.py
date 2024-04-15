cf514fa5c5d1f99a363528c968792dfc552d8520
VincentCauchois <vincent.cauchois@mnhn.fr>
2024-04-15 15:41:50 +0200

    fix(sentry): fix `set_sentry_context` before_request function
    Set user ; ID, username and email ; from `flask_login.current_user`, if `current_user.is_authenticated`, rather than from `g.current_user` that may not exist because of uncontrolled order for before_request functions calls.

--- backend/geonature/app.py
+++ backend/geonature/app.py
@@ -162,22 +162,23 @@ def create_app(with_external_mods=True):
         g._permissions_by_user = {}
         g._permissions = {}
 
-    # FIXME : order of @app.before_request is random thus g.current_user can be empty...
-    # if config.get("SENTRY_DSN"):
-    #     from sentry_sdk import set_tag, set_user
+    if config.get("SENTRY_DSN"):
+        from sentry_sdk import set_tag, set_user
 
-    #     @app.before_request
-    #     def set_sentry_context():
-    #         if "FLASK_REQUEST_ID" in request.environ:
-    #             set_tag("request.id", request.environ["FLASK_REQUEST_ID"])
-    #         if g.current_user:
-    #             set_user(
-    #                 {
-    #                     "id": g.current_user.id_role,
-    #                     "username": g.current_user.identifiant,
-    #                     "email": g.current_user.email,
-    #                 }
-    #             )
+        @app.before_request
+        def set_sentry_context():
+            from flask_login import current_user
+
+            if "FLASK_REQUEST_ID" in request.environ:
+                set_tag("request.id", request.environ["FLASK_REQUEST_ID"])
+            if current_user.is_authenticated:
+                set_user(
+                    {
+                        "id": current_user.id_role,
+                        "username": current_user.identifiant,
+                        "email": current_user.email,
+                    }
+                )
 
     admin.init_app(app)
 
