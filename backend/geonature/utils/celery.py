ab4ce7bda0805b106fec330ba7d1f4efafa61b8e
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-05-24 16:08:26 +0200

    override Celery class by integrating flask app context

--- backend/geonature/utils/celery.py
+++ backend/geonature/utils/celery.py
@@ -2,7 +2,10 @@ from celery import Celery, Task
 from geonature.utils.env import db
 from geonature.utils.config import config
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker, scoped_session
+from sqlalchemy.orm import sessionmaker, scoped_session
+
+import flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 class SQLASessionTask(Task):
@@ -14,9 +17,7 @@ class SQLASessionTask(Task):
             config["SQLALCHEMY_DATABASE_URI"],
         )
         session_factory = sessionmaker(bind=engine)
-        session = scoped_session(session_factory)
-        db.session = session
-        self.sessions[task_id] = session
+        self.sessions[task_id] = scoped_session(session_factory)
         super().before_start(task_id, args, kwargs)
 
     def after_return(self, status, retval, task_id, args, kwargs, einfo):
@@ -24,9 +25,35 @@ class SQLASessionTask(Task):
         session.close()
         super().after_return(status, retval, task_id, args, kwargs, einfo)
 
-    @property
-    def session(self):
-        return self.sessions[self.request.id]
+
+class FlaskCelery(Celery):
+
+    def __init__(self, *args, **kwargs):
+
+        super(FlaskCelery, self).__init__(*args, **kwargs)
+        self.patch_task()
+
+        if "app" in kwargs:
+            self.init_app(kwargs["app"])
+
+    def patch_task(self):
+        _celery = self
+
+        class ContextTask(SQLASessionTask):
+            abstract = True
+
+            def __call__(self, *args, **kwargs):
+                if flask.has_app_context():
+                    return SQLASessionTask.__call__(self, *args, **kwargs)
+                else:
+                    with _celery.app.app_context():
+                        return SQLASessionTask.__call__(self, *args, **kwargs)
+
+        self.Task = ContextTask
+
+    def init_app(self, app):
+        self.app = app
+        self.config_from_object(app.config)
 
 
-celery_app = Celery("geonature", task_cls=SQLASessionTask)
+celery_app = FlaskCelery("geonature")
