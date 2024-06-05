432860102a857f0aadf7aa3d7ccd828d93d2a750
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-06-05 17:11:37 +0200

    fix uuid_generation (in progress)

--- contrib/gn_module_occhab/backend/gn_module_occhab/imports/checks.py
+++ contrib/gn_module_occhab/backend/gn_module_occhab/imports/checks.py
@@ -6,12 +6,23 @@ from geonature.utils.env import db
 
 def generate_id_station(imprt, entity):
     transient_table = imprt.destination.get_transient_table()
+    cte = (
+        sa.select(
+            sa.distinct(transient_table.c.unique_id_sinp_station).label("unique_id_sinp_station"),
+            sa.func.nextval("pr_occhab.t_stations_id_station_seq").label("id_station"),
+        )
+        .group_by(transient_table.c.unique_id_sinp_station)
+        .cte("cte_id_station")
+    )
+
     db.session.execute(
         sa.update(transient_table)
         .where(transient_table.c.id_import == imprt.id_import)
         .where(transient_table.c[entity.validity_column].is_(True))
-        .values({"id_station": sa.func.nextval("pr_occhab.t_stations_id_station_seq")})
+        .where(transient_table.c.unique_id_sinp_station == cte.c.unique_id_sinp_station)
+        .values({"id_station": cte.c.id_station})
     )
+    db.session.flush()
 
 
 def set_id_station_from_line_no(imprt, station_entity, habitat_entity):
