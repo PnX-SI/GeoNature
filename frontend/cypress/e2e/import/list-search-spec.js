1f3b899da50cc881cf35946651df5ebc3eae6ba3
Andria Capai <andria_capai@natural-solutions.eu>
2024-06-06 18:08:59 +0200

    refactor(test): create fct to be more readable
    Reviewed-by: andriacap

--- frontend/cypress/e2e/import/list-search-spec.js
+++ frontend/cypress/e2e/import/list-search-spec.js
@@ -56,69 +56,73 @@ describe('Tests list import columns and rows content', () => {
   });
 });
 
-//  NOTES : WORKS ONLY IF UPLOAD two valid file (Synthese and Occhab --> from test files backend)
 describe('Tests Filter Search List Import', () => {
-  VIEWPORTS.forEach((viewport) => {
+  VIEWPORTS.forEach(viewport => {
     context(`viewport: ${viewport.width}x${viewport.height}`, () => {
-      // TODO: rechercher le user par son id
       const user = USERS[0];
+
       context(`user: ${user.login.username}`, () => {
-        FILTERS_TABLE.forEach((filter) => {
-          context(`filter on column: ${filter.columnName} for term: ${filter.searchTerm}`, () => {
-            beforeEach(() => {
-              cy.viewport(viewport.width, viewport.height);
-              cy.geonatureLogin(user.login.username, user.login.password);
-              cy.visitImport();
-              // Ensure the import list table is present before proceeding
-              cy.get('[data-qa=import-list-table]', { timeout: timeoutWait })
-                .should('be.visible')
-                .then(($table) => {
-                  // Calculate column index here, once the table is visible
-                  FILTERS_TABLE.forEach((filter) => {
-                    filter.columnIndex = $table
-                      .find('datatable-header-cell')
-                      .toArray()
-                      .findIndex((headerCell) => {
-                        return Cypress.$(headerCell).text().trim() === filter.columnName;
-                      });
-                    expect(filter.columnIndex).to.be.gte(0); // Ensure the column is found
-                  });
-                });
-            });
-            filter.searchTerm.forEach((searchTerm, index) => {
-              const expectedRowsCount = filter.expectedRowsCount[index];
-
-              it(`Should get ${expectedRowsCount} rows for search term "${searchTerm}" in column "${filter.columnName}"`, () => {
-                filterMapList(searchTerm);
-                cy.wait(timeoutWait); // Ensure any UI updates complete
-                cy.get('[data-qa=import-list-table] datatable-body', {
-                  timeout: timeoutWait,
-                }).within(() => {
-                  cy.get('datatable-body-row').should('have.length', expectedRowsCount);
-                });
+        beforeEach(() => {
+          cy.viewport(viewport.width, viewport.height);
+          cy.geonatureLogin(user.login.username, user.login.password);
+          cy.visitImport();
+          cy.get('[data-qa=import-list-table]', { timeout: timeoutWait })
+            .should('be.visible')
+            .then($table => {
+              FILTERS_TABLE.forEach(filter => {
+                filter.columnIndex = getColumnIndex($table, filter.columnName);
               });
-
-              if (expectedRowsCount > 0) {
-                it(`Should get a row containing "${searchTerm}" in column "${filter.columnName}"`, () => {
-                  filterMapList(searchTerm);
-                  cy.wait(timeoutWait);
-
-                  cy.get('[data-qa=import-list-table] datatable-body-row', { timeout: timeoutWait })
-                    .eq(0)
-                    .as('firstRow');
-                  cy.get('@firstRow')
-                    .find('datatable-body-cell')
-                    .eq(filter.columnIndex)
-                    .invoke('text')
-                    .then((rowValue) => {
-                      cy.wrap(rowValue.trim()).should('contain', searchTerm);
-                    });
-                });
-              }
             });
+        });
+
+        FILTERS_TABLE.forEach(filter => {
+          filter.searchTerm.forEach((searchTerm, index) => {
+            const expectedRowsCount = filter.expectedRowsCount[index];
+
+            it(`Should get ${expectedRowsCount} rows for search term "${searchTerm}" in column "${filter.columnName}"`, () => {
+              performSearchAndAssertRowCount(filter, searchTerm, expectedRowsCount);
+            });
+
+            if (expectedRowsCount > 0) {
+              it(`Should get a row containing "${searchTerm}" in column "${filter.columnName}"`, () => {
+                performSearchAndAssertRowContent(filter, searchTerm);
+              });
+            }
           });
         });
       });
     });
   });
+
+  function getColumnIndex($table, columnName) {
+    const index = $table.find('datatable-header-cell')
+      .toArray()
+      .findIndex(headerCell => Cypress.$(headerCell).text().trim() === columnName);
+    expect(index).to.be.gte(0);
+    return index;
+  }
+
+  function performSearchAndAssertRowCount(filter, searchTerm, expectedRowsCount) {
+    filterMapList(searchTerm);
+    cy.wait(timeoutWait);
+    cy.get('[data-qa=import-list-table] datatable-body', { timeout: timeoutWait })
+      .within(() => {
+        cy.get('datatable-body-row').should('have.length', expectedRowsCount);
+      });
+  }
+
+  function performSearchAndAssertRowContent(filter, searchTerm) {
+    filterMapList(searchTerm);
+    cy.wait(timeoutWait);
+    cy.get('[data-qa=import-list-table] datatable-body-row', { timeout: timeoutWait })
+      .eq(0)
+      .as('firstRow');
+    cy.get('@firstRow')
+      .find('datatable-body-cell')
+      .eq(filter.columnIndex)
+      .invoke('text')
+      .then(rowValue => {
+        cy.wrap(rowValue.trim()).should('contain', searchTerm);
+      });
+  }
 });
