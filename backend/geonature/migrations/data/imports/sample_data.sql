731a0676e000fcacc4b1186169c563dc6b9bac9e
Andria Capai <andria_capai@natural-solutions.eu>
2024-06-13 10:38:24 +0200

    test(front): add jdd inactive and set id_import
    Reviewed-by: andriacap

--- backend/geonature/migrations/data/imports/sample_data.sql
+++ backend/geonature/migrations/data/imports/sample_data.sql
@@ -437,6 +437,34 @@ VALUES (
         true,
         '2018-09-01 16:57:44.45879',
         null
+    ),
+    (
+        '5f45d560-1ce3-420c-b45c-3d589eedaee1',
+        (
+            SELECT id_acquisition_framework
+            FROM gn_meta.t_acquisition_frameworks
+            WHERE unique_acquisition_framework_id = '5b054340-210c-4350-9034-300543210c43'
+        ),
+        'JDD-TEST-IMPORT-INACTIF',
+        'Jeu de données - test import inactif',
+        'Jeu de données - test import inactif',
+        ref_nomenclatures.get_id_nomenclature('DATA_TYP', '1'),
+        'Aléatoire, hors protocole, faune, flore, fonge',
+        false,
+        true,
+        ref_nomenclatures.get_id_nomenclature('JDD_OBJECTIFS', '1.1'),
+        4.85695,
+        6.85654,
+        44.5020,
+        45.25,
+        ref_nomenclatures.get_id_nomenclature('METHO_RECUEIL', '1'),
+        ref_nomenclatures.get_id_nomenclature('DS_PUBLIQUE', 'Pu'),
+        ref_nomenclatures.get_id_nomenclature('STATUT_SOURCE', 'Te'),
+        ref_nomenclatures.get_id_nomenclature('RESOURCE_TYP', '1'),
+        false,
+        true,
+        '2018-09-01 16:57:44.45879',
+        null
     );
 -- ajout des JDD dans les modules IMPORT et IMPORT dupliqué
 INSERT INTO gn_commons.cor_module_dataset (id_module, id_dataset)
@@ -486,6 +514,18 @@ VALUES (
             SELECT id_dataset
             FROM gn_meta.t_datasets
             WHERE unique_dataset_id = '9f86d081-8292-466e-9e7b-16f3960d255f'
+        )
+    ),
+    (
+        (
+            SELECT id_module
+            FROM gn_commons.t_modules
+            WHERE module_code = 'IMPORT'
+        ),
+        (
+            SELECT id_dataset
+            FROM gn_meta.t_datasets
+            WHERE unique_dataset_id = '5f45d560-1ce3-420c-b45c-3d589eedaee1'
         )
     );
 -- Renseigner les tables de correspondance
@@ -686,6 +726,24 @@ VALUES (
             WHERE nom_organisme = 'ma structure test'
         ),
         ref_nomenclatures.get_id_nomenclature('ROLE_ACTEUR', '1')
+    ),
+    (
+        (
+            SELECT id_dataset
+            FROM gn_meta.t_datasets
+            WHERE unique_dataset_id = '5f45d560-1ce3-420c-b45c-3d589eedaee1'
+        ),
+        (
+            SELECT id_role
+            FROM utilisateurs.t_roles
+            WHERE identifiant = 'admin-test-import'
+        ),
+        (
+            SELECT id_organisme
+            FROM utilisateurs.bib_organismes
+            WHERE nom_organisme = 'ma structure test'
+        ),
+        ref_nomenclatures.get_id_nomenclature('ROLE_ACTEUR', '1')
     );
 INSERT INTO gn_meta.cor_dataset_territory (
         id_dataset,
@@ -737,6 +795,7 @@ VALUES (
     );
 -- #On peuple la liste d'import
 INSERT INTO gn_imports.t_imports (
+        id_import,
         id_dataset,
         id_destination,
         format_source_file,
@@ -757,6 +816,7 @@ INSERT INTO gn_imports.t_imports (
         erroneous_rows
     )
 VALUES (
+        1000,
         (
             SELECT id_dataset
             FROM gn_meta.t_datasets
@@ -785,6 +845,7 @@ VALUES (
         '{5}'
     ),
     (
+        1001,
         (
             SELECT id_dataset
             FROM gn_meta.t_datasets
@@ -813,10 +874,40 @@ VALUES (
         '{}'
     ),
     (
+        1002,
         (
             SELECT id_dataset
             FROM gn_meta.t_datasets
             WHERE unique_dataset_id = 'a1b2c3d4-e5f6-4a3b-2c1d-e6f5a4b3c2d1'
+        ),
+        (
+            SELECT id_destination
+            FROM gn_imports.bib_destinations
+            WHERE code = 'synthese'
+        ),
+        'CSV',
+        4326,
+        ';',
+        'UTF-8',
+        'valid_file_test_import_synthese.csv',
+        100,
+        95,
+        true,
+        true,
+        '2022-01-01 00:00:00',
+        '2022-12-31 23:59:59',
+        '{"entity_source_pk_value": "id_synthese", "unique_id_sinp": "uuid_perm_sinp", "meta_create_date": "date_creation", "meta_update_date": "date_modification", "id_nomenclature_grp_typ": "type_regroupement", "unique_id_sinp_grp": "uuid_perm_grp_sinp", "date_min": "date_debut", "date_max": "date_fin", "hour_min": "heure_debut", "hour_max": "heure_fin", "altitude_min": "alti_min", "altitude_max": "alti_max", "depth_min": "prof_min", "depth_max": "prof_max", "observers": "observateurs", "comment_context": "comment_releve", "WKT": "geometrie_wkt_4326", "id_nomenclature_geo_object_nature": "nature_objet_geo", "place_name": "nom_lieu", "precision": "precision_geographique", "cd_hab": "cd_habref", "grp_method": "methode_regroupement", "nom_cite": "nom_cite", "cd_nom": "cd_nom", "id_nomenclature_obs_technique": "technique_observation", "id_nomenclature_bio_status": "biologique_statut", "id_nomenclature_bio_condition": "etat_biologique", "id_nomenclature_biogeo_status": "biogeographique_statut", "id_nomenclature_naturalness": "naturalite", "comment_description": "comment_occurrence", "id_nomenclature_sensitivity": "niveau_sensibilite", "id_nomenclature_diffusion_level": "niveau_precision_diffusion", "id_nomenclature_observation_status": "statut_observation", "id_nomenclature_blurring": "floutage_dee", "id_nomenclature_source_status": "statut_source", "reference_biblio": "reference_biblio", "id_nomenclature_behaviour": "comportement", "id_nomenclature_life_stage": "stade_vie", "id_nomenclature_sex": "sexe", "id_nomenclature_type_count": "type_denombrement", "id_nomenclature_obj_count": "objet_denombrement", "count_min": "nombre_min", "count_max": "nombre_max", "id_nomenclature_determination_method": "methode_determination", "determiner": "determinateur", "id_nomenclature_exist_proof": "preuve_existante", "digital_proof": "preuve_numerique_url", "non_digital_proof": "preuve_non_numerique", "id_nomenclature_valid_status": "niveau_validation", "validator": "validateur", "meta_validation_date": "date_validation", "validation_comment": "comment_validation"}',
+        '{"OCC_COMPORTEMENT": {"Non renseign\u00e9": "1"}, "ETA_BIO": {"Non renseign\u00e9": "1"}, "STATUT_BIO": {"Non renseign\u00e9": "1"}, "STAT_BIOGEO": {"Non renseign\u00e9": "1"}, "DEE_FLOU": {"Non": "NON"}, "METH_DETERMIN": {"Autre m\u00e9thode de d\u00e9termination": "2"}, "NIV_PRECIS": {"Pr\u00e9cise": "5"}, "PREUVE_EXIST": {"Oui": "1"}, "NAT_OBJ_GEO": {"Inventoriel": "In"}, "TYP_GRP": {"OBS": "OBS"}, "STADE_VIE": {"Adulte": "2", "Immature": "4", "Juv\u00e9nile": "3"}, "NATURALITE": {"Sauvage": "1"}, "OBJ_DENBR": {"Individu": "IND"}, "METH_OBS": {"Galerie/terrier": "23"}, "STATUT_OBS": {"Pr\u00e9sent": "Pr"}, "SENSIBILITE": {"Non sensible - Diffusion pr\u00e9cise": "0"}, "SEXE": {"Femelle": "2"}, "STATUT_SOURCE": {"Terrain": "Te"}, "TYP_DENBR": {"Compt\u00e9": "Co"}, "STATUT_VALID": {"En attente de validation": "0"}}',
+        ';',
+        NULL,
+        '{5}'
+    ),
+    (
+        1003,
+        (
+            SELECT id_dataset
+            FROM gn_meta.t_datasets
+            WHERE unique_dataset_id = '5f45d560-1ce3-420c-b45c-3d589eedaee1'
         ),
         (
             SELECT id_destination
@@ -847,12 +938,10 @@ SELECT ti.id_import,
 FROM gn_imports.t_imports ti
     JOIN gn_meta.t_datasets td ON ti.id_dataset = td.id_dataset
 WHERE td.dataset_name ILIKE '%JDD-TEST-IMPORT%';
-
 INSERT INTO gn_imports.cor_role_import (id_role, id_import)
 SELECT cda.id_role,
     tfi.id_import
 FROM temp_filtered_imports tfi
     JOIN gn_meta.cor_dataset_actor cda ON tfi.id_dataset = cda.id_dataset
 WHERE cda.id_role IS NOT NULL;
-
 DROP TABLE temp_filtered_imports;
