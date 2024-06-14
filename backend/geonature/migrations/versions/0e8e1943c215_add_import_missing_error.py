76cd7c8831dabe20cfad110944a777a1fdb3b7a5
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-06-14 15:44:06 +0200

    feat(import, check): add numeric type

--- backend/geonature/migrations/versions/0e8e1943c215_add_import_missing_error.py
+++ backend/geonature/migrations/versions/0e8e1943c215_add_import_missing_error.py
@@ -1,4 +1,4 @@
-"""add_import_missing_boolean_error
+"""add_import_missing_error
 
 Revision ID: 0e8e1943c215
 Revises: 8b149244d586
@@ -26,9 +26,21 @@ def upgrade():
                 "name": "INVALID_BOOL",
                 "description": "Le champ doit être renseigné avec une valeur binaire (0 ou 1, true ou false).",
             },
+            {
+                "error_type": "Données incohérentes d'une ou plusieurs entités",
+                "name": "INCOHERENT_DATA",
+                "description": "Les données indiquées pour une ou plusieurs entités sont incohérentes sur différentes lignes.",
+            },
+            {
+                "error_type": "Erreur de format nombre",
+                "name": "INVALID_NUMERIC",
+                "description": "Le champ doit être renseigné avec une valeur numérique (entier, flottant).",
+            },
         ],
     )
 
 
 def downgrade():
     op.execute(sa.delete(ImportUserErrorType).where(ImportUserErrorType.name == "INVALID_BOOL"))
+    op.execute(sa.delete(ImportUserErrorType).where(ImportUserErrorType.name == "INCOHERENT_DATA"))
+    op.execute(sa.delete(ImportUserErrorType).where(ImportUserErrorType.name == "INVALID_NUMERIC"))
