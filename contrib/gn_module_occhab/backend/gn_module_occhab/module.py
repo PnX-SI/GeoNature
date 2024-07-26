1859a6fe8861c59dc26433f639783ec038a6ed47
Etienne Delclaux <etienne_delclaux@natural-solutions.eu>
2024-07-26 12:20:46 +0200

    chore: repalce abc abstract by raise NotImplementedError in ImportMixin

--- contrib/gn_module_occhab/backend/gn_module_occhab/module.py
+++ contrib/gn_module_occhab/backend/gn_module_occhab/module.py
@@ -1,10 +1,9 @@
 from geonature.core.gn_commons.models import TModules
-from geonature.utils.metaclass_utils import metaclass_resolver
 
 from .imports import OcchabImportMixin
 
 
-class OcchabModule(metaclass_resolver(TModules, OcchabImportMixin)):
+class OcchabModule(TModules, OcchabImportMixin):
     __mapper_args__ = {"polymorphic_identity": "occhab"}
 
     def generate_input_url_for_dataset(self, dataset):
