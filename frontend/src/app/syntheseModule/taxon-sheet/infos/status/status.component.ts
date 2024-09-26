91031c22d7151c2c183101d9954b7002439c4837
Etienne Delclaux <etienne_delclaux@natural-solutions.eu>
2024-09-26 17:50:14 +0200

    refact: move badge to a separate component

--- frontend/src/app/syntheseModule/taxon-sheet/infos/status/status.component.ts
+++ frontend/src/app/syntheseModule/taxon-sheet/infos/status/status.component.ts
@@ -3,30 +3,13 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Taxon } from '@geonature_common/form/taxonomy/taxonomy.component';
 import { GN2CommonModule } from '@geonature_common/GN2Common.module';
 import { TaxonSheetService } from '../../taxon-sheet.service';
+import { BadgeSymbology } from '@geonature_common/others/badge/badge.component';
 
 interface Status {
   badge: string;
   tooltip: string;
-  symbologyAsCSS: string;
+  symbology: BadgeSymbology | null;
 }
-
-function computeContrastColor(backgroundColor: string) {
-  // Convertir la couleur en un format RGB
-  const r = parseInt(backgroundColor.slice(1, 3), 16);
-  const g = parseInt(backgroundColor.slice(3, 5), 16);
-  const b = parseInt(backgroundColor.slice(5, 7), 16);
-
-  // Calculer la luminosité
-  const luminance = 0.299 * r + 0.587 * g + 0.114 * b;
-
-  // Retourner une couleur claire ou foncée selon la luminosité
-  return luminance < 128 ? '#ffffff' : '#444';
-}
-
-function colorToCSS(color: string) {
-  return `--bgColor: ${color}; --textColor: ${computeContrastColor(color)};`;
-}
-
 @Component({
   standalone: true,
   selector: 'status',
@@ -38,12 +21,7 @@ export class StatusComponent implements OnInit {
   _taxon: Taxon | null;
   _symbology: Array<{
     types: Array<string>;
-    values: Record<
-      string,
-      {
-        symbologyAsCSSStyle: string;
-      }
-    >;
+    values: Record<string, BadgeSymbology>;
   }>;
   status: Array<Status> = [];
 
@@ -55,33 +33,27 @@ export class StatusComponent implements OnInit {
       if (!symbology || !symbology.symbologies) {
         return;
       }
-      for (const symbologyItem of symbology.symbologies) {
-        const values = {};
-        for (const key of Object.keys(symbologyItem.values)) {
-          values[key] = {
-            symbologyAsCSSStyle: colorToCSS(symbologyItem.values[key].color),
-          };
-        }
-        this._symbology.push({
-          types: symbologyItem.types,
-          values: values,
-        });
-      }
+      this._symbology = symbology.symbologies;
+
       this.computeStatus();
     });
     this._tss.fetchStatusSymbology();
   }
 
-  _getSymbologyAsCSSStyle(type: string, value: string): string {
+  _getSymbologyAsBadgeSymbology(type: string, value: string): BadgeSymbology | null {
     if (!this._symbology) {
-      return '';
+      return null;
     }
     const symbologieItem = this._symbology.find((item) => item.types.includes(type));
     if (!symbologieItem) {
-      return '';
+      return null;
     }
-
-    return symbologieItem.values[value]?.symbologyAsCSSStyle ?? '';
+    if (!('color' in symbologieItem.values[value])) {
+      return null;
+    }
+    return {
+      color: symbologieItem.values[value].color,
+    };
   }
 
   @Input()
@@ -106,7 +78,7 @@ export class StatusComponent implements OnInit {
           this.status.push({
             badge: badgeValue,
             tooltip: `${status.cd_type_statut} : ${value.display} - ${text.full_citation}`,
-            symbologyAsCSS: this._getSymbologyAsCSSStyle(status.cd_type_statut, value.code_statut),
+            symbology: this._getSymbologyAsBadgeSymbology(status.cd_type_statut, value.code_statut),
           });
         }
       }
