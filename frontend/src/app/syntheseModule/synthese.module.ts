c1018138ad6aa59eecccc2a1758e4263a9ab7208
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-10-07 14:53:19 +0200

    fix(lint):  + remove unused import

--- frontend/src/app/syntheseModule/synthese.module.ts
+++ frontend/src/app/syntheseModule/synthese.module.ts
@@ -1,54 +1,53 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { Routes, RouterModule } from "@angular/router";
-import { GN2CommonModule } from "@geonature_common/GN2Common.module";
-import { SyntheseComponent } from "./synthese.component";
-import { SyntheseListComponent } from "./synthese-results/synthese-list/synthese-list.component";
-import { SyntheseCarteComponent } from "./synthese-results/synthese-carte/synthese-carte.component";
-import { SyntheseFormService } from "@geonature_common/form/synthese-form/synthese-form.service";
-import { MapService } from "@geonature_common/map/map.service";
-import { TreeModule } from "@circlon/angular-tree-component";
-import { DynamicFormService } from "@geonature_common/form/dynamic-form-generator/dynamic-form.service";
-import { TaxonAdvancedStoreService } from "@geonature_common/form/synthese-form/advanced-form/synthese-advanced-form-store.service";
-import { SharedSyntheseModule } from "@geonature/shared/syntheseSharedModule/synthese-shared.module";
-import { SyntheseInfoObsComponent } from "@geonature/shared/syntheseSharedModule/synthese-info-obs/synthese-info-obs.component";
-import { SyntheseModalDownloadComponent } from "./synthese-results/synthese-list/modal-download/modal-download.component";
-import { DiscussionCardComponent } from "@geonature/shared/discussionCardModule/discussion-card.component";
-import { AlertInfoComponent } from "../shared/alertInfoModule/alert-Info.component";
-import { TaxonSheetComponent } from "./taxon-sheet/taxon-sheet.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { GN2CommonModule } from '@geonature_common/GN2Common.module';
+import { SyntheseComponent } from './synthese.component';
+import { SyntheseListComponent } from './synthese-results/synthese-list/synthese-list.component';
+import { SyntheseCarteComponent } from './synthese-results/synthese-carte/synthese-carte.component';
+import { SyntheseFormService } from '@geonature_common/form/synthese-form/synthese-form.service';
+import { MapService } from '@geonature_common/map/map.service';
+import { TreeModule } from '@circlon/angular-tree-component';
+import { DynamicFormService } from '@geonature_common/form/dynamic-form-generator/dynamic-form.service';
+import { TaxonAdvancedStoreService } from '@geonature_common/form/synthese-form/advanced-form/synthese-advanced-form-store.service';
+import { SharedSyntheseModule } from '@geonature/shared/syntheseSharedModule/synthese-shared.module';
+import { SyntheseInfoObsComponent } from '@geonature/shared/syntheseSharedModule/synthese-info-obs/synthese-info-obs.component';
+import { SyntheseModalDownloadComponent } from './synthese-results/synthese-list/modal-download/modal-download.component';
+import { DiscussionCardComponent } from '@geonature/shared/discussionCardModule/discussion-card.component';
+import { AlertInfoComponent } from '../shared/alertInfoModule/alert-Info.component';
+import { TaxonSheetComponent } from './taxon-sheet/taxon-sheet.component';
 import {
   RouteService,
   ALL_TAXON_SHEET_ADVANCED_INFOS_ROUTES,
   ROUTE_MANDATORY,
-} from "./taxon-sheet/taxon-sheet.route.service";
-import { NgbActiveModal, NgbModule } from "@ng-bootstrap/ng-bootstrap";
-import { SyntheseObsModalWrapperComponent } from "@geonature/shared/syntheseSharedModule/synthese-info-obs-container.component";
-import { BrowserModule } from "@angular/platform-browser";
+} from './taxon-sheet/taxon-sheet.route.service';
+import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SyntheseObsModalWrapperComponent } from '@geonature/shared/syntheseSharedModule/synthese-info-obs-container.component';
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: SyntheseComponent,
     children: [
       {
-        path: "occurrence/:id_synthese",
-        redirectTo: "occurrence/:id_synthese/details",
-        pathMatch: "full",
+        path: 'occurrence/:id_synthese',
+        redirectTo: 'occurrence/:id_synthese/details',
+        pathMatch: 'full',
       },
       {
-        path: "occurrence/:id_synthese/:tab",
+        path: 'occurrence/:id_synthese/:tab',
         component: SyntheseObsModalWrapperComponent,
       },
     ],
   },
   {
-    path: "taxon/:cd_ref",
+    path: 'taxon/:cd_ref',
     component: TaxonSheetComponent,
     canActivateChild: [RouteService],
     children: [
       {
-        path: "",
+        path: '',
         redirectTo: ROUTE_MANDATORY.path,
-        pathMatch: "prefix",
+        pathMatch: 'prefix',
       },
       ...ALL_TAXON_SHEET_ADVANCED_INFOS_ROUTES.map((tab) => {
         return {
