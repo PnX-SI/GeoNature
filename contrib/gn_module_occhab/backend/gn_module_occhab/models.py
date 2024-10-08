ff74c51625bee10ee089138c1498f4ead5c389c2
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-10-08 15:17:12 +0200

    fix

--- contrib/gn_module_occhab/backend/gn_module_occhab/models.py
+++ contrib/gn_module_occhab/backend/gn_module_occhab/models.py
@@ -152,10 +152,12 @@ class Station(NomenclaturesMixin, db.Model):
             return False
         elif scope in (1, 2):
             ds_list = Dataset.filter_by_scope(scope).with_only_columns(Dataset.id_dataset)
+
             return sa.or_(
                 Station.observers.any(id_role=user.id_role),
                 Station.id_dataset.in_([ds.id_dataset for ds in db.session.execute(ds_list).all()]),
             )
+        return True
 
 
 @serializable
