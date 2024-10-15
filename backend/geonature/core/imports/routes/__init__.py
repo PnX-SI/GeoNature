517aeeaa4022e3f9b354d395b29786cad21e1980
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-10-15 14:10:00 +0200

    feat(import, destination): merge routes into one

--- backend/geonature/core/imports/routes/__init__.py
+++ backend/geonature/core/imports/routes/__init__.py
@@ -5,13 +5,23 @@ from sqlalchemy.orm import joinedload
 from geonature.core.imports.schemas import DestinationSchema
 from geonature.core.imports.blueprint import blueprint
 from geonature.utils.env import db
-from flask_login import current_user
+
+import sqlalchemy as sa
+from flask import g
 
 
-@blueprint.route("/destinations/", methods=["GET"])
-def list_all_destinations():
+@blueprint.route("/destinations/", methods=["GET"], defaults={"action_code": None})
+@blueprint.route("/destinations/<action_code>", methods=["GET"])
+@login_required
+def list_all_destinations(action_code):
     """
-    Return the list of all destinations.
+    Return the list of all destinations. If an action code is provided, only the destinations
+    that the user has permission (based on the action_code) to access are returned.
+
+    Parameters:
+    ----------
+    action_code : str
+        The action code to filter destinations. Possible values are 'C', 'R', 'U', 'V', 'E', 'D'.
 
     Returns:
     -------
@@ -20,33 +30,8 @@ def list_all_destinations():
     """
 
     schema = DestinationSchema()
-    destinations = Destination.query.all()
+    query = sa.select(Destination)
+    if action_code:
+        query = query.where(Destination.filter_by_role(g.current_user, action_code))
+    destinations = db.session.execute(query).scalars().all()
     return schema.dump(destinations, many=True)
-
-
-@blueprint.route("/destinations/allowed", methods=["GET"])
-@login_required
-def allowed_destinations():
-    """
-    Return a list of allowed destinations for the current user.
-
-    Returns:
-    -------
-    destinations : List of Destination
-        List of allowed destinations for the current user.
-    """
-    schema = DestinationSchema()
-    destinations = Destination.allowed_destinations(current_user)
-    return schema.dump(destinations, many=True)
-
-
-@blueprint.route("/destination/<destinationCode>", methods=["GET"])
-@login_required
-def get_destination(destinationCode):
-    schema = DestinationSchema(only=["module"])
-    destination = db.session.execute(
-        db.select(Destination)
-        .options(joinedload("module"))
-        .where(Destination.code == destinationCode)
-    ).scalar_one_or_none()
-    return schema.dump(destination)
