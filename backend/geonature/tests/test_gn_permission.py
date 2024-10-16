44879a728eec837565b639304ea05a5f79fb6cbc
Élie Bouttier <elie@bouttier.eu>
2024-10-16 18:14:55 +0200

    feat(permissions): route to list available perms

--- backend/geonature/tests/test_gn_permission.py
+++ backend/geonature/tests/test_gn_permission.py
@@ -1,6 +1,8 @@
 import pytest
 from flask import url_for
 
+from pypnusershub.tests.utils import set_logged_user
+
 
 @pytest.mark.usefixtures("client_class")
 class TestGnPermissionsRoutes:
@@ -9,3 +11,9 @@ class TestGnPermissionsRoutes:
 
         assert response.status_code == 200
         assert response.data == b"Logout"
+
+    def test_list_permissions_availables(self, users):
+        set_logged_user(self.client, users["user"])
+        response = self.client.get(url_for("gn_permissions.list_permissions_availables"))
+
+        assert response.status_code == 200
