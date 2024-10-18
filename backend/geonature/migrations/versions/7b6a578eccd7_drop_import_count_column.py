a70d1f1a7ef02146974ef4fecc650b0e7ae08b81
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2024-10-18 18:36:33 +0200

    feat(migration): modify revision to include import_count transfer data to statistics + fix notification template revision

--- backend/geonature/migrations/versions/7b6a578eccd7_drop_import_count_column.py
+++ backend/geonature/migrations/versions/7b6a578eccd7_drop_import_count_column.py
@@ -18,6 +18,12 @@ depends_on = None
 
 
 def upgrade():
+    op.execute(
+        """
+        UPDATE gn_imports.t_imports
+        SET statistics = statistics::jsonb || jsonb_build_object('import_count', import_count);
+    """
+    )
     op.drop_column(
         schema="gn_imports",
         table_name="t_imports",
@@ -33,4 +39,28 @@ def downgrade():
             "import_count",
             sa.Integer,
         ),
+    )
+
+    op.execute(
+        """
+        WITH count_ AS (
+            SELECT
+                id_import as id_import,
+                (statistics->>'import_count')::integer as import_count
+            FROM
+                gn_imports.t_imports
+            WHERE
+                statistics->>'import_count' IS NOT NULL
+        )
+        UPDATE gn_imports.t_imports as GN
+        SET import_count = count_.import_count
+        FROM count_
+        where GN.id_import = count_.id_import;
+    """
+    )
+    op.execute(
+        """
+        UPDATE gn_imports.t_imports as GN
+        SET statistics = statistics::jsonb #- '{import_count}';
+    """
     )
