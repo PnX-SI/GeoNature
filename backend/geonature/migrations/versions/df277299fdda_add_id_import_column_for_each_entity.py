945d67ad3ca14c2c6d47fb1b3f31080ace37efb9
Julien Corny <julien_corny@natural-soltuions.eu>
2024-12-04 15:57:40 +0100

    feat: add id_import columns in monitoring tables

---
+++ backend/geonature/migrations/versions/df277299fdda_add_id_import_column_for_each_entity.py
@@ -0,0 +1,39 @@
+"""[monitoring] add id_import column for each entity
+
+Revision ID: df277299fdda
+Revises: 6734d8f7eb2a
+Create Date: 2024-11-28 18:20:49.512808
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "df277299fdda"
+down_revision = "6734d8f7eb2a"
+branch_labels = None
+depends_on = None
+
+import_column_name = "id_import"
+schema = "gn_monitoring"
+entity_tables = ["t_base_sites", "t_base_visits", "t_observations"]
+
+
+def upgrade():
+    for table in entity_tables:
+        op.add_column(
+            schema=schema,
+            table_name=table,
+            column=sa.Column(import_column_name, sa.Integer, nullable=True),
+        )
+
+
+def downgrade():
+    for table in entity_tables:
+        op.drop_column(
+            schema=schema,
+            table_name=table,
+            column_name=import_column_name,
+        )
