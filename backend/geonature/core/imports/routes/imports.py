6058db749288c6d81d00ba1ac77bd5734f826b88
20cents <vincent_bourgeois@natural-solutions.eu>
2024-12-16 14:41:31 +0100

    Feat (import): Support constant values in the mapping (#3271)
    * feat(import): can post fieldmapping in new structured format
    * fix(import): insert_import_data_in_transient_table
    * fix(import): get_import_values
    * fix(import): prepare_import
    * feat(import): support default values
    * feat(import): default value edition support nomenclature widget
     refactor(import): minor refact on getFieldMappingValues()
    * feat(import): improve default value json edition
    * feat(import): present default values on report
    * feat(import): db migration
    * feat(import): test default value
    * test(import): support fieldmapping format v2
    * feat(import): dynamic form on default values
    * fix(import): no longer send empty string for date default value
    * fix(import): test field jsonschema_definitions
    * feat(import): fix occhab revisions
    * feat(import): rename obsolete revision
    * style(import): code format
    * feat(import): fieldmapping validators deal with default values
    * feat(import): occhab revision to set bib_fields.type_field
    * Fix (migration) move migration to geonature branch
    
    ---------
    
    Co-authored-by: Pierre-Narcisi <pierre.narcisi@epitech.eu>

--- backend/geonature/core/imports/routes/imports.py
+++ backend/geonature/core/imports/routes/imports.py
@@ -368,8 +368,11 @@ def get_import_values(scope, imprt):
             # this nomenclated field is not mapped
             continue
         source = imprt.fieldmapping[field.name_field]
-        if source not in imprt.columns:
-            # the file do not contain this field expected by the mapping
+        if (
+            source.get("column_src", None) not in imprt.columns
+            and source.get("default_value", None) is None
+        ):
+            # the file do not contain this field expected by the mapping and there is no default value
             continue
         # TODO: vérifier que l’on a pas trop de valeurs différentes ?
         column = field.source_column
