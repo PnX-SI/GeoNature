5e166f3747735f457ec007b64a0060c1a9bc211f
Pierre-Narcisi <pierre.narcisi@mnhn.fr>
2025-01-15 10:34:18 +0100

    Fix(model): temporary fix for dataset counts of habs and taxs (#3320)

--- backend/geonature/core/gn_meta/models/datasets.py
+++ backend/geonature/core/gn_meta/models/datasets.py
@@ -139,27 +139,6 @@ class TDatasets(db.Model):
     def organism_actors(self):
         return [actor.organism for actor in self.cor_dataset_actor if actor.organism is not None]
 
-    @hybrid_property
-    def obs_count(self):
-        from geonature.core.gn_synthese.models import Synthese
-
-        return db.session.scalar(
-            select(func.count(Synthese.id_synthese))
-            .select_from(Synthese)
-            .where(Synthese.id_dataset == self.id_dataset)
-        )
-
-    @hybrid_property
-    def hab_count(self):
-        from gn_module_occhab.models import OccurenceHabitat, Station
-
-        return db.session.scalar(
-            select(func.count(OccurenceHabitat.id_habitat))
-            .select_from(OccurenceHabitat)
-            .where(Station.id_station == OccurenceHabitat.id_station)
-            .where(Station.id_dataset == self.id_dataset)
-        )
-
     def is_deletable(self):
         return not DB.session.execute(self.synthese_records.exists().select()).scalar()
 
