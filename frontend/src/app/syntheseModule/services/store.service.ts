e7effee8c4e719ae171b42a58b282c92b3ccbd95
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2025-01-31 15:40:48 +0100

    feat(synthese): improve data loading and parsing
    Code refactoring and improvements.
    Move search bar toggle button in the middle of page height (like
    Validation, and more visible).

--- frontend/src/app/syntheseModule/services/store.service.ts
+++ frontend/src/app/syntheseModule/services/store.service.ts
@@ -4,8 +4,53 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class SyntheseStoreService {
-  public idSyntheseList: Array<number>;
-  public gridData: Array<any>;
-  public pointData: Array<any>;
+  public idSyntheseList: Set<number> = new Set();
+  private gridData: Object = {};
+  private pointData: Object = {};
+
   constructor() {}
+
+  public hasData(type): boolean {
+    let hasData = false;
+    if (type == 'grid') {
+      hasData = this.gridData && Object.keys(this.gridData).length > 0;
+      console.log(`Grid data length: ${this.gridData && Object.keys(this.gridData).length > 0}`);
+      console.log(`Grid data: ${this.gridData}`, this.gridData);
+    } else if (type == 'point') {
+      hasData = this.pointData && Object.keys(this.pointData).length > 0;
+      console.log(`Point data length: ${this.pointData && Object.keys(this.pointData).length > 0}`);
+      console.log(`Point data: ${this.pointData}`, this.pointData);
+    } else {
+      throw new Error(`Unknown data type: ${type}`);
+    }
+    return hasData;
+  }
+
+  public getData(type): Object {
+    let storedData = {};
+    if (type == 'grid') {
+      storedData = this.gridData;
+    } else if (type == 'point') {
+      storedData = this.pointData;
+    } else {
+      throw new Error(`Unknown data type: ${type}`);
+    }
+    return storedData;
+  }
+
+  public setData(type, data: Object) {
+    if (type === 'grid') {
+      this.gridData = data;
+    } else if (type === 'point') {
+      this.pointData = data;
+    } else {
+      throw new Error(`Unknown data type: ${type}`);
+    }
+  }
+
+  public clearData() {
+    this.idSyntheseList = new Set();
+    this.gridData = {};
+    this.pointData = {};
+  }
 }
