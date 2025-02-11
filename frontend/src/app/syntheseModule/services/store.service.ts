e2e50c05c7e916695ca569eab4ac669b7036f03e
Jean-Pascal MILCENT <jp.milcent@cbn-alpin.fr>
2025-02-11 18:16:22 +0100

    feat(synthese): add new map criteria with aggregation areas

--- frontend/src/app/syntheseModule/services/store.service.ts
+++ frontend/src/app/syntheseModule/services/store.service.ts
@@ -1,56 +1,37 @@
 import { Injectable } from '@angular/core';
 
+// TODO: rename this service to SyntheseCacheService
 @Injectable({
   providedIn: 'root',
 })
 export class SyntheseStoreService {
   public idSyntheseList: Set<number> = new Set();
-  private gridData: Object = {};
-  private pointData: Object = {};
+  public data: Object = {};
 
   constructor() {}
 
   public hasData(type): boolean {
     let hasData = false;
-    if (type == 'grid') {
-      hasData = this.gridData && Object.keys(this.gridData).length > 0;
-      console.log(`Grid data length: ${this.gridData && Object.keys(this.gridData).length > 0}`);
-      console.log(`Grid data: ${this.gridData}`, this.gridData);
-    } else if (type == 'point') {
-      hasData = this.pointData && Object.keys(this.pointData).length > 0;
-      console.log(`Point data length: ${this.pointData && Object.keys(this.pointData).length > 0}`);
-      console.log(`Point data: ${this.pointData}`, this.pointData);
-    } else {
-      throw new Error(`Unknown data type: ${type}`);
+    if (this.data[type] && Object.keys(this.data[type]).length > 0) {
+      hasData = true;
     }
     return hasData;
   }
 
   public getData(type): Object {
-    let storedData = {};
-    if (type == 'grid') {
-      storedData = this.gridData;
-    } else if (type == 'point') {
-      storedData = this.pointData;
-    } else {
-      throw new Error(`Unknown data type: ${type}`);
+    let storedData = [];
+    if (this.data[type] && Object.keys(this.data[type]).length > 0) {
+      storedData = this.data[type];
     }
     return storedData;
   }
 
-  public setData(type, data: Object) {
-    if (type === 'grid') {
-      this.gridData = data;
-    } else if (type === 'point') {
-      this.pointData = data;
-    } else {
-      throw new Error(`Unknown data type: ${type}`);
-    }
+  public setData(type: string, dataToStore: Object) {
+    this.data[type] = dataToStore;
   }
 
   public clearData() {
     this.idSyntheseList = new Set();
-    this.gridData = {};
-    this.pointData = {};
+    this.data = {};
   }
 }
