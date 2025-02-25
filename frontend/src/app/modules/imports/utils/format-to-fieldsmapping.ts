1dbc7052d5e9697431a62db8632ee5ce5222b97a
Etienne Delclaux <etienne_delclaux@natural-solutions.eu>
2025-02-25 16:43:25 +0100

    feat: adjust frontend

--- frontend/src/app/modules/imports/utils/format-to-fieldsmapping.ts
+++ frontend/src/app/modules/imports/utils/format-to-fieldsmapping.ts
@@ -1,19 +1,18 @@
-import { FieldMappingValues } from "../models/mapping.model";
+import { FieldMappingValues } from '../models/mapping.model';
 
 export function formatQueryParams(queryParams: { [key: string]: string }): FieldMappingValues {
-    const formattedParams: FieldMappingValues = {};
-  
-    Object.keys(queryParams).forEach((key) => {
-      const value = queryParams[key];
-      
-      const parsedValue = !isNaN(Number(value)) ? Number(value) : value;
-      
-      formattedParams[key] = {
-        column_src: key,
-        default_value: parsedValue,
-      };
-    });
-  
-    return formattedParams;
-  }
-  
+  const formattedParams: FieldMappingValues = {};
+
+  Object.keys(queryParams).forEach((key) => {
+    const value = queryParams[key];
+
+    const parsedValue = !isNaN(Number(value)) ? Number(value) : value;
+
+    formattedParams[key] = {
+      column_src: '',
+      default_value: parsedValue,
+    };
+  });
+
+  return formattedParams;
+}
