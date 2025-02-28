3dda7e5b684ce32fcca815e12e06c321dc0fdf00
Jacques Fize <4259846+jacquesfize@users.noreply.github.com>
2025-02-28 15:29:24 +0100

    feat(test): Use populated db image to speed up test GH Actions (#3369)

--- frontend/cypress/e2e/occhab-spec.js
+++ frontend/cypress/e2e/occhab-spec.js
@@ -17,7 +17,7 @@ describe('Testing occhab', () => {
     cy.get(
       '[data-qa="pnx-occhab-form"] > div:nth-child(1) > pnx-map > div > div.leaflet-container.leaflet-touch.leaflet-fade-anim.leaflet-grab.leaflet-touch-drag.leaflet-touch-zoom > div.leaflet-control-container > div.leaflet-top.leaflet-left > div.leaflet-draw.leaflet-control > div:nth-child(1) > div > a'
     ).click();
-    cy.wait(10000);
+    cy.wait(2000);
     const positions = [
       [250, 250],
       [300, 250],
@@ -27,7 +27,7 @@ describe('Testing occhab', () => {
     ];
     positions.forEach((pos) => {
       cy.get(canvas).click(pos[0], pos[1]);
-      cy.wait(10000);
+      cy.wait(500);
     });
     cy.get('#validateButton').should('be.disabled');
 
@@ -44,19 +44,25 @@ describe('Testing occhab', () => {
 
     cy.get('#add-hab-btn').click();
     cy.get('#taxonInput').type('dune');
-    cy.get('#ngb-typeahead-3-0').click();
-    cy.get('[data-qa="pnx-occhab-form-technique-collect"] > div > select').select('1: Object');
-    cy.get('[data-qa="pnx-occhab-form-valid-button"]').click();
+    var selected_element = '';
+    cy.get('#ngb-typeahead-3-0').then(($el) => {
+      selected_element = $el.text().trim();
+      cy.get('#ngb-typeahead-3-0').click();
 
-    cy.get('#validateButton').click();
+      cy.get('[data-qa="pnx-occhab-form-technique-collect"] > div > select').select('1: Object');
+      cy.get('[data-qa="pnx-occhab-form-valid-button"]').click();
+
+      cy.get('#validateButton').click();
+    });
 
     const listHabit = await promisify(
       cy.get(
         '[data-qa="pnx-occhab-map-list-datatable"] > div > datatable-body > datatable-selection > datatable-scroller'
       )
     );
+    cy.wait(1000);
     expect(listHabit[0].children[0].children[0].children[1].children[4].innerText).contains(
-      'Prés salés du contact haut schorre/dune'
+      selected_element
     );
     listHabit[0].children[0].children[0].children[1].children[2].children[0].children[0].click();
   });
