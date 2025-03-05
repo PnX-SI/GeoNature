a38c1bea5e0a169c92b2b8092bfe56694977564b
andriacap <111564663+andriacap@users.noreply.github.com>
2025-03-05 12:06:32 +0100

    [IMPORT] Feat context parameter import (#3277)
    ---------
    
    Co-authored-by: Pierre-Narcisi <pierre.narcisi@epitech.eu>
    Co-authored-by: Etienne Delclaux <etienne_delclaux@natural-solutions.eu>

---
+++ frontend/src/app/modules/imports/utils/fieldmapping-preset-utils.ts
@@ -0,0 +1,43 @@
+import { FieldMappingItem, FieldMappingValues } from '../models/mapping.model';
+
+export class FieldMappingPresetUtils {
+  static readonly PRESET_KEY = '__preset__';
+
+  static formatQueryParamsToFieldMapping(queryParams: {
+    [key: string]: string;
+  }): FieldMappingValues | null {
+    const formattedParams: Record<string, FieldMappingItem> = {};
+
+    Object.keys(queryParams).forEach((key) => {
+      const value = queryParams[key];
+
+      const parsedValue = !isNaN(Number(value)) ? Number(value) : value;
+
+      formattedParams[key] = {
+        column_src: '',
+        default_value: parsedValue,
+      };
+    });
+
+    return {
+      [FieldMappingPresetUtils.PRESET_KEY]: formattedParams,
+    };
+  }
+
+  /**
+   * Apply preset if exists
+   */
+  static patchMappingValuesWithPreset(
+    mappingValues: FieldMappingValues,
+    preset: FieldMappingValues | null
+  ): FieldMappingValues {
+    // Replace input mapping entries if a preset is set in "importFieldMapping"
+    const presetValues = preset?.__preset__;
+    if (presetValues) {
+      for (const presetKey in presetValues) {
+        mappingValues[presetKey] = presetValues[presetKey];
+      }
+    }
+    return mappingValues;
+  }
+}
