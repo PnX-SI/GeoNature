c7ac108e735499c97ca13257a70470c0e046515e
Andria Capai <andria_capai@natural-solutions.eu>
2025-05-12 15:01:50 +0200

    refact: remvoe defaultDict variable
    Reviewed-by: acapai

--- frontend/src/app/GN2CommonModule/service/validator-message.service.ts
+++ frontend/src/app/GN2CommonModule/service/validator-message.service.ts
@@ -5,24 +5,6 @@ import { TranslateService } from "@ngx-translate/core";
 @Injectable({ providedIn: 'root' })
 export class ValidatorMessageService {
 
-  private defaultDict: Record<string,string> = {
-    // Angular built-in
-    required   : 'Ce champ est obligatoire.',
-    minlength  : 'Le texte doit contenir au moins {{requiredLength}} caractères (actuellement {{actualLength}}).',
-    maxlength  : 'Le texte doit contenir au plus {{requiredLength}} caractères (actuellement {{actualLength}}).',
-    pattern    : 'Valeur non valide.',
-    min        : 'La valeur doit être ≥ {{min}} (actuellement {{actual}}).',
-    max        : 'La valeur doit être ≤ {{max}} (actuellement {{actual}}).',
-
-    //validateurs custom
-    [ValidationErrorsId.ARRAY_MIN_LENGTH_ERROR]: 'Il faut au moins {{arrayLength}} éléments.',
-    [ValidationErrorsId.IS_OBJECT_ERROR]       : 'Format d’objet invalide.',
-    [ValidationErrorsId.MIN_GREATER_THAN_MAX]  : 'Le minimum doit être ≤ le maximum.',
-    [ValidationErrorsId.NOT_NUMBER_ERROR]      : 'Le champ doit être un nombre valide.',
-    [ValidationErrorsId.COMMA_NOT_ALLOWED]      : 'Le champ de type nombre ne doit pas contenir de virgule. Utilisez le point pour la décimale.',
-    file                    : 'Le fichier est trop volumineux.',
-    medias                  : 'Format de médias invalide.',
-  };
 
   constructor(private translate: TranslateService) {}
    /**
@@ -50,10 +32,6 @@ export class ValidatorMessageService {
     if (translated !== translationKey) {
       return this.interpolate(translated, details); 
     }
-
-    // 3) fallback sur dictionnaire default
-    const tmpl = this.defaultDict[key] || 'Valeur invalide.';
-    return this.interpolate(tmpl, details);
   }
 
   /** remplace {{foo}} par details.foo dans une chaîne */
