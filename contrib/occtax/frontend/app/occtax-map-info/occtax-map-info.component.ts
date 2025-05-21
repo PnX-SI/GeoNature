1cab7a88a8841fbdc59f993971105aad2723b0ab
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2025-05-21 10:26:30 +0200

    apply correct lint

--- contrib/occtax/frontend/app/occtax-map-info/occtax-map-info.component.ts
+++ contrib/occtax/frontend/app/occtax-map-info/occtax-map-info.component.ts
@@ -1,42 +1,42 @@
-import { Component, OnInit, AfterViewInit } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
-import { BehaviorSubject } from "rxjs";
-import { map, filter } from "rxjs/operators";
-import { isEqual } from "lodash";
-import { MapService } from "@geonature_common/map/map.service";
-import { OcctaxDataService } from "../services/occtax-data.service";
-import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
-import { CommonService } from "@geonature_common/service/common.service";
-import { MediaService } from "@geonature_common/service/media.service";
-import { DataFormService } from "@geonature_common/form/data-form.service";
-import { ModuleService } from "@geonature/services/module.service";
-import { ConfigService } from "@geonature/services/config.service";
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+import { isEqual } from 'lodash';
+import { MapService } from '@geonature_common/map/map.service';
+import { OcctaxDataService } from '../services/occtax-data.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CommonService } from '@geonature_common/service/common.service';
+import { MediaService } from '@geonature_common/service/media.service';
+import { DataFormService } from '@geonature_common/form/data-form.service';
+import { ModuleService } from '@geonature/services/module.service';
+import { ConfigService } from '@geonature/services/config.service';
 
 const NOMENCLATURES = [
-  "TECHNIQUE_OBS",
-  "TYP_GRP",
-  "METH_DETERMIN",
-  "STATUT_OBS",
-  "METH_OBS",
-  "ETA_BIO",
-  "NATURALITE",
-  "STATUT_BIO",
-  "STATUT_SOURCE",
-  "NIV_PRECIS",
-  "DEE_FLOU",
-  "PREUVE_EXIST",
-  "STADE_VIE",
-  "SEXE",
-  "OBJ_DENBR",
-  "TYP_DENBR",
-  "NAT_OBJ_GEO",
-  "OCC_COMPORTEMENT",
+  'TECHNIQUE_OBS',
+  'TYP_GRP',
+  'METH_DETERMIN',
+  'STATUT_OBS',
+  'METH_OBS',
+  'ETA_BIO',
+  'NATURALITE',
+  'STATUT_BIO',
+  'STATUT_SOURCE',
+  'NIV_PRECIS',
+  'DEE_FLOU',
+  'PREUVE_EXIST',
+  'STADE_VIE',
+  'SEXE',
+  'OBJ_DENBR',
+  'TYP_DENBR',
+  'NAT_OBJ_GEO',
+  'OCC_COMPORTEMENT',
 ];
 
 @Component({
-  selector: "pnx-occtax-map-info",
-  templateUrl: "occtax-map-info.component.html",
-  styleUrls: ["./occtax-map-info.component.scss"],
+  selector: 'pnx-occtax-map-info',
+  templateUrl: 'occtax-map-info.component.html',
+  styleUrls: ['./occtax-map-info.component.scss'],
 })
 export class OcctaxMapInfoComponent implements OnInit, AfterViewInit {
   public occtaxData: BehaviorSubject<any> = new BehaviorSubject(null);
@@ -50,9 +50,7 @@ export class OcctaxMapInfoComponent implements OnInit, AfterViewInit {
   public countingAddFields: Array<any> = [];
 
   get releve() {
-    return this.occtaxData.getValue()
-      ? this.occtaxData.getValue().properties
-      : null;
+    return this.occtaxData.getValue() ? this.occtaxData.getValue().properties : null;
   }
 
   get id() {
@@ -69,9 +67,7 @@ export class OcctaxMapInfoComponent implements OnInit, AfterViewInit {
   }
 
   get occurrences() {
-    return this.releve && this.releve.t_occurrences_occtax
-      ? this.releve.t_occurrences_occtax
-      : [];
+    return this.releve && this.releve.t_occurrences_occtax ? this.releve.t_occurrences_occtax : [];
   }
 
   get nbCounting() {
@@ -97,8 +93,8 @@ export class OcctaxMapInfoComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     //si modification, récuperation de l'ID du relevé
-    let id = this._route.snapshot.paramMap.get("id");
-    let id_counting = this._route.snapshot.paramMap.get("id_counting");
+    let id = this._route.snapshot.paramMap.get('id');
+    let id_counting = this._route.snapshot.paramMap.get('id_counting');
 
     if (id && Number.isInteger(Number(id))) {
       this.getOcctaxData(Number(id));
@@ -106,7 +102,7 @@ export class OcctaxMapInfoComponent implements OnInit, AfterViewInit {
       //si id_counting de passé
       this.occtaxDataService
         .getOneCounting(Number(id_counting))
-        .pipe(map((data) => data["id_releve"]))
+        .pipe(map((data) => data['id_releve']))
         .subscribe((id_releve) => {
           this.getOcctaxData(id_releve);
         });
@@ -164,7 +160,7 @@ export class OcctaxMapInfoComponent implements OnInit, AfterViewInit {
                   OCCTAX_OCCURENCE: this.occurrenceAddFields,
                   OCCTAX_DENOMBREMENT: this.countingAddFields,
                 };
-                if (field.type_widget != "html") {
+                if (field.type_widget != 'html') {
                   field.objects.forEach((object) => {
                     if (object.code_object in map) {
                       map[object.code_object].push(field);
@@ -175,7 +171,7 @@ export class OcctaxMapInfoComponent implements OnInit, AfterViewInit {
             });
           this.dataFormS
             .getadditionalFields({
-              id_dataset: "null",
+              id_dataset: 'null',
               module_code: [this._moduleService.currentModule.module_code],
             })
             .subscribe((additionalFields) => {
@@ -194,20 +190,15 @@ export class OcctaxMapInfoComponent implements OnInit, AfterViewInit {
             });
         },
         (error) => {
-          this._commonService.translateToaster(
-            "error",
-            "Occtax.Releve.Messages.DoesNotExist"
-          );
-          const currentModulePath =
-            this._moduleService.currentModule.module_path.toLowerCase();
+          this._commonService.translateToaster('error', 'Occtax.Releve.Messages.DoesNotExist');
+          const currentModulePath = this._moduleService.currentModule.module_path.toLowerCase();
           this._router.navigate([currentModulePath]);
         }
       );
   }
 
   goToEdit(idReleve) {
-    const currentModulePath =
-      this._moduleService.currentModule.module_path.toLowerCase();
+    const currentModulePath = this._moduleService.currentModule.module_path.toLowerCase();
     this._router.navigate([`${currentModulePath}/form/releve/${idReleve}`]);
   }
 
@@ -228,10 +219,8 @@ export class OcctaxMapInfoComponent implements OnInit, AfterViewInit {
       .subscribe((nomenclatures) => (this.nomenclatures = nomenclatures));
   }
 
-  getLibelleByID(ID: number, lang: string = "default") {
-    return this.nomenclatures[ID]
-      ? this.nomenclatures[ID][`label_${lang}`]
-      : null;
+  getLibelleByID(ID: number, lang: string = 'default') {
+    return this.nomenclatures[ID] ? this.nomenclatures[ID][`label_${lang}`] : null;
   }
 
   openModalDelete(modalDelete) {
@@ -242,18 +231,17 @@ export class OcctaxMapInfoComponent implements OnInit, AfterViewInit {
     this.occtaxDataService.deleteReleve(this.id).subscribe(
       () => {
         this._commonService.translateToaster(
-          "success",
-          "Occtax.Releve.Messages.DeleteSuccessfully"
+          'success',
+          'Occtax.Releve.Messages.DeleteSuccessfully'
         );
-        const currentModulePath =
-          this._moduleService.currentModule.module_path.toLowerCase();
+        const currentModulePath = this._moduleService.currentModule.module_path.toLowerCase();
         this._router.navigate([currentModulePath]);
       },
       (error) => {
         if (error.status === 403) {
-          this._commonService.translateToaster("error", "Errors.NotAllowed");
+          this._commonService.translateToaster('error', 'Errors.NotAllowed');
         } else {
-          this._commonService.translateToaster("error", "Errors.ErrorMessage");
+          this._commonService.translateToaster('error', 'Errors.ErrorMessage');
         }
       }
     );
