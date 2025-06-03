ceae1cd3669eed03be93a7642a52d0b14a61cbdf
Zacharie Moulin <zacharie.moulin@rnfrance.org>
2025-06-03 21:38:49 +0200

    add permissions argument on delete route

--- backend/geonature/core/gn_synthese/blueprints/workshop/delete.py
+++ backend/geonature/core/gn_synthese/blueprints/workshop/delete.py
@@ -6,7 +6,7 @@ from werkzeug.exceptions import NotFound
 from geonature.core.gn_permissions.decorators import permissions_required
 
 @permissions_required("D", module_code="SYNTHESE")
-def delete_observation(id_synthese):
+def delete_observation(id_synthese, permissions):
     observation_item = db.session.scalar(sa.exists(Synthese).where(Synthese.id_synthese == id_synthese).select())
     if not observation_item :
         raise NotFound
