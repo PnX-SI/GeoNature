fe213446349afc2c475630c269342a3d28f7d8a8
Zacharie Moulin <zacharie.moulin@rnfrance.org>
2025-06-03 18:09:54 +0200

    add delete_observation function

--- backend/geonature/core/gn_synthese/blueprints/workshop/delete.py
+++ backend/geonature/core/gn_synthese/blueprints/workshop/delete.py
@@ -1,2 +1,14 @@
+from sqlalchemy import select
+from geonature.utils.env import db
+from geonature.core.gn_synthese.models import Synthese
+import sqlalchemy as sa
+from werkzeug.exceptions import NotFound
+
 def delete_observation(id_synthese):
-    pass
+    observation_item = db.session.scalar(sa.exists(Synthese).where(Synthese.id_synthese == id_synthese).select())
+    if not observation_item :
+        raise NotFound
+    db.session.execute(sa.delete(Synthese).where(Synthese.id_synthese == id_synthese))
+    db.session.commit()
+    return "",204
+
