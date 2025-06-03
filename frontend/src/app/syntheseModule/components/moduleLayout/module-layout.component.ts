8059b26561b13425f908cd55b6f64c277e3e2a3a
Etienne Delclaux <etienne_delclaux@natural-solutions.eu>
2025-06-03 18:11:17 +0200

    wip: adjust frontend strucutre

---
+++ frontend/src/app/syntheseModule/components/moduleLayout/module-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
+@Component({
+  standalone: true,
+  selector: 'pnx-module-layout',
+  templateUrl: 'module-layout.component.html',
+  styleUrls: ['module-layout.component.scss'],
+  imports: [NgbCollapse]
+})
+export class ModuleLayoutComponent {
+  public collapseFilters = false;
+}
--- frontend/src/app/syntheseModule/synthese.module.ts
+++ frontend/src/app/syntheseModule/synthese.module.ts
@@ -3,25 +3,16 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { GN2CommonModule } from '@geonature_common/GN2Common.module';
 import { SyntheseComponent } from './synthese.component';
-import { SyntheseListComponent } from './synthese-results/synthese-list/synthese-list.component';
-import { SyntheseCarteComponent } from './synthese-results/synthese-carte/synthese-carte.component';
-import { SyntheseFormService } from '@geonature_common/form/synthese-form/synthese-form.service';
-import { MapService } from '@geonature_common/map/map.service';
 import { TreeModule } from '@circlon/angular-tree-component';
-import { DynamicFormService } from '@geonature_common/form/dynamic-form-generator/dynamic-form.service';
-import { TaxonAdvancedStoreService } from '@geonature_common/form/synthese-form/advanced-form/synthese-advanced-form-store.service';
 import { SharedSyntheseModule } from '@geonature/shared/syntheseSharedModule/synthese-shared.module';
-import { SyntheseInfoObsComponent } from '@geonature/shared/syntheseSharedModule/synthese-info-obs/synthese-info-obs.component';
-import { SyntheseModalDownloadComponent } from './synthese-results/synthese-list/modal-download/modal-download.component';
-import { DiscussionCardComponent } from '@geonature/shared/discussionCardModule/discussion-card.component';
-import { AlertInfoComponent } from '../shared/alertInfoModule/alert-Info.component';
 import { TaxonSheetComponent } from './taxon-sheet/taxon-sheet.component';
 import {
   RouteService,
   ALL_TAXON_SHEET_ADVANCED_INFOS_ROUTES,
 } from './taxon-sheet/taxon-sheet.route.service';
-import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SyntheseObsModalWrapperComponent } from '@geonature/shared/syntheseSharedModule/synthese-info-obs-container.component';
+import { SyntheseFormService } from './services/form.service';
 const routes: Routes = [
   {
     path: '',
@@ -65,28 +56,13 @@ const routes: Routes = [
     TreeModule,
     NgbModule,
     TaxonSheetComponent,
-  ],
-  declarations: [
-    SyntheseComponent,
-    SyntheseListComponent,
-    SyntheseCarteComponent,
-    SyntheseModalDownloadComponent,
+    SyntheseComponent
   ],
   entryComponents: [
     SyntheseComponent,
-    SyntheseInfoObsComponent,
-    SyntheseModalDownloadComponent,
-    DiscussionCardComponent,
-    AlertInfoComponent,
-    SyntheseObsModalWrapperComponent,
   ],
   providers: [
-    MapService,
-    DynamicFormService,
-    TaxonAdvancedStoreService,
-    SyntheseFormService,
-    NgbActiveModal,
-    RouteService,
+    RouteService
   ],
 })
 export class SyntheseModule {}
