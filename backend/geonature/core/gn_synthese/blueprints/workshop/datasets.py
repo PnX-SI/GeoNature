5597c6d7be69536544f81251356c85fb4fe2501d
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2025-06-04 14:55:01 +0200

    add datasets route

--- backend/geonature/core/gn_synthese/blueprints/workshop/datasets.py
+++ backend/geonature/core/gn_synthese/blueprints/workshop/datasets.py
@@ -1,2 +1,76 @@
-def datasets():
-    pass
+import datetime
+from geonature.core.gn_permissions.decorators import permissions_required
+from flask import request, current_app, g, jsonify
+from geonature.core.gn_synthese.utils.query_select_sqla import SyntheseQuery
+from utils_flask_sqla.generic import GenericTable
+from geonature.core.gn_synthese.models import (
+    CorAreaSynthese,
+    Synthese,
+    VSyntheseForWebApp,
+    TReport,
+)
+import sqlalchemy as sa
+from geonature.utils.env import db
+
+
+@permissions_required("R", module_code="SYNTHESE")
+def datasets(permissions):
+    """Route to export the metadata in CSV
+
+    .. :quickref: Synthese;
+
+    The table synthese is join with gn_synthese.v_metadata_for_export
+    The column jdd_id is mandatory in the view gn_synthese.v_metadata_for_export
+
+    TODO: Remove the following comment line ? or add the where clause for id_synthese in id_list ?
+    POST parameters: Use a list of id_synthese (in POST parameters) to filter the v_synthese_for_export_view
+    """
+    parameters = request.json or {}
+    per_page = parameters.pop("per_page", None)
+    page = parameters.pop("page", None)
+
+    metadata_view = GenericTable(
+        tableName="v_metadata_for_export",
+        schemaName="gn_synthese",
+        engine=db.engine,
+    )
+
+    # Test de conformité de la vue v_metadata_for_export
+    try:
+        assert hasattr(metadata_view.tableDef.columns, "jdd_id")
+    except AssertionError as e:
+        return (
+            {
+                "msg": """
+                        View v_metadata_for_export
+                        must have a jdd_id column \n
+                        trace: {}
+                        """.format(
+                    str(e)
+                )
+            },
+            500,
+        )
+
+    query = sa.select(sa.func.distinct(VSyntheseForWebApp.id_dataset), metadata_view.tableDef)
+
+    synthese_query_class = SyntheseQuery(
+        VSyntheseForWebApp,
+        query,
+        parameters,
+    )
+    synthese_query_class.add_join(
+        metadata_view.tableDef,
+        getattr(
+            metadata_view.tableDef.columns,
+            current_app.config["SYNTHESE"]["EXPORT_METADATA_ID_DATASET_COL"],
+        ),
+        VSyntheseForWebApp.id_dataset,
+    )
+
+    # Filter query with permissions (scope, sensitivity, ...)
+    synthese_query_class.filter_query_all_filters(g.current_user, permissions)
+    query = synthese_query_class.build_query()
+    if per_page and page:
+        return jsonify(db.paginate(select=query, page=page, per_page=per_page, error_out=False))
+    return db.session.execute(query).all()
