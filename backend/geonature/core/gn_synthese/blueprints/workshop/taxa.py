99b486ff9b99915a7bdd47bb3128a533c27e28f1
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2025-06-04 15:20:41 +0200

    refact taxa

--- backend/geonature/core/gn_synthese/blueprints/workshop/taxa.py
+++ backend/geonature/core/gn_synthese/blueprints/workshop/taxa.py
@@ -38,26 +38,36 @@ def taxa(permissions):
     per_page = parameters.pop("per_page", None)
     page = parameters.pop("page", None)
 
+    taxon_columns = [
+        VSyntheseForWebApp.classe,
+        VSyntheseForWebApp.famille,
+        VSyntheseForWebApp.group1_inpn,
+        VSyntheseForWebApp.group2_inpn,
+        VSyntheseForWebApp.group3_inpn,
+        VSyntheseForWebApp.id_rang,
+        VSyntheseForWebApp.ordre,
+        VSyntheseForWebApp.phylum,
+        VSyntheseForWebApp.regne,
+    ]
     sub_query = select(
         VSyntheseForWebApp.cd_ref,
         func.count(distinct(VSyntheseForWebApp.id_synthese)).label("nb_obs"),
         func.min(VSyntheseForWebApp.date_min).label("date_min"),
         func.max(VSyntheseForWebApp.date_max).label("date_max"),
-    ).group_by(VSyntheseForWebApp.cd_ref)
+        *taxon_columns,
+    ).group_by(
+        VSyntheseForWebApp.cd_ref,
+        *taxon_columns,
+    )
 
     synthese_query_class = SyntheseQuery(
         VSyntheseForWebApp,
         sub_query,
         parameters,
     )
-
     synthese_query_class.filter_query_all_filters(g.current_user, permissions)
 
-    subq = synthese_query_class.query.alias("subq")
-
-    query = select(*columns, subq.c.nb_obs, subq.c.date_min, subq.c.date_max).join(
-        subq, subq.c.cd_ref == columns.cd_ref
-    )
+    query = synthese_query_class.query
     if per_page and page:
         return jsonify(db.paginate(select=query, page=page, per_page=per_page, error_out=False))
     return db.session.execute(query).all()
