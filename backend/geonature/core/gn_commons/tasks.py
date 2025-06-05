1f4a046280692d2fb4b8a76da462295b3791f074
TheoLechemia <lechemia.theo@gmail.com>
2025-06-05 11:47:54 +0200

    Ajout object task celery

--- backend/geonature/core/gn_commons/tasks.py
+++ backend/geonature/core/gn_commons/tasks.py
@@ -1,7 +1,14 @@
+from datetime import datetime, timedelta
+from pathlib import Path
+
 from celery.schedules import crontab
 from celery.utils.log import get_task_logger
+from flask import current_app
+from sqlalchemy import delete
 
 from geonature.core.gn_commons.repositories import TMediumRepository
+from geonature.core.gn_commons.models import Task
+from geonature.utils.env import db
 from geonature.utils.celery import celery_app
 from geonature.utils.config import config
 
@@ -25,9 +32,40 @@ def setup_periodic_tasks(sender, **kwargs):
             name="clean medias",
         )
 
+    sender.add_periodic_task(
+        crontab(minute="0", hour="0"),
+        delete_file_and_tasks_celery.s(),
+        name="Clean export files",
+    )
+
 
 @celery_app.task(bind=True)
 def clean_attachments(self):
     logger.info("Cleaning medias...")
     TMediumRepository.sync_medias()
     logger.info("Medias cleaned")
+
+
+def delete_file_and_tasks():
+    """
+    Fonction permettant de supprimer les fichiers générés
+    par le module export ayant plus de X jours
+
+    .. :quickref: Fonction permettant de supprimer les
+        fichiers générés par le module export ayant plus de X jours
+
+    """
+    limit_date = datetime.today() - timedelta(days=15)
+    path_to_delete = Path(current_app.config["MEDIA_FOLDER"]) / "exports/usr_generated"
+    for item in path_to_delete.glob("**/*"):
+        item_time = item.stat().st_mtime
+        if item_time < datetime.timestamp(limit_date):
+            if item.is_file():
+                item.unlink()
+    # delete tasks
+    db.session.execute(delete(Task).where(Task.end < limit_date))
+
+
+@celery_app.task(bind=True)
+def delete_file_and_tasks_celery(self):
+    delete_file_and_tasks()
---
+++ backend/geonature/migrations/versions/67b70584ade0_add_tasks_table.py
@@ -0,0 +1,42 @@
+"""add tasks table
+
+Revision ID: 67b70584ade0
+Revises: 707390c722fe
+Create Date: 2025-06-03 09:44:22.628841
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import UUID
+
+
+# revision identifiers, used by Alembic.
+revision = "67b70584ade0"
+down_revision = "707390c722fe"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table(
+        "t_tasks",
+        sa.Column("id_task", sa.Integer, primary_key=True),
+        sa.Column("uuid_celery", UUID(as_uuid=True), nullable=False),
+        sa.Column(
+            "id_role", sa.Integer, sa.ForeignKey("utilisateurs.t_roles.id_role"), nullable=False
+        ),
+        sa.Column(
+            "id_module", sa.Integer, sa.ForeignKey("gn_commons.t_modules.id_module"), nullable=False
+        ),
+        sa.Column("start", sa.DateTime, nullable=False),
+        sa.Column("end", sa.DateTime),
+        sa.Column("status", sa.Unicode(50)),
+        sa.Column("message", sa.Text),
+        sa.Column("file_name", sa.Unicode(500)),
+        schema="gn_commons",
+    )
+
+
+def downgrade():
+    op.drop_table("t_tasks", schema="gn_commons")
