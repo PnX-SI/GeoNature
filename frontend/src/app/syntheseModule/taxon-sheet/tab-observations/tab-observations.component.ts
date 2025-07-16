696ca02204be4d69fbe3c1dbc73daa08a8dfdaac
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2025-07-16 15:59:26 +0200

    apply @edelclaux suggestions

--- frontend/src/app/syntheseModule/taxon-sheet/tab-observations/tab-observations.component.ts
+++ frontend/src/app/syntheseModule/taxon-sheet/tab-observations/tab-observations.component.ts
@@ -18,6 +18,7 @@ import { MatSliderModule } from '@angular/material/slider';
 import { Loadable } from '../loadable';
 import { finalize } from 'rxjs/operators';
 import { CommonService } from '@geonature_common/service/common.service';
+import { ActivatedRoute, Router } from '@librairies/@angular/router';
 
 interface MapAreasStyle {
   color: string;
@@ -71,7 +72,9 @@ export class TabObservationsComponent extends Loadable implements OnInit {
     public formService: SyntheseFormService,
     public translateService: TranslateService,
     private _ms: MapService,
-    private _commonService: CommonService
+    private _commonService: CommonService,
+    private _route: ActivatedRoute,
+    private _router: Router
   ) {
     super();
 
@@ -180,13 +183,15 @@ export class TabObservationsComponent extends Loadable implements OnInit {
         this.setAreasStyle(layer as L.Path, obsCount);
         popupContent = `${obsCount} observations`;
       } else {
-        const linktoObs = `${this.config.URL_APPLICATION}/#/synthese/occurrence/${observations[0].id_synthese}`;
+        const url = new URL(window.location.href);
+        url.hash = this._router.serializeUrl(
+          this._router.createUrlTree(['synthese', 'occurrence', observations[0].id_synthese])
+        );
         popupContent = `
           ${observations[0].nom_vern_or_lb_nom || ''}<br>
           <b>Observé le :</b> ${observations[0].date_min || 'Non défini'}<br>
           <b>Par :</b> ${observations[0].observers || 'Non défini'}<br>
-          <a href="${linktoObs}">Lien vers l'observation</a>
-        `;
+          <a href="${url.href}">Lien vers l'observation</a>`;
       }
 
       layer.bindPopup(popupContent);
