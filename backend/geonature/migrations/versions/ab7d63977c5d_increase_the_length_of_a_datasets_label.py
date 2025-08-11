ed69d3336dfce44822bc7171c37969af6dbe7d27
Jacques Fize <4259846+jacquesfize@users.noreply.github.com>
2025-08-11 10:27:13 +0200

    feat(metadata): increase size of the dataset name string (#3659)

---
+++ backend/geonature/migrations/versions/ab7d63977c5d_increase_the_length_of_a_datasets_label.py
@@ -0,0 +1,39 @@
+"""increase the length of a datasets label
+
+Revision ID: ab7d63977c5d
+Revises: a7f95c66819a
+Create Date: 2025-08-08 14:17:29.671513
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "ab7d63977c5d"
+down_revision = "a7f95c66819a"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute(
+        """
+    update pg_attribute 
+    set atttypmod = 1004
+    WHERE attrelid = 'gn_meta.t_datasets'::regclass
+    AND attname = 'dataset_name';
+    """
+    )
+
+
+def downgrade():
+    op.execute(
+        """
+    update pg_attribute 
+    set atttypmod = 259
+    WHERE attrelid = 'gn_meta.t_datasets'::regclass
+    AND attname = 'dataset_name';
+    """
+    )
