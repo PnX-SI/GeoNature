2c4c0baf8eacbb28aaed949992d99da1ed717667
Etienne Delclaux <etienne_delclaux@natural-solutions.eu>
2026-01-14 12:59:20 +0100

    test: add some intercepts on executeImport, importVErification and verifyImport. Fight against the timeout :D

--- frontend/cypress/support/import/triggerImportVerification.js
+++ frontend/cypress/support/import/triggerImportVerification.js
@@ -1,3 +1,7 @@
 Cypress.Commands.add('triggerImportVerification', () => {
+  cy.intercept('POST', '**/imports/*/prepare').as('prepareImport');
+  cy.intercept('GET', '**/imports/*/preview_valid_data').as('previewValidData');
   cy.get('[data-qa=import-new-verification-start]').should('exist').should('be.enabled').click();
+  cy.wait('@prepareImport', { timeout: 60000 });
+  cy.wait('@previewValidData', { timeout: 60000 });
 });
