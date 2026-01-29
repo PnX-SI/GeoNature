16570886b6bc6416b5fe69fb3c4fa3a5580b8627
Vincent Cauchois <vincent.cauchois@mnhn.fr>
2026-01-29 13:32:04 +0100

    fix(alembic): ensure cypress-* not depend on uninstalled module (#3895)
    * fix(alembic): ensure cypress-* not depend on uninstalled module
    
    Ensure that the `cypress-samples-test` Alembic branch does not depend on migrations associated to uninstalled modules, i.e. 'OCCHAB' or 'OCCTAX'.
    
    Fixes #3885
    
    * complete migration
    
    ---------
    
    Co-authored-by: jacquesfize <jacques.fize@ecrins-parcnational.fr>

--- backend/geonature/migrations/versions/d17db834aca5_add_sample_data_for_frontend_tests.py
+++ backend/geonature/migrations/versions/d17db834aca5_add_sample_data_for_frontend_tests.py
@@ -9,20 +9,53 @@ Create Date: 2025-10-13 12:18:26.053230
 from alembic import op
 import sqlalchemy as sa
 
+from geonature.utils.module import get_dist_from_code, PackageNotFoundError
+from utils_flask_sqla.migrations.utils import logger
+
+
+def is_module_installed(module_code):
+    try:
+        get_dist_from_code(module_code)
+        return True
+    except PackageNotFoundError:
+        return False
+
 
 # revision identifiers, used by Alembic.
 revision = "d17db834aca5"
 down_revision = None
 branch_labels = ("cypress-samples-test",)
-depends_on = (
+
+_depends_on = [
     "1f223c509a80",  # geonature@1f223c509a80 - "add additional_fields support for acquisition frameworks"
-    "a81f74d0a518",  # import-samples@a81f74d0a518 - "insert_import_sample_data"
-    "21f661247023",  # occhab-samples@2984569d5df6 - "insert occhab sample data"
-    "2a0ab7644e1c",  # occtax-samples-test@2a0ab7644e1c - "occtax sample test"
-)
+]
+
+# To prevent bug with verification of `depends_on` migrations associated with modules that are not installed
+#   - See: https://github.com/PnX-SI/GeoNature/issues/3885
+if is_module_installed("OCCHAB"):
+    _depends_on.append("21f661247023")  # occhab-samples@2984569d5df6 - "insert occhab sample data"
+if is_module_installed("OCCTAX"):
+    _depends_on.append("2a0ab7644e1c")  # occtax-samples-test@2a0ab7644e1c - "occtax sample test"
+if is_module_installed("OCCHAB") and is_module_installed("OCCTAX"):
+    _depends_on.append("a81f74d0a518")  # import-samples@a81f74d0a518 - "insert_import_sample_data"
+
+depends_on = tuple(_depends_on)
 
 
 def upgrade():
+    # Warn the user if any of 'OCCHAB' module or 'OCCTAX' module is not installed
+    if not is_module_installed("OCCHAB"):
+        logger.warning(
+            "⚠️ OCCHAB module is not installed, thus the upgrade of `d17db834aca5, add sample data for frontend tests` will be performed without upgrading the `occhab-samples` branch."
+        )
+    if not is_module_installed("OCCTAX"):
+        logger.warning(
+            "⚠️ OCCTAX module is not installed, thus the upgrade of `d17db834aca5, add sample data for frontend tests` will be performed without upgrading the `occtax-samples-test` branch."
+        )
+    if not is_module_installed("OCCHAB") and not is_module_installed("OCCTAX"):
+        logger.warning(
+            "⚠️ OCCTAX and OCCHAB modules are not installed, thus the upgrade of `a81f74d0a518, add sample data for frontend tests` will be performed without upgrading the `import-samples-test` branch."
+        )
     # Create a new additional field for test
     #   named "test_champs_additionnel"
     op.execute(
