8cc4679e6d7662517b2542b4e2cfb9f4c10fbd78
Vincent Bourgeois <vincent_bourgeois@natural-solutions.eu>
2026-02-19 21:32:37 +0100

    multi occtax config + some fixes

--- contrib/occtax/frontend/app/occtax-form/counting/counting.component.ts
+++ contrib/occtax/frontend/app/occtax-form/counting/counting.component.ts
@@ -1,28 +1,36 @@
-import { Component, OnInit, Input, OnDestroy, Output, EventEmitter } from '@angular/core';
-import { UntypedFormGroup, UntypedFormArray } from '@angular/forms';
-import { Subscription } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnDestroy,
+  Output,
+  EventEmitter,
+} from "@angular/core";
+import { UntypedFormGroup, UntypedFormArray } from "@angular/forms";
+import { Subscription } from "rxjs";
+import { filter } from "rxjs/operators";
 
-import { OcctaxFormService } from '../occtax-form.service';
-import { OcctaxFormOccurrenceService } from '../occurrence/occurrence.service';
-import { OcctaxFormCountingService } from './counting.service';
-import { ConfigService } from '@geonature/services/config.service';
-import { ModuleService } from '@geonature/services/module.service';
+import { OcctaxFormService } from "../occtax-form.service";
+import { OcctaxFormOccurrenceService } from "../occurrence/occurrence.service";
+import { OcctaxFormCountingService } from "./counting.service";
+import { ConfigService } from "@geonature/services/config.service";
+import { ModuleService } from "@geonature/services/module.service";
+import { OcctaxConfigService } from "../../services/occtax-config.service";
 
-import { ValidationErrorsId } from '@geonature/services/validators';
-import { ViewChild } from '@angular/core';
-import { MediasComponent } from '@geonature_common/form/media/medias.component';
+import { ValidationErrorsId } from "@geonature/services/validators";
+import { ViewChild } from "@angular/core";
+import { MediasComponent } from "@geonature_common/form/media/medias.component";
 
 @Component({
-  selector: 'pnx-occtax-form-counting',
-  templateUrl: './counting.component.html',
-  styleUrls: ['./counting.component.scss'],
+  selector: "pnx-occtax-form-counting",
+  templateUrl: "./counting.component.html",
+  styleUrls: ["./counting.component.scss"],
   providers: [OcctaxFormCountingService],
 })
 export class OcctaxFormCountingComponent implements OnInit, OnDestroy {
   public data: any;
   @ViewChild(MediasComponent) mediasComp: MediasComponent;
-  @Input('value')
+  @Input("value")
   set counting(value: any) {
     this.occtaxFormCountingService.counting.next(value);
   }
@@ -42,19 +50,24 @@ export class OcctaxFormCountingComponent implements OnInit, OnDestroy {
     public occtaxFormOccurrenceService: OcctaxFormOccurrenceService,
     private occtaxFormCountingService: OcctaxFormCountingService,
     public config: ConfigService,
-    public moduleService: ModuleService
+    public moduleService: ModuleService,
+    public occtaxConfig: OcctaxConfigService,
   ) {}
 
   // Expose enum to html
   ValidationErrorsId = ValidationErrorsId;
 
-  ngOnInit() {    
+  ngOnInit() {
     this.form = this.occtaxFormCountingService.form;
     this.sub = this.form
-      .get('id_nomenclature_life_stage')
-      .valueChanges.pipe(filter((idNomenclatureLifeStage) => idNomenclatureLifeStage !== null))
+      .get("id_nomenclature_life_stage")
+      .valueChanges.pipe(
+        filter((idNomenclatureLifeStage) => idNomenclatureLifeStage !== null),
+      )
       .subscribe((idNomenclatureLifeStage) => {
-        this.occtaxFormOccurrenceService.lifeStage.next(idNomenclatureLifeStage);
+        this.occtaxFormOccurrenceService.lifeStage.next(
+          idNomenclatureLifeStage,
+        );
       });
   }
   validateMediasDeletions() {
@@ -64,11 +77,15 @@ export class OcctaxFormCountingComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     //delete elem from form.get('cor_counting_occtax')
     const idx = (
-      this.occtaxFormOccurrenceService.form.get('cor_counting_occtax') as UntypedFormArray
+      this.occtaxFormOccurrenceService.form.get(
+        "cor_counting_occtax",
+      ) as UntypedFormArray
     ).controls.findIndex((elem) => elem === this.form);
     if (idx !== -1) {
       (
-        this.occtaxFormOccurrenceService.form.get('cor_counting_occtax') as UntypedFormArray
+        this.occtaxFormOccurrenceService.form.get(
+          "cor_counting_occtax",
+        ) as UntypedFormArray
       ).removeAt(idx);
     }
   }
@@ -88,20 +105,26 @@ export class OcctaxFormCountingComponent implements OnInit, OnDestroy {
       };
     }
 
-    const observers = (occtaxData && occtaxData.releve.properties.observers) || [];
-    const author = observers.map((o) => o.nom_complet).join(', ');
+    const observers =
+      (occtaxData && occtaxData.releve.properties.observers) || [];
+    const author = observers.map((o) => o.nom_complet).join(", ");
 
-    const date_min = (occtaxData && occtaxData.releve.properties.date_min) || null;
+    const date_min =
+      (occtaxData && occtaxData.releve.properties.date_min) || null;
 
-    const cd_nom = String(taxref && taxref.cd_nom) || '';
-    const lb_nom = (taxref && `${taxref.lb_nom}`) || '';
-    const date_txt = date_min ? `${date_min.year}_${date_min.month}_${date_min.day}` : '';
-    const date_txt2 = date_min ? `${date_min.day}/${date_min.month}/${date_min.year}` : '';
+    const cd_nom = String(taxref && taxref.cd_nom) || "";
+    const lb_nom = (taxref && `${taxref.lb_nom}`) || "";
+    const date_txt = date_min
+      ? `${date_min.year}_${date_min.month}_${date_min.day}`
+      : "";
+    const date_txt2 = date_min
+      ? `${date_min.day}/${date_min.month}/${date_min.year}`
+      : "";
 
     return {
       displayDetails: false,
       author: author,
-      title_fr: `${date_txt}_${lb_nom.replace(' ', '_')}_${cd_nom}`,
+      title_fr: `${date_txt}_${lb_nom.replace(" ", "_")}_${cd_nom}`,
       description_fr: `${lb_nom} observé le ${date_txt2}`,
     };
   }
@@ -110,9 +133,7 @@ export class OcctaxFormCountingComponent implements OnInit, OnDestroy {
     this.nbIndiv = nbIndiv;
   }
 
-  setCdNomenclatureObjDen(cdNomenclature: string) {    
-    this.cdNomenclatureObjDen = cdNomenclature
+  setCdNomenclatureObjDen(cdNomenclature: string) {
+    this.cdNomenclatureObjDen = cdNomenclature;
   }
-
-
 }
