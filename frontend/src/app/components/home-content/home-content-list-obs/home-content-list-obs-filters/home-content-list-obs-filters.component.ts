f61fa62db8d3ca78aec3d67c09fc62c8e0f90619
Etienne Delclaux <etienne_delclaux@natural-solutions.eu>
2026-03-25 15:52:18 +0100

    feazt: adjust filters

--- frontend/src/app/components/home-content/home-content-list-obs/home-content-list-obs-filters/home-content-list-obs-filters.component.ts
+++ frontend/src/app/components/home-content/home-content-list-obs/home-content-list-obs-filters/home-content-list-obs-filters.component.ts
@@ -29,7 +29,6 @@ export class HomeContentListObsFiltersComponent implements OnInit, OnDestroy {
 
   readonly group2InpnControl = new UntypedFormControl(null);
   readonly group3InpnControl = new UntypedFormControl(null);
-  readonly statusControl = new UntypedFormControl(false);
   group2InpnOptions: HomeContentListObsFilterOption[] = [];
   group3InpnOptions: HomeContentListObsFilterOption[] = [];
 
@@ -46,19 +45,6 @@ export class HomeContentListObsFiltersComponent implements OnInit, OnDestroy {
 
   get showGroup3Filter(): boolean {
     return this.config.FRONTEND?.LIST_LAST_OBS_CONFIG?.FILTERS?.TAXONOMY_GROUP3_INPN ?? true;
-  }
-
-  get showStatusFilter(): boolean {
-    return this.config.FRONTEND?.LIST_LAST_OBS_CONFIG?.FILTERS?.STATUS?.ENABLED ?? false;
-  }
-
-  get statusFilterLabel(): string {
-    return this.config.FRONTEND?.LIST_LAST_OBS_CONFIG?.FILTERS?.STATUS?.LABEL ?? 'Avec statuts';
-  }
-
-  get statusFilterItems(): Array<{ key: string; values: string[] }> {
-    const items = this.config.FRONTEND?.LIST_LAST_OBS_CONFIG?.FILTERS?.STATUS?.ITEMS;
-    return Array.isArray(items) ? items : [];
   }
 
   ngOnInit() {
@@ -112,10 +98,6 @@ export class HomeContentListObsFiltersComponent implements OnInit, OnDestroy {
     this.group3InpnControl.valueChanges.pipe(takeUntil(this.destroy$)).subscribe(() => {
       this._emitFilters();
     });
-
-    this.statusControl.valueChanges.pipe(takeUntil(this.destroy$)).subscribe(() => {
-      this._emitFilters();
-    });
   }
 
   ngOnDestroy() {
@@ -140,15 +122,6 @@ export class HomeContentListObsFiltersComponent implements OnInit, OnDestroy {
       this.group3InpnControl.value.length > 0
     ) {
       filters.taxonomy_group3_inpn = [this.group3InpnControl.value];
-    }
-
-    if (this.showStatusFilter && this.statusControl.value === true) {
-      this.statusFilterItems.forEach((item) => {
-        if (typeof item?.key === 'string' && item.key.length > 0 && Array.isArray(item.values)) {
-          filters[item.key] = item.values;
-        }
-      });
-      console.log(this.statusFilterItems);
     }
 
     this.filtersChange.emit(filters);
