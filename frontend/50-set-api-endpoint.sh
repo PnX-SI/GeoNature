5509b076b3f2723924336b7435772575b104f1ea
Élie Bouttier <bouttier@users.noreply.github.com>
2026-06-10 09:13:44 +0200

    docker: force regenerating api endpoint (#4161)

--- frontend/50-set-api-endpoint.sh
+++ frontend/50-set-api-endpoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
-if [ ! -f /usr/share/nginx/html/assets/config.json ] && [ ! -z "${API_ENDPOINT}" ]; then
-    echo "{\"API_ENDPOINT\": ${API_ENDPOINT}}" > /usr/share/nginx/html/assets/config.json
+if [ ! -z "${API_ENDPOINT}" ]; then
+  echo "{\"API_ENDPOINT\": ${API_ENDPOINT}}" >/usr/share/nginx/html/assets/config.json
 fi
