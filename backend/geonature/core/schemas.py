53cb60fcaae4cbf5d517c7aa367aa0d5c5abd343
TheoLechemia <lechemia.theo@gmail.com>
2026-08-06 16:04:53 +0200

    Add a label on all additionnal (json)  nomenclature field : additonnal nomenclature field are now duplicated : the id_nomenclature is store + the label under le `_label` prefix key this commit add a custom marshmallow field to auto `load` the `label` key for additionnal fields A data migration is done for occtax historical data (mobile and web)

---
+++ backend/geonature/core/schemas.py
@@ -0,0 +1,48 @@
+from marshmallow import fields
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
+from werkzeug.exceptions import BadRequest
+
+from geonature.utils.env import db
+from geonature.core.gn_commons.models import TAdditionalFields
+from pypnnomenclature.models import TNomenclatures
+
+
+def _duplicate_nomenclature(value, module_code, object_code):
+    duplicate_key_dict = {}
+    fields = (
+        db.session.execute(
+            select(TAdditionalFields)
+            .options(joinedload(TAdditionalFields.type_widget))
+            .where(TAdditionalFields.modules.any(module_code=module_code))
+            .where(TAdditionalFields.objects.any(code_object=object_code))
+        )
+        .scalars()
+        .all()
+    )
+    nomenclature_fields_name = [
+        field.field_name for field in fields if field.type_widget.widget_name == "nomenclature"
+    ]
+    for key, val in (value or {}).items():
+        duplicate_key_dict[key] = val
+        if key in nomenclature_fields_name:
+            nomenclature = db.session.get(TNomenclatures, val)
+            if nomenclature:
+                duplicate_key_dict["_label_" + key] = nomenclature.label_default
+    return duplicate_key_dict
+
+
+class AdditionnalDataDuplicateField(fields.Dict):
+    def __init__(self, object_code, module_code=None, keys=None, values=None, **kwargs):
+        kwargs.setdefault("allow_none", True)
+        super().__init__(keys, values, **kwargs)
+        self.module_code = module_code
+        self.object_code = object_code
+
+    def _deserialize(self, value, attr, data, **kwargs):
+        # module_code may only be known at request time (eg. g.current_module),
+        # in which case it's set on the parent schema instance instead of here.
+        module_code = self.module_code or self.parent.module_code
+        if module_code is None:
+            raise BadRequest("No module code provided in AdditionnalDataDuplicateField")
+        return _duplicate_nomenclature(value, module_code=module_code, object_code=self.object_code)
--- contrib/occtax/backend/occtax/schemas.py
+++ contrib/occtax/backend/occtax/schemas.py
@@ -16,6 +16,7 @@ from geonature.core.gn_meta.schemas import DatasetSchema
 from geonature.core.gn_commons.schemas import MediaSchema
 from geonature.core.taxonomie.schemas import TaxrefSchema
 from geonature.core.gn_monitoring.schema import TIndividualsSchema
+from geonature.core.schemas import AdditionnalDataDuplicateField
 from geonature.utils.config import config
 from pypnusershub.db.models import User
 from pypn_habref_api.schemas import HabrefSchema
@@ -68,10 +69,15 @@ class ObserverSchema(MA.SQLAlchemyAutoSchema):
 
 
 class CountingSchema(MA.SQLAlchemyAutoSchema):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.module_code = g.current_module.module_code
+
     class Meta:
         model = CorCountingOccurrence
         load_instance = True
 
+    additional_fields = AdditionnalDataDuplicateField(object_code="OCCTAX_DENOMBREMENT")
     medias = MA.Nested(MediaSchema, many=True)
     id_individual = MA.auto_field()
     individual = MA.Nested(TIndividualsSchema, dump_only=True)
@@ -84,16 +90,25 @@ class CountingSchema(MA.SQLAlchemyAutoSchema):
 
 
 class OccurrenceSchema(MA.SQLAlchemyAutoSchema):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.module_code = g.current_module.module_code
+
     class Meta:
         model = TOccurrencesOccurrence
         load_instance = True
         include_fk = True
 
+    additional_fields = AdditionnalDataDuplicateField(object_code="OCCTAX_OCCURENCE")
     cor_counting_occtax = MA.Nested(CountingSchema, many=True)
     taxref = MA.Nested(TaxrefSchema, dump_only=True)
 
 
 class ReleveSchema(MA.SQLAlchemyAutoSchema):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.module_code = g.current_module.module_code
+
     class Meta:
         model = TRelevesOccurrence
         load_instance = True
@@ -113,6 +128,7 @@ class ReleveSchema(MA.SQLAlchemyAutoSchema):
     digitiser = MA.Nested(ObserverSchema, dump_only=True)
     dataset = MA.Nested(DatasetSchema, dump_only=True)
     habitat = MA.Nested(HabrefSchema, dump_only=True)
+    additional_fields = AdditionnalDataDuplicateField(object_code="OCCTAX_RELEVE")
 
     # __init__ is overridden because g.module_conf is only available during
     # a request context (i.e., at instantiation time), not at class definition time.
@@ -140,7 +156,7 @@ class GeojsonReleveSchema(MA.Schema):
     # load_instance = True
 
     id = fields.Integer()
-    properties = fields.Nested(ReleveSchema(exclude=("geom_4326",)))
+    properties = fields.Nested(ReleveSchema, exclude=("geom_4326",))
     geometry = GeojsonSerializationField()
     af_opened = fields.Boolean()
 
