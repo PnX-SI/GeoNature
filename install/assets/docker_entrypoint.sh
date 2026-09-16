5fa888f16c2685158c57e478030ab3f412bed17f
Élie Bouttier <elie@bouttier.eu>
2026-09-16 09:23:18 +0200

    docker backend entrypoint: touch /tmp/ready

--- install/assets/docker_entrypoint.sh
+++ install/assets/docker_entrypoint.sh
@@ -9,6 +9,7 @@
 set -o errexit
 set -o pipefail
 
+rm -f /tmp/ready
 
 # creer les tables et peupler la base
 if [ "${GEONATURE_SKIP_POPULATE_DB}" = true ]; then
@@ -16,11 +17,9 @@ if [ "${GEONATURE_SKIP_POPULATE_DB}" = true ]; then
     :
 else
     . /populate_db.sh
-    if [ "$usershub" = true ];
-    then
+    if [ "$usershub" = true ]; then
         geonature db upgrade usershub@head
-        if [ "$usershub_samples" = true ];
-        then
+        if [ "$usershub_samples" = true ]; then
             geonature db upgrade usershub-samples@head
         fi
     fi
@@ -31,5 +30,7 @@ else
         geonature permissions supergrant --yes ${GEONATURE_SUPERGRANT_ARGS}
     fi
 fi
+
+touch /tmp/ready
 
 exec "$@"
