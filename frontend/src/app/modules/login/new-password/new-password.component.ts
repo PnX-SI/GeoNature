1a96532cdeb8ca3f9732e9d1cd99ddbb7b5d8754
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2026-09-17 13:04:08 +0200

    chore: drop margin below password field only in signup and password renewal from

--- frontend/src/app/modules/login/new-password/new-password.component.ts
+++ frontend/src/app/modules/login/new-password/new-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { UntypedFormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 import { similarValidator } from '@geonature/services/validators';
 
@@ -13,6 +14,11 @@ import { CommonService } from '@geonature_common/service/common.service';
   selector: 'pnx-new-password',
   templateUrl: 'new-password.component.html',
   styleUrls: ['./new-password.component.scss'],
+  providers: [
+    // Avoid the fixed-height blank space mat-form-field reserves below each field
+    // for hints/errors when there is none to show on this page.
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { subscriptSizing: 'dynamic' } },
+  ],
 })
 export class NewPasswordComponent implements OnInit {
   token: string;
--- frontend/src/app/modules/login/sign-up/sign-up.component.ts
+++ frontend/src/app/modules/login/sign-up/sign-up.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UntypedFormGroup, UntypedFormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 import { similarValidator } from '@geonature/services/validators/validators';
 import { CommonService } from '@geonature_common/service/common.service';
@@ -14,6 +15,11 @@ import { LoginExistsValidator } from '@geonature/userModule/services/login-exist
   selector: 'pnx-signup',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
+  providers: [
+    // Avoid the fixed-height blank space mat-form-field reserves below each field
+    // for hints/errors when there is none to show on this page.
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { subscriptSizing: 'dynamic' } },
+  ],
 })
 export class SignUpComponent implements OnInit {
   form: UntypedFormGroup;
