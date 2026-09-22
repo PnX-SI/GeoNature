777d40aa1c0b233c6e324a35a774264fe33444d6
Pierre-Narcisi <pierre.narcisi@mnhn.fr>
2026-09-22 13:43:05 +0200

    Habref update (#4227)
    * Update habref

---
+++ contrib/gn_module_occhab/backend/gn_module_occhab/migrations/53e6505aaaa9_add_fk_to_t_habitats_cd_hab.py
@@ -0,0 +1,38 @@
+"""add fk to t_habitats cd_hab
+
+Revision ID: 53e6505aaaa9
+Revises: fc6f7f3cb801
+Create Date: 2026-06-28 19:16:58.760718
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = "53e6505aaaa9"
+down_revision = "fc6f7f3cb801"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_foreign_key(
+        "fk_t_habitats_cd_hab",
+        "t_habitats",
+        "habref",
+        ["cd_hab"],
+        ["cd_hab"],
+        source_schema="pr_occhab",
+        referent_schema="ref_habitats",
+        onupdate="CASCADE",
+    )
+
+
+def downgrade():
+    op.drop_constraint(
+        "fk_t_habitats_cd_hab",
+        "t_habitats",
+        schema="pr_occhab",
+        type_="foreignkey",
+    )
